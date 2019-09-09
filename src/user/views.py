b16bcba847bb804e7c6f724ce4921a6eeccf110d
valmack <valmack@protonmail.com>
2019-09-09 16:40:22 -0400

    Add view to show all users

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import User
+from .serializers import *
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
