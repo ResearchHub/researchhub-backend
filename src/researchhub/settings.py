cdbf1ead68f7f14457e3ad7df7644534f425aeb4
valmack <valmack@protonmail.com>
2019-09-09 11:43:53 -0400

    Store secret key in config

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from config import keys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2q2#jz$!rd@vol&8@+e^&@xh!^sfysg%18p5k%#5o6+e&hfe6-'
+SECRET_KEY = os.environ.get('SECRET_KEY', keys.SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
