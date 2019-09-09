49d1a3b9b420ac6f99ab72a2abd9b63bc466be19
valmack <valmack@protonmail.com>
2019-09-09 16:41:00 -0400

    Register user view in urls

--- src/researchhub/urls.py
+++ src/researchhub/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+
+import user.views
+
+router = routers.DefaultRouter()
+
+router.register(r'user', user.views.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls')),
 ]
