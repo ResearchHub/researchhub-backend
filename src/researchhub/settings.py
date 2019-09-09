20cfa6ecb0f02d81fed647a19c62b815c534ab68
valmack <valmack@protonmail.com>
2019-09-09 16:30:23 -0400

    Fix - Use tuple for rest framework authentication classes in settings

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -74,9 +74,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'researchhub.urls'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    },
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.AllowAny', # FOR TESTING ONLY
