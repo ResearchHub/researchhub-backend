d4db8c5d40ecf0ca483ae2b121cdf5e3af63f1b3
valmack <valmack@protonmail.com>
2019-09-10 19:46:38 -0400

    Add token login and google login views to oauth app

--- src/oauth/views.py
+++ src/oauth/views.py
@@ -1,3 +1,67 @@
-from allauth.account.views import LoginView
+from allauth.socialaccount.helpers import render_authentication_error
+from allauth.socialaccount.models import SocialLogin
+from allauth.socialaccount.providers.base import ProviderException
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Error
+from allauth.socialaccount.providers.oauth2.views import OAuth2CallbackView, OAuth2LoginView
+from allauth.utils import get_request_param
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import AllowAny
 
-login = LoginView.as_view()
+from .helpers import complete_social_login
+
+
+class TokenLogin(ObtainAuthToken):
+    throttle_classes = ()
+    permission_classes = (AllowAny,)
+
+
+class CallbackView(OAuth2CallbackView):
+    """
+    This class is copied from allauth/socialaccount/providers/oauth2/views.py
+    but uses a custom method for `complete_social_login`
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if 'error' in request.GET or 'code' not in request.GET:
+            # Distinguish cancel from error
+            auth_error = request.GET.get('error', None)
+            if auth_error == self.adapter.login_cancelled_error:
+                error = AuthError.CANCELLED
+            else:
+                error = AuthError.UNKNOWN
+            return render_authentication_error(
+                request,
+                self.adapter.provider_id,
+                error=error)
+        app = self.adapter.get_provider().get_app(self.request)
+        client = self.get_client(request, app)
+        try:
+            access_token = client.get_access_token(request.GET['code'])
+            token = self.adapter.parse_token(access_token)
+            token.app = app
+            login = self.adapter.complete_login(request,
+                                                app,
+                                                token,
+                                                response=access_token)
+            login.token = token
+            if self.adapter.supports_state:
+                login.state = SocialLogin \
+                    .verify_and_unstash_state(
+                        request,
+                        get_request_param(request, 'state'))
+            else:
+                login.state = SocialLogin.unstash_state(request)
+            return complete_social_login(request, login)
+        except (PermissionDenied,
+                OAuth2Error,
+                RequestException,
+                ProviderException) as e:
+            return render_authentication_error(
+                request,
+                self.adapter.provider_id,
+                exception=e)
+
+
+token_login = TokenLogin.as_view()
+google_login = OAuth2LoginView.adapter_view(GoogleOAuth2Adapter)
+google_callback = CallbackView.adapter_view(GoogleOAuth2Adapter)
