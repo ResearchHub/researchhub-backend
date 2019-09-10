aa858ea15ae859bcaf8d5e2ef242fecaa914d9d3
valmack <valmack@protonmail.com>
2019-09-10 19:45:41 -0400

    Add comment to oauth config and remove ready method

--- src/oauth/apps.py
+++ src/oauth/apps.py
@@ -2,8 +2,14 @@ from django.apps import AppConfig
 
 
 class OAuthConfig(AppConfig):
+    """
+    Configuration for the OAuth app
+
+    Set the OAUTH_METHOD variable in the project's settings.py to
+    'token' to send Django Rest Framework tokens upon login.
+
+    Otherwise, successfull logins will redirect to the url set by
+    LOGIN_REDIRECT_URL.
+    """
     name = 'oauth'
     verbose_name = 'OAuth'
-
-    def ready(self):
-        print('OAuth app is ready')
