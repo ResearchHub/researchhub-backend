3ec0246f20648b636cab4514b5b203e09d6be7e3
valmack <valmack@protonmail.com>
2019-09-12 12:41:10 -0400

    Make username optional when signing up

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -1,8 +1,27 @@
-from rest_framework import serializers
+import rest_framework.serializers as rest_framework_serializers
+import rest_auth.registration.serializers as rest_auth_serializers
 
 from .models import User
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+class UserSerializer(rest_framework_serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
+
+
+class RegisterSerializer(rest_auth_serializers.RegisterSerializer):
+    username = rest_auth_serializers.serializers.CharField(
+        max_length=rest_auth_serializers.get_username_max_length(),
+        min_length=rest_auth_serializers.allauth_settings.USERNAME_MIN_LENGTH,
+        required=False,
+        allow_blank=True
+    )
+
+    def validate_username(self, username):
+        if username:
+            username = rest_auth_serializers.get_adapter().clean_username(username)
+        return username
+
+    def save(self, request):
+        return super().save(request)
