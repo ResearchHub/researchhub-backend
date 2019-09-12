b837248160173860569169b58271536cbc963333
valmack <valmack@protonmail.com>
2019-09-12 12:42:17 -0400

    Add comment with default user model fields

--- src/user/models.py
+++ src/user/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    """
+    User objects have the following fields by default:
+        https://docs.djangoproject.com/en/2.2/ref/contrib/auth/#django.contrib.auth.models.User
+    """
 
     def __str__(self):
         return self.email
