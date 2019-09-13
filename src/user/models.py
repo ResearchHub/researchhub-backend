c55b003286f4d71dc4eeeae850e20da24dd27a89
valmack <valmack@protonmail.com>
2019-09-13 16:54:54 -0400

    Create author and university models

--- src/user/models.py
+++ src/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     """
     User objects have the following fields by default:
@@ -9,3 +10,36 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class University(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    user = models.OneToOneField(
+        User,
+        related_name='author_profile',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    university = models.ForeignKey(
+        University,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        university = self.university
+        if university is None:
+            university = ''
+        return f'{self.first_name}_{self.last_name}_{university}'
