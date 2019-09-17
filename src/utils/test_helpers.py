ac26781d153df305c9cab5ce957d605d78e00c3a
valmack <valmack@protonmail.com>
2019-09-17 18:57:22 -0400

    Add user, author, and university creator methods to utils

---
+++ src/utils/test_helpers.py
@@ -0,0 +1,64 @@
+from user.models import Author, University, User
+
+class TestHelper:
+    first_name = 'Regulus'
+    last_name = 'Black'
+    author_first_name = 'R. A.'
+    author_last_name = 'Black'
+
+    invalid_email = 'testuser@gmail'
+    invalid_password = 'pass'
+    valid_email = 'testuser@gmail.com'
+    valid_password = 'ReHub940'
+
+    university_name = 'Hogwarts'
+    university_country = 'England'
+    university_state = 'London'
+    university_city = 'London'
+
+    def create_user(
+        self,
+        email=valid_email,
+        password=valid_password):
+        return User.objects.create(
+            email=email,
+            password=password
+        )
+
+    def create_author(
+        self,
+        user,
+        university,
+        first_name=author_first_name,
+        last_name=author_last_name):
+        return Author.objects.create(
+            user=user,
+            first_name=first_name,
+            last_name=last_name,
+            university=university
+        )
+
+    def create_author_without_user(
+        self,
+        university,
+        first_name=author_first_name,
+        last_name=author_last_name):
+        return Author.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            university=university
+        )
+
+    def create_university(
+        self,
+        name=university_name,
+        country=university_country,
+        state=university_state,
+        city=university_city):
+        return University.objects.create(
+            name=name,
+            country=country,
+            state=state,
+            city=city
+        )
+
