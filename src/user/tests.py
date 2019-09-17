3c73dc31a5a96abb435912313956616619a03255
valmack <valmack@protonmail.com>
2019-09-17 19:59:34 -0400

    Update user test to show that duplicate usernames are valid

--- src/user/tests.py
+++ src/user/tests.py
@@ -134,8 +134,7 @@ class AuthenticationTests(BaseTests):
         username = 'test_username'
         response1 = self.valid_signup(username)
         response2 = self.signup(username, 'different@gmail.com', self.valid_password)
-        response_text = 'A user with that username already exists'
-        self.assertContains(response2, response_text, status_code=400)
+        self.assertContainsToken(response2, 201)
 
     def test_signup_with_duplicate_blank_username_and_different_email(self):
         response1 = self.valid_signup()
