747ae47fd1f01b5ce764bd49c818461679365d03
valmack <valmack@protonmail.com>
2019-09-18 19:21:04 -0400

    Fix - Get paper title from test data

--- src/paper/tests.py
+++ src/paper/tests.py
@@ -7,5 +7,5 @@ class PaperTests(TestCase, TestHelper):
 
     def test_string_representation(self):
         paper = self.create_paper_without_authors()
-        text = '%s: []' % self.paper_title
+        text = '%s: []' % self.test_data.paper_title
         self.assertEqual(str(paper), text)
