07e270c76c3acf9cd337a3e64bdf13d8a3f1daf6
valmack <valmack@protonmail.com>
2019-09-18 19:19:26 -0400

    Add parent fields to discussion models

--- src/discussion/models.py
+++ src/discussion/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
 class Thread(models.Model):
     title = models.CharField(max_length=255)
 
+
 class Post(models.Model):
-    pass
+    parent = models.ForeignKey(
+        Thread,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+
 
 class Reply(models.Model):
-    pass
+    parent = models.ForeignKey(
+        Post,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
