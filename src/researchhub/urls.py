550810a9324a32f158c1c63dbe24fb0e20e33c89
valmack <valmack@protonmail.com>
2019-09-19 11:33:51 -0400

    Add paper viewset and route

--- src/researchhub/urls.py
+++ src/researchhub/urls.py
@@ -3,12 +3,12 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 """
-from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 from rest_framework import routers
 
 from .views import index
+import paper.views
 import oauth.urls
 import oauth.views
 import user.views
@@ -17,13 +17,22 @@ import user.views
 router = routers.DefaultRouter()
 
 router.register(r'user', user.views.UserViewSet)
+router.register(r'paper', paper.views.PaperViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/', include(router.urls)),
-    url(r'^auth/google/login/callback/', oauth.views.google_callback, name='google_callback'),
-    url(r'^auth/google/login/', oauth.views.google_login, name='google_login'),
-    url(r'^auth/signup/', include(oauth.urls.registration_urls)),
-    url(r'^auth/', include(oauth.urls.default_urls)),
+    re_path(r'^api/', include(router.urls)),
+    re_path(
+        r'^auth/google/login/callback/',
+        oauth.views.google_callback,
+        name='google_callback'
+    ),
+    re_path(
+        r'^auth/google/login/',
+        oauth.views.google_login,
+        name='google_login'
+    ),
+    re_path(r'^auth/signup/', include(oauth.urls.registration_urls)),
+    re_path(r'^auth/', include(oauth.urls.default_urls)),
     path('', index, name='index'),
 ]
