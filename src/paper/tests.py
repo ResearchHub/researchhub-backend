39998d70efe80b091fe940571a7ecc256b27d725
valmack <valmack@protonmail.com>
2019-09-19 12:38:07 -0400

    Add test for paper router

--- src/paper/tests.py
+++ src/paper/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from utils.test_helpers import TestHelper
+from utils.test_helpers import IntegrationTestHelper, TestHelper
 
 
 class PaperTests(TestCase, TestHelper):
@@ -9,3 +9,11 @@ class PaperTests(TestCase, TestHelper):
         paper = self.create_paper_without_authors()
         text = '%s: []' % self.test_data.paper_title
         self.assertEqual(str(paper), text)
+
+
+class PaperIntegrationTests(TestCase, IntegrationTestHelper):
+
+    def test_get_base_route(self):
+        url = '/api/paper/'
+        response = self.get_response(url)
+        self.assertEqual(response.status_code, 200)
