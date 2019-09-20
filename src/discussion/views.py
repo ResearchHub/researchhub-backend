39571f5360ccee0a40fa0311c0675db18f378449
valmack <valmack@protonmail.com>
2019-09-20 12:53:15 -0400

    Add view to show discussion threads

--- src/discussion/views.py
+++ src/discussion/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from .models import Thread
+from .serializers import ThreadSerializer
+
+
+class DiscussionViewSet(viewsets.ModelViewSet):
+    serializer_class = ThreadSerializer
+
+    # Optional attributes
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        paper_id = self.get_paper_id_from_path()
+        threads = Thread.objects.filter(paper=paper_id)
+        return threads
+
+    def get_paper_id_from_path(self):
+        SECOND = 1
+        paper_id = None
+        path_parts = self.request.path.split('/')
+        if path_parts[SECOND] == 'paper':
+            try:
+                paper_id = int(path_parts[SECOND + 1])
+            except ValueError:
+                print('Failed to get paper id')
+        return paper_id
