2efd1f181e68de0b907f7c69c7cde77007aa737d
valmack <valmack@protonmail.com>
2019-09-20 19:27:15 -0400

    Send additional fields through thread serializer

--- src/discussion/serializers.py
+++ src/discussion/serializers.py
@@ -3,9 +3,17 @@ import rest_framework.serializers as rest_framework_serializers
 from .models import Thread
 
 
-class ThreadSerializer(rest_framework_serializers.HyperlinkedModelSerializer):
+class ThreadSerializer(rest_framework_serializers.ModelSerializer):
 
     # TODO: Ensure user gets added to the thread when the form is submitted
     class Meta:
-        fields = ['title', 'text']
+        fields = [
+            'title',
+            'text',
+            'paper',
+            'created_by',
+            'created_date',
+            'is_public',
+            'is_removed'
+        ]
         model = Thread
