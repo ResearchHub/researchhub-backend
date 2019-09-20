9069d9ac7f394007a32469fcbdc4b95bc3affd1a
valmack <valmack@protonmail.com>
2019-09-20 15:19:10 -0400

    Add test for reply to reply

--- src/discussion/tests.py
+++ src/discussion/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.sites.models import Site
 
-from .models import Thread
+from .models import Thread, Comment, Reply
 from utils.test_helpers import IntegrationTestHelper, TestHelper
 
 
@@ -10,10 +10,42 @@ class BaseTestCase(TestCase, TestHelper):
 
     thread_title = 'Thread Title'
     thread_text = 'This is a thread.'
+    comment_text = 'This is a comment.'
+    reply_text = 'This is a reply.'
+
+    def create_default_reply(self):
+        comment = self.create_default_comment()
+        user = self.create_random_default_user('reply')
+        text = self.reply_text
+        reply = self.create_reply(comment, user, text)
+        return reply
+
+    def create_reply(self, parent, user, text):
+        reply = Reply.objects.create(
+            parent=parent,
+            created_by=user,
+            text=text
+        )
+        return reply
+
+    def create_default_comment(self):
+        thread = self.create_default_thread()
+        user = self.create_random_default_user('comment')
+        text = self.comment_text
+        comment = self.create_comment(thread, user, text)
+        return comment
+
+    def create_comment(self, thread, user, text):
+        comment = Comment.objects.create(
+            parent=thread,
+            created_by=user,
+            text=text
+        )
+        return comment
 
     def create_default_thread(self):
         paper = self.create_paper_without_authors()
-        user = self.create_user()
+        user = self.create_random_default_user('thread')
         title = self.thread_title
         text = self.thread_text
         thread = self.create_thread(paper, user, title, text)
@@ -35,7 +67,35 @@ class ThreadTests(BaseTestCase):
         thread = self.create_default_thread()
         self.assertEqual(
             str(thread),
-            'testuser@gmail.com: Thread Title'
+            'threadtestuser@gmail.com: Thread Title'
         )
 
 
+class ReplyTests(BaseTestCase):
+
+    def test_reply_to_reply(self):
+        user = self.create_user()
+        reply = self.create_default_reply()
+        text = self.reply_text + ' 2'
+        reply2 = self.create_reply(
+            reply,
+            user,
+            text
+        )
+        self.assertEqual(reply2.parent, reply)
+
+# class ThreadIntegrationTests(BaseTestCase, IntegrationTestHelper):
+
+#     def test_create_thread(self):
+#         paper = self.create_paper_without_authors()
+#         response = self.add_thread_to(paper.id)
+#         self.assertEqual(response.status_code, 200)
+
+#     def add_thread_to(self, paper_id):
+#         url = f'/api/paper/{paper_id}/discussion/'
+#         body = {
+#             "title": "hello",
+#         }
+#         client = self.get_default_authenticated_client()
+#         response = self.post_response(url, body, client=client)
+#         return response
