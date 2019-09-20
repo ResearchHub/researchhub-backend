a0206aaa8700f8bdbd77a6769935589e9fbfccf6
valmack <valmack@protonmail.com>
2019-09-20 17:10:39 -0400

    Add test for paper upload

--- src/paper/tests.py
+++ src/paper/tests.py
@@ -12,8 +12,27 @@ class PaperTests(TestCase, TestHelper):
 
 
 class PaperIntegrationTests(TestCase, IntegrationTestHelper):
+    base_url = '/api/paper/'
 
     def test_get_base_route(self):
-        url = '/api/paper/'
-        response = self.get_response(url)
+        response = self.get_response(self.base_url)
         self.assertEqual(response.status_code, 200)
+
+    def test_upload_paper(self):
+        response = self.submit_paper_form()
+        text = self.paper_title
+        self.assertContains(response, text, status_code=201)
+
+    def submit_paper_form(self):
+        client = self.get_default_authenticated_client()
+        url = self.base_url
+        form_data = self.build_default_paper_form()
+        response = client.post(url, form_data)
+        return response
+
+    def build_default_paper_form(self):
+        title = self.paper_title
+        form = {
+            'title': title
+        }
+        return form
