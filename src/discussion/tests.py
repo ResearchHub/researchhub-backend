a5754088fe0b5d40d60d1c21810dfb73166b06a4
valmack <valmack@protonmail.com>
2019-09-20 19:27:15 -0400

    Send additional fields through thread serializer

--- src/discussion/tests.py
+++ src/discussion/tests.py
@@ -85,7 +85,56 @@ class ReplyTests(BaseTestCase):
         self.assertEqual(reply2.parent, reply)
 
 
-class ThreadIntegrationTests(BaseTestCase, IntegrationTestHelper):
+class BaseIntegrationTestCase(BaseTestCase, IntegrationTestHelper):
+
+    def post_default_thread(self):
+        paper = self.create_paper_without_authors()
+        response = self.submit_thread_form(paper.id)
+        return (response, paper.id)
+
+    def submit_thread_form(self, paper_id):
+        client = self.get_default_authenticated_client()
+        url = self.base_url + f'{paper_id}/discussion/'
+        form_data = self.build_default_thread_form(paper_id)
+        response = client.post(url, form_data)
+        return response
+
+    def build_default_thread_form(self, paper_id):
+        title = self.thread_title
+        text = self.thread_text
+        form = {
+            'title': title,
+            'text': text,
+            'paper': paper_id
+        }
+        return form
+
+
+class DiscussionIntegrationTests(BaseIntegrationTestCase):
+    base_url = '/api/paper/'
+
+    def test_discussion_view_shows_threads(self):
+        thread_data = self.post_default_thread()
+        url = self.build_discussion_url(thread_data)
+        response = self.get_response(url)
+        text = self.parse_thread_title(thread_data)
+        self.assertContains(response, text, status_code=200)
+
+    def parse_thread_title(self, thread_data):
+        RESPONSE = 0
+        thread = thread_data[RESPONSE]
+        thread_json = self.bytes_to_json(thread.content)
+        title = thread_json.get('title')
+        return title
+
+    def build_discussion_url(self, thread_data):
+        PAPER_ID = 1
+        paper_id = thread_data[PAPER_ID]
+        url = self.base_url + f'{paper_id}/discussion/'
+        return url
+
+
+class ThreadIntegrationTests(BaseIntegrationTestCase):
     base_url = '/api/paper/'
 
     def test_create_thread(self):
@@ -93,19 +142,3 @@ class ThreadIntegrationTests(BaseTestCase, IntegrationTestHelper):
         response = self.submit_thread_form(paper.id)
         text = self.thread_title
         self.assertContains(response, text, status_code=201)
-
-    def submit_thread_form(self, paper_id):
-        client = self.get_default_authenticated_client()
-        url = self.base_url + f'{paper_id}/discussion/'
-        form_data = self.build_default_thread_form()
-        response = client.post(url, form_data)
-        return response
-
-    def build_default_thread_form(self):
-        title = self.thread_title
-        text = self.thread_text
-        form = {
-            'title': title,
-            'text': text
-        }
-        return form
