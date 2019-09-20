2ba2fc6c40d83a33f7a883f192ae6f8ad4a14143
valmack <valmack@protonmail.com>
2019-09-20 18:42:35 -0400

    Update discussion serializer fields and test thread form submission

--- src/discussion/serializers.py
+++ src/discussion/serializers.py
@@ -4,7 +4,8 @@ from .models import Thread
 
 
 class ThreadSerializer(rest_framework_serializers.HyperlinkedModelSerializer):
-    fields = '__all__'
 
+    # TODO: Ensure user gets added to the thread when the form is submitted
     class Meta:
+        fields = ['title', 'text']
         model = Thread
