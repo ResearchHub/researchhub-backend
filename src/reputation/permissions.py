1f5c794013101ab1f525c1f36a111fffc9724b6b
valmack <valmack@protonmail.com>
2019-09-23 16:32:53 -0400

    Build custom permissions

---
+++ src/reputation/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class RuleBasedPermission(BasePermission):
+    class Meta:
+        abstract = True
+
+    def has_permission(self, request, view):
+        if self.is_read_only_request(request):
+            return True
+        return self.satisfies_rule(request)
+
+    def is_read_only_request(self, request):
+        return request.method in SAFE_METHODS
+
+
+class CreateDiscussionThread(RuleBasedPermission):
+    message = 'Not enough reputation to create thread.'
+
+    def satisfies_rule(self, request):
+        return request.user.reputation >= 1
+
+
+class CreatePaper(RuleBasedPermission):
+    message = 'Not enough reputation to upload paper.'
+
+    def satisfies_rule(self, request, view):
+        return request.user.reputation >= 0
