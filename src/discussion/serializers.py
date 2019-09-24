bfd313466dea4aba17a8f49e20c25bd9e60fc566
valmack <valmack@protonmail.com>
2019-09-24 15:15:51 -0400

    Fix - Automatically add current user to thread created_by field

--- src/discussion/serializers.py
+++ src/discussion/serializers.py
@@ -1,22 +1,27 @@
 import rest_framework.serializers as serializers
 
 from .models import Thread
+from user.models import User
 
 
 class ThreadSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(
-        read_only=True,
+    created_by = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        read_only=False,
         default=serializers.CurrentUserDefault()
     )
 
     class Meta:
         fields = [
-            'user',
             'title',
             'text',
             'paper',
             'created_by',
             'created_date',
+            'is_public',
+            'is_removed'
+        ]
+        read_only_fields = [
             'is_public',
             'is_removed'
         ]
