3cc72ae6cfd2ec9e2f333c9ac2b3c037a9595b0f
valmack <valmack@protonmail.com>
2019-09-26 13:44:04 -0400

    Add fields for paper serializer

--- src/paper/serializers.py
+++ src/paper/serializers.py
@@ -1,10 +1,26 @@
-import rest_framework.serializers as rest_framework_serializers
+import rest_framework.serializers as serializers
 
 from .models import Paper
+from user.models import User
 
 
-class PaperSerializer(rest_framework_serializers.HyperlinkedModelSerializer):
+class PaperSerializer(serializers.ModelSerializer):
+    uploaded_by = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        read_only=False,
+        default=serializers.CurrentUserDefault()
+    )
 
     class Meta:
-        fields = ['title', 'authors', 'paper_publish_date']
+        fields = [
+            'title',
+            'uploaded_by',
+            'authors',
+            'paper_publish_date',
+            'doi',
+            'hubs',
+            'url',
+            'uploaded_date',
+            'updated_date',
+        ]
         model = Paper
