1a2c48acf1739074c3d289f10e8bf4f518cda4a9
valmack <valmack@protonmail.com>
2019-09-27 08:32:32 -0400

    Add comment serializer

--- src/discussion/serializers.py
+++ src/discussion/serializers.py
@@ -1,8 +1,10 @@
 import rest_framework.serializers as serializers
 
-from .models import Thread
+from .models import Comment, Thread
 from user.models import User
 
+
+# TODO: Add isOwner permission and make is_public editable
 
 class ThreadSerializer(serializers.ModelSerializer):
     created_by = serializers.PrimaryKeyRelatedField(
@@ -26,3 +28,23 @@ class ThreadSerializer(serializers.ModelSerializer):
             'is_removed'
         ]
         model = Thread
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    created_by = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        read_only=False,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = [
+            'text',
+            'parent',
+            'created_by',
+        ]
+        read_only_fields = [
+            'is_public',
+            'is_removed'
+        ]
+        model = Comment
