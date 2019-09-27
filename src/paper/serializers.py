de0d8ec770e81d3ef2562eda30309a4e2821d6b1
Craig Lu <craig@quantfive.org>
2019-09-26 18:13:17 -0700

    setting up paper serializer to return author and summary information

--- src/paper/serializers.py
+++ src/paper/serializers.py
@@ -2,7 +2,8 @@ import rest_framework.serializers as serializers
 
 from .models import Paper
 from user.models import User
-
+from user.serializers import AuthorSerializer
+from summary.serializers import SummarySerializer 
 
 class PaperSerializer(serializers.ModelSerializer):
     uploaded_by = serializers.PrimaryKeyRelatedField(
@@ -10,7 +11,8 @@ class PaperSerializer(serializers.ModelSerializer):
         read_only=False,
         default=serializers.CurrentUserDefault()
     )
-
+    summary = serializers.SerializerMethodField()
+    authors = serializers.SerializerMethodField()
     class Meta:
         fields = [
             'title',
@@ -22,5 +24,20 @@ class PaperSerializer(serializers.ModelSerializer):
             'url',
             'uploaded_date',
             'updated_date',
+            'summary',
         ]
         model = Paper
+
+    def get_summary(self, obj):
+        summary_queryset = obj.summary.filter(current=True)
+        summary = {}
+        if summary_queryset:
+            summary = SummarySerializer(summary_queryset.first()).data
+        return summary
+
+    def get_authors(self, obj):
+        authors_queryset = obj.authors.all()
+        authors = []
+        if authors_queryset:
+            authors = AuthorSerializer(authors_queryset, many=True).data
+        return authors
--- src/user/serializers.py
+++ src/user/serializers.py
@@ -1,7 +1,7 @@
 import rest_framework.serializers as rest_framework_serializers
 import rest_auth.registration.serializers as rest_auth_serializers
 
-from .models import User
+from .models import User, Author
 
 
 class UserSerializer(rest_framework_serializers.HyperlinkedModelSerializer):
@@ -27,3 +27,8 @@ class RegisterSerializer(rest_auth_serializers.RegisterSerializer):
 
     def save(self, request):
         return super().save(request)
+
+class AuthorSerializer(rest_framework_serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
