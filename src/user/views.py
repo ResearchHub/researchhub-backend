103410bf55fba3e8b6f9e89011333c9aee4ddc30
valmack <valmack@protonmail.com>
2019-09-30 19:04:24 -0400

    Set user to view to auth or readonly

--- src/user/views.py
+++ src/user/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import User
 from .serializers import UserSerializer
@@ -7,3 +8,5 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
