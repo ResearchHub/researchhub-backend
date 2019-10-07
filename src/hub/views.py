3eb52fb4bc8656aff37f009b9bebe8efd94b7115
valmack <valmack@protonmail.com>
2019-10-07 11:58:48 -0400

    Clean linter warnings

--- src/hub/views.py
+++ src/hub/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Hub
 from .serializers import HubSerializer
+
 
 class HubViewSet(viewsets.ModelViewSet):
     queryset = Hub.objects.all()
