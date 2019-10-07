01e93c75b34844b9f1d49ae24c8d8d80090590a5
Craig Lu <craig@quantfive.org>
2019-10-07 15:43:07 -0700

    adding exclude filter to authors

---
+++ src/user/filters.py
@@ -0,0 +1,11 @@
+from django_filters import rest_framework as filters
+from .models import *
+from utils.filters import ListExcludeFilter
+
+class AuthorFilter(filters.FilterSet):
+    id__ne = ListExcludeFilter(field_name='status')
+
+    class Meta:
+        model = Author
+        fields = [field.name for field in model._meta.fields]
+        fields.append('id__ne')
