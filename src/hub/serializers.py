9951748323791f99de70b13a84265bebeb0664c7
valmack <valmack@protonmail.com>
2019-10-07 11:58:48 -0400

    Clean linter warnings

--- src/hub/serializers.py
+++ src/hub/serializers.py
@@ -1,7 +1,7 @@
 import rest_framework.serializers as rest_framework_serializers
-import rest_auth.registration.serializers as rest_auth_serializers
 
 from .models import Hub
+
 
 class HubSerializer(rest_framework_serializers.ModelSerializer):
     class Meta:
