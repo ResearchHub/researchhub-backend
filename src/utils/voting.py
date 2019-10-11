004cf195525557eaa9442faaab98901dd0d37d04
valmack <valmack@protonmail.com>
2019-10-11 14:18:34 -0400

    Refactor paper serializer utils

--- src/utils/voting.py
+++ src/utils/voting.py
@@ -3,10 +3,3 @@ def calculate_score(obj, upvote_type, downvote_type):
     downvotes = obj.votes.filter(vote_type=downvote_type)
     score = len(upvotes) - len(downvotes)
     return score
-
-
-def get_user_from_request(ctx):
-    request = ctx.get('request')
-    if request and hasattr(request, 'user'):
-        return request.user
-    return None
