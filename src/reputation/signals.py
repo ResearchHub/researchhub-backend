889584e8a0be470971b126257218b3fc3f613f8c
valmack <valmack@protonmail.com>
2019-10-18 10:15:38 -0400

    Increase reputation for comment upvote

--- src/reputation/signals.py
+++ src/reputation/signals.py
@@ -4,15 +4,16 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .distributor import Distributor
-from .distributions import CreatePaper
+from .distributions import CommentUpvoted, CommentDownvoted, CreatePaper
 
+from discussion.models import Comment, Vote
 from paper.models import Paper
 
 
-@receiver(post_save, sender=Paper, dispatch_uid='create_paper_issuance')
-def distribute_create_paper_distribution(sender, instance, created, **kwargs):
+@receiver(post_save, sender=Paper, dispatch_uid='create_paper')
+def distribute_for_create_paper(sender, instance, created, **kwargs):
     timestamp = time()
-    if created and instance.uploaded_by:
+    if created and is_eligible(instance.uploaded_by):
         distributor = Distributor(
             CreatePaper,
             instance.uploaded_by,
@@ -20,3 +21,44 @@ def distribute_create_paper_distribution(sender, instance, created, **kwargs):
             timestamp
         )
         distributor.distribute()
+
+
+@receiver(post_save, sender=Vote, dispatch_uid='comment_voted')
+def distribute_for_vote(sender, instance, created, update_fields, **kwargs):
+    timestamp = time()
+    distributor = None
+    recipient = instance.item.created_by
+
+    if (created or vote_type_updated(update_fields)) and is_eligible(
+        recipient
+    ):
+        distribution = get_vote_item_distribution(instance)
+        distributor = Distributor(
+            distribution,
+            recipient,
+            instance,
+            timestamp
+        )
+    if distributor is not None:
+        distributor.distribute()
+
+
+def is_eligible(user):
+    if user is not None:
+        return user.is_active
+    return False
+
+
+def vote_type_updated(update_fields):
+    if update_fields is not None:
+        return update_fields['vote_type'] is not None
+    return False
+
+
+def get_vote_item_distribution(instance):
+    if instance.vote_type == Vote.UPVOTE:
+        if type(instance.item) == Comment:
+            return CommentUpvoted
+    elif instance.vote_type == Vote.DOWNVOTE:
+        if type(instance.item) == Comment:
+            return CommentDownvoted
--- src/reputation/tests/test_signals.py
+++ src/reputation/tests/test_signals.py
@@ -8,10 +8,15 @@ from user.test_helpers import create_random_default_user
 class SignalTests(TestCase):
 
     def setUp(self):
-        pass
+        self.user = create_random_default_user('Molly')
 
     def test_create_paper_increases_rep_by_1(self):
-        unique_value = 'Ronald Weasley'
-        user = self.create_random_default_user(unique_value)
+        user = create_random_default_user('Ronald')
         create_paper(uploaded_by=user)
         self.assertEqual(user.reputation, 2)
+
+    def test_comment_upvoted_increases_rep_by_5(self):
+        user = create_random_default_user('Ginny')
+        comment = create_comment(created_by=user)
+        upvote_comment(comment=comment, created_by=self.user)
+        self.assertEqual(user.reputation, 6)
