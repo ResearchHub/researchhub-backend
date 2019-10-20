7b25dc715e4b96175379431e026b8956abb74b0b
valmack <valmack@protonmail.com>
2019-10-20 12:51:08 -0400

    Test summary permissions

--- src/summary/permissions.py
+++ src/summary/permissions.py
@@ -5,14 +5,36 @@ class CreateSummary(RuleBasedPermission):
     message = 'Not enough reputation to create summary.'
 
     def satisfies_rule(self, request):
-        return request.user.reputation >= 5
+        if request.method == 'POST':
+            return request.user.reputation >= 5
+        return True
 
 
 class UpdateSummary(RuleBasedPermission):
     message = 'Not enough reputation to update summary.'
 
     def satisfies_rule(self, request):
-        return request.user.reputation >= 50
+        if (request.method == 'PUT') or (request.method == 'PATCH'):
+            return request.user.reputation >= 50
+        return True
+
+
+class ProposeSummaryEdit(RuleBasedPermission):
+    message = 'Not enough reputation to propose summary edit.'
+
+    def satisfies_rule(self, request):
+        if request.method == 'POST':
+            return request.user.reputation >= 5
+        return True
+
+
+# TODO: Implement
+class UpdateSummaryEdit(AuthorizationBasedPermission):
+    message = 'Action not permitted.'
+
+    def is_authorized(self, request):
+        # Request user is summary edit proposer
+        pass
 
 
 class FlagSummary(RuleBasedPermission):
