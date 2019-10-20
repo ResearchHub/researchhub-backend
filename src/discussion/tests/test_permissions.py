2fd2f337dbb48098e006010b1ce996635b119df8
valmack <valmack@protonmail.com>
2019-10-20 11:08:35 -0400

    Test discussion vote permissions

--- src/discussion/tests/test_permissions.py
+++ src/discussion/tests/test_permissions.py
@@ -3,6 +3,7 @@ import random
 from .helpers import (
     build_comment_data,
     build_discussion_default_url,
+    build_discussion_detail_url,
     build_reply_data,
     build_thread_form,
     create_comment,
@@ -52,23 +53,84 @@ class DiscussionThreadPermissionsIntegrationTests(
         self.assertEqual(response.status_code, 403)
 
     def test_can_post_comment_with_minimum_reputation(self):
-        user = self.create_user_with_reputation(2)
+        user = self.create_user_with_reputation(1)
         response = self.get_comment_post_response(user)
         self.assertEqual(response.status_code, 201)
 
     def test_can_NOT_post_comment_below_minimum_reputation(self):
-        user = self.create_user_with_reputation(-1)
+        user = self.create_user_with_reputation(0)
         response = self.get_comment_post_response(user)
         self.assertEqual(response.status_code, 403)
 
     def test_can_post_reply_with_minimum_reputation(self):
-        user = self.create_user_with_reputation(3)
+        user = self.create_user_with_reputation(1)
         response = self.get_reply_post_response(user)
         self.assertEqual(response.status_code, 201)
 
     def test_can_NOT_post_reply_below_minimum_reputation(self):
-        user = self.create_user_with_reputation(-2)
+        user = self.create_user_with_reputation(0)
         response = self.get_reply_post_response(user)
+        self.assertEqual(response.status_code, 403)
+
+    def test_can_upvote_thread_with_minimum_reputation(self):
+        user = self.create_user_with_reputation(1)
+        response = self.get_thread_upvote_post_response(user)
+        self.assertEqual(response.status_code, 201)
+
+    def test_can_NOT_upvote_thread_below_minimum_reputation(self):
+        user = self.create_user_with_reputation(0)
+        response = self.get_thread_upvote_post_response(user)
+        self.assertEqual(response.status_code, 403)
+
+    def test_can_downvote_thread_with_minimum_reputation(self):
+        user = self.create_user_with_reputation(25)
+        response = self.get_thread_downvote_post_response(user)
+        self.assertEqual(response.status_code, 201)
+
+    def test_can_NOT_downvote_thread_below_minimum_reputation(self):
+        user = self.create_user_with_reputation(24)
+        response = self.get_thread_downvote_post_response(user)
+        import pdb; pdb.set_trace()
+        self.assertEqual(response.status_code, 403)
+
+    def test_can_upvote_comment_with_minimum_reputation(self):
+        user = self.create_user_with_reputation(1)
+        response = self.get_comment_upvote_post_response(user)
+        self.assertEqual(response.status_code, 201)
+
+    def test_can_NOT_upvote_comment_below_minimum_reputation(self):
+        user = self.create_user_with_reputation(0)
+        response = self.get_comment_upvote_post_response(user)
+        self.assertEqual(response.status_code, 403)
+
+    def test_can_downvote_comment_with_minimum_reputation(self):
+        user = self.create_user_with_reputation(25)
+        response = self.get_comment_downvote_post_response(user)
+        self.assertEqual(response.status_code, 201)
+
+    def test_can_NOT_downvote_comment_below_minimum_reputation(self):
+        user = self.create_user_with_reputation(24)
+        response = self.get_comment_downvote_post_response(user)
+        self.assertEqual(response.status_code, 403)
+
+    def test_can_upvote_reply_with_minimum_reputation(self):
+        user = self.create_user_with_reputation(1)
+        response = self.get_reply_upvote_post_response(user)
+        self.assertEqual(response.status_code, 201)
+
+    def test_can_NOT_upvote_reply_below_minimum_reputation(self):
+        user = self.create_user_with_reputation(0)
+        response = self.get_reply_upvote_post_response(user)
+        self.assertEqual(response.status_code, 403)
+
+    def test_can_downvote_reply_with_minimum_reputation(self):
+        user = self.create_user_with_reputation(25)
+        response = self.get_reply_downvote_post_response(user)
+        self.assertEqual(response.status_code, 201)
+
+    def test_can_NOT_downvote_reply_below_minimum_reputation(self):
+        user = self.create_user_with_reputation(24)
+        response = self.get_reply_downvote_post_response(user)
         self.assertEqual(response.status_code, 403)
 
     def create_user_with_reputation(self, reputation):
@@ -102,6 +164,16 @@ class DiscussionThreadPermissionsIntegrationTests(
         )
         return response
 
+    def get_thread_upvote_post_response(self, user):
+        url = build_discussion_detail_url(self, 'thread')
+        response = self.get_upvote_response(user, url)
+        return response
+
+    def get_thread_downvote_post_response(self, user):
+        url = build_discussion_detail_url(self, 'thread')
+        response = self.get_downvote_response(user, url)
+        return response
+
     def get_comment_post_response(self, user):
         url = build_discussion_default_url(self, 'comment')
         data = build_comment_data(self.thread.id, 'test permissions comment')
@@ -113,9 +185,51 @@ class DiscussionThreadPermissionsIntegrationTests(
         )
         return response
 
+    def get_comment_upvote_post_response(self, user):
+        url = build_discussion_detail_url(self, 'comment')
+        response = self.get_upvote_response(user, url)
+        return response
+
+    def get_comment_downvote_post_response(self, user):
+        url = build_discussion_detail_url(self, 'comment')
+        response = self.get_downvote_response(user, url)
+        return response
+
     def get_reply_post_response(self, user):
         url = build_discussion_default_url(self, 'reply')
         data = build_reply_data(self.comment.id, 'test permissions reply')
+        response = get_authenticated_post_response(
+            user,
+            url,
+            data,
+            content_type='application/json'
+        )
+        return response
+
+    def get_reply_upvote_post_response(self, user):
+        url = build_discussion_detail_url(self, 'reply')
+        response = self.get_upvote_response(user, url)
+        return response
+
+    def get_reply_downvote_post_response(self, user):
+        url = build_discussion_detail_url(self, 'reply')
+        response = self.get_downvote_response(user, url)
+        return response
+
+    def get_upvote_response(self, user, url):
+        url += 'upvote/'
+        data = {}
+        response = get_authenticated_post_response(
+            user,
+            url,
+            data,
+            content_type='application/json'
+        )
+        return response
+
+    def get_downvote_response(self, user, url):
+        url += 'downvote/'
+        data = {}
         response = get_authenticated_post_response(
             user,
             url,
