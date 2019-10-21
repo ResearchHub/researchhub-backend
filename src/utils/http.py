90be391bdaa425d258b4c2b11a77d81bd75a825a
valmack <valmack@protonmail.com>
2019-10-21 14:50:24 -0400

    Create request methods enum util

--- src/utils/http.py
+++ src/utils/http.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+
+class RequestMethods(Enum):
+    POST = 'POST'
+    PATCH = 'PATCH'
+    PUT = 'PUT'
+    DELETE = 'DELETE'
+
+
 def get_user_from_request(ctx):
     request = ctx.get('request')
     if request and hasattr(request, 'user'):
