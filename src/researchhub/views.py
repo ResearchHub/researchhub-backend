bb02af24cb03c1d8c6242882f7325ddca8d9e4b8
Patrick Lu <lightning.lu7@gmail.com>
2019-10-21 16:05:13 -0700

    added healthcheck URL

--- src/researchhub/views.py
+++ src/researchhub/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 def index(request):
     return HttpResponse(
         "Authenticate with a token in the Authorization header."
     )
+
+
+@api_view(['GET'])
+@permission_classes(())
+def healthcheck(request):
+    """
+    Health check for elastic beanstalk
+    """
+
+    return Response({'PONG'})
