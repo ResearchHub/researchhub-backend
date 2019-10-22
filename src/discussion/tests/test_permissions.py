89d37147bdf766a0b8ceada3474425ac35ac6630
valmack <valmack@protonmail.com>
2019-10-22 13:15:27 -0400

    Test endorsement permissions

--- src/discussion/tests/test_permissions.py
+++ src/discussion/tests/test_permissions.py
@@ -13,6 +13,7 @@ from .helpers import (
 from .tests import (
     BaseIntegrationTestCase as DiscussionIntegrationTestCase
 )
+from user.models import Author
 from user.tests.helpers import create_random_authenticated_user
 from paper.tests.helpers import create_paper
 from utils.test_helpers import (
@@ -35,6 +36,11 @@ class DiscussionThreadPermissionsIntegrationTests(
         self.comment = create_comment(thread=self.thread, created_by=self.user)
         self.reply = create_reply(parent=self.comment, created_by=self.user)
         self.trouble_maker = create_random_authenticated_user('trouble_maker')
+        self.author = create_random_authenticated_user('author')
+        self.moderator = create_random_authenticated_user('moderator')
+
+        self.add_paper_author(Author.objects.get(user=self.author))
+        self.add_paper_moderator(self.moderator)
 
     def test_all_users_can_view_threads(self):
         user = self.create_user_with_reputation(0)
@@ -70,6 +76,99 @@ class DiscussionThreadPermissionsIntegrationTests(
     def test_can_NOT_post_reply_below_minimum_reputation(self):
         user = self.create_user_with_reputation(0)
         response = self.get_reply_post_response(user)
+        self.assertEqual(response.status_code, 403)
+
+    def test_author_can_endorse_thread(self):
+        thread = create_thread(title='A', paper=self.paper)
+        response = self.get_thread_endorsement_post_response(
+            self.author,
+            thread
+        )
+        self.assertContains(
+            response,
+            thread.id,
+            status_code=201
+        )
+
+    def test_moderator_can_endorse_thread(self):
+        thread = create_thread(title='M', paper=self.paper)
+        response = self.get_thread_endorsement_post_response(
+            self.moderator,
+            thread
+        )
+        self.assertContains(
+            response,
+            thread.id,
+            status_code=201
+        )
+
+    def test_can_NOT_endorse_thread_if_not_author_or_moderator(self):
+        thread = create_thread(title='N', paper=self.paper)
+        response = self.get_thread_endorsement_post_response(self.user, thread)
+        self.assertEqual(response.status_code, 403)
+
+    def test_author_can_endorse_comment(self):
+        comment = create_comment(text='A', thread=self.thread)
+        response = self.get_comment_endorsement_post_response(
+            self.author,
+            comment
+        )
+        self.assertContains(
+            response,
+            comment.id,
+            status_code=201
+        )
+
+    def test_moderator_can_endorse_comment(self):
+        comment = create_comment(text='M', thread=self.thread)
+        response = self.get_comment_endorsement_post_response(
+            self.moderator,
+            comment
+        )
+        self.assertContains(
+            response,
+            comment.id,
+            status_code=201
+        )
+
+    def test_can_NOT_endorse_comment_if_not_author_or_moderator(self):
+        comment = create_comment(text='N', thread=self.thread)
+        response = self.get_comment_endorsement_post_response(
+            self.user,
+            comment
+        )
+        self.assertEqual(response.status_code, 403)
+
+    def test_author_can_endorse_reply(self):
+        reply = create_reply(text='A', parent=self.comment)
+        response = self.get_reply_endorsement_post_response(
+            self.author,
+            reply
+        )
+        self.assertContains(
+            response,
+            reply.id,
+            status_code=201
+        )
+
+    def test_moderator_can_endorse_reply(self):
+        reply = create_reply(text='M', parent=self.comment)
+        response = self.get_reply_endorsement_post_response(
+            self.moderator,
+            reply
+        )
+        self.assertContains(
+            response,
+            reply.id,
+            status_code=201
+        )
+
+    def test_can_NOT_endorse_reply_if_not_author_or_moderator(self):
+        reply = create_reply(text='N', parent=self.comment)
+        response = self.get_reply_endorsement_post_response(
+            self.user,
+            reply
+        )
         self.assertEqual(response.status_code, 403)
 
     def test_can_flag_thread_with_minimum_reputation(self):
@@ -162,6 +261,14 @@ class DiscussionThreadPermissionsIntegrationTests(
         response = self.get_reply_downvote_post_response(user)
         self.assertEqual(response.status_code, 403)
 
+    def add_paper_author(self, author):
+        self.paper.authors.add(author)
+        self.paper.save()
+
+    def add_paper_moderator(self, moderator):
+        self.paper.moderators.add(moderator)
+        self.paper.save()
+
     def create_user_with_reputation(self, reputation):
         unique_value = self.random_generator.random()
         user = self.create_random_authenticated_user(unique_value)
@@ -193,6 +300,14 @@ class DiscussionThreadPermissionsIntegrationTests(
         )
         return response
 
+    def get_thread_endorsement_post_response(self, user, thread):
+        url = self.get_thread_endorsement_url(thread)
+        response = self.get_endorsement_response(user, url)
+        return response
+
+    def get_thread_endorsement_url(self, thread):
+        return self.base_url + f'paper/{self.paper.id}/discussion/{thread.id}/'
+
     def get_thread_flag_post_response(self, user):
         url = build_discussion_detail_url(self, 'thread')
         reason = 'This thread is inappropriate'
@@ -219,6 +334,17 @@ class DiscussionThreadPermissionsIntegrationTests(
             content_type='application/json'
         )
         return response
+
+    def get_comment_endorsement_post_response(self, user, comment):
+        url = self.get_comment_endorsement_url(comment)
+        response = self.get_endorsement_response(user, url)
+        return response
+
+    def get_comment_endorsement_url(self, comment):
+        return self.base_url + (
+            f'paper/{self.paper.id}/discussion/{self.thread.id}/'
+            + f'comment/{comment.id}/'
+        )
 
     def get_comment_flag_post_response(self, user):
         url = build_discussion_detail_url(self, 'comment')
@@ -247,6 +373,17 @@ class DiscussionThreadPermissionsIntegrationTests(
         )
         return response
 
+    def get_reply_endorsement_post_response(self, user, reply):
+        url = self.get_reply_endorsement_url(reply)
+        response = self.get_endorsement_response(user, url)
+        return response
+
+    def get_reply_endorsement_url(self, reply):
+        return self.base_url + (
+            f'paper/{self.paper.id}/discussion/{self.thread.id}/'
+            + f'comment/{self.comment.id}/reply/{reply.id}/'
+        )
+
     def get_reply_flag_post_response(self, user):
         url = build_discussion_detail_url(self, 'reply')
         reason = 'This reply is inappropriate'
@@ -261,6 +398,17 @@ class DiscussionThreadPermissionsIntegrationTests(
     def get_reply_downvote_post_response(self, user):
         url = build_discussion_detail_url(self, 'reply')
         response = self.get_downvote_response(user, url)
+        return response
+
+    def get_endorsement_response(self, user, url):
+        url += 'endorse/'
+        data = None
+        response = get_authenticated_post_response(
+            user,
+            url,
+            data,
+            content_type='application/json'
+        )
         return response
 
     def get_flag_response(self, user, url, reason):
