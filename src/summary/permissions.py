dfeebabe9da6e082684f5d40aa0570642abc5efc
valmack <valmack@protonmail.com>
2019-10-24 19:42:22 -0400

    Update summary edit permissions

--- src/summary/permissions.py
+++ src/summary/permissions.py
@@ -1,57 +1,44 @@
+from utils.http import RequestMethods
 from utils.permissions import AuthorizationBasedPermission, RuleBasedPermission
-
-
-class CreateSummary(RuleBasedPermission):
-    message = 'Not enough reputation to create summary.'
-
-    def satisfies_rule(self, request):
-        if request.method == 'POST':
-            return request.user.reputation >= 5
-        return True
-
-
-class UpdateSummary(RuleBasedPermission):
-    message = 'Not enough reputation to update summary.'
-
-    def satisfies_rule(self, request):
-        if (request.method == 'PUT') or (request.method == 'PATCH'):
-            return request.user.reputation >= 50
-        return True
 
 
 class ProposeSummaryEdit(RuleBasedPermission):
     message = 'Not enough reputation to propose summary edit.'
 
     def satisfies_rule(self, request):
-        if request.method == 'POST':
+        if request.method == RequestMethods.POST:
             return request.user.reputation >= 5
         return True
 
 
-# TODO: Implement
-class UpdateSummaryEdit(AuthorizationBasedPermission):
+class UpdateOrDeleteSummaryEdit(AuthorizationBasedPermission):
     message = 'Action not permitted.'
 
-    def is_authorized(self, request):
-        # Request user is summary edit proposer
-        pass
+    def is_authorized(self, request, view, obj):
+        if (request.method == RequestMethods.PUT) or (
+            request.method == RequestMethods.PATCH
+        ) or (request.method == RequestMethods.DELETE):
+            if obj.approved is True:
+                return False
+            return request.user == obj.proposed_by
+        return True
 
 
-class FlagSummary(RuleBasedPermission):
+class FlagSummaryEdit(RuleBasedPermission):
     message = 'Not enough reputation to flag summary.'
 
     def satisfies_rule(self, request):
         return request.user.reputation >= 50
 
 
-class ApproveSummary(RuleBasedPermission):
+class ApproveSummaryEdit(RuleBasedPermission):
     message = 'Not enough reputation to approve summary.'
 
     def satisfies_rule(self, request):
         return request.user.reputation >= 50
 
 
-class RejectSummary(RuleBasedPermission):
+class RejectSummaryEdit(RuleBasedPermission):
     message = 'Not enough reputation to reject summary.'
 
     def satisfies_rule(self, request):
