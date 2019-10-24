8f8d9a8d717b795285ab8d54915192f112ad6b37
valmack <valmack@protonmail.com>
2019-10-24 12:31:48 -0400

    Add subscribe actions to hub view

--- src/hub/views.py
+++ src/hub/views.py
@@ -1,10 +1,15 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
+from rest_framework.response import Response
 
 from .models import Hub
-from .permissions import CreateHub
+from .permissions import CreateHub, IsSubscribed, IsNotSubscribed
 from .serializers import HubSerializer
 
 
@@ -15,3 +20,38 @@ class HubViewSet(viewsets.ModelViewSet):
     filter_backends = (SearchFilter, DjangoFilterBackend, OrderingFilter)
     permission_classes = [IsAuthenticatedOrReadOnly & CreateHub]
     search_fields = ('name')
+
+    @action(
+        detail=True,
+        methods=['post', 'put', 'patch'],
+        permission_classes=[IsNotSubscribed]
+    )
+    def subscribe(self, request, pk=None):
+        hub = self.get_object()
+        try:
+            hub.subscribers.add(request.user)
+            hub.save()
+            return self._get_hub_serialized_response(hub, 200)
+        except Exception as e:
+            return Response(e, status=400)
+
+    @action(
+        detail=True,
+        methods=['post', 'put', 'patch'],
+        permission_classes=[IsSubscribed]
+    )
+    def unsubscribe(self, request, pk=None):
+        hub = self.get_object()
+        try:
+            hub.subscribers.remove(request.user)
+            hub.save()
+            return self._get_hub_serialized_response(hub, 200)
+        except Exception as e:
+            return Response(e, status=400)
+
+    def _get_hub_serialized_response(self, hub, status_code):
+        serialized = HubSerializer(hub)
+        return Response(serialized.data, status=status_code)
+
+    def _is_subscribed(self, user, hub):
+        return user in hub.subscribers.all()
