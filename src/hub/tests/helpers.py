a1d012eadb3dab39800a8e8e215d5109f2136b15
valmack <valmack@protonmail.com>
2019-10-24 17:52:05 -0400

    Add subscriber count and if user is subscriber to hub

--- src/hub/tests/helpers.py
+++ src/hub/tests/helpers.py
@@ -15,3 +15,9 @@ def create_hub(name=TestData.hub_name):
     return Hub.objects.create(
         name=name
     )
+
+
+def subscribe(hub, user):
+    hub.subscribers.add(user)
+    hub.save()
+    return hub
