0156c5c5a8f17a6ded7bdacc9c11be929e0ca7d3
valmack <valmack@protonmail.com>
2019-10-24 12:30:01 -0400

    Add hub subscribed permissions

--- src/hub/permissions.py
+++ src/hub/permissions.py
@@ -1,4 +1,4 @@
-from utils.permissions import RuleBasedPermission
+from utils.permissions import AuthorizationBasedPermission, RuleBasedPermission
 
 
 class CreateHub(RuleBasedPermission):
@@ -6,3 +6,17 @@ class CreateHub(RuleBasedPermission):
 
     def satisfies_rule(self, request):
         return request.user.reputation >= 100
+
+
+class IsSubscribed(AuthorizationBasedPermission):
+    message = 'Unauthorized.'
+
+    def is_authorized(self, request, view, obj):
+        return request.user in obj.subscribers.all()
+
+
+class IsNotSubscribed(AuthorizationBasedPermission):
+    message = 'Unauthorized.'
+
+    def is_authorized(self, request, view, obj):
+        return request.user not in obj.subscribers.all()
