d162ff3bd835bbaa7833d6c50f0ffd7daf590b5e
valmack <valmack@protonmail.com>
2019-10-24 12:29:05 -0400

    Add subscribers to hub model

--- src/hub/models.py
+++ src/hub/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+from user.models import User
+
 
 class Hub(models.Model):
     name = models.CharField(max_length=255, unique=True)
     is_locked = models.BooleanField(default=True)
+    subscribers = models.ManyToManyField(User, related_name='subscribed_hubs')
