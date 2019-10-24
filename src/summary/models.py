e1e1c4eaa7ce7f392b988cb4e5f36269c8b05555
valmack <valmack@protonmail.com>
2019-10-24 17:08:49 -0400

    WIP - Summary auto approve

--- src/summary/models.py
+++ src/summary/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
@@ -40,3 +41,9 @@ class Summary(models.Model):
 
     def __str__(self):
         return self.id
+
+    def approve(self, by):
+        self.approved = True
+        self.approved_by = by
+        self.approved_at = datetime.now()
+        self.save()
