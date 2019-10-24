88b89421369b2f9e06ddc6155a19329e364f6c2c
valmack <valmack@protonmail.com>
2019-10-24 19:43:29 -0400

    Test put, patch, and delete summary edits

--- src/summary/tests/test_permissions.py
+++ src/summary/tests/test_permissions.py
@@ -7,6 +7,7 @@ from user.tests.helpers import (
     create_random_authenticated_user_with_reputation
 )
 from utils.test_helpers import (
+    get_authenticated_delete_response,
     get_authenticated_patch_response,
     get_authenticated_post_response,
     get_authenticated_put_response
@@ -36,24 +37,37 @@ class SummaryPermissionsTests(TestCase):
         response = self.get_summary_post_response(user)
         self.assertEqual(response.status_code, 403)
 
-    def test_can_patch_summary_with_minimum_reputation(self):
+    def test_can_patch_summary_when_user_is_proposer(self):
         user = create_random_authenticated_user_with_reputation(50, 50)
-        response = self.get_summary_patch_response(user)
+        summary = create_summary('patch summary', user, self.paper.id)
+        response = self.get_summary_patch_response(user, summary=summary)
         self.assertEqual(response.status_code, 200)
 
-    def test_can_NOT_patch_summary_below_minimum_reputation(self):
+    def test_can_NOT_patch_summary_when_not_proposer(self):
         user = create_random_authenticated_user_with_reputation(49, 49)
         response = self.get_summary_patch_response(user)
         self.assertEqual(response.status_code, 403)
 
-    def test_can_put_summary_with_minimum_reputation(self):
+    def test_can_put_summary_when_user_is_proposer(self):
         user = create_random_authenticated_user_with_reputation(50, 50)
-        response = self.get_summary_put_response(user)
+        summary = create_summary('put summary', user, self.paper.id)
+        response = self.get_summary_put_response(user, summary=summary)
         self.assertEqual(response.status_code, 200)
 
-    def test_can_NOT_put_summary_below_minimum_reputation(self):
+    def test_can_NOT_put_summary_when_not_proposer(self):
         user = create_random_authenticated_user_with_reputation(49, 49)
         response = self.get_summary_put_response(user)
+        self.assertEqual(response.status_code, 403)
+
+    def test_can_delete_summary_when_user_is_proposer(self):
+        user = create_random_authenticated_user_with_reputation(50, 50)
+        summary = create_summary('delete summary', user, self.paper.id)
+        response = self.get_summary_delete_response(user, summary=summary)
+        self.assertEqual(response.status_code, 204)
+
+    def test_can_NOT_delete_summary_when_not_proposer(self):
+        user = create_random_authenticated_user_with_reputation(49, 49)
+        response = self.get_summary_delete_response(user)
         self.assertEqual(response.status_code, 403)
 
     def get_summary_post_response(self, user):
@@ -67,8 +81,10 @@ class SummaryPermissionsTests(TestCase):
         )
         return response
 
-    def get_summary_patch_response(self, user):
-        url = self.base_url + f'{self.summary.id}/'
+    def get_summary_patch_response(self, user, summary=None):
+        if summary is None:
+            summary = self.summary
+        url = self.base_url + f'{summary.id}/'
         data = {'summary': 'A patch update'}
         response = get_authenticated_patch_response(
             user,
@@ -78,10 +94,25 @@ class SummaryPermissionsTests(TestCase):
         )
         return response
 
-    def get_summary_put_response(self, user):
-        url = self.base_url + f'{self.summary.id}/'
+    def get_summary_put_response(self, user, summary=None):
+        if summary is None:
+            summary = self.summary
+        url = self.base_url + f'{summary.id}/'
         data = build_summary_data('A put update', self.paper.id, None)
         response = get_authenticated_put_response(
+            user,
+            url,
+            data,
+            content_type='application/json'
+        )
+        return response
+
+    def get_summary_delete_response(self, user, summary=None):
+        if summary is None:
+            summary = self.summary
+        url = self.base_url + f'{summary.id}/'
+        data = None
+        response = get_authenticated_delete_response(
             user,
             url,
             data,
