4eb119f4ef58dfca817ec7e719505897183f3d09
Patrick Lu <lightning.lu7@gmail.com>
2019-10-25 18:52:57 -0700

    added hub filters

--- src/hub/models.py
+++ src/hub/models.py
@@ -4,6 +4,13 @@ from user.models import User
 
 
 class Hub(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=1024, unique=True)
     is_locked = models.BooleanField(default=True)
     subscribers = models.ManyToManyField(User, related_name='subscribed_hubs')
+
+    def __str__(self):
+        return '{}, locked: {}'.format(self.name, self.is_locked)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        return super(Hub, self).save(*args, **kwargs)
