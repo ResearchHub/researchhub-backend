22e06b5e835a4a3441d560cc53a9ea30de93ef97
Patrick Lu <lightning.lu7@gmail.com>
2019-10-26 01:57:00 -0700

    added new AWS storage buckets

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -273,7 +273,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Storage
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_STORAGE_BUCKET_NAME = 'researchhub-paper-dev'
+
+AWS_STORAGE_BUCKET_NAME = 'researchhub-paper-dev1'
 AWS_S3_REGION_NAME = 'us-west-2'
 AWS_ACCESS_KEY_ID = os.environ.get(
     'AWS_ACCESS_KEY_ID',
@@ -283,6 +284,9 @@ AWS_SECRET_ACCESS_KEY = os.environ.get(
     'AWS_SECRET_ACCESS_KEY',
     keys.AWS_SECRET_ACCESS_KEY
 )
+
+if PRODUCTION:
+    AWS_STORAGE_BUCKET_NAME = 'researchhub-paper-prod'
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
