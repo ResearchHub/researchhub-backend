fc076390b7aa6fb44f2c2b62d25e9bb45f408132
valmack <valmack@protonmail.com>
2019-10-31 19:58:36 -0400

    Use fuzzy search for authors

--- src/search/filters.py
+++ src/search/filters.py
@@ -1,13 +1,17 @@
 from rest_framework import filters
+from elasticsearch_dsl.query import Fuzzy
 
 
 class ElasticsearchFilter(filters.SearchFilter):
+
     def filter_queryset(self, request, queryset, view):
         search = getattr(view, 'search', None)
-        s = search.query(
-            'multi_match',
-            query=' '.join(self.get_search_terms(request)),
-            fields=self.get_search_fields(view, request)
-        )
+        terms = ' '.join(self.get_search_terms(request))
+
+        # TODO: Get field names from request instead of hardcoding
+        # fuzzy_field = request.get('fuzzy', None)
+        f = Fuzzy(** {'first_name': terms})
+        s = search.query(f)
+
         response = s.execute()
         return response
