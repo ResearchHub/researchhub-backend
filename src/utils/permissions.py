086ffcac01d3799ae457692225f2e2ff4138b1a5
valmack <valmack@protonmail.com>
2019-10-31 16:29:45 -0400

    Create read only permission

--- src/utils/permissions.py
+++ src/utils/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+
 class AuthorizationBasedPermission(BasePermission):
     class Meta:
         abstract = True
