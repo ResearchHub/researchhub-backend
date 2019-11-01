3982e874bf7ccd017e9b02c08ebf4e7b4bf8db4a
valmack <valmack@protonmail.com>
2019-11-01 11:19:30 -0400

    Add created and update dates to hub model

--- src/hub/models.py
+++ src/hub/models.py
@@ -7,6 +7,8 @@ class Hub(models.Model):
     name = models.CharField(max_length=1024, unique=True)
     is_locked = models.BooleanField(default=True)
     subscribers = models.ManyToManyField(User, related_name='subscribed_hubs')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{}, locked: {}'.format(self.name, self.is_locked)
@@ -14,3 +16,7 @@ class Hub(models.Model):
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
         return super(Hub, self).save(*args, **kwargs)
+
+    @property
+    def subscriber_count_indexing(self):
+        return len(self.subscribers.all())
