38733e90f176025a344eebee32fe90bf4fb78e50
valmack <valmack@protonmail.com>
2019-11-04 16:21:48 -0500

    Add first action to summary viewset Add an auto-approved summary to a paper without a summary using the first route

--- src/summary/views.py
+++ src/summary/views.py
@@ -1,6 +1,9 @@
 from django.db import transaction
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -36,6 +39,59 @@ class SummaryViewSet(viewsets.ModelViewSet):
 
     @transaction.atomic
     def create(self, request):
+        paper_id = request.data.get('paper')
+        summary = self._create_summary(request)
+
+        if self._user_can_direct_edit(request.user):
+            self._approve_and_add_summary_to_paper(
+                paper_id,
+                summary,
+                request.user
+            )
+
+        return Response(SummarySerializer(summary).data, status=201)
+
+    @action(
+        detail=False,
+        methods=['post'],
+        permission_classes=[IsAuthenticated]
+    )
+    def first(self, request):
+        paper_id = request.data.get('paper')
+
+        try:
+            paper = Paper.objects.get(pk=paper_id)
+        except Paper.DoesNotExist:
+            return Response(
+                f'Failed to get paper with id {paper_id}',
+                status=400
+            )
+
+        if request.user != paper.uploaded_by:
+            return Response(
+                'Summary paper must be uploaded by request user',
+                status=403
+            )
+        else:
+            with transaction.atomic():
+                summary = self._create_summary(request)
+
+                if self._paper_has_no_summary(paper_id):
+                    # approved_by = None denotes the paper's first summary
+                    approved_by = None
+                    self._approve_and_add_summary_to_paper(
+                        paper_id,
+                        summary,
+                        approved_by
+                    )
+
+                return Response(
+                    SummarySerializer(summary).data,
+                    status=201
+                )
+
+    @transaction.atomic
+    def _create_summary(self, request):
         user = request.user
         summary = request.data.get('summary')
         paper_id = request.data.get('paper')
@@ -52,15 +108,21 @@ class SummaryViewSet(viewsets.ModelViewSet):
             previous=previous_summary
         )
 
-        if self._user_can_direct_edit(user):
-            new_summary.approve(by=user)
-            self._set_paper_summary(paper_id, new_summary)
+        return new_summary
 
-        return Response(SummarySerializer(new_summary).data, status=201)
+    def _paper_has_no_summary(self, paper_id):
+        paper = Paper.objects.get(id=paper_id)
+        return paper.summary is None
 
     def _user_can_direct_edit(self, user):
         return user.reputation >= 50
 
-    def _set_paper_summary(self, paper_id, summary):
+    def _approve_and_add_summary_to_paper(self, paper_id, summary, user):
+        summary.approve(by=user)
+        summary.save()
+        self._update_paper_summary(paper_id, summary)
+
+    def _update_paper_summary(self, paper_id, summary):
         paper = Paper.objects.get(id=paper_id)
         paper.update_summary(summary)
+        paper.save()
