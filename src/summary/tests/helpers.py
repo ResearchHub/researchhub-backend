a198bcfc511e761c97a9c592d9dc18ae62f8c9f4
valmack <valmack@protonmail.com>
2019-11-07 10:06:20 -0500

    Add plain text field to summary model

--- src/summary/tests/helpers.py
+++ src/summary/tests/helpers.py
@@ -4,6 +4,7 @@ from summary.models import Summary
 def build_summary_data(summary, paper_id, previous_summary_id):
     return {
         'summary': summary,
+        'summary_plain_text': summary,
         'paper': paper_id,
         'previousSummaryId': previous_summary_id,
     }
@@ -12,6 +13,7 @@ def build_summary_data(summary, paper_id, previous_summary_id):
 def create_summary(summary, proposed_by, paper_id):
     return Summary.objects.create(
         summary=summary,
+        summary_plain_text=summary,
         proposed_by=proposed_by,
         paper_id=paper_id
     )
