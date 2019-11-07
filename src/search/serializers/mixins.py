0d8ea6e7350c48f302c0497dcaf6a343421cf4eb
valmack <valmack@protonmail.com>
2019-11-07 15:59:17 -0500

    Remove highlight serializer mixin And remove empty file

--- src/search/serializers/mixins.py
+++
@@ -1,6 +0,0 @@
-class HighlightSerializerMixin:
-
-    def get_highlight(self, obj):
-        if hasattr(obj.meta, 'highlight'):
-            return obj.meta.highlight.__dict__['_d_']
-        return {}
