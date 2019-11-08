21ce64520515cad684c17fb87338083b15a1d1b6
valmack <valmack@protonmail.com>
2019-11-08 13:26:21 -0500

    Add fields to thread document

--- src/search/documents/thread.py
+++ src/search/documents/thread.py
@@ -7,7 +7,19 @@ from researchhub.settings import DEVELOPMENT, TESTING
 
 @registry.register_document
 class ThreadDocument(Document):
-    paper = es_fields.StringField(attr='paper_indexing')
+    comment_count = es_fields.IntegerField(attr='comment_count_indexing')
+    # TODO: Make this a nested user field with author inside
+    created_by_author_profile = es_fields.ObjectField(
+        attr='created_by_author_profile_indexing',
+        properties={
+            'id': es_fields.IntegerField(),
+            'first_name': es_fields.StringField(),
+            'last_name': es_fields.StringField(),
+        }
+    )
+    paper = es_fields.IntegerField(attr='paper_indexing')
+    paper_title = es_fields.StringField(attr='paper_title_indexing')
+    score = es_fields.IntegerField(attr='score_indexing')
     text = es_fields.StringField(attr='text_indexing')
 
     class Index:
