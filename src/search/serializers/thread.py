dc3b1f78304d64c47181035a35485b2418468b53
valmack <valmack@protonmail.com>
2019-11-08 13:27:59 -0500

    Add serialized fields for thread search result

--- src/search/serializers/thread.py
+++ src/search/serializers/thread.py
@@ -4,25 +4,46 @@ from discussion.models import Thread
 
 
 class ThreadDocumentSerializer(serializers.ModelSerializer):
+    comment_count = serializers.SerializerMethodField()
+    created_by_author_profile = serializers.SerializerMethodField()
     paper = serializers.SerializerMethodField()
+    paper_title = serializers.SerializerMethodField()
+    score = serializers.SerializerMethodField()
     text = serializers.SerializerMethodField()
 
     class Meta(object):
         model = Thread
         fields = [
             'id',
+            'comment_count',
+            'created_by_author_profile',
             'created_date',
             'is_public',
             'is_removed',
             'paper',
+            'paper_title',
+            'score',
             'text',
             'title',
             'updated_date',
         ]
         read_only_fields = fields
 
+    def get_comment_count(self, document):
+        return document.comment_count
+
+    def get_created_by_author_profile(self, document):
+        if document.created_by_author_profile is not None:
+            return document.created_by_author_profile.to_dict()
+
     def get_paper(self, document):
         return document.paper
+
+    def get_paper_title(self, document):
+        return document.paper_title
+
+    def get_score(self, document):
+        return document.score
 
     def get_text(self, document):
         return document.text
