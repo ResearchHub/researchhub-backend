499934f4ed56435230001cb6c0a243e99afefd81
valmack <valmack@protonmail.com>
2019-11-11 18:21:03 -0500

    Make authors optional in paper serializer

---
+++ src/paper/tests/test_serializers.py
@@ -0,0 +1,20 @@
+from urllib.parse import urlencode
+
+from django.test import TestCase
+from django.http import QueryDict
+from django.forms.models import model_to_dict
+
+from paper.serializers import PaperSerializer
+from paper.tests import helpers
+
+
+class PaperSerializersTests(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_authors_field_is_optional(self):
+        paper = helpers.create_paper(title='Serialized Paper Title')
+        paper_dict = model_to_dict(paper)
+        serialized = PaperSerializer(data=paper_dict)
+        self.assertTrue(serialized.is_valid())
