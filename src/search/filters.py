a82412cb78cd6711a16f7bc6d4edd96c6cdd55bf
valmack <valmack@protonmail.com>
2019-11-12 16:09:43 -0500

    Remove unneeded search filter

--- src/search/filters.py
+++ src/search/filters.py
@@ -1,21 +1,5 @@
 from rest_framework import filters
-from elasticsearch_dsl.query import Fuzzy
 from elasticsearch_dsl import Q
-
-
-class ElasticsearchFilter(filters.SearchFilter):
-
-    def filter_queryset(self, request, queryset, view):
-        search = getattr(view, 'search', None)
-        terms = ' '.join(self.get_search_terms(request))
-
-        # TODO: Get field names from request instead of hardcoding
-        # fuzzy_field = request.get('fuzzy', None)
-        f = Fuzzy(** {'first_name': terms})
-        s = search.query(f)
-
-        response = s.execute()
-        return response
 
 
 class ElasticsearchFuzzyFilter(filters.SearchFilter):
@@ -24,7 +8,7 @@ class ElasticsearchFuzzyFilter(filters.SearchFilter):
         search = getattr(view, 'search')
         fields = getattr(view, 'search_fields')
         terms = ' '.join(self.get_search_terms(request))
-        query = Q("multi_match", query=terms, fields=fields, fuzziness="AUTO")
+        query = Q('multi_match', query=terms, fields=fields, fuzziness='AUTO')
         es = search.query(query)
 
         response = es.execute()
