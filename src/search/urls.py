8a7283b79398dc20db9951a29a6430a519046f49
valmack <valmack@protonmail.com>
2019-11-12 16:10:16 -0500

    Add back search document views

--- src/search/urls.py
+++ src/search/urls.py
@@ -2,9 +2,29 @@ from django.conf.urls import url, include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import CombinedView
+from .views import (
+    AuthorDocumentView,
+    CombinedView,
+    PaperDocumentView,
+    ThreadDocumentView
+)
 
 router = DefaultRouter()
+author = router.register(
+    r'author',
+    AuthorDocumentView,
+    basename='author_document'
+)
+paper = router.register(
+    r'paper',
+    PaperDocumentView,
+    basename='paper_document'
+)
+thread = router.register(
+    r'thread',
+    ThreadDocumentView,
+    basename='thread_document'
+)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
