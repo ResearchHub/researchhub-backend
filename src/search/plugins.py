7b785ab814249de8734cbe30cfabd855f42b851a
valmack <valmack@protonmail.com>
2019-11-15 18:59:43 -0500

    Index paper pdf with post save signal

--- src/search/plugins.py
+++ src/search/plugins.py
@@ -9,43 +9,50 @@ from .exceptions import ElasticsearchPluginError
 from paper.models import Paper
 from researchhub.settings import ELASTICSEARCH_DSL
 import utils.sentry as sentry
+from utils.http import http_request, RequestMethods as methods
 
 
-class IngestPdfProcessor(object):
+class IngestPdfPipeline:
     def __init__(self):
-        self.es_host = ELASTICSEARCH_DSL.get('default').get('hosts')
-        self.pipeline_url = self.es_host + f'/_ingest/pipeline/pdf'
+        self.host = ELASTICSEARCH_DSL.get('default').get('hosts')
+        self.url = self.host + f'/_ingest/pipeline/pdf'
+        self._build_pipeline_if_not_exists()
 
-    def attach(self, document):
-        self.document = document
-        self._get_document_index()
-        self._create_pipeline_if_not_exists()
-        for paper in Paper.objects.all():
-            self._index_pdf(paper)
+    def attach_pdf_to_document(self, index, paper):
+        """Encodes the `paper` file and adds it to Elasticsearch.
+
+        Arguments:
+            index (str) -- name of the index where the document is located
+            paper (obj) -- Paper object to get and add the pdf
+
+        Return:
+            response (requests.Response) -- result of attachment put request
+
+        """
+        url = self.host + f'/{index}/_doc/{paper.id}?pipeline=pdf'
+        pdf = self.encode_file(paper.file.url)
+        data = {
+            'filename': paper.file.name,
+            'pdf': pdf.decode('utf-8')
+        }
+        headers = {'Content-Type': 'application/json'}
+        return self._send_put_request(
+            url,
+            data,
+            headers,
+            'Failed to index pdf'
+        )
 
     def encode_file(self, url):
         with open(url, 'rb') as f:
-            # f.seek(0)
             return base64.b64encode(f.read())
 
-    def _get_document_index(self):
-        index_meta = getattr(self.document, 'Index')
-
-        if not index_meta:
-            message = (
-                f'You must declare the Index class inside '
-                f'{self.document.__name__}'
-            )
-            raise ElasticsearchPluginError(ImproperlyConfigured, message)
-
-        self.index = index_meta.name
-
-    def _create_pipeline_if_not_exists(self):
+    def _build_pipeline_if_not_exists(self):
         if self._check_pipeline_exists() is False:
-            self._create_pipeline()
+            self._build_pipeline()
 
     def _check_pipeline_exists(self):
-        response = requests.get(self.pipeline_url)
+        response = http_request(methods.GET, self.url)
         if response.status_code == 200:
             return True
         if response.status_code == 404:
@@ -55,31 +62,22 @@ class IngestPdfProcessor(object):
             f'Check pipeline failed with status code {response.status_code}'
         )
 
-    def _create_pipeline(self):
+    def _build_pipeline(self):
         description = 'Extract pdf attachment'
         processors = [{'attachment': {'field': 'pdf'}}]
         data = {'description': description, 'processors': processors}
         headers = {'Content-Type': 'application/json'}
         self._send_put_request(
-            self.pipeline_url,
+            self.url,
             data,
             headers,
             'Failed to add attachment to pipeline'
         )
 
-    def _index_pdf(self, paper):
-        url = self.es_host + f'/{self.index}/_doc/{paper.id}?pipeline=pdf'
-        pdf = self.encode_file(paper.file.url)
-        data = {
-            'filename': paper.file.name,
-            'pdf': pdf.decode('utf-8')
-        }
-        headers = {'Content-Type': 'application/json'}
-        self._send_put_request(url, data, headers, 'Failed to index pdf')
-
     def _send_put_request(self, url, data, headers, error_message):
         try:
-            response = requests.put(
+            response = http_request(
+                methods.PUT,
                 url,
                 json.dumps(data),
                 headers=headers
@@ -93,8 +91,9 @@ class IngestPdfProcessor(object):
                     response.text,
                     message
                 )
+            return response
         except Exception as e:
             raise ElasticsearchPluginError(e, error_message)
 
 
-ingest_pdf_processor = IngestPdfProcessor()
+pdf_pipeline = IngestPdfPipeline()
