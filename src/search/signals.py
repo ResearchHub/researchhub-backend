14a59c1dd820e7e2e2763bc1e8ad865e5b384da8
valmack <valmack@protonmail.com>
2019-11-15 18:59:43 -0500

    Index paper pdf with post save signal

--- src/search/signals.py
+++ src/search/signals.py
@@ -1,12 +1,24 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from search.plugins import pdf_pipeline
+from paper.models import Paper
 
-@receiver(post_save, sender='Paper', dispatch_uid='update_paper')
-def attach_file_to_document(sender, instance, created, update_fields, **kwargs):
+
+@receiver(post_save, sender=Paper, dispatch_uid='update_paper')
+def attach_file_to_document(
+    sender,
+    instance,
+    created,
+    update_fields,
+    **kwargs
+):
     if not created and check_file_updated(update_fields):
-        # TODO: Encode file and add it to this paper's document
-        pass
+        response = pdf_pipeline.attach_pdf_to_document(
+            'paper',
+            instance
+        )
+        return response.ok
 
 
 def check_file_updated(update_fields):
