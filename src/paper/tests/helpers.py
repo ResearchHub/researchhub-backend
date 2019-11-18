77161531277f932f71a732ca37d68e4305081cce
valmack <valmack@protonmail.com>
2019-11-18 15:55:41 -0500

    Add submit paper helpers

--- src/paper/tests/helpers.py
+++ src/paper/tests/helpers.py
@@ -1,6 +1,8 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
 from paper.models import Flag, Paper, Vote
 
 from user.tests.helpers import create_random_default_user
+from utils.test_helpers import get_authenticated_post_response
 
 
 class TestData:
@@ -70,3 +72,22 @@ def update_to_upvote(vote):
 def update_to_downvote(vote):
     vote.vote_type = Vote.DOWNVOTE
     vote.save(update_fields=['vote_type'])
+
+
+def submit_paper_form(user, title='Building a Paper'):
+    form_data = build_paper_form(title)
+    return get_authenticated_post_response(
+        user,
+        '/api/paper/',
+        form_data,
+        content_type='multipart/form-data'
+    )
+
+
+def build_paper_form(title='Building a Paper'):
+    file = SimpleUploadedFile('../config/paper.pdf', b'file_content')
+    form = {
+        'title': title,
+        'file': file,
+    }
+    return form
