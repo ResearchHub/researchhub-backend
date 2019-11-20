5d1c35338d327496ae1aa832a4b4c1002ae54725
Craig Lu <craig@quantfive.org>
2019-11-19 16:22:46 -0800

    refactoring paper document serializer to return paper tagline properly

--- src/search/serializers/paper.py
+++ src/search/serializers/paper.py
@@ -1,13 +1,20 @@
 # TODO: Refactor to remove drf
 from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
+from rest_framework import serializers
 
 from search.documents.paper import PaperDocument
 
 
-class PaperDocumentSerializer(DocumentSerializer):
+class PaperDocumentSerializer(serializers.ModelSerializer):
+    authors = serializers.SerializerMethodField()
+    discussion_count = serializers.SerializerMethodField()
+    hubs = serializers.SerializerMethodField()
+    score = serializers.SerializerMethodField()
+    summary = serializers.SerializerMethodField()
+    title = serializers.SerializerMethodField()
+    tagline = serializers.SerializerMethodField()
 
     class Meta(object):
-        document = PaperDocument
         fields = [
             'id',
             'authors',
@@ -22,3 +29,24 @@ class PaperDocumentSerializer(DocumentSerializer):
             'title',
             'url',
         ]
+
+        def get_authors(self, document):
+            return document.authors
+
+        def get_discussion_count(self, document):
+            return document.discussion_count
+
+        def get_hubs(self, document):
+            return document.hubs
+
+        def get_score(self, document):
+            return document.score
+
+        def get_summary(self, document):
+            return document.summary
+
+        def get_title(self, document):
+            return document.title
+
+        def get_tagline(self, document):
+            return document.tagline
