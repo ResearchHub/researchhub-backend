2e07e72850731d20c8278e8938232330edda8fb5
Craig Lu <craig@quantfive.org>
2019-11-19 16:39:08 -0800

    revert back to drf

--- src/search/serializers/paper.py
+++ src/search/serializers/paper.py
@@ -1,21 +1,13 @@
 # TODO: Refactor to remove drf
 from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
-from rest_framework import serializers
 
-from paper.models import Paper
+from search.documents.paper import PaperDocument
 
 
-class PaperDocumentSerializer(serializers.ModelSerializer):
-    authors = serializers.SerializerMethodField()
-    discussion_count = serializers.SerializerMethodField()
-    hubs = serializers.SerializerMethodField()
-    score = serializers.SerializerMethodField()
-    summary = serializers.SerializerMethodField()
-    title = serializers.SerializerMethodField()
-    tagline = serializers.SerializerMethodField()
+class PaperDocumentSerializer(DocumentSerializer):
 
     class Meta(object):
-        model = Paper
+        document = PaperDocument
         fields = [
             'id',
             'authors',
@@ -30,24 +22,3 @@ class PaperDocumentSerializer(serializers.ModelSerializer):
             'title',
             'url',
         ]
-
-    def get_authors(self, document):
-        return document.authors
-
-    def get_discussion_count(self, document):
-        return document.discussion_count
-
-    def get_hubs(self, document):
-        return document.hubs
-
-    def get_score(self, document):
-        return document.score
-
-    def get_summary(self, document):
-        return document.summary
-
-    def get_title(self, document):
-        return document.title
-
-    def get_tagline(self, document):
-        return document.tagline
