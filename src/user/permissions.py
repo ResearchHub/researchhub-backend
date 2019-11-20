e22afaec5bca344fe44e0deea9bf07c700da628d
valmack <valmack@protonmail.com>
2019-11-20 17:58:47 -0500

    WIP - Add wallet model and viewset

--- src/user/permissions.py
+++ src/user/permissions.py
@@ -11,3 +11,10 @@ class UpdateAuthor(AuthorizationBasedPermission):
         ) or (request.method == RequestMethods.DELETE):
             return request.user == obj.user
         return True
+
+
+class IsWalletOwner(AuthorizationBasedPermission):
+    message = 'Action not permitted.'
+
+    def is_authorized(self, request, view, obj):
+        return request.user == obj.user
