4d62968a1f1a366341ee4d00c6caca6c7a2c6b20
valmack <valmack@protonmail.com>
2019-11-22 18:05:44 -0500

    Remove transactions app

--- src/transactions/serializers.py
+++
@@ -1,8 +0,0 @@
-import rest_framework.serializers as serializers
-
-from .models import Withdrawl
-
-class WithdrawlSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = '__all__'
-        model = Withdrawl
