60afad09245690d8e907c2a074e700023b3956cd
valmack <valmack@protonmail.com>
2019-11-22 18:33:15 -0500

    Fix - Add unique value to test user

--- src/reputation/tests/test_views.py
+++ src/reputation/tests/test_views.py
@@ -12,7 +12,7 @@ class ReputationViewsTests(APITestCase):
         self.all_withdrawals = len(Withdrawal.objects.all())
 
     def test_user_can_only_see_own_withdrawals(self):
-        user = create_random_authenticated_user()
+        user = create_random_authenticated_user('rep_user')
         user_withdrawals = len(user.withdrawals.all())
         response = self.get_withdrawals_get_response(user)
         self.assertGreater(self.all_withdrawals, user_withdrawals)
