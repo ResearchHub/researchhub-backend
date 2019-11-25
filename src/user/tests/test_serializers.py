c133002e1aa04508f45d53fdd66d30cc7437fcf9
valmack <valmack@protonmail.com>
2019-11-25 18:15:38 -0500

    Test author serializer

--- src/user/tests/test_serializers.py
+++ src/user/tests/test_serializers.py
@@ -1,9 +1,19 @@
 from django.test import TestCase
 
-from .helpers import create_user
+from user.serializers import AuthorSerializer
+from user.tests.helpers import create_user, create_university
 
 
 class UserSerializersTests(TestCase):
 
     def setUp(self):
         self.user = create_user(first_name='Serializ')
+        self.university = create_university()
+
+    def test_author_serializer_succeeds_without_user_or_university(self):
+        data = {
+            'first_name': 'Ray',
+            'last_name': 'Man',
+        }
+        serializer = AuthorSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
