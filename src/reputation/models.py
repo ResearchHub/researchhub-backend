3a0f3d6203ece08d4dffdf29e1f997b624194a76
valmack <valmack@protonmail.com>
2019-11-25 18:26:22 -0500

    Add all distribution type choices

--- src/reputation/models.py
+++ src/reputation/models.py
@@ -1,13 +1,34 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
+import reputation.distributions as distributions
 from user.models import User
-from .distributions import CreatePaper
 
 
 class Distribution(models.Model):
     DISTRIBUTION_TYPE_CHOICES = [
-        (CreatePaper.name, CreatePaper.name),
+        (distributions.CreatePaper.name, distributions.CreatePaper.name),
+        (
+            distributions.CommentEndorsed.name,
+            distributions.CommentEndorsed.name
+        ),
+        (distributions.CommentFlagged.name, distributions.CommentFlagged.name),
+        (distributions.CommentUpvoted.name, distributions.CommentUpvoted.name),
+        (
+            distributions.CommentDownvoted.name,
+            distributions.CommentDownvoted.name
+        ),
+        (distributions.ReplyEndorsed.name, distributions.ReplyEndorsed.name),
+        (distributions.ReplyFlagged.name, distributions.ReplyFlagged.name),
+        (distributions.ReplyUpvoted.name, distributions.ReplyUpvoted.name),
+        (distributions.ReplyDownvoted.name, distributions.ReplyDownvoted.name),
+        (distributions.ThreadEndorsed.name, distributions.ThreadEndorsed.name),
+        (distributions.ThreadFlagged.name, distributions.ThreadFlagged.name),
+        (distributions.ThreadUpvoted.name, distributions.ThreadUpvoted.name),
+        (
+            distributions.ThreadDownvoted.name,
+            distributions.ThreadDownvoted.name
+        ),
     ]
 
     recipient = models.ForeignKey(
