270528280830ece519ebf574b325d49b7549b54d
valmack <valmack@protonmail.com>
2019-11-25 14:58:35 -0500

    Increase elastic search plugin timeout and log errors - Closes #110

--- src/utils/sentry.py
+++ src/utils/sentry.py
@@ -1,6 +1,12 @@
 from sentry_sdk import capture_exception, configure_scope
 
 
+def log_error(base_error, message):
+    with configure_scope() as scope:
+        scope.set_extra('base_error', base_error)
+        capture_exception(message)
+
+
 def log_request_error(response, message, extra=None):
     with configure_scope() as scope:
         if extra:
