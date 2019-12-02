2c06fdba0bd3d689f80676451e0daab103cbfb48
valmack <valmack@protonmail.com>
2019-12-02 16:46:07 -0500

    Add token addresses

--- src/ethereum/contracts.py
+++ src/ethereum/contracts.py
@@ -2,6 +2,7 @@ import json
 import os
 from researchhub.settings import BASE_DIR
 from ethereum.apps import w3
+from ethereum.lib import TOKENS
 
 
 class ContractComposer:
@@ -27,6 +28,6 @@ class ContractComposer:
 
 
 research_coin_contract = ContractComposer(
-    '0x7D50101BbFa12f4A1B4e6de0Dd58Ad36dE150D55',
+    TOKENS['rhc']['contract_address'],
     'MiniMeToken.json'
 ).contract
