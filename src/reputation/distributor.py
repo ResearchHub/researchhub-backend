42d3dba8bcc92a0d69ba677540f899a9e1ee2c24
valmack <valmack@protonmail.com>
2019-12-03 16:18:28 -0500

    Prevent reputation updates from race conditions Use select_for_update with an atomic transaction for distribution signals

--- src/reputation/distributor.py
+++ src/reputation/distributor.py
@@ -1,18 +1,36 @@
-from .models import Distribution
-from .serializers import get_model_serializer
+from django.db import transaction
 
-# TODO: Add logging flag
+from reputation.exceptions import ReputationDistributorError
+from reputation.models import Distribution
+from reputation.serializers import get_model_serializer
+from user.models import User
+import utils.sentry as sentry
 
 
 class Distributor:
     '''
     Distributes an amount to the request user's reputation and logs this event
     by creating an Distribution record in the database.
+
+    Args:
+        distribution (obj) - Distribution class object
+        recipient (obj) - User receiving the distribution amount
+        db_record (obj) - model instance that triggered the distribution
+        timestamp (str) - timestamp when the triggering event was received
+
+    Attributes:
+        distribution (obj) - (same as above)
+        recipient (obj) - (same as above)
+        proof (json) - JSON formatted object including the db_record and
+            timestamp
+        proof_item (obj) - (same as db_record above)
+
     '''
     def __init__(self, distribution, recipient, db_record, timestamp):
         self.distribution = distribution
         self.recipient = recipient
         self.proof = self.generate_proof(db_record, timestamp)
+        self.proof_item = db_record
 
     @staticmethod
     def generate_proof(db_record, timestamp):
@@ -25,28 +43,36 @@ class Distributor:
         return proof
 
     def distribute(self):
+        record = self._record_distribution()
         try:
-            self._record_distribution()
+            record.set_distributed_pending()
             self._update_reputation()
-            # print('Distribution complete')
+            record.set_distributed()
         except Exception as e:
-            print('Distribution failed', e)
+            record.set_distributed_failed()
+
+            error_message = f'Distribution {record.id} failed'
+            error = ReputationDistributorError(e, error_message)
+            sentry.log_error(error)
+            print(error_message, e)
 
     def _record_distribution(self):
-        Distribution.objects.create(
+        record = Distribution.objects.create(
             recipient=self.recipient,
             amount=self.distribution.amount,
             distribution_type=self.distribution.name,
-            proof=self.proof
+            proof=self.proof,
+            proof_item=self.proof_item
+        )
+        return record
+
+    def _update_reputation(self):
+        users = User.objects.filter(pk=self.recipient.id).select_for_update(
+            of=('self',)
         )
 
-        # print('Distribution created:', str(record))
-
-    # TODO: Queue this so that there is no race condition
-    def _update_reputation(self):
-        user = self.recipient
-        current = user.reputation
-        user.reputation = current + self.distribution.amount
-        user.save(update_fields=['reputation'])
-
-        # print(f'Reputation updated for user {user}:', str(user.reputation))
+        with transaction.atomic():
+            user = users.get()
+            current = user.reputation
+            user.reputation = current + self.distribution.amount
+            user.save(update_fields=['reputation'])
