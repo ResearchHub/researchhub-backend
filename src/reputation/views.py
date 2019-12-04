c55cc98b36c73f127d3a653d9fd2e55766555de4
valmack <valmack@protonmail.com>
2019-12-04 17:52:59 -0500

    Restrict withdrawal viewset to create or read only

--- src/reputation/views.py
+++ src/reputation/views.py
@@ -4,14 +4,14 @@ from rest_framework.response import Response
 
 from reputation.lib import get_user_balance
 from reputation.models import Withdrawal
-from reputation.permissions import UpdateOrDeleteWithdrawal
 from reputation.serializers import WithdrawalSerializer
+from utils.permissions import CreateOrReadOnly
 
 
 class WithdrawalViewSet(viewsets.ModelViewSet):
     queryset = Withdrawal.objects.all()
     serializer_class = WithdrawalSerializer
-    permission_classes = [IsAuthenticated, UpdateOrDeleteWithdrawal]
+    permission_classes = [IsAuthenticated, CreateOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
