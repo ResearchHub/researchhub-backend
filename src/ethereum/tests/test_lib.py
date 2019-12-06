dd5e7d7b929784e4544d9c2545f171e3b9b851c7
valmack <valmack@protonmail.com>
2019-12-06 18:29:43 -0500

    Add withdrawal amount to withdrawal in pay_withdrawal signal

--- src/ethereum/tests/test_lib.py
+++ src/ethereum/tests/test_lib.py
@@ -9,14 +9,26 @@ class EthereumLibTests(TestCase):
 
     def test_convert_reputation_amount_to_token_amount(self):
         rep = 1
-        res = convert_reputation_amount_to_token_amount(self.token_ticker, rep)
-        self.assertEqual(res, 100000000000000000)
+        integer, decimal = convert_reputation_amount_to_token_amount(
+            self.token_ticker,
+            rep
+        )
+        self.assertEqual(integer, 100000000000000000)
+        self.assertEqual(decimal, '0.1')
         rep = 12
-        res = convert_reputation_amount_to_token_amount(self.token_ticker, rep)
-        self.assertEqual(res, 1200000000000000000)
+        integer, decimal = convert_reputation_amount_to_token_amount(
+            self.token_ticker,
+            rep
+        )
+        self.assertEqual(integer, 1200000000000000000)
+        self.assertEqual(decimal, '1.2')
         rep = 210
-        res = convert_reputation_amount_to_token_amount(self.token_ticker, rep)
-        self.assertEqual(res, 21000000000000000000)
+        integer, decimal = convert_reputation_amount_to_token_amount(
+            self.token_ticker,
+            rep
+        )
+        self.assertEqual(integer, 21000000000000000000)
+        self.assertEqual(decimal, '21.0')
 
     def test_convert_reputation_amount_to_token_amount_with_negatives(self):
         rep = -5
