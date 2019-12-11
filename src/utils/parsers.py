e224209b3a362ab071ed39647aab93d8a1688a95
valmack <valmack@protonmail.com>
2019-12-11 12:28:31 -0500

    Add plaintext parser

---
+++ src/utils/parsers.py
@@ -0,0 +1,14 @@
+from rest_framework.parsers import BaseParser
+
+
+class PlainTextParser(BaseParser):
+    """
+    Plain text parser.
+    """
+    media_type = 'text/plain'
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        """
+        Simply return a string representing the body of the request.
+        """
+        return stream.read()
