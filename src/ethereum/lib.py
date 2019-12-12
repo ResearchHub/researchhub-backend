5dbd5bbcff4e1cf81ae6530d003b9aba7367056e
valmack <valmack@protonmail.com>
2019-12-12 16:48:39 -0500

    Change rhc exchange rate to 1:1

--- src/ethereum/lib.py
+++ src/ethereum/lib.py
@@ -7,7 +7,7 @@ TOKENS = {
         'contract_address': '0x7D50101BbFa12f4A1B4e6de0Dd58Ad36dE150D55',
         'ticker': 'rhc',
         'denomination': 18,
-        'reputation_exchange_rate': '0.1'
+        'reputation_exchange_rate': '1.0'
     }
 }
 
