8ee27d1d3b8d81e6a3995ca8ccc9744f751930db
valmack <valmack@protonmail.com>
2019-12-12 16:51:43 -0500

    Update tests for rhc token conversion

--- src/ethereum/tests/test_lib.py
+++ src/ethereum/tests/test_lib.py
@@ -13,22 +13,22 @@ class EthereumLibTests(TestCase):
             self.token_ticker,
             rep
         )
-        self.assertEqual(integer, 100000000000000000)
-        self.assertEqual(decimal, '0.1')
+        self.assertEqual(integer, 1000000000000000000)
+        self.assertEqual(decimal, '1.0')
         rep = 12
         integer, decimal = convert_reputation_amount_to_token_amount(
             self.token_ticker,
             rep
         )
-        self.assertEqual(integer, 1200000000000000000)
-        self.assertEqual(decimal, '1.2')
+        self.assertEqual(integer, 12000000000000000000)
+        self.assertEqual(decimal, '12.0')
         rep = 210
         integer, decimal = convert_reputation_amount_to_token_amount(
             self.token_ticker,
             rep
         )
-        self.assertEqual(integer, 21000000000000000000)
-        self.assertEqual(decimal, '21.0')
+        self.assertEqual(integer, 210000000000000000000)
+        self.assertEqual(decimal, '210.0')
 
     def test_convert_reputation_amount_to_token_amount_with_negatives(self):
         rep = -5
