3b8ccc9099d6ea8940d1bfa7fa283cc5c20edfb3
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-01-04 19:40:33 -0500

    search: replace StringField with TextField
    closes #134

--- src/search/documents/summary.py
+++ src/search/documents/summary.py
@@ -11,10 +11,10 @@ from summary.models import Summary
 
 @registry.register_document
 class SummaryDocument(Document):
-    summary_plain_text = es_fields.StringField(analyzer=title_analyzer)
-    proposed_by = es_fields.StringField(attr='proposed_by_indexing')
+    summary_plain_text = es_fields.TextField(analyzer=title_analyzer)
+    proposed_by = es_fields.TextField(attr='proposed_by_indexing')
     paper = es_fields.IntegerField(attr='paper_indexing')
-    paper_title = es_fields.StringField(
+    paper_title = es_fields.TextField(
         attr='paper_title_indexing',
         analyzer=title_analyzer
     )
