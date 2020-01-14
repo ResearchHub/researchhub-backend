ef02e0cca0ceb2c6573d78d3136642f6ed5ad2d1
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-01-13 21:49:38 -0500

    enable ci default config to read env vars

--- src/config/ci/db.py
+++ src/config/ci/db.py
@@ -1,5 +1,7 @@
+from . import get_env_var
+
 NAME = 'researchhub'
-HOST = 'localhost'
-PORT = 5432
-USER = 'rh_developer'
-PASS = 'not_secure'
+HOST = get_env_var('DB_HOST', 'localhost')
+PORT = get_env_var('DB_PORT', 5432)
+USER = get_env_var('DB_USER', 'rh_developer')
+PASS = get_env_var('DB_USER', 'not_secure')
--- src/config/ci/wallet.py
+++ src/config/ci/wallet.py
@@ -1,2 +1,4 @@
+from . import get_env_var
+
 KEYSTORE_FILE = 'keystore.json'
-KEYSTORE_PASSWORD = '3K$DvLFUfANd'
+KEYSTORE_PASSWORD = get_env_var('KEYSTORE_PASSWORD', '3K$DvLFUfANd')
