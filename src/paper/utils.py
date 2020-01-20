b83d821d631519880e1ea2117a2cccaf085dc720
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-01-20 15:56:42 -0500

    Add get_best_oa_location_for_csl_item function

--- src/paper/utils.py
+++ src/paper/utils.py
@@ -11,3 +11,16 @@ def get_csl_item(url) -> dict:
     citekey = standardize_citekey(citekey)
     csl_item = citekey_to_csl_item(citekey)
     return csl_item
+
+
+def get_best_oa_location_for_csl_item(csl_item):
+    from manubot.cite.unpaywall import Unpaywall
+    upw = Unpaywall.from_csl_item(csl_item)
+    # get best open access location with a license,
+    # with preference to a location with an OA license
+    oa_location = upw.best_openly_licensed_pdf or upw.best_pdf
+    return oa_location
+
+
+
+
