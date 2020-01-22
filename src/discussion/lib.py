4a1f5528d3ca7aeda1124caf2447d6785de59305
valmack <valmack@protonmail.com>
2020-01-22 14:48:36 -0500

    Replace paper field with paper property for discussion items

--- src/discussion/lib.py
+++ src/discussion/lib.py
@@ -1,28 +1,16 @@
-from discussion.models import Comment, Reply
-
-
 def check_thread_in_papers(thread, papers):
     return thread.parent in papers
 
 
 def check_reply_in_threads(reply, threads):
-    comment = get_comment_of_reply(reply)
+    comment = reply.get_comment_of_reply()
     return check_comment_in_threads(comment, threads)
 
 
 def check_reply_in_comments(reply, comments):
-    comment = get_comment_of_reply(reply)
+    comment = reply.get_comment_of_reply()
     return comment in comments
 
 
 def check_comment_in_threads(comment, threads):
     return comment.parent in threads
-
-
-def get_comment_of_reply(parent):
-    if isinstance(parent, Reply):
-        return get_comment_of_reply(parent.parent)
-    elif isinstance(parent, Comment):
-        return parent
-    else:
-        raise TypeError(f'Unsupported {type(parent)}')
