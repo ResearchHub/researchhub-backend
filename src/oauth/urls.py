739c9aaf8962528bebbd3dbfe8d219749e4664b6
valmack <valmack@protonmail.com>
2020-01-31 14:27:47 -0500

    Fix - Add back default and registration urls

--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -1,9 +1,4 @@
+# flake8: noqa
+
 from rest_auth.urls import urlpatterns as default_urls
 from rest_auth.registration.urls import urlpatterns as registration_urls
-from allauth.socialaccount.urls import urlpatterns as social_account_urls
-
-with_default = (
-    default_urls
-    + registration_urls
-    + social_account_urls
-)
