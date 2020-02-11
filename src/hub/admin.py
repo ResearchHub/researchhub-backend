8d05e2d10ccd38f6e96986472b437cfa14c3d729
lightninglu10 <lightning.lu7@gmail.com>
2020-02-11 14:31:33 -0800

    hub admin showing subscribers

--- src/hub/admin.py
+++ src/hub/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Hub
 
-admin.site.register(Hub)
+
+class HubAdmin(admin.ModelAdmin):
+     model=Hub
+     filter_horizontal = ('subscribers',) #If you don't specify this, you will get a multiple select widget.
+
+admin.site.register(Hub, HubAdmin)
