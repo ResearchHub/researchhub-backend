0d7ebb8fbf26aa64b71a340413b3af7d13f9bf95
valmack <valmack@protonmail.com>
2020-02-11 14:59:48 -0500

    Remove unneeded adapter method

--- src/oauth/adapters.py
+++ src/oauth/adapters.py
@@ -1,29 +1,10 @@
 from time import time
 
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from allauth.socialaccount.models import SocialAccount
-from allauth.socialaccount.providers.google.provider import GoogleProvider
 from allauth.socialaccount.providers.orcid.provider import OrcidProvider
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def pre_social_login(self, request, sociallogin):
-        email = sociallogin.user.email
-        if email:
-            other_provider = OrcidProvider.id
-            if sociallogin.account.provider == OrcidProvider.id:
-                other_provider = GoogleProvider.id
-            try:
-                if SocialAccount.objects.get(
-                    user__email=email,
-                    provider=other_provider
-                ).exists():
-                    sociallogin.state['process'] = 'connect'
-            except Exception as e:
-                print(e)
-
-        return super().pre_social_login(request, sociallogin)
-
     def save_user(self, request, sociallogin, form=None):
         if sociallogin.account.provider == OrcidProvider.id:
             sociallogin.user.username = self._generate_temporary_username(
