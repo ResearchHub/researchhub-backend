6c3dc1bc9e79a779a56124f72301bdf5f839952e
valmack <valmack@protonmail.com>
2020-02-13 19:35:19 -0500

    Fix - Import crossref view

--- src/search/views/__init__.py
+++ src/search/views/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 from .author import AuthorDocumentView
-from .combined import CombinedView
+from .combined import CombinedView, crossref
 from .paper import PaperDocumentView
 from .thread import ThreadDocumentView
