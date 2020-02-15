8fe4fd201e0eff6184f6d885db5cb8482890ec67
piccoloman <piccoloman@users.noreply.github.com>
2020-02-14 17:31:01 -0800

    weekly digest update

--- src/hub/models.py
+++ src/hub/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import Q, Count
+
+from paper.models import Vote as PaperVote
 
 
 class Hub(models.Model):
@@ -24,6 +27,69 @@ class Hub(models.Model):
     @property
     def subscriber_count_indexing(self):
         return len(self.subscribers.all())
+
+    def email_context(self, start_date, end_date):
+
+        upvotes = Count(
+            'vote',
+            filter=Q(
+                vote__vote_type=PaperVote.UPVOTE,
+                vote__updated_date__gte=start_date,
+                vote__updated_date__lte=end_date
+            )
+        )
+
+        downvotes = Count(
+            'vote',
+            filter=Q(
+                vote__vote_type=PaperVote.DOWNVOTE,
+                vote__created_date__gte=start_date,
+                vote__created_date__lte=end_date
+            )
+        )
+
+        thread_counts = Count(
+            'threads',
+            filter=Q(
+                threads__created_date__gte=start_date,
+                threads__created_date__lte=end_date,
+            )
+        )
+
+        comment_counts = Count(
+            'threads__comments',
+            filter=Q(
+                threads__comments__created_date__gte=start_date,
+                threads__comments__created_date__lte=end_date,
+            )
+        )
+
+        reply_counts = Count(
+            'threads__comments__replies',
+            filter=Q(
+                threads__comments__replies__created_date__gte=start_date,
+                threads__comments__replies__created_date__lte=end_date,
+            )
+        )
+
+        papers = []
+        # Most Voted uploaded in Interval
+        paper = self.papers.filter(uploaded_date__gte=start_date, uploaded_date__lte=end_date).annotate(score=upvotes - downvotes).order_by('-score').first()
+        if paper:
+            papers.append(paper)
+
+        # Most Discussed
+        # TODO grab second if neccesary?
+        paper = self.papers.annotate(discussion_count=thread_counts + comment_counts + reply_counts).order_by('-discussion_count').first()
+        if paper and paper not in papers:
+            papers.append(paper)
+
+        # Most Voted in Interval
+        # TODO grab second if necessary
+        paper = self.papers.annotate(score=upvotes - downvotes).filter(score__gt=0).order_by('-score').first()
+        if paper and paper not in papers:
+            papers.append(paper)
+        return papers
 
     def unlock(self):
         self.is_locked = False
--- src/mailing_list/lib.py
+++ src/mailing_list/lib.py
@@ -4,6 +4,7 @@ class NotificationFrequencies:
     IMMEDIATE = 0
     DAILY = 1440
     THREE_HOUR = 180
+    WEEKLY = 10080
 
 base_email_context = {
     'opt_out': BASE_FRONTEND_URL + '/email/opt-out/'
