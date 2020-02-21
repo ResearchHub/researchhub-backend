a2dc40cda1ad712155140767f94b7ed26f73de62
piccoloman <piccoloman@users.noreply.github.com>
2020-02-21 14:38:17 -0800

    staging test for celery-beat

--- src/.ebextensions/celery-worker.sh
+++ src/.ebextensions/celery-worker.sh
@@ -41,14 +41,44 @@ priority=998
 
 environment=$celery_env"
 
+celerybeatconf="[program:celerybeat]
+; Set full path to celery program if using virtualenv
+command=/opt/python/run/venv/bin/celery beat –A researchhub -S redbeat.RedBeatScheduler --loglevel=INFO -Q ${app}
+
+directory=/opt/python/current/app
+user=ec2-user
+numprocs=1
+stdout_logfile=/var/log/celery/beat.out.log
+stderr_logfile=/var/log/celery/beat.err.log
+autostart=true
+autorestart=true
+startsecs=10
+startretries=2
+
+; Need to wait for currently executing tasks to finish at shutdown.
+; Increase this if you have very long running tasks.
+stopwaitsecs=60
+
+; When resorting to send SIGKILL to the program to terminate it
+; send SIGKILL to its whole process group instead,
+; taking care of its children as well.
+killasgroup=true
+
+; if rabbitmq is supervised, set its priority higher
+; so it starts first
+priority=997
+
+environment=$celeryenv"
+
 # Copy the above script into celery.conf file
 echo "$celery_conf" | tee /opt/python/etc/celery.conf
+echo "$celerybeatconff" | tee /opt/python/etc/celerybeat.conf
 
 # Add the conf to supervisord (if not already there)
 if ! grep -Fxq "[include]" /opt/python/etc/supervisord.conf
   then
   echo "[include]" | tee -a /opt/python/etc/supervisord.conf
-  echo "files=/opt/python/etc/celery.conf" | tee -a /opt/python/etc/supervisord.conf
+  echo "files=/opt/python/etc/celery.conf /opt/python/etc/celerybeat.conf" | tee -a /opt/python/etc/supervisord.conf
 fi
 
 # Reread the conf
@@ -59,3 +89,4 @@ fi
 
 # Restart celeryd
 /usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf restart celeryd-worker
+/usr/local/bin/supervisorctl -c /opt/python/etc/supervisord.conf restart celerybeat
