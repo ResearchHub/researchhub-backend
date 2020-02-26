dbe0ae8e1172af98b0c54f0950a0868c33fa8005
valmack <valmack@protonmail.com>
2020-02-26 17:51:48 -0500

    Update period mailing list tasks to send hub digest

--- src/mailing_list/tasks.py
+++ src/mailing_list/tasks.py
@@ -16,42 +16,38 @@ from user.models import Action, User
 from hub.models import Hub
 from paper.models import Paper, Vote as PaperVote
 
-import time
-
 
 @app.task
 def notify_immediate(action_id):
-    # TODO switch signals to save method
-    # TODO @val I think this is why I think signals are unreliable
-    i = 0
-    while not Action.objects.filter(id=action_id).exists() and i < 10:
-        time.sleep(.1)
-        i += 1
     actions_notifications([action_id], NotificationFrequencies.IMMEDIATE)
 
 
 @periodic_task(run_every=crontab(minute='30', hour='1'), priority=7)
 def notify_daily():
-    interval = timezone.now() - timedelta(days=1)
-    action_ids = list(
-        Action.objects.filter(
-            created_date__gte=interval
-        ).values_list('id', flat=True)
-    )
+    send_hub_digest(NotificationFrequencies.DAILY)
+    # TODO: Do we need this still?
+    # interval = timezone.now() - timedelta(days=1)
+    # action_ids = list(
+    #     Action.objects.filter(
+    #         created_date__gte=interval
+    #     ).values_list('id', flat=True)
+    # )
 
-    actions_notifications(action_ids, NotificationFrequencies.DAILY)
+    # actions_notifications(action_ids, NotificationFrequencies.DAILY)
 
 
 @periodic_task(run_every=crontab(minute='0', hour='*/3'), priority=7)
 def notify_three_hours():
-    interval = timezone.now() - timedelta(hours=3)
-    action_ids = list(
-        Action.objects.filter(
-            created_date__gte=interval
-        ).values_list('id', flat=True)
-    )
+    send_hub_digest(NotificationFrequencies.THREE_HOUR)
+    # TODO: Do we need this still?
+    # interval = timezone.now() - timedelta(hours=3)
+    # action_ids = list(
+    #     Action.objects.filter(
+    #         created_date__gte=interval
+    #     ).values_list('id', flat=True)
+    # )
 
-    actions_notifications(action_ids, NotificationFrequencies.THREE_HOUR)
+    # actions_notifications(action_ids, NotificationFrequencies.THREE_HOUR)
 
 
 @periodic_task(
@@ -59,9 +55,12 @@ def notify_three_hours():
     priority=9
 )
 def notify_weekly():
+    send_hub_digest(NotificationFrequencies.WEEKLY)
 
+
+def send_hub_digest(frequency):
     end_date = timezone.now()
-    start_date = timezone.now() - timedelta(days=7)
+    start_date = calculate_hub_digest_start_date(end_date, frequency)
 
     upvotes = Count(
         'vote',
@@ -115,7 +114,7 @@ def notify_weekly():
 
     # TODO find best by hub and then in mem sort for each user? more efficient?
     for user in User.objects.filter(id__in=users):
-        if not check_can_receive_digest(user):
+        if not check_can_receive_digest(user, frequency):
             continue
         users_papers = Paper.objects.filter(
             hubs__in=user.subscribed_hubs.all()
@@ -161,6 +160,15 @@ def notify_weekly():
         )
 
 
+def calculate_hub_digest_start_date(end_date, frequency):
+    if frequency == NotificationFrequencies.DAILY:
+        return end_date - timedelta(days=1)
+    elif frequency == NotificationFrequencies.THREE_HOUR:
+        return end_date - timedelta(hours=3)
+    else:  # weekly
+        return end_date - timedelta(days=7)
+
+
 def actions_notifications(
     action_ids,
     notif_interval=NotificationFrequencies.IMMEDIATE
@@ -172,11 +180,6 @@ def actions_notifications(
             for user in set(action.item.users_to_notify):
                 try:
                     r = user.emailrecipient
-                    # TODO: Frequency is now set on the subscription instead
-                    # of the email recipient so need to update this
-                    # if r.receives_notifications and (
-                    #     notif_interval == r.notification_frequency
-                    # ):
                     if r.receives_notifications:
                         if user_to_action.get(r):
                             user_to_action[r].append(action)
@@ -221,14 +224,13 @@ def build_notification_context(actions):
     }
 
 
-def check_can_receive_digest(user):
+def check_can_receive_digest(user, frequency):
     try:
         email_recipient = user.emailrecipient
-        if (email_recipient.digest_subscription is not None):
-            return (
-                email_recipient.receives_notifications
-                and not email_recipient.digest_subscription.none
-            )
-        return False
+        return (
+            email_recipient.receives_notifications
+            and not email_recipient.digest_subscription.none
+            and (email_recipient.digest_subscription.notification_frequency == frequency)  # noqa: E501
+        )
     except Exception:
         return False
