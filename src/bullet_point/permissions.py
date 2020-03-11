86868a87985cce1f8453a8af166832a0c58a9c13
valmack <valmack@protonmail.com>
2020-03-11 09:50:35 -0400

    Add route actions for censor, endorse, and flag bullet points - Closes #272

--- src/bullet_point/permissions.py
+++ src/bullet_point/permissions.py
@@ -1,3 +1,6 @@
+from paper.models import Paper
+from researchhub.lib import get_paper_id_from_path
+from user.models import Author
 from utils.http import POST, PATCH, PUT, DELETE
 from utils.permissions import AuthorizationBasedPermission, RuleBasedPermission
 
@@ -32,3 +35,27 @@ class UpdateOrDeleteBulletPoint(AuthorizationBasedPermission):
 def check_user_is_moderator(user, bullet_point):
     moderators = bullet_point.paper.moderators.all()
     return user in moderators
+
+
+class Censor(AuthorizationBasedPermission):
+    message = 'Need to be a moderator to delete bullet point.'
+
+    def is_authorized(self, request, view, obj):
+        return request.user.moderator
+
+
+class Endorse(AuthorizationBasedPermission):
+    message = 'Not authorized to endorse bullet point.'
+
+    def is_authorized(self, request, view, obj):
+        paper_id = get_paper_id_from_path(request)
+        paper = Paper.objects.get(pk=paper_id)
+        author = Author.objects.get(user=request.user)
+        return author in paper.authors.all()
+
+
+class Flag(RuleBasedPermission):
+    message = 'Not enough reputation to flag bullet point.'
+
+    def satisfies_rule(self, request):
+        return request.user.reputation >= 1
