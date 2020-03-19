0de78d2cfd84eb8f2272e47931efe2050d1f2d02
Leo Sun <leo.sun@berkeley.edu>
2020-03-19 15:59:54 -0700

    making consumer async

--- src/notification/consumers.py
+++ src/notification/consumers.py
@@ -1,47 +1,51 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .models import Notification
 from .serializers import NotificationSerializer
 from user.models import User
 
 
-class NotificationConsumer(WebsocketConsumer):
-    def connect(self):
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         kwargs = self.scope['url_route']['kwargs']
         if 'user' in self.scope:
             print('--------- user in scope ---------')
             user = self.scope['user']
         else:
             user_id = kwargs['user_id']
-            user = User.objects.get(id=user_id)
+            user = await database_sync_to_async(User.objects.get)(id=user_id)
+
+        if user.is_anonymous:
+            await self.close()
+
         self.user = user
         room = f'notification_{user.id}_{user.first_name}_{user.last_name}'
         self.room_group_name = room
         print(self.room_group_name)
         print(self.channel_name)
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Can Ignore - Backend testing
-    def receive(self, text_data, **kwargs):
+    async def receive(self, text_data, **kwargs):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -50,13 +54,14 @@ class NotificationConsumer(WebsocketConsumer):
         )
 
     # Can Ignore - Backend testing
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message
         }))
 
-    def send_notification(self, event):
+    @database_sync_to_async
+    async def send_notification(self, event):
         notification_type = event['notification_type']
         notification_id = event['id']
         notification = Notification.objects.get(id=notification_id)
@@ -66,4 +71,4 @@ class NotificationConsumer(WebsocketConsumer):
             'notification_type': notification_type,
             'data': serialized_data
         }
-        self.send(text_data=json.dumps(data))
+        await self.send(text_data=json.dumps(data))
