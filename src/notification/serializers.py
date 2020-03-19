89cb92563027a4d332f01537a09e7489d1d263f6
valmack <valmack@protonmail.com>
2020-03-19 17:36:50 -0400

    Add to notification read only fields

--- src/notification/serializers.py
+++ src/notification/serializers.py
@@ -7,17 +7,19 @@ from user.serializers import UserActions, UserSerializer
 class NotificationSerializer(serializers.ModelSerializer):
     action = serializers.SerializerMethodField()
     action_user = serializers.PrimaryKeyRelatedField(read_only=True)
+    paper = serializers.PrimaryKeyRelatedField(read_only=True)
     recipient = UserSerializer(
         read_only=False,
         default=serializers.CurrentUserDefault()
     )
-    paper = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Notification
         read_only_fields = [
             'id',
+            'action',
+            'action_user',
             'paper',
             'recipient',
             'created_date',
