e016cb581cbb31afa62712793818b25badd15570
lightninglu10 <lightning.lu7@gmail.com>
2020-03-19 18:37:23 -0700

    setting up channels deployment

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -531,7 +531,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [(REDIS_HOST, REDIS_PORT)],
         },
     },
 }
