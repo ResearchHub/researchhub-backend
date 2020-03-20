971cba003def1eea3ef4347060ee7f19e1dc88ba
Leo Sun <leo.sun@berkeley.edu>
2020-03-20 14:49:11 -0700

    removing old testing code

--- src/notification/consumers.py
+++ src/notification/consumers.py
@@ -42,26 +42,6 @@ class NotificationConsumer(WebsocketConsumer):
                 self.channel_name
             )
 
-    # Can Ignore - Backend testing
-    def receive(self, text_data, **kwargs):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Can Ignore - Backend testing
-    def chat_message(self, event):
-        message = event['message']
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
-
     def send_notification(self, event):
         notification_type = event['notification_type']
         notification_id = event['id']
