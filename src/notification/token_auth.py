80b73a6af5f3f9cb4b603649156d4a67f2a50e54
Leo Sun <leo.sun@berkeley.edu>
2020-03-19 17:57:07 -0700

    temporarily removing auth middleware. updating consumer disconnect

--- src/notification/token_auth.py
+++ src/notification/token_auth.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AnonymousUser
+from django.db import close_old_connections
 
 from channels.auth import AuthMiddlewareStack
 from rest_framework.authtoken.models import Token
@@ -21,6 +22,7 @@ class TokenAuthMiddleware:
                 if token_name == 'Token':
                     token = Token.objects.get(key=token_key)
                     scope['user'] = token.user
+                    close_old_connections()
             except Token.DoesNotExist:
                 scope['user'] = AnonymousUser()
         return self.inner(scope)
