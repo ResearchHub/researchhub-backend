eaff37acaac399caf19505ae1b7bcf4225f84e28
Leo Sun <leo.sun@berkeley.edu>
2020-03-27 11:52:09 -0700

    added command to add figures

---
+++ src/paper/management/commands/add_figures.py
@@ -0,0 +1,29 @@
+'''
+Add or create figures for all papers without figures.
+'''
+from django.core.management.base import BaseCommand
+
+from paper.models import Paper, Figure
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        exclude_ids = Figure.objects.all().distinct('paper').values_list(
+            'paper',
+            flat=True
+        )
+        papers = Paper.objects.exclude(id__in=exclude_ids)
+        for paper in papers:
+            try:
+                paper.extract_figures()
+                paper.extract_pdf_preview()
+                self.stdout.write(self.style.SUCCESS(
+                    f'Queued task to add references for paper {paper.id}'
+                ))
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(
+                    f'Failed to queue task for paper {paper.id}: {e}'
+                ))
+
+        self.stdout.write(self.style.SUCCESS(f'Done adding reference papers'))
