700eaeac0a9204e2bbd95a19c10640e937bfb743
lightninglu10 <lightning.lu7@gmail.com>
2020-04-01 15:34:44 -0700

    add references update

--- src/paper/management/commands/add_references.py
+++ src/paper/management/commands/add_references.py
@@ -12,19 +12,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         papers = Paper.objects.filter(
             doi__isnull=False,
-            is_public=True
+            is_public=True,
+            references__isnull=True,
         )
-        for paper in papers:
-            if len(paper.references.all()) == 0:
-                # If the paper has no references we are assuming it also has
-                # no papers referencing it in the db yet.
-                try:
-                    add_references.apply_async((paper.id,), priority=5)
-                    self.stdout.write(self.style.SUCCESS(
-                        f'Queued task to add references for paper {paper.id}'
-                    ))
-                except Exception as e:
-                    self.stdout.write(self.style.ERROR(
-                        f'Failed to queue task for paper {paper.id}: {e}'
-                    ))
-        self.stdout.write(self.style.SUCCESS(f'Done adding reference papers'))
+        count = papers.count()
+        for i, paper in enumerate(papers):
+            print('{} / {}').format(i, count)
+            # If the paper has no references we are assuming it also has
+            # no papers referencing it in the db yet.
+            try:
+                add_references(paper.id)
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(
+                    f'Failed to queue task for paper {paper.id}: {e}'
+                ))
