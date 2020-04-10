8218311baf9c8592f5632f762edbe8d3a43c07e9
lightninglu10 <lightning.lu7@gmail.com>
2020-04-09 18:52:21 -0700

    adding in a field for whether the uuid saw signup banner

--- src/analytics/models.py
+++ src/analytics/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class WebsiteVisits(models.Model):
     uuid = models.CharField(max_length=32, unique=True)
+    saw_signup_banner = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
