a365bb849b0a2fbb338c70498c218a3420bf0868
Leo Sun <leo.sun@berkeley.edu>
2020-04-16 11:39:08 -0700

    added formatting to models. cleaned up profiler middleware and settings

--- src/profiler/models.py
+++ src/profiler/models.py
@@ -1,6 +1,17 @@
+import re
+import sqlparse
+
 from django.db import models
 
 # Create your models here.
+
+words_re = re.compile(r'\s+')
+
+group_prefix_re = [
+    re.compile(r'^.*/django/[^/]+'),
+    re.compile(r'^(.*)/[^/]+$'),  # extract module path
+    re.compile(r'.*'),            # catch strange entries
+]
 
 
 class Profile(models.Model):
@@ -32,3 +43,67 @@ class Traceback(models.Model):
         related_name='traceback',
         on_delete=models.CASCADE
     )
+
+    def get_group(self, file):
+        for g in group_prefix_re:
+            name = g.findall(file)
+            if name:
+                return name[0]
+
+    def get_summary(self, results_dict, _sum):
+        list = [(item[1], item[0]) for item in results_dict.items()]
+        list.sort(reverse=True)
+        list = list[:40]
+
+        res = '      tottime\n'
+        for item in list:
+            res += '%4.1f%% %7.3f %s\n' % (
+                100*item[0]/_sum if _sum else 0, item[0], item[1]
+            )
+
+        return res
+
+    def summary_for_files(self, stats_str):
+        stats_str = stats_str.split("\n")[5:]
+
+        mystats = {}
+        mygroups = {}
+
+        _sum = 0
+
+        for s in stats_str:
+            fields = words_re.split(s)
+            if len(fields) == 7:
+                time = float(fields[2])
+                _sum += time
+                file = fields[6].split(":")[0]
+
+                if file not in mystats:
+                    mystats[file] = 0
+                mystats[file] += time
+
+                group = self.get_group(file)
+                if group not in mygroups:
+                    mygroups[group] = 0
+                mygroups[group] += time
+
+        return '<pre>' + \
+               ' ---- By file ----\n\n' + self.get_summary(mystats, _sum) + \
+               '\n' + \
+               ' ---- By group ---\n\n' + self.get_summary(mygroups, _sum) + \
+               '</pre>'
+
+    def get_formatted_trace(self):
+        if self.choice_type == self.VIEW_TRACE:
+            return self.summary_for_files(self.trace)
+
+    def get_formatted_sql(self):
+        if not self.sql:
+            return
+
+        formatted_sql = sqlparse.format(
+            self.sql,
+            reindent=True,
+            keyword_case='upper'
+        )
+        return formatted_sql
