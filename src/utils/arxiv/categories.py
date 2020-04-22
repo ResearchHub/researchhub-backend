8856b8d80b942477fcfb9b9d542346425714e215
valmack <valmack@protonmail.com>
2020-04-22 18:41:12 -0400

    Add arxiv metadata parser

---
+++ src/utils/arxiv/categories.py
@@ -0,0 +1,51 @@
+CS = {
+    'cs.AI': 'Artificial Intelligence',
+    'cs.CL': 'Computation and Language',
+    'cs.CC': 'Computational Complexity',
+    'cs.CE': 'Computational Engineering, Finance, and Science',
+    'cs.CG': 'Computational Geometry',
+    'cs.GT': 'Computer Science and Game Theory',
+    'cs.CV': 'Computer Vision and Pattern Recognition',
+    'cs.CY': 'Computers and Society',
+    'cs.CR': 'Cryptography and Security',
+    'cs.DS': 'Data Structures and Algorithms',
+    'cs.DB': 'Databases',
+    'cs.DL': 'Digital Libraries',
+    'cs.DM': 'Discrete Mathematics',
+    'cs.DC': 'Distributed, Parallel, and Cluster Computing',
+    'cs.ET': 'Emerging Technologies',
+    'cs.FL': 'Formal Languages and Automata Theory',
+    'cs.GL': 'General Literature',
+    'cs.GR': 'Graphics',
+    'cs.AR': 'Hardware Architecture',
+    'cs.HC': 'Human-Computer Interaction',
+    'cs.IR': 'Information Retrieval',
+    'cs.IT': 'Information Theory',
+    'cs.LG': 'Learning',
+    'cs.LO': 'Logic in Computer Science',
+    'cs.MS': 'Mathematical Software',
+    'cs.MA': 'Multiagent Systems',
+    'cs.MM': 'Multimedia',
+    'cs.NI': 'Networking and Internet Architecture',
+    'cs.NE': 'Neural and Evolutionary Computing',
+    'cs.NA': 'Numerical Analysis',
+    'cs.OS': 'Operating Systems',
+    'cs.OH': 'Other Computer Science',
+    'cs.PF': 'Performance',
+    'cs.PL': 'Programming Languages',
+    'cs.RO': 'Robotics',
+    'cs.SI': 'Social and Information Networks',
+    'cs.SE': 'Software Engineering',
+    'cs.SD': 'Sound',
+    'cs.SC': 'Symbolic Computation',
+    'cs.SY': 'Systems and Control',
+}
+
+CATEGORIES = {
+    'cs': CS,
+}
+
+
+def get_category_name(category):
+    prefix = category.split('.')[0]
+    return CATEGORIES[prefix][category]
