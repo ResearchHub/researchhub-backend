ebec35994eb58e4a5eddf7e12c809c21ccaf79ec
lightninglu10 <lightning.lu7@gmail.com>
2020-04-22 22:32:31 -0700

    created management command to get the arxiv_watchdog

--- src/paper/management/commands/arxiv_watchdog.py
+++ src/paper/management/commands/arxiv_watchdog.py
@@ -3,23 +3,38 @@ Setting up watchdog watcher to read files from arxiv.
 '''
 
 from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
+from watchdog.events import FileSystemEventHandler
 
 from django.core.management.base import BaseCommand
 
 from paper.models import Paper
 
+class MyEventHandler(FileSystemEventHandler):
+    def __init__(self, observer):
+        self.observer = observer
+
+    def on_created(self, event):
+        """
+        After a file is created, run processing on it
+        """
+        # TODO: Process the new file that came in
+        # event.src_path gives you the path to the file and the file name as a string: i.e. './hello.py'
+        # split this string to get the name of the event and then can run the metadata_parser on the file.
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        patterns = "*"
-        ignore_patterns = ""
-        ignore_directories = False
-        case_sensitive = True
-        my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
-        my_event_handler.on_created = self.on_created
-        import pdb; pdb.set_trace()
+        path = '.' #TODO: Change this to the path we want to actually watch
+        observer = Observer()
+        event_handler = MyEventHandler(observer)
+        observer.schedule(event_handler, path, recursive=True)
+        observer.start()
+        try:
+            while observer.isAlive():
+                observer.join(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
 
     def on_created(self, event):
         import pdb; pdb.set_trace()
