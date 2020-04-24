cc68385f5f376d89e1cf0a08aad0985afae81414
valmack <valmack@protonmail.com>
2020-04-24 19:31:01 -0400

    Add all arxiv categories

--- src/utils/arxiv/categories.py
+++ src/utils/arxiv/categories.py
@@ -4,7 +4,6 @@ CS = {
     'cs.CC': 'Computational Complexity',
     'cs.CE': 'Computational Engineering, Finance, and Science',
     'cs.CG': 'Computational Geometry',
-    'cs.GT': 'Computer Science and Game Theory',
     'cs.CV': 'Computer Vision and Pattern Recognition',
     'cs.CY': 'Computers and Society',
     'cs.CR': 'Cryptography and Security',
@@ -16,13 +15,14 @@ CS = {
     'cs.ET': 'Emerging Technologies',
     'cs.FL': 'Formal Languages and Automata Theory',
     'cs.GL': 'General Literature',
+    'cs.GT': 'Computer Science and Game Theory',
     'cs.GR': 'Graphics',
     'cs.AR': 'Hardware Architecture',
     'cs.HC': 'Human-Computer Interaction',
     'cs.IR': 'Information Retrieval',
     'cs.IT': 'Information Theory',
-    'cs.LG': 'Learning',
     'cs.LO': 'Logic in Computer Science',
+    'cs.LG': 'Machine Learning',
     'cs.MS': 'Mathematical Software',
     'cs.MA': 'Multiagent Systems',
     'cs.MM': 'Multimedia',
@@ -39,6 +39,19 @@ CS = {
     'cs.SD': 'Sound',
     'cs.SC': 'Symbolic Computation',
     'cs.SY': 'Systems and Control',
+}
+
+ECON = {
+    'econ.EM': 'Econometrics',
+    'econ.GN': 'General Economics',
+    'econ.TH': 'Theoretical Economics'
+}
+
+EESS = {
+    'eess.AS': 'Audio and Speech Processing',
+    'eess.IV': 'Image and Video Processing',
+    'eess.SP': 'Signal Processing',
+    'eess.SY': 'Systems and Control'
 }
 
 MATH = {
@@ -73,22 +86,160 @@ MATH = {
     'math.RA': 'Rings and Algebras',
     'math.SP': 'Spectral Theory',
     'math.ST': 'Statistics Theory',
-    'math.SG': 'Symplectic Geometry',
+    'math.SG': 'Symplectic Geometry'
+}
+
+ASTRO_PH = {
+    'astro-ph.GA': 'Astrophysics of Galaxies',
+    'astro-ph.CO': 'Cosmology and Nongalactic Astrophysics',
+    'astro-ph.EP': 'Earth and Planetary Astrophysics',
+    'astro-ph.HE': 'High Energy Astrophysical Phenomena',
+    'astro-ph.IM': 'Instrumentation and Methods for Astrophysics',
+    'astro-ph.SR': 'Solar and Stellar Astrophysics'
+}
+
+COND_MAT = {
+    'cond-mat.dis-nn': 'Disordered Systems and Neural Networks',
+    'cond-mat.mtrl-sci': 'Materials Science',
+    'cond-mat.mes-hall': 'Mesoscale and Nanoscale Physics',
+    'cond-mat.other': 'Other Condensed Matter',
+    'cond-mat.quant-gas': 'Quantum Gases',
+    'cond-mat.soft': 'Soft Condensed Matter',
+    'cond-mat.stat-mech': 'Statistical Mechanics',
+    'cond-mat.str-el': 'Strongly Correlated Electrons',
+    'cond-mat.supr-con': 'Superconductivity'
+}
+
+GR_QC = {
+    'gr-qc': 'General Relativity and Quantum Cosmology',
+}
+
+HEP_EX = {
+    'hep-ex': 'High Energy Physics - Experiment',
+}
+
+HEP_LAT = {
+    'hep-lat': 'High Energy Physics - Lattice',
+}
+
+HEP_PH = {
+    'hep-ph': 'High Energy Physics - Phenomenology',
+}
+
+HEP_TH = {
+    'hep-th': 'High Energy Physics - Theory',
+}
+
+MATH_PH = {
+    'math-ph': 'Mathematical Physics',
+}
+
+NLIN = {
+    'nlin.AO': 'Adaptation and Self-Organizing Systems',
+    'nlin.CG': 'Cellular Automata and Lattice Gases',
+    'nlin.CD': 'Chaotic Dynamics',
+    'nlin.SI': 'Exactly Solvable and Integrable Systems',
+    'nlin.PS': 'Pattern Formation and Solitons'
+}
+
+NUCL_EX = {
+    'nucl-ex': 'Nuclear Experiment'
+}
+
+NUCL_TH = {
+    'nucl-th': 'Nuclear Theory'
+}
+
+PHYSICS = {
+    'physics.acc-ph': 'Accelerator Physics',
+    'physics.app-ph': 'Applied Physics',
+    'physics.ao-ph': 'Atmospheric and Oceanic Physics',
+    'physics.atm-clus': 'Atomic and Molecular Clusters',
+    'physics.atom-ph': 'Atomic Physics',
+    'physics.bio-ph': 'Biological Physics',
+    'physics.chem-ph': 'Chemical Physics',
+    'physics.class-ph': 'Classical Physics',
+    'physics.comp-ph': 'Computational Physics',
+    'physics.data-an': 'Data Analysis, Statistics and Probability',
+    'physics.flu-dyn': 'Fluid Dynamics',
+    'physics.gen-ph': 'General Physics',
+    'physics.geo-ph': 'Geophysics',
+    'physics.hist-ph': 'History and Philosophy of Physics',
+    'physics.ins-det': 'Instrumentation and Detectors',
+    'physics.med-ph': 'Medical Physics',
+    'physics.optics': 'Optics',
+    'physics.soc-ph': 'Physics and Society',
+    'physics.ed-ph': 'Physics Education',
+    'physics.plasm-ph': 'Plasma Physics',
+    'physics.pop-ph': 'Popular Physics',
+    'physics.space-ph': 'Space Physics'
+}
+
+QUANT_PH = {
+    'quant-ph': 'Quantum Physics',
+}
+
+Q_BIO = {
+    'q-bio.BM': 'Biomolecules',
+    'q-bio.CB': 'Cell Behavior',
+    'q-bio.GN': 'Genomics',
+    'q-bio.MN': 'Molecular Networks',
+    'q-bio.NC': 'Neurons and Cognition',
+    'q-bio.OT': 'Other Quantitative Biology',
+    'q-bio.PE': 'Populations and Evolution',
+    'q-bio.QM': 'Quantitative Methods',
+    'q-bio.SC': 'Subcellular Processes',
+    'q-bio.TO': 'Tissues and Organs'
+}
+
+Q_FIN = {
+    'q-fin.CP': 'Computational Finance',
+    'q-fin.EC': 'Economics',
+    'q-fin.GN': 'General Finance',
+    'q-fin.MF': 'Mathematical Finance',
+    'q-fin.PM': 'Portfolio Management',
+    'q-fin.PR': 'Pricing of Securities',
+    'q-fin.RM': 'Risk Management',
+    'q-fin.ST': 'Statistical Finance',
+    'q-fin.TR': 'Trading and Market Microstructure'
+}
+
+STAT = {
+    'stat.AP': 'Applications',
+    'stat.CO': 'Computation',
+    'stat.ML': 'Machine Learning',
+    'stat.ME': 'Methodology',
+    'stat.OT': 'Other Statistics',
+    'stat.TH': 'Statistics Theory'
 }
 
 CATEGORIES = {
-    'cs': 'Computer Science',
-    'econ': 'Economics',
-    'eess': 'Electrical Engineering and Systems Science',
-    'math': 'Mathematics',
-    'astro-ph': 'Astrophysics',
-    'cond-mat': 'Condensed Matter',
+    'cs': CS,
+    'econ': ECON,
+    'eess': EESS,
+    'math': MATH,
+    'astro-ph': ASTRO_PH,
+    'cond-mat': COND_MAT,
+    'gr-qc': GR_QC,
+    'hep-ex': HEP_EX,
+    'hep-lat': HEP_LAT,
+    'hep-ph': HEP_PH,
+    'hep-th': HEP_TH,
+    'math-ph': MATH_PH,
+    'nlin': NLIN,
+    'nucl-ex': NUCL_EX,
+    'nucl-th': NUCL_TH,
+    'physics': PHYSICS,
+    'quant-ph': QUANT_PH,
+    'q-bio': Q_BIO,
+    'q-fin': Q_FIN,
+    'stat': STAT,
 }
 
 
 def get_category_name(category):
     try:
         prefix = category.split('.')[0]
-        return CATEGORIES[prefix]
+        return CATEGORIES[prefix][category]
     except KeyError:
         return 'General'
