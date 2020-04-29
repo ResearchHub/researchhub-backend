c95781d44d64e6982ae310e2df59b6f2effd8808
Leo Sun <leo.sun@berkeley.edu>
2020-04-29 15:34:48 -0700

    adding in paper search in admin panel

--- src/paper/admin.py
+++ src/paper/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Paper, Vote
 
+
+class PaperAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'title', 'paper_title',)
+
+
 admin.site.register(Vote)
-admin.site.register(Paper)
+admin.site.register(Paper, PaperAdmin)
