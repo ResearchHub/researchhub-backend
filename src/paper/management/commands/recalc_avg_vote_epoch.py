94b1e3ff73ed2d6a5e2b6f3fcf44c540f14f8a92
piccoloman <piccoloman@users.noreply.github.com>
2020-04-29 17:35:30 -0700

    bring back original pagination

--- src/paper/management/commands/recalc_avg_vote_epoch.py
+++ src/paper/management/commands/recalc_avg_vote_epoch.py
@@ -20,13 +20,18 @@ class Command(BaseCommand):
         for i, paper in enumerate(papers):
             try:
                 print(f'Paper: {paper.id} - {i + 1}/{count}')
+                new_score = paper.calculate_score()
 
-                ALGO_START_UNIX = 1588199677
-                vote_avg_epoch = paper.votes.aggregate(avg=Avg(Extract('created_date', 'epoch'), output_field=IntegerField()))['avg']
-                avg_hours_since_algo_start = (vote_avg_epoch - ALGO_START_UNIX) / 3600
-                hot_score = avg_hours_since_algo_start + paper.score + paper.discussion_count * 2
+                if new_score > 0:
+                    ALGO_START_UNIX = 1588199677
+                    vote_avg_epoch = paper.votes.aggregate(avg=Avg(Extract('created_date', 'epoch'), output_field=IntegerField()))['avg']
+                    avg_hours_since_algo_start = (vote_avg_epoch - ALGO_START_UNIX) / 3600
+                    hot_score = avg_hours_since_algo_start + new_score + paper.discussion_count * 2
 
-                paper.vote_avg_epoch = hot_score
+                    paper.vote_avg_epoch = hot_score
+                else:
+                    paper.vote_avg_epoch = 0
+
                 paper.save()
             except Exception as e:
                 print(f'Error updating score for paper: {paper.id}', e)
