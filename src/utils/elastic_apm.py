d815645705b25dcaec28b628e33e1737f23d9742
valmack <valmack@protonmail.com>
2020-05-05 18:27:27 -0400

    Test - Log custom elastic apm processor

--- src/utils/elastic_apm.py
+++ src/utils/elastic_apm.py
@@ -4,10 +4,12 @@ from utils.sentry import log_info
 
 
 @for_events(TRANSACTION)
-def paper_processor(client, event):
-    if event['type'] == 'request':
-        event_url = event['context']['request']['url']['full']
-        log_info(event_url)
-        if 'ignore_apm' in event_url:
-            return False
+def custom_processor(client, event):
+    event_url = event['context']['request']['url']['full']
+    log_info(event_url)
+    print(event_url)
+    if 'ignore_apm' in event_url:
+        log_info('Blocking')
+        print('Blocking')
+        return False
     return event
