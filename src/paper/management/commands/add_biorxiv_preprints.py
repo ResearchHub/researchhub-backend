f822d2519f1cf3c5161c9ef85f4f0c19901765a8
Leo Sun <leo.sun@berkeley.edu>
2020-05-06 12:34:35 -0700

    updating script to use bulk create

--- src/paper/management/commands/add_biorxiv_preprints.py
+++ src/paper/management/commands/add_biorxiv_preprints.py
@@ -37,16 +37,20 @@ CATEGORIES = [
     'scientific-communication-and-education',
     'synthetic-biology',
     'systems-biology',
-    'zoology'
-]
-
-HUBS = [
-    Hub.objects.get_or_create(name=category.lower())[0]
-    for category in CATEGORIES
+    'zoology',
+    'other'
 ]
 
 
 class Command(BaseCommand):
+
+    def __init__(self, *args, **kwargs):
+        HUBS = [
+            Hub.objects.get_or_create(name=category)[0]
+            for category in CATEGORIES
+        ]
+        self.HUBS = HUBS
+        super().__init__(*args, **kwargs)
 
     def handle(self, *args, **options):
         base_url = 'https://api.rxivist.org/v1/papers?metric=downloads&'
@@ -63,8 +67,7 @@ class Command(BaseCommand):
                 break
 
             results = response.json()['results']
-            for res in results:
-                self.create_paper(res)
+            self.create_papers(results)
 
     def construct_authors(self, authors):
         authors_list = []
@@ -80,49 +83,58 @@ class Command(BaseCommand):
             )
         return authors_list
 
-    def create_paper(self, data):
-        doi = data.get('doi')
-        abstract = data.get('abstract')
-        publish_date = data.get('first_posted')
-        title = data.get('title')
-        url = data.get('biorxiv_url')
-        pdf_url = url + '.full.pdf'
-        authors = self.construct_authors(data.get('authors'))
-        external_source = 'biorxiv'
-        paper = None
+    def create_papers(self, results):
+        papers = []
+        hub_ids = []
+        for data in results:
+            doi = data.get('doi')
+            abstract = data.get('abstract')
+            publish_date = data.get('first_posted')
+            title = data.get('title')
+            url = data.get('biorxiv_url')
+            pdf_url = url + '.full.pdf'
+            authors = self.construct_authors(data.get('authors'))
+            external_source = 'biorxiv'
+            category = data.get('category')
+            paper = None
 
-        # TODO: Add in logic for downloading the pdf?
-        # pdf_request = requests.get(pdf_url)
-        # if pdf_request.status_code == 200:
-        #     pass
-        # else:
-        #     pdf_url = None
+            if category == 'unknown':
+                category = 'other'
 
-        query = Q(url=url) | Q(pdf_url=pdf_url)
-        if doi is not None:
-            query = query | Q(doi=doi)
+            paper_hub = Hub.objects.get(name=category)
 
-        paper_results = Paper.objects.filter(query)
-        if len(paper_results) > 0:
-            paper = paper_results[0]
-        else:
-            try:
-                paper = Paper.objects.create(
-                    doi=doi,
-                    abstract=abstract,
-                    is_public=True,
-                    paper_publish_date=publish_date,
-                    paper_title=title,
-                    title=title,
-                    pdf_url=pdf_url,
-                    raw_authors=authors,
-                    retrieved_from_external_source=True,
-                    external_source=external_source,
-                    url=url
-                )
-            except Exception as e:
-                print(e)
-        if paper is not None:
-            paper.hubs.add(*HUBS)
-        else:
-            print('No paper for biorxiv id', data.get('id'))
+            query = Q(url=url) | Q(pdf_url=pdf_url)
+            if doi is not None:
+                query = query | Q(doi=doi)
+
+            paper_results = Paper.objects.filter(query)
+            if len(paper_results) > 0:
+                paper = paper_results[0]
+                paper.hubs.add(paper_hub)
+            else:
+                try:
+                    paper = Paper(
+                        doi=doi,
+                        abstract=abstract,
+                        is_public=True,
+                        paper_publish_date=publish_date,
+                        paper_title=title,
+                        title=title,
+                        pdf_url=pdf_url,
+                        raw_authors=authors,
+                        retrieved_from_external_source=True,
+                        external_source=external_source,
+                        url=url
+                    )
+                    papers.append(paper)
+                    hub_ids.append(paper_hub.id)
+                except Exception as e:
+                    print(e)
+
+        hub_tags = []
+        created_papers = Paper.objects.bulk_create(papers)
+        paper_ids = list(map(lambda p: p.id, created_papers))
+        for paper_id, hub_id in zip(paper_ids, hub_ids):
+            hub_tag = Paper.hubs.through(paper_id=paper_id, hub_id=hub_id)
+            hub_tags.append(hub_tag)
+        Paper.hubs.through.objects.bulk_create(hub_tags)
