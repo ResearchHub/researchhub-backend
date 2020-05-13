bc944aee573c3cd5d0a14951c7012e9bc8c52608
valmack <valmack@protonmail.com>
2020-05-13 12:07:15 -0400

    Fix - Send 0 value and fix string for google analytics

--- src/google_analytics/utils.py
+++ src/google_analytics/utils.py
@@ -17,7 +17,8 @@ def get_event_hit_response(
     fields = Hit.build_event_fields(
         category=category,
         action=action,
-        label=label
+        label=label,
+        value=0
     )
     hit = Hit(Hit.EVENT, utc_datetime, fields)
     return ga.send_hit(hit)
