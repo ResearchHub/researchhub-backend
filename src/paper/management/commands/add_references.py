485f7da97ba3e3507e0ce744209d58119ef4b3ba
valmack <valmack@protonmail.com>
2020-05-13 10:54:31 -0400

    Add print statements to add references

--- src/paper/management/commands/add_references.py
+++ src/paper/management/commands/add_references.py
@@ -4,7 +4,6 @@ Add or create references for all papers with doi and without reference.
 from django.core.management.base import BaseCommand
 
 from paper.models import Paper
-from paper.tasks import add_references
 
 
 class Command(BaseCommand):
@@ -12,12 +11,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         papers = Paper.objects.filter(
             is_public=True,
-            # references__isnull=True,
             external_source='arxiv'
         )
         count = papers.count()
         for i, paper in enumerate(papers):
-            print('{} / {}'.format(i, count))
-            # If the paper has no references we are assuming it also has
-            # no papers referencing it in the db yet.
+            print('Paper {} / {}'.format(i, count))
             paper.add_references()
