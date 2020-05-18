9f36e2e5065da301746206a164b87b2a19ca9329
lightninglu10 <lightning.lu7@gmail.com>
2020-05-18 09:31:56 -0700

    remove newrelic from backend

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -22,10 +22,6 @@ PRODUCTION = 'production' in APP_ENV
 STAGING = 'staging' in APP_ENV
 CELERY_WORKER = os.environ.get('CELERY_WORKER', False)
 
-if not CELERY_WORKER:
-    import newrelic.agent
-    newrelic.agent.initialize('researchhub/newrelic.ini', 'production')
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
