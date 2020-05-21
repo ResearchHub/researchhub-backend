63c1e74e876813a68d5d02b967792f9fda0ab906
Leo Sun <leo.sun@berkeley.edu>
2020-05-21 15:20:05 -0700

    added script to clean abstracts. cleaning title when creating paper by url

---
+++ src/paper/management/commands/clean_paper_abstracts.py
@@ -0,0 +1,32 @@
+'''
+Clean HTML tags in abstract
+'''
+
+import utils.sentry as sentry
+
+from bs4 import BeautifulSoup
+
+from django.core.management.base import BaseCommand
+
+from paper.models import Paper
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        papers = Paper.objects.iterator()
+        paper_count = Paper.objects.count()
+
+        for i, paper in enumerate(papers):
+            print(f'{i}/{paper_count}')
+            try:
+                abstract = paper.abstract
+                if abstract:
+                    soup = BeautifulSoup(abstract, 'html.parser')
+                    strings = soup.strings
+                    cleaned_text = ' '.join(strings)
+                    paper.abstract = cleaned_text
+                    paper.save()
+            except Exception as e:
+                print(e)
+                sentry.log_error(e)
