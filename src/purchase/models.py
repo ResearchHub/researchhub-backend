5b6ecd9917ca1c905bd6cea0671874360d830f99
valmack <valmack@protonmail.com>
2020-06-19 14:07:49 -0400

    Use mixin for paid status

--- src/purchase/models.py
+++ src/purchase/models.py
@@ -1,41 +1,23 @@
 import json
 import hashlib
-
 from datetime import datetime
 
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from utils.models import PaidStatusModelMixin
 
-class Purchase(models.Model):
+
+class Purchase(PaidStatusModelMixin):
     OFF_CHAIN = 'OFF_CHAIN'
     ON_CHAIN = 'ON_CHAIN'
-
-    INITIATED = 'INITIATED'
-    PENDING = 'PENDING'
-    SUCCESS = 'SUCCESS'
-    FAILED = 'FAILED'
 
     BOOST = 'BOOST'
 
     PURCHASE_METHOD_CHOICES = [
         (OFF_CHAIN, OFF_CHAIN),
         (ON_CHAIN, ON_CHAIN),
-    ]
-
-    STATUS_CHOICES = [
-        (INITIATED, INITIATED),
-        (PENDING, PENDING),
-        (SUCCESS, SUCCESS),
-        (FAILED, FAILED)
-    ]
-
-    PAID = 'PAID'
-    PAID_STATUS_CHOICES = [
-        (FAILED, FAILED),
-        (PAID, PAID),
-        (PENDING, PENDING),
     ]
 
     PURCHASE_TYPE_CHOICES = [
@@ -61,21 +43,9 @@ class Purchase(models.Model):
         choices=PURCHASE_METHOD_CHOICES,
         max_length=16,
     )
-    status = models.CharField(
-        choices=STATUS_CHOICES,
-        default=INITIATED,
-        max_length=16,
-    )
     purchase_type = models.CharField(
         choices=PURCHASE_TYPE_CHOICES,
         max_length=16
-    )
-    paid_date = models.DateTimeField(default=None, null=True)
-    paid_status = models.CharField(
-        max_length=255,
-        choices=PAID_STATUS_CHOICES,
-        default=None,
-        null=True
     )
 
     transaction_hash = models.CharField(
@@ -112,7 +82,7 @@ class Purchase(models.Model):
             'object_id': self.object_id,
             'purchase_method': self.purchase_method,
             'purchase_type': self.purchase_type,
-            'status': self.status,
+            'paid_status': self.paid_status,
             'transaction_hash': self.transaction_hash,
             'amount': self.amount,
             'created_date': self.created_date.isoformat(),
