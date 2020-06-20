ef7a7992c09752e8ac65227512fe0787fb18f8c2
valmack <valmack@protonmail.com>
2020-06-19 20:15:11 -0400

    Use latest distribution for inital balance script and move calculator

--- src/purchase/management/commands/add_initial_balance.py
+++ src/purchase/management/commands/add_initial_balance.py
@@ -5,7 +5,7 @@ Creates an initial balance for pre-existing users
 from django.core.management.base import BaseCommand
 from django.contrib.contenttypes.models import ContentType
 
-from reputation.lib import get_user_balance
+from reputation.models import Distribution
 from purchase.models import Balance
 from user.models import User
 
@@ -25,14 +25,29 @@ class Command(BaseCommand):
         for i, user in enumerate(users):
             try:
                 print(f'{i}/{users_len}')
-                balance_amount = get_user_balance(user)
-                first_distribution = user.reputation_records.first().id
+                balance_amount = self._get_user_balance(user)
+                latest_distribution_id = user.reputation_records.order_by(
+                    '-distributed_date'
+                ).first().id
                 Balance.objects.create(
                     user=user,
                     content_type=DISTRIBUTION_CONTENT_TYPE,
-                    object_id=first_distribution,
+                    object_id=latest_distribution_id,
                     amount=balance_amount
                 )
             except Exception as e:
                 print(e)
                 print(f'No distribution exists for {user.email}')
+
+    def _get_user_balance(self, user):
+        """Old method of calculating balance that is being used here for a one
+        time conversion.
+        """
+        unpaid_distributions = user.reputation_records.filter(
+            paid_status=None,
+            distributed_status=Distribution.DISTRIBUTED
+        )
+        return (
+            sum([u_d.amount for u_d in unpaid_distributions]),
+            unpaid_distributions
+        )
