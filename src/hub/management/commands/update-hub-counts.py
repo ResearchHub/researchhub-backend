fbd1730d2d02d70d48d8ddeca4737eb355b923ca
Thomas Vu <shuhari@macbook.local>
2020-10-02 17:45:21 -0600

    updated hub management scripts

--- src/hub/management/commands/update-hub-counts.py
+++ src/hub/management/commands/update-hub-counts.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Sum
 from hub.models import Hub
 
 
@@ -7,7 +8,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         all_hubs = Hub.objects.all()
         for hub in all_hubs:
+            print('calculating subscriber_count for', hub.name)
             hub.subscriber_count = hub.subscribers.count()
+            print('calculating paper_count for', hub.name)
             hub.paper_count = hub.papers.count()
-            hub.discussion_count = sum(paper.discussion_count_indexing for paper in hub.papers.all())
+            print('calculating discussion_count for', hub.name)
+            annotation = Hub.objects.filter(id=hub.id).annotate(hub_discussion_count=Sum('papers__discussion_count'))
+            hub.discussion_count = annotation.first().hub_discussion_count or 0
             hub.save()
