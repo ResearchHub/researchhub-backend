2b121117a331abd86f92377f53284aa77a0b3231
lightninglu10 <lightning.lu7@gmail.com>
2020-11-03 15:34:56 -0800

    is_removed can be null

--- src/paper/models.py
+++ src/paper/models.py
@@ -831,7 +831,7 @@ class Vote(models.Model):
     created_date = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_date = models.DateTimeField(auto_now=True, db_index=True)
     vote_type = models.IntegerField(choices=VOTE_TYPE_CHOICES)
-    is_removed = models.BooleanField(default=False)
+    is_removed = models.BooleanField(default=False, blank=True, null=True)
 
     class Meta:
         constraints = [
