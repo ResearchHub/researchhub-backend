e90508b6c81d20b3de846c874855d6f874c386eb
lightninglu10 <lightning.lu7@gmail.com>
2020-11-04 16:44:44 -0800

    removing bad users from the app

--- src/user/management/commands/remove_bad_users.py
+++ src/user/management/commands/remove_bad_users.py
@@ -3,6 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from user.models import User
 import uuid
+from user.tasks import handle_spam_user_task
 
 class Command(BaseCommand):
 
@@ -12,3 +13,4 @@ class Command(BaseCommand):
         for i, user in enumerate(objects):
             print('{} / {}'.format(i, count))
             handle_spam_user_task(user.id)
+            user.papers.update(is_removed=True)
