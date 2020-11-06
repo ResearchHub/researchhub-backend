9644424477a17e04c73c73f06849f6399fe3817b
alexcui508 <alexcui@berkeley.edu>
2020-11-05 17:29:02 -0800

    user score update on logins

--- src/oauth/serializers.py
+++ src/oauth/serializers.py
@@ -21,7 +21,7 @@ from researchhub.settings import GOOGLE_REDIRECT_URL, GOOGLE_YOLO_REDIRECT_URL
 from user.models import Author, User
 from user.utils import merge_author_profiles
 from utils import sentry
-from utils.siftscience import events_api
+from utils.siftscience import events_api, get_tracked_content_score
 from analytics.models import WebsiteVisits
 
 from django.contrib.gis.geoip2 import GeoIP2
@@ -170,11 +170,16 @@ class SocialLoginSerializer(serializers.Serializer):
             pass
         except NoReverseMatch as e:
             if 'account_inactive' in str(e):
-                events_api.track_login(
-                    login.account.user,
+                login_user = login.account.user
+                tracked_login = events_api.track_login(
+                    login_user,
                     '$failure',
                     request
                 )
+                if tracked_login:
+                    login_risk_score = get_tracked_content_score(tracked_login)
+                    login_user.sift_risk_score = login_risk_score
+                    login_user.save(update_fields=['sift_risk_score'])
                 raise LoginError(None, 'Account is suspended')
         except Exception as e:
             error = LoginError(e, 'Login failed')
@@ -210,7 +215,12 @@ class SocialLoginSerializer(serializers.Serializer):
 
         login_user = login.account.user
         attrs['user'] = login_user
-        events_api.track_login(login_user, '$success', request)
+        tracked_login = events_api.track_login(login_user, '$success', request)
+        if tracked_login:
+            login_risk_score = get_tracked_content_score(tracked_login)
+            login_user.sift_risk_score = login_risk_score
+            login_user.save(update_fields=['sift_risk_score'])
+
         try:
             visits = WebsiteVisits.objects.get(uuid=attrs['uuid'])
             visits.user = attrs['user']
