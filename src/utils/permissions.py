deac493e1caf2b09762aa7732bf35ae8ffb1b41b
lightninglu10 <lightning.lu7@gmail.com>
2020-11-06 15:35:40 -0800

    adding a new permission class for withdrawals

--- src/utils/permissions.py
+++ src/utils/permissions.py
@@ -7,6 +7,9 @@ class ReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
 
+class UserNotSpammer(BasePermission):
+    def has_permission(self, request, view):
+        return not request.user.probable_spammer
 
 class CreateOrUpdateIfAllowed(BasePermission):
     def has_permission(self, request, view):
