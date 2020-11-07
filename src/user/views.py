730844708e766b10efbe40518f261baf8570df9e
lightninglu10 <lightning.lu7@gmail.com>
2020-11-07 15:25:16 -0800

    remove user's comments, etc. on ban

--- src/user/views.py
+++ src/user/views.py
@@ -79,7 +79,7 @@ class UserViewSet(viewsets.ModelViewSet):
         user_to_censor = User.objects.get(author_profile__id=author_id)
         user_to_censor.set_probable_spammer()
         user_to_censor.set_suspended()
-        handle_spam_user_task.apply_async((user_to_censor.id,), priority=10)
+        handle_spam_user_task(user_to_censor.id)
 
         user = request.user
         decisions_api.apply_bad_user_decision(user_to_censor, user)
