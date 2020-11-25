d6582049d00d196b3a74b7ab0168854b44264b48
Leo Sun <leo.sun@berkeley.edu>
2020-11-24 18:16:25 -0800

    reward logic fix

--- src/reputation/tasks.py
+++ src/reputation/tasks.py
@@ -460,13 +460,13 @@ def distribute_rewards(starting_date=None, end_date=None, distribute=True):
             bulletpoint_vote_count = bulletpoint_votes_count.get(key, 0)
             summary_vote_count = summary_votes_count.get(key, 0)
 
-            vote_count = upload_vote_count + comment_vote_count + bulletpoint_upvotes_count.get(key, 0) + summary_upvotes_count.get(key, 0)
+            vote_count = upload_vote_count + comment_vote_count + bulletpoint_vote_count + summary_vote_count
 
             upvotes_count = (
                 paper_votes_count.get(key, 0) +
                 comment_upvotes_count.get(key, 0) +
-                bulletpoint_vote_count +
-                summary_vote_count
+                bulletpoint_upvotes_count.get(key, 0) +
+                summary_upvotes_count.get(key, 0)
             )
             upvoted_amount = math.floor((vote_count / (total_score)) * score_reward_amount)
             upvotes_amount = math.floor(upvotes_count / total_score * upvote_reward_amount)
