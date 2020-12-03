5c0aca7fb752c33059ce22f6d69beec40c297fe5
Leo Sun <leo.sun@berkeley.edu>
2020-12-02 17:39:33 -0800

    migrations

--- src/user/migrations/0057_author_academic_verification.py
+++ src/user/migrations/0057_author_academic_verification.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='author',
             name='academic_verification',
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(default=None, null=True),
         ),
     ]
