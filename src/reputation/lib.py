82bc14a25b568bbaf2373ca2d99792ee7c827b0c
lightninglu10 <lightning.lu7@gmail.com>
2020-12-04 16:23:13 -0800

    setting withdrawal min to 5k

--- src/reputation/lib.py
+++ src/reputation/lib.py
@@ -6,7 +6,7 @@ import ethereum.lib
 from researchhub.settings import ASYNC_SERVICE_HOST, WEB3_SHARED_SECRET
 from utils.http import http_request, RequestMethods
 
-WITHDRAWAL_MINIMUM = 150
+WITHDRAWAL_MINIMUM = 5000
 WITHDRAWAL_PER_TWO_WEEKS = 1
 
 class PendingWithdrawal:
