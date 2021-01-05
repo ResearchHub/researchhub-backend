74e14e35490ea4e2975705a9d522e6ee7492b65e
Leo Sun <leo.sun@berkeley.edu>
2021-01-04 16:52:11 -0800

    removing preview extraction from signal and putting it in the paper serializer

--- src/paper/serializers.py
+++ src/paper/serializers.py
@@ -315,6 +315,8 @@ class PaperSerializer(BasePaperSerializer):
                     countdown=10
                 )
 
+                paper.extract_pdf_preview(use_celery=True)
+
                 return paper
         except IntegrityError as e:
             raise e
