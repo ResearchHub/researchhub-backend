038cf12976fdf80df9053b2ad32086c1b6a0e31c
Leo Sun <leo.sun@berkeley.edu>
2021-01-29 10:24:23 -0800

    removed bulletpoint cache

--- src/bullet_point/views.py
+++ src/bullet_point/views.py
@@ -1,4 +1,3 @@
-from django.utils.decorators import method_decorator
 from django.db import transaction
 from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
@@ -132,14 +131,6 @@ class BulletPointViewSet(viewsets.ModelViewSet, ActionableViewSet):
             countdown=10
         )
         return response
-
-    @method_decorator(cache_page(60*60*6))
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
-    @method_decorator(cache_page(60*60*6))
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
 
     def update(self, request, *args, **kwargs):
         if (
