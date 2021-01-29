1776b2ce40d7daff6a9bfbf344aef77959a1535f
Leo Sun <leo.sun@berkeley.edu>
2021-01-29 10:34:33 -0800

    setting twitter base score as 1. newest filter shows auto pulled papers for subscribed hubs

--- src/paper/migrations/0073_paper_twitter_score.py
+++ src/paper/migrations/0073_paper_twitter_score.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='paper',
             name='twitter_score',
-            field=models.IntegerField(default=0),
+            field=models.IntegerField(default=1),
         ),
     ]
