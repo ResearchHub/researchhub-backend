d09f3773c43d945561d2bf67a2b906586d4cea88
Leo Sun <leo.sun@berkeley.edu>
2021-04-02 21:35:01 -0700

    updated follow endpoint to handle unfollowing

--- src/user/views.py
+++ src/user/views.py
@@ -253,6 +253,11 @@ class UserViewSet(viewsets.ModelViewSet):
                 followee=followee
             )
         except IntegrityError:
+            follow = Follow.objects.get(
+                user=user,
+                followee=followee
+            )
+            follow.delete()
             return Response('User is already following Author', status=400)
 
         is_following = user.following.filter(followee=followee).exists()
