ce5901e7f5d6bcef6e4a37bbd0144e6e70a1ecf8
lightninglu10 <lightning.lu7@gmail.com>
2021-04-09 10:21:58 -0700

    more logging for twitter autopull

--- src/paper/management/commands/queue_twitter_score_calculation.py
+++ src/paper/management/commands/queue_twitter_score_calculation.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from paper.models import Paper
 import os
 from paper.tasks import celery_calculate_paper_twitter_score
-
+import time
 
 class Command(BaseCommand):
 
@@ -10,8 +10,8 @@ class Command(BaseCommand):
         today_papers = Paper.objects.filter(uploaded_date__gte='2021-04-09', twitter_score_updated_date__isnull=True)
         count = today_papers.count()
         for i, paper in enumerate(today_papers):
-          print('{} / {}'.format(i, count))
-          success = celery_calculate_paper_twitter_score(paper.id)
+          success, score = celery_calculate_paper_twitter_score(paper.id)
+          print('{}, Score: {} : {} / {}'.format(success, score, i, count))
           if not success:
             time.sleep(900)
             celery_calculate_paper_twitter_score(paper.id)
