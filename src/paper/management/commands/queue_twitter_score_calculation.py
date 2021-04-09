343c11dcd13dc805bd862cd7f7a192b934b15e0d
lightninglu10 <lightning.lu7@gmail.com>
2021-04-09 10:16:47 -0700

    management command field__isnull

--- src/paper/management/commands/queue_twitter_score_calculation.py
+++ src/paper/management/commands/queue_twitter_score_calculation.py
@@ -7,7 +7,7 @@ from paper.tasks import celery_calculate_paper_twitter_score
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        today_papers = Paper.objects.filter(uploaded_date__gte='2021-04-09')
+        today_papers = Paper.objects.filter(uploaded_date__gte='2021-04-09', twitter_score_updated_date__isnull=True)
         count = today_papers.count()
         for i, paper in enumerate(today_papers):
           print('{} / {}'.format(i, count))
