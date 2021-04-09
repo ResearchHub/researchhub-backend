e2af406034fa3996c95a7b4ef254883c90904ce7
lightninglu10 <lightning.lu7@gmail.com>
2021-04-09 10:22:46 -0700

    better logging for twitter autopull

--- src/paper/management/commands/queue_twitter_score_calculation.py
+++ src/paper/management/commands/queue_twitter_score_calculation.py
@@ -10,8 +10,8 @@ class Command(BaseCommand):
         today_papers = Paper.objects.filter(uploaded_date__gte='2021-04-09', twitter_score_updated_date__isnull=True)
         count = today_papers.count()
         for i, paper in enumerate(today_papers):
-          success, score = celery_calculate_paper_twitter_score(paper.id)
-          print('{}, Score: {} : {} / {}'.format(success, score, i, count))
+          success = celery_calculate_paper_twitter_score(paper.id)
+          print('{} : {} / {}'.format(success, i, count))
           if not success:
             time.sleep(900)
             celery_calculate_paper_twitter_score(paper.id)
