03295242cef3d5fa295fadfd16eeec2cc0a11311
Calvin Lee <calvinhlee23@gmail.com>
2021-04-29 12:24:13 -1000

    abc

--- src/user/models.py
+++ src/user/models.py
@@ -1,10 +1,10 @@
 from .related_models.action_model import Action
 from .related_models.author_model import Author
 from .related_models.follow_model import Follow
+from .related_models.profile_image_storage import ProfileImageStorage
 from .related_models.school_models import Major, University
 from .related_models.user_model import User
 from .related_models.verification_model import Verification
-from .related_models.profile_image_storage import ProfileImageStorage
 
 exportable_models = [
     Action,
