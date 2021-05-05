bef437f5f2d7bf8d71f863231685db82fcedb049
Leo Sun <leo.sun@berkeley.edu>
2021-05-04 18:51:41 -0700

    updating deploy script

--- deploy_lambda.sh
+++ deploy_lambda.sh
@@ -1,4 +1,4 @@
 docker build -t researchhub-backend-lambda:latest .
 docker tag researchhub-backend-lambda:latest 794128250202.dkr.ecr.us-west-2.amazonaws.com/researchhub-backend-staging
 docker push 794128250202.dkr.ecr.us-west-2.amazonaws.com/researchhub-backend-staging:latest
-aws lambda update-function-code --function-name  test2 --image-uri 794128250202.dkr.ecr.us-west-2.amazonaws.com/researchhub-backend-staging:latest
+aws lambda update-function-code --function-name  ResearchHub-Backend-Staging --image-uri 794128250202.dkr.ecr.us-west-2.amazonaws.com/researchhub-backend-staging:latest
