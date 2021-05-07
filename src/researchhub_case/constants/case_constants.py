c2608d2f9775bccb09e3e350c5c9aeded6ffdfd9
Calvin Lee <calvinhlee23@gmail.com>
2021-05-07 16:14:50 -0700

    ...

--- src/researchhub_case/constants/case_constants.py
+++ src/researchhub_case/constants/case_constants.py
@@ -1,14 +1,20 @@
+AUTHOR_CLAIM = "AUTHOR_CLAIM"
+CLOSED = "CLOSED"
+DENIED = "DENIED"
+NULLIFIED = "NULLIFIED"
+OPEN = "NULLIFIED"
+
 """
   Nullified signifies that the author was
   claimed by someone else during process
 """
 AUTHOR_CLAIM_CASE_STATUS = [
-    ('CLOSED', 'CLOSED'),
-    ('DENIED', 'DENIED'),
-    ('NULLIFIED', 'NULLIFIED'),
-    ('OPEN', 'OPEN'),
+    (CLOSED, CLOSED),
+    (DENIED, DENIED),
+    (NULLIFIED, NULLIFIED),
+    (OPEN, OPEN),
 ]
 
 RH_CASE_TYPES = [
-    ('AUTHOR_CLAIM', 'AUTHOR_CLAIM')
+    (AUTHOR_CLAIM, AUTHOR_CLAIM)
 ]
---
+++ src/researchhub_case/utils/author_claim_case_utils.py
@@ -0,0 +1,22 @@
+import base64
+
+
+# TODO: calvinhlee - improve this in the future
+def decode_validation_token(encoded_str):
+    return base64.urlsafe_b64decode(encoded_str)
+
+
+def encode_validation_token(str):
+    return base64.urlsafe_b64encode(str)
+
+
+def format_valid_ids(case, requestor, target_author):
+    return '&'.join([
+      f'case_id={case.id}',
+      f'&requestor_id={requestor.id}',
+      f'&target_author_id={target_author}'
+    ])
+
+
+def send_validation_email(case):
+    return True
