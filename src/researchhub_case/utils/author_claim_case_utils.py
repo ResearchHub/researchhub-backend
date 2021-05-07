8393467f4e2f3d711c15d59fdd2747e9eabb6631
Calvin Lee <calvinhlee23@gmail.com>
2021-05-07 16:21:44 -0700

    ab

--- src/researchhub_case/utils/author_claim_case_utils.py
+++ src/researchhub_case/utils/author_claim_case_utils.py
@@ -2,7 +2,7 @@ import base64
 import time
 
 
-# TODO: calvinhlee - improve this in the future
+# TODO: calvinhlee - maybe improve these in the future
 def decode_validation_token(encoded_str):
     return base64.urlsafe_b64decode(encoded_str)
 
@@ -14,9 +14,9 @@ def encode_validation_token(str):
 def format_valid_ids(case, requestor, target_author):
     return '&'.join([
       f'case_id={case.id}',
+      f'&generated_time={int(time.time())}'
       f'&requestor_id={requestor.id}',
       f'&target_author_id={target_author}'
-      f'&generated_time={int(time.time())}'
     ])
 
 
