3be35f06bb4ec6bb3fdeca4f2fbae70f2b49f27f
Calvin Lee <calvinhlee23@gmail.com>
2021-05-07 14:59:15 -0700

    added auth

--- src/researchhub_case/views/author_claim_case_view.py
+++ src/researchhub_case/views/author_claim_case_view.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from researchhub_case.models import AuthorClaimCase
 from researchhub_case.serializers import AuthorClaimCaseSerializer
@@ -8,7 +8,7 @@ from researchhub_case.serializers import AuthorClaimCaseSerializer
 class AuthorClaimCaseViewSet(viewsets.ModelViewSet):
     permission_classes = [
         # TODO: calvinhlee - add more privacy later
-        IsAuthenticated
+        AllowAny
     ]
     queryset = AuthorClaimCase.objects.all()
     serializer_class = AuthorClaimCaseSerializer
