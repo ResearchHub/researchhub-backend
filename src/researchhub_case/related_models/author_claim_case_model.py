c649329ac5b0f8bc95e36743d46c4343879b83d7
Calvin Lee <calvinhlee23@gmail.com>
2021-05-18 14:22:59 -0700

    update

--- src/researchhub_case/related_models/author_claim_case_model.py
+++ src/researchhub_case/related_models/author_claim_case_model.py
@@ -34,20 +34,20 @@ class AuthorClaimCase(AbstractResearchhubCase):
     token_generated_time = models.IntegerField(
       blank=True,
       default=None,
-      help_text="Intentionally setting as a int field",
+      help_text='Intentionally setting as a int field',
       null=True,
     )
     validation_attempt_count = models.IntegerField(
       blank=False,
       default=-1,
-      help_text="Number of attempts to validate themselves given token",
+      help_text='Number of attempts to validate themselves given token',
       null=False
     )
     validation_token = models.CharField(
       blank=True,
       db_index=True,
       default=None,
-      help_text="See author_claim_case_post_create_signal",
+      help_text='See author_claim_case_post_create_signal',
       max_length=255,
       null=True,
       unique=True,
