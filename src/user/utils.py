4cec10da63a2f7b2364078b243c43aed1887efdf
Leo Sun <leo.sun@berkeley.edu>
2021-05-20 17:02:55 -0700

    updated author merge function. created function to distribute a reward to claimed author profiles. added permissions

--- src/user/utils.py
+++ src/user/utils.py
@@ -1,3 +1,6 @@
+from utils.sentry import log_error
+
+
 def merge_author_profiles(source, target):
     # Remap papers
     for paper in source.authored_papers.all():
@@ -5,6 +8,35 @@ def merge_author_profiles(source, target):
         paper.authors.add(target)
         paper.save()
 
+    if target.user and source.user:
+        target.user = source.user
+
+    attributes = [
+        'first_name',
+        'last_name',
+        'description',
+        'profile_image',
+        'author_score',
+        'university',
+        'orcid_id',
+        'orcid_account',
+        'education',
+        'headline',
+        'facebook',
+        'linkedin',
+        'twitter',
+        'academic_verification'
+    ]
+    for attr in attributes:
+        try:
+            source_val = getattr(source, attr)
+            setattr(target, attr, source_val)
+        except Exception as e:
+            print(e)
+            log_error(e)
+
     target.save()
-    source.delete()
+    source.user = None
+    source.orcid_account = None
+    source.save()
     return target
