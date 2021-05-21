f489953ba2117adee48f70341327a2caf7a3fac1
Calvin Lee <calvinhlee23@gmail.com>
2021-05-21 12:47:23 -0700

    ..

---
+++ src/researchhub_case/views/author_claim_case_moderator_view.py
@@ -0,0 +1,55 @@
+import json
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from utils.http import GET
+
+from researchhub_case.constants.case_constants import (
+    APPROVED, DENIED, INVALIDATED, NULLIFIED, OPEN,
+)
+from researchhub_case.models import AuthorClaimCase
+from researchhub_case.serializers import AuthorClaimCaseSerializer
+
+
+@api_view(http_method_names=[GET])
+@permission_classes([AllowAny])
+def get_author_claim_cases_for_mods(request):
+    # TODO: calvinhlee - paginate this
+    try:
+        case_status = request.query_params.get('case_status')
+        if (case_status == 'CLOSED'):
+            case_query_status = [APPROVED, DENIED, INVALIDATED, NULLIFIED]
+        elif (case_status == 'OPEN'):
+            case_query_status = [OPEN]
+        else:
+            return Response('Bad case status', status=400)
+
+        target_case_set = AuthorClaimCase.objects.filter(
+            status__in=case_query_status
+        )
+        serialized_result = AuthorClaimCaseSerializer(
+          target_case_set, many=True
+        )
+        return Response(data=serialized_result.data, status=200)
+    except (KeyError, TypeError) as e:
+        return Response(e, status=400)
+
+
+@api_view(http_method_names=[GET])
+@permission_classes([AllowAny])
+def get_author_claim_counts_for_mods():
+    try:
+        close_count = AuthorClaimCase.objects.filter(
+            status__in=[APPROVED, DENIED, INVALIDATED, NULLIFIED]
+        ).count()
+        open_count = AuthorClaimCase.objects.filter(
+            status__in=[OPEN]
+        ).count()
+        result_data = json.dumps({
+          'close_count': close_count,
+          'open_count': open_count
+        })
+        return Response(data=result_data, status=200)
+    except (KeyError, TypeError) as e:
+        return Response(e, status=400)
