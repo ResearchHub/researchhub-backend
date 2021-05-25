cff6efc2e215afbc9d060bdfa809e9c73a06a98f
Calvin Lee <calvinhlee23@gmail.com>
2021-05-25 15:04:14 -0700

    updated

--- src/researchhub_case/constants/case_constants.py
+++ src/researchhub_case/constants/case_constants.py
@@ -1,18 +1,18 @@
 ALLOWED_VALIDATION_ATTEMPT_COUNT = 10
-AUTHOR_CLAIM = "AUTHOR_CLAIM"
+AUTHOR_CLAIM = 'AUTHOR_CLAIM'
 
-APPROVED = "APPROVED"
+APPROVED = 'APPROVED'
 # "Denied" signifies moderators manually denying the request
-DENIED = "DENIED"
-INITIATED = "INITIATED"
+DENIED = 'DENIED'
+INITIATED = 'INITIATED'
 # Invalidations occurs through the system such as too many attempts
-INVALIDATED = "INVALIDATED"
+INVALIDATED = 'INVALIDATED'
 """
   Nullified signifies that the author was
   claimed by someone else during process
 """
-NULLIFIED = "NULLIFIED"
-OPEN = "OPEN"
+NULLIFIED = 'NULLIFIED'
+OPEN = 'OPEN'
 
 
 AUTHOR_CLAIM_CASE_STATUS = [
