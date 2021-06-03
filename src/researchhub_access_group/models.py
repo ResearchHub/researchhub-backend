b600307d95de2580eeb2a699e9451c1fde075e8b
Calvin Lee <calvinhlee23@gmail.com>
2021-06-03 14:54:22 -0700

    cleanup

--- src/researchhub_access_group/models.py
+++ src/researchhub_access_group/models.py
@@ -5,21 +5,23 @@ from utils.models import DefaultModel
 
 
 class ResearchhubAccessGroup(DefaultModel):
-    is_public = models.BooleanField(
-      blank=False,
-      default=True,
-      help_text="Public means it's accessible disregarding user's status",
-      null=False,
-    )
+    # Groups
     admins = models.ManyToManyField(
         User,
         related_name="access_admin_groups"
+    )
+    editors = models.ManyToManyField(
+        User,
+        related_name="access_editor_groups"
     )
     viewers = models.ManyToManyField(
         User,
         related_name="access_viewing_groups"
     )
-    editors = models.ManyToManyField(
-        User,
-        related_name="access_editor_groups"
+
+    is_public = models.BooleanField(
+      blank=False,
+      default=True,
+      help_text="Public means it's accessible disregarding user's status",
+      null=False,
     )
