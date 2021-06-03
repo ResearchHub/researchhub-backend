ab86b294878056262c23d944b64c1f07d19203ba
Calvin Lee <calvinhlee23@gmail.com>
2021-06-03 15:10:48 -0700

    update

--- src/researchhub_document/related_models/researchhub_post_model.py
+++ src/researchhub_document/related_models/researchhub_post_model.py
@@ -2,18 +2,32 @@ from django.db import models
 
 from researchhub_document.related_models.researchhub_unified_document_model \
   import ResearchhubUnifiedDocument
-from researchhub_document.related_models.constants.document_type import (
-  DISCUSSION, DOCUMENT_TYPES
+from researchhub_document.related_models.constants.editor_type import (
+  CK_EDITOR, EDITOR_TYPES,
 )
+from utils.models import DefaultModel
 
 
-class ResearchhubPost(models.Model):
-    post_type = models.CharField(
-      blank=False,
-      choices=DOCUMENT_TYPES,
-      default=DISCUSSION,
+class ResearchhubPost(DefaultModel):
+    discussion_src = models.FileField(
+        max_length=512,
+        upload_to='uploads/post_discussion/%Y/%m/%d/',
+        default=None,
+        null=True,
+        blank=True
+    )
+    editor_type = models.CharField(
+      choices=EDITOR_TYPES,
+      default=CK_EDITOR,
       max_length=32,
-      null=False,
+      help_text='Editor used to compose the post',
+    )
+    eln_src = models.FileField(
+        max_length=512,
+        upload_to='uploads/post_eln/%Y/%m/%d/',
+        default=None,
+        null=True,
+        blank=True
     )
     prev_version = models.OneToOneField(
         'self',
@@ -21,18 +35,22 @@ class ResearchhubPost(models.Model):
         default=None,
         null=True,
         on_delete=models.SET_NULL,
-        related_name="next_version",
+        related_name='next_version',
     )
-    unified_document = models.ForeignKey(
-        ResearchhubUnifiedDocument,
-        db_index=True,
-        on_delete=models.CASCADE,
-        related_name="posts",
+    renderable_text = models.TextField(
+        blank='true',
+        default='',
     )
     version_number = models.IntegerField(
         blank=False,
         default=1,
         null=False,
+    )
+    unified_document = models.ForeignKey(
+        ResearchhubUnifiedDocument,
+        db_index=True,
+        on_delete=models.CASCADE,
+        related_name='posts',
     )
 
     def is_latest_version(self):
