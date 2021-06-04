22433db9738603eeb8e7cf2bbf0442c6888a47f1
Calvin Lee <calvinhlee23@gmail.com>
2021-06-04 01:57:03 -0700

    need to customize this

--- src/researchhub_document/views/researchhub_post_views.py
+++ src/researchhub_document/views/researchhub_post_views.py
@@ -1,11 +1,17 @@
+from django.core.files.base import ContentFile
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import (
     IsAuthenticated, AllowAny
 )
 
+from hub.models import Hub
+from researchhub_document.related_models.constants.document_type \
+    import DISCUSSION
 from researchhub_document.models import ResearchhubPost
 from researchhub_document.serializers.researchhub_post_serializer \
     import ResearchhubPostSerializer
+from user.models import User
 
 
 class AuthorClaimCaseViewSet(ModelViewSet):
@@ -15,7 +21,7 @@ class AuthorClaimCaseViewSet(ModelViewSet):
     ordering = ('-created_date',)
     serializer_class = ResearchhubPostSerializer
 
-    def get_queryset(self):
+    def get_queryset(self, prefetch=True):
         query_params = self.request.query_params
         query_set = ResearchhubPost.objects.all()
 
@@ -24,3 +30,31 @@ class AuthorClaimCaseViewSet(ModelViewSet):
             query_set = query_set.filter()
         
         return query_set
+
+    @action(
+        detail=True,
+        methods=['get', 'post', 'put'],
+    )
+    def post_create(self, request, pk='post_create'):
+        print("HEYYYYYY")
+        request_data = request.data
+        created_by_user = User.objects.filter(
+            id=request_data.get('created_by_id')
+        ).first()
+        prev_version = ResearchhubPost.objects.filter(
+            id=request_data.get('prev_version_id')
+        ).first()
+        hubs = Hub.objects.filter(
+          id__in=request_data['hub_ids']
+        )
+        full_src_file = ContentFile(request_data['full_src'])
+        is_discussion = request.data.get('document_type') == DISCUSSION
+
+        return ResearchhubPost.create(
+            # **validated_data,
+            created_by=created_by_user,
+            discussion_src=full_src_file if is_discussion else None,
+            eln_src=full_src_file if not is_discussion else None,
+            hubs=hubs,
+            prev_version=prev_version,
+        )
