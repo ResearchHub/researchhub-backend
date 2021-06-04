b900c7b1679bca8b2a26a9c19513bbfd60648d08
Calvin Lee <calvinhlee23@gmail.com>
2021-06-04 01:57:03 -0700

    need to customize this

--- src/researchhub_document/serializers/researchhub_post_serializer.py
+++ src/researchhub_document/serializers/researchhub_post_serializer.py
@@ -1,11 +1,8 @@
-from django.core.files.base import ContentFile
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from hub.models import Hub
 from researchhub_document.related_models.constants.document_type \
     import DISCUSSION
 from researchhub_document.models import ResearchhubPost
-from user.models import User
 from user.serializers import UserSerializer
 
 
@@ -40,35 +37,12 @@ class ResearchhubPostSerializer(ModelSerializer):
         method_name='get_unified_document_id'
     )
 
-    def create(self, validated_data):
-        request_data = self.context.get('request').data
-        created_by_user = User.objects.filter(
-            id=request_data['created_by_id']
-        ).first()
-        prev_version = ResearchhubPost.objects.filter(
-            id=request_data['prev_version_id']
-        ).first()
-        hubs = Hub.objects.filter(
-          id__in=request_data['hub_ids']
-        )
-        full_src_file = ContentFile(request_data['full_src'])
-        is_discussion = validated_data['document_type'] == DISCUSSION
-
-        return ResearchhubPost.create(
-            **validated_data,
-            created_by=created_by_user,
-            discussion_src=full_src_file if is_discussion else None,
-            eln_src=full_src_file if not is_discussion else None,
-            hubs=hubs,
-            prev_version=prev_version,
-        )
-  
     def get_post_src(self, instance):
         if (instance.document_type == DISCUSSION):
             return instance.discussion_src
         else:
             return instance.eln_src
-  
+
     def get_created_by(self, instance):
         return UserSerializer(instance.created_by, read_only=True)
 
