b80be24cd9d4dc680c1d7ac16566ee30b9866bc1
Calvin Lee <calvinhlee23@gmail.com>
2021-06-03 23:42:26 -0700

    serializers coming along

--- src/researchhub_document/related_models/researchhub_post_model.py
+++ src/researchhub_document/related_models/researchhub_post_model.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from researchhub_document.related_models.constants.document_type \
+    import DISCUSSION, DOCUMENT_TYPES
 from researchhub_document.related_models.researchhub_unified_document_model \
   import ResearchhubUnifiedDocument
 from researchhub_document.related_models.constants.editor_type import (
@@ -10,24 +12,30 @@ from utils.models import DefaultModel
 
 class ResearchhubPost(DefaultModel):
     discussion_src = models.FileField(
-        max_length=512,
-        upload_to='uploads/post_discussion/%Y/%m/%d/',
+        blank=True,
         default=None,
+        max_length=512,
         null=True,
-        blank=True
+        upload_to='uploads/post_discussion/%Y/%m/%d/',
+    )
+    document_type = models.CharField(
+        choices=DOCUMENT_TYPES,
+        default=DISCUSSION,
+        max_length=32,
+        null=False,
     )
     editor_type = models.CharField(
-      choices=EDITOR_TYPES,
-      default=CK_EDITOR,
-      max_length=32,
-      help_text='Editor used to compose the post',
+        choices=EDITOR_TYPES,
+        default=CK_EDITOR,
+        max_length=32,
+        help_text='Editor used to compose the post',
     )
     eln_src = models.FileField(
-        max_length=512,
-        upload_to='uploads/post_eln/%Y/%m/%d/',
+        blank=True,
         default=None,
+        max_length=512,
         null=True,
-        blank=True
+        upload_to='uploads/post_eln/%Y/%m/%d/',
     )
     prev_version = models.OneToOneField(
         'self',
@@ -42,13 +50,8 @@ class ResearchhubPost(DefaultModel):
         default='',
     )
     title = models.TextField(
-        blank=True, 
+        blank=True,
         default=''
-    )
-    version_number = models.IntegerField(
-        blank=False,
-        default=1,
-        null=False,
     )
     unified_document = models.ForeignKey(
         ResearchhubUnifiedDocument,
@@ -56,9 +59,16 @@ class ResearchhubPost(DefaultModel):
         on_delete=models.CASCADE,
         related_name='posts',
     )
+    version_number = models.IntegerField(
+        blank=False,
+        default=1,
+        null=False,
+    )
 
+    @property
     def is_latest_version(self):
         return self.next_version is None
 
+    @property
     def is_root_version(self):
         return self.version_number == 1
