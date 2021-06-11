01af1fb73d492419034704a8c970d9ce0bb9cb14
Calvin Lee <calvinhlee23@gmail.com>
2021-06-11 01:05:32 -0700

    remove print

--- src/researchhub_document/views/researchhub_post_views.py
+++ src/researchhub_document/views/researchhub_post_views.py
@@ -40,7 +40,6 @@ class ResearchhubPostViewSet(ModelViewSet, ReactionViewActionMixin):
                 query_set = query_set.filter(created_by__id=created_by_id)
             if (document_id is not None):
                 query_set = query_set.filter(unified_document_id=document_id)
-            print("REACHING: ", query_set)
             return query_set
         except (KeyError, TypeError) as exception:
             return Response(exception, status=400)
