3bde4ee901c69f307c3cc5d7d0e2b94ca31ffd3c
Leo Sun <leo.sun@berkeley.edu>
2021-06-18 11:06:46 -0700

    updated constants

--- src/researchhub_document/related_models/constants/document_type.py
+++ src/researchhub_document/related_models/constants/document_type.py
@@ -1,7 +1,7 @@
 DISCUSSION = 'DISCUSSION'
 ELN = 'ELN'
 PAPER = 'PAPER'
-POST = 'POST'  # POSTS include discussion and eln
+POSTS = 'POSTS'  # POSTS include discussion and eln
 ALL = 'ALL'  # ALL includes paper and posts
 
 DOCUMENT_TYPES = (
