f6f58ab9f31bd75d376fbb8d72ff339c9bdcb9e7
Kobe Attias <kobeattias@gmail.com>
2021-06-29 11:49:15 -0400

    Refactor for USE_DEBUG_TOOLBAR

--- src/search/views/paper.py
+++ src/search/views/paper.py
@@ -13,22 +13,6 @@ from django_elasticsearch_dsl_drf.filter_backends import (
     SearchFilterBackend,
 )
 
-from django_elasticsearch_dsl_drf import (
-    constants
-)
-
-from django_elasticsearch_dsl_drf.filter_backends.search.query_backends import (
-    MatchPhrasePrefixQueryBackend,
-    NestedQueryBackend,
-    BaseSearchQueryBackend,
-    MultiMatchQueryBackend,
-
-)
-
-from django_elasticsearch_dsl_drf.filter_backends.search import (
-    BaseSearchFilterBackend
-
-)
 from elasticsearch_dsl.query import Q
 
 from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
@@ -40,28 +24,6 @@ from utils.permissions import ReadOnly
 import re
 
 
-
-class MatchPhraseQueryBackend(BaseSearchQueryBackend):
-    matching = 'should'
-
-    @classmethod
-    def construct_search(cls, request, view, search_backend):
-
-        field = 'title'
-        __queries = []
-        __queries.append(
-            # Q('term', **{field: 'stdp'})
-            Q('match', **{field: 'stdp'}    )
-        )
-        return __queries
-
-class PhraseSearchFilterBackend(BaseSearchFilterBackend):
-    matching = 'should'
-    query_backends = [
-        MatchPhraseQueryBackend,
-    ]
-
-
 class PaperDocumentView(DocumentViewSet):
     document = PaperDocument
     permission_classes = [ReadOnly]
@@ -70,7 +32,7 @@ class PaperDocumentView(DocumentViewSet):
     lookup_field = 'id'
     filter_backends = [
         # PhraseSearchFilterBackend,
-        # MultiMatchSearchFilterBackend,
+        MultiMatchSearchFilterBackend,
         CompoundSearchFilterBackend,
         FacetedSearchFilterBackend,
         FilteringFilterBackend,
@@ -153,20 +115,17 @@ class PaperDocumentView(DocumentViewSet):
     }
 
 
-    # def get_queryset(self, **kwargs):
-    #     gen_query = super().get_queryset(**kwargs)
+    def get_queryset(self, **kwargs):
+        gen_query = super().get_queryset(**kwargs)
 
-    #     query = self.request.query_params.get('search')
-    #     doi_regex = '(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?![%"#? ])\\S)+)'
+        query = self.request.query_params.get('search')
+        doi_regex = '(10[.][0-9]{4,}(?:[.][0-9]+)*/(?:(?![%"#? ])\\S)+)'
 
-    #     # If DOI is detexted, we want to override the configured queries
-    #     # and insead, execute a single DOI query
-    #     if re.match(doi_regex, query):
-    #         self.search_fields = {
-    #             'doi': {'boost': 3, 'fuzziness': 0}
-    #         }
-    #         self.multi_match_search_fields = {
-    #             'doi': {'boost': 3, 'fuzziness': 0}
-    #         }            
+        # If DOI is detected, we want to override the configured queries
+        # and insead, execute a single DOI query
+        if re.match(doi_regex, query):
+            self.search_fields = {
+                'doi': {'boost': 3, 'fuzziness': 0}
+            }
 
-    #     return gen_query
+        return gen_query
