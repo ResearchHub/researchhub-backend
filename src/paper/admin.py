307d2d5b68d2e35e7e9b3c4a01f8a94a765fdb25
Leo Sun <leo.sun@berkeley.edu>
2021-07-07 18:07:36 -0700

    adding a timeout paginator to paper, post, unified doc admin panel

--- src/paper/admin.py
+++ src/paper/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from researchhub.admin import IdFilter, UploadedDateFilter
+from researchhub.admin import IdFilter, UploadedDateFilter, TimeoutPaginator
+
 from .models import Paper, Vote
 
 
 class PaperAdmin(admin.ModelAdmin):
     list_filter = (IdFilter, UploadedDateFilter)
+    paginator = TimeoutPaginator
 
 
 admin.site.register(Vote)
--- src/researchhub_document/admin.py
+++ src/researchhub_document/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from researchhub.admin import IdFilter
+from researchhub.admin import IdFilter, TimeoutPaginator
 from .models import ResearchhubPost, ResearchhubUnifiedDocument
 
 
 class PostAdmin(admin.ModelAdmin):
+    paginator = TimeoutPaginator
     list_filter = (IdFilter,)
 
 
