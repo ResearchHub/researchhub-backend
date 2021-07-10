42264da18b7aaa3eb71c5c17aa84bf96b946db84
Leo Sun <leo.sun@berkeley.edu>
2021-07-09 17:57:23 -0700

    updating paper admin panel to exclude certain fields that cause a lot of db calls

--- src/paper/admin.py
+++ src/paper/admin.py
@@ -8,6 +8,8 @@ from .models import Paper, Vote
 class PaperAdmin(admin.ModelAdmin):
     list_filter = (IdFilter, UploadedDateFilter)
     paginator = TimeoutPaginator
+    exclude = ['summary', 'moderators']
+    raw_id_fields = ['authors', 'references']
 
 
 admin.site.register(Vote)
