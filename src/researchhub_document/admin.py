bb43659589672662db0a021f88b900d569a3970a
Leo Sun <leo.sun@berkeley.edu>
2021-07-12 10:44:36 -0700

    using raw id field for unified doc and posts

--- src/researchhub_document/admin.py
+++ src/researchhub_document/admin.py
@@ -7,7 +7,14 @@ from .models import ResearchhubPost, ResearchhubUnifiedDocument
 class PostAdmin(admin.ModelAdmin):
     paginator = TimeoutPaginator
     list_filter = (IdFilter,)
+    raw_id_fields = ['unified_document']
+
+
+class UnifiedDocumentAdmin(admin.ModelAdmin):
+    paginator = TimeoutPaginator
+    list_filter = (IdFilter,)
+    raw_id_fields = ['paper']
 
 
 admin.site.register(ResearchhubPost, PostAdmin)
-admin.site.register(ResearchhubUnifiedDocument, PostAdmin)
+admin.site.register(ResearchhubUnifiedDocument, UnifiedDocumentAdmin)
