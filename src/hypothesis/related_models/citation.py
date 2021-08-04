8c0e6b0bfd1f264a225b630b24570e9a1f948d49
Leo Sun <leo.sun@berkeley.edu>
2021-08-04 11:47:43 -0700

    separating files

---
+++ src/hypothesis/related_models/citation.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from discussion.reaction_models import AbstractGenericReactionModel
+from researchhub_document.models import ResearchhubUnifiedDocument
+from hypothesis.models import Hypothesis
+from user.models import User
+
+
+class Citation(AbstractGenericReactionModel):
+    created_by = models.ForeignKey(
+        User,
+        db_index=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='created_citations',
+    )
+    hypothesis = models.ForeignKey(
+        Hypothesis,
+        db_index=True,
+        null=False,
+        on_delete=models.CASCADE,
+        related_name='citations'
+    )
+    source = models.OneToOneField(
+        ResearchhubUnifiedDocument,
+        related_name='citation',
+        null=False,
+        on_delete=models.CASCADE
+    )
