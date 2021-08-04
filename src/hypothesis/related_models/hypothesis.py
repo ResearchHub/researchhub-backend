63c7c7d4f661580ba0491245ac88e5233aacae61
Leo Sun <leo.sun@berkeley.edu>
2021-08-04 11:47:43 -0700

    separating files

---
+++ src/hypothesis/related_models/hypothesis.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+
+from discussion.reaction_models import AbstractGenericReactionModel
+from researchhub_document.models import ResearchhubUnifiedDocument
+from user.models import User
+
+
+class Hypothesis(AbstractGenericReactionModel):
+    actions = GenericRelation(
+        'user.Action',
+        object_id_field='object_id',
+        content_type_field='content_type',
+        related_query_name='hypothesis'
+    )
+    created_by = models.ForeignKey(
+        User,
+        db_index=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='created_hypotheses',
+    )
+    result_score = models.DecimalField(
+        max_digits=5,
+        decimal_places=2
+    )
+    slug = models.SlugField(max_length=1024)
+    title = models.TextField(blank=True, default='')
+    unified_document = models.OneToOneField(
+        ResearchhubUnifiedDocument,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='hypothesis'
+    )
+
+    def calculate_result_score(self, save=False):
+        pass
