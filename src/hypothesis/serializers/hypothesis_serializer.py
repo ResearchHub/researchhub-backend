87b03638287f0ade8e8b0fe72c52cacf5973dfb2
Leo Sun <leo.sun@berkeley.edu>
2021-08-10 16:04:45 -0700

    adding hubs git hypothesis serializer

--- src/hypothesis/serializers/hypothesis_serializer.py
+++ src/hypothesis/serializers/hypothesis_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from hypothesis.models import Hypothesis
+from hub.serializers import SimpleHubSerializer, DynamicHubSerializer
 from researchhub.serializers import DynamicModelFieldSerializer
 from researchhub_document.serializers import (
   DynamicUnifiedDocumentSerializer
@@ -11,6 +12,7 @@ from user.serializers import UserSerializer, DynamicUserSerializer
 class HypothesisSerializer(ModelSerializer):
     created_by = UserSerializer()
     full_markdown = SerializerMethodField()
+    hubs = SerializerMethodField()
 
     class Meta:
         model = Hypothesis
@@ -18,6 +20,7 @@ class HypothesisSerializer(ModelSerializer):
             'id',
             'created_by',
             'full_markdown',
+            'hubs',
             'result_score',
             'renderable_text',
             'slug',
@@ -40,9 +43,17 @@ class HypothesisSerializer(ModelSerializer):
         full_markdown = byte_string.decode('utf-8')
         return full_markdown
 
+    def get_hubs(self, hypothesis):
+        serializer = SimpleHubSerializer(
+            hypothesis.unified_document.hubs,
+            many=True
+        )
+        return serializer.data
+
 
 class DynamicHypothesisSerializer(DynamicModelFieldSerializer):
     created_by = SerializerMethodField()
+    hubs = SerializerMethodField()
     unified_document = SerializerMethodField()
 
     class Meta(object):
@@ -54,6 +65,17 @@ class DynamicHypothesisSerializer(DynamicModelFieldSerializer):
         _context_fields = context.get('hyp_dhs_get_created_by', {})
         serializer = DynamicUserSerializer(
             hypothesis.created_by,
+            context=context,
+            **_context_fields
+        )
+        return serializer.data
+
+    def get_hubs(self, hypothesis):
+        context = self.context
+        _context_fields = context.get('hyp_dhs_get_hubs', {})
+        serializer = DynamicHubSerializer(
+            hypothesis.unified_document.hubs,
+            many=True,
             context=context,
             **_context_fields
         )
