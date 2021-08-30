e308327811a9be185c70708047af67693644fe90
Leo Sun <leo.sun@berkeley.edu>
2021-08-30 11:14:52 -0700

    note latest verision is now updated on note content create

---
+++ src/note/signals/note_signal.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from note.models import NoteContent
+
+
+@receiver(post_save, sender=NoteContent, dispatch_uid='update_latest_version')
+def update_latest_version(
+    sender,
+    instance,
+    created,
+    update_fields,
+    **kwargs
+):
+    if created:
+        source = instance.note
+        source.latest_version = instance
+        source.save()
