1eb980d3fda27398e5869adf55f24b35527a5314
Leo Sun <leo.sun@berkeley.edu>
2021-09-03 11:26:28 -0700

    updating top rated filter

--- src/hypothesis/related_models/hypothesis.py
+++ src/hypothesis/related_models/hypothesis.py
@@ -45,6 +45,11 @@ class Hypothesis(AbstractGenericReactionModel):
         on_delete=models.CASCADE,
         related_name='hypothesis'
     )
+    votes = GenericRelation(
+        'discussion.Vote',
+        object_id_field='object_id',
+        content_type_field='content_type'
+    )
     purchases = GenericRelation(
         'purchase.Purchase',
         object_id_field='object_id',
