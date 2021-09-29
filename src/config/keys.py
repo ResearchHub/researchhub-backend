f1bc7de76d7e30ff35ad81ad91f5535687f449c0
Kobe Attias <kobeattias@gmail.com>
2021-09-28 21:35:40 -0400

    CTX: Feed, key

--- src/config/keys.py
+++ src/config/keys.py
@@ -31,4 +31,4 @@ SENTRY_DSN = os.environ.get('SENTRY_DSN', '')
 APM_URL = os.environ.get('APM_URL', '')
 
 ELASTICSEARCH_HOST = os.environ.get('ELASTICSEARCH_HOST', '')
-RH_API_Key = os.environ.get('RH_API_Key', '')
+RH_API_KEY = os.environ.get('RH_API_KEY', '')
