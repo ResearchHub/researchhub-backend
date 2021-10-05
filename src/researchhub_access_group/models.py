abcfcb1d375a54e54b2e9a85754931d0c85528df
Leo Sun <leo.sun@berkeley.edu>
2021-10-05 10:32:12 -0700

    updatedd permission model

--- src/researchhub_access_group/models.py
+++ src/researchhub_access_group/models.py
@@ -1,27 +1,42 @@
 from django.db import models
 
+from researchhub.settings import BASE_FRONTEND_URL
 from user.models import User
 from utils.models import DefaultModel
 
 
 class ResearchhubAccessGroup(DefaultModel):
-    # Groups
-    admins = models.ManyToManyField(
-        User,
-        related_name="access_admin_groups"
-    )
-    editors = models.ManyToManyField(
-        User,
-        related_name="access_editor_groups"
-    )
-    viewers = models.ManyToManyField(
-        User,
-        related_name="access_viewing_groups"
+    key = models.CharField(max_length=32)
+    name = models.CharField(max_length=32)
+
+    @property
+    def sharable_link(self):
+        # TODO: UPDATE URL
+        return f'{BASE_FRONTEND_URL}/placeholder/{self.key}'
+
+
+class Permission(DefaultModel):
+    ADMIN = 'ADMIN'
+    EDITOR = 'EDITOR'
+    VIEWER = 'VIEWER'
+    ACCESS_TYPE_CHOICES = (
+        (ADMIN, ADMIN),
+        (EDITOR, EDITOR),
+        (VIEWER, VIEWER),
     )
 
-    is_public = models.BooleanField(
-      blank=False,
-      default=True,
-      help_text="Public means it's accessible disregarding user's status",
-      null=False,
+    access_group = models.ForeignKey(
+        ResearchhubAccessGroup,
+        related_name='permissions',
+        on_delete=models.CASCADE,
+    )
+    access_type = models.CharField(
+        choices=ACCESS_TYPE_CHOICES,
+        default=VIEWER,
+        max_length=8
+    )
+    user = models.ForeignKey(
+        User,
+        related_name='permissions',
+        on_delete=models.CASCADE
     )
