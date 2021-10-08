6e56379ed4b3dcabc82043c06af7400814cfd940
lightninglu10 <lightning.lu7@gmail.com>
2021-10-08 16:13:38 -0700

    adding to email whitelist

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -495,6 +495,8 @@ EMAIL_WHITELIST = [
     'kobe@researchhub.com',
     'kobe+1@researchhub.com',
     'contact@notesalong.com',
+    'patricklu@researchhub.com',
+    'thomas@researchhub.com'
 ]
 
 # Sentry
