0544314a2adc92e9f6fea205f0cde5617c0c1bfe
Leo Sun <leo.sun@berkeley.edu>
2021-10-08 12:31:44 -0700

    added more note endpoints for invites

--- src/invite/serializers/note_invitation_serializer.py
+++ src/invite/serializers/note_invitation_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from invite.models import NoteInvitation
+from note.serializers import DynamicNoteSerializer
 from researchhub.serializers import DynamicModelFieldSerializer
 from user.serializers import DynamicUserSerializer
 
@@ -13,6 +14,7 @@ class NoteInvitationSerializer(ModelSerializer):
 
 class DynamicNoteInvitationSerializer(DynamicModelFieldSerializer):
     inviter = SerializerMethodField()
+    note = SerializerMethodField()
     recipient = SerializerMethodField()
 
     class Meta:
@@ -24,6 +26,16 @@ class DynamicNoteInvitationSerializer(DynamicModelFieldSerializer):
         _context_fields = context.get('inv_dnis_get_inviter', {})
         serializer = DynamicUserSerializer(
             invitation.inviter,
+            context=context,
+            **_context_fields
+        )
+        return serializer.data
+
+    def get_note(self, invitation):
+        context = self.context
+        _context_fields = context.get('inv_dnis_get_note', {})
+        serializer = DynamicNoteSerializer(
+            invitation.note,
             context=context,
             **_context_fields
         )
