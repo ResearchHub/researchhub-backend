ab1504d98f847637f19cf92c16fdd927943e0621
Leo Sun <leo.sun@berkeley.edu>
2021-10-19 11:11:49 -0700

    updating migrations

--- src/researchhub_access_group/migrations/0010_auto_20211015_2159.py
+++ src/researchhub_access_group/migrations/0010_auto_20211015_2159.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='permission',
             name='organization',
-            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, related_name='permissions', to='user.Organization'),
+            field=models.ForeignKey(null=True, default=None, on_delete=django.db.models.deletion.CASCADE, related_name='permissions', to='user.Organization'),
             preserve_default=False,
         ),
     ]
--- src/researchhub_access_group/migrations/0012_auto_20211018_1813.py
+++ src/researchhub_access_group/migrations/0012_auto_20211018_1813.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='permission',
             name='organization',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='user_permissions', to='user.Organization'),
+            field=models.ForeignKey(null=False, on_delete=django.db.models.deletion.CASCADE, related_name='user_permissions', to='user.Organization'),
         ),
     ]
