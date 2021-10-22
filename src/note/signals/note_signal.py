60d4dd3890208b6850d574ce1ce0aebfd9f99807
Leo Sun <leo.sun@berkeley.edu>
2021-10-22 11:56:52 -0700

    atomicity test

--- src/note/signals/note_signal.py
+++ src/note/signals/note_signal.py
@@ -4,15 +4,15 @@ from django.dispatch import receiver
 from note.models import NoteContent
 
 
-@receiver(post_save, sender=NoteContent, dispatch_uid='update_latest_version')
-def update_latest_version(
-    sender,
-    instance,
-    created,
-    update_fields,
-    **kwargs
-):
-    if created:
-        source = instance.note
-        source.latest_version = instance
-        source.save()
+# @receiver(post_save, sender=NoteContent, dispatch_uid='update_latest_version')
+# def update_latest_version(
+#     sender,
+#     instance,
+#     created,
+#     update_fields,
+#     **kwargs
+# ):
+#     if created:
+#         source = instance.note
+#         source.latest_version = instance
+#         source.save()
