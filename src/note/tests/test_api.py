9963b9e324029382ea8d2b879cbe9e7d3996c3a8
Kobe Attias <kobeattias@gmail.com>
2021-11-08 15:19:22 -0500

    [ELN] moving tests to the right place

---
+++ src/note/tests/test_api.py
@@ -0,0 +1,289 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from allauth.utils import (
+    get_user_model,
+)
+from user.related_models.user_model import User
+from researchhub_document.models import (
+    ResearchhubUnifiedDocument
+)
+from researchhub_access_group.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from note.models import (
+    Note
+)
+from user.models import Organization
+
+class NoteTests(APITestCase):
+    def setUp(self):
+        # Create + auth user
+        username = "test@test.com"
+        password = "password"
+        self.user = get_user_model().objects.create_user(username=username, password=password, email=username)
+        self.client.force_authenticate(self.user)
+
+        # Create org
+        response = self.client.post("/api/organization/", {"name": "some org"})
+        self.org = response.data
+
+    def test_create_workspace_note(self):
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })       
+        created_note = response.data
+        self.assertEqual(created_note["access"], "WORKSPACE")
+
+    def test_create_private_note(self):
+        response = self.client.post("/api/note/", {"grouping": "PRIVATE", "organization_slug": self.org["slug"], "title": "TEST" })       
+        created_note = response.data
+        self.assertEqual(created_note["access"], "PRIVATE")
+
+    def test_cannot_create_shared_note_manually(self):
+        response = self.client.post("/api/note/", {"grouping": "SHARED", "organization_slug": self.org["slug"], "title": "TEST" })       
+        created_note = response.data
+
+        # NOTE: Should only be able to created SHARED note by inviting useres
+        self.assertNotEqual(created_note["access"], "SHARED")
+
+    def test_note_editor_cannot_invite_others(self):
+        # Create workspace note
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })       
+        note = response.data
+
+        # Create another user
+        editor_user = get_user_model().objects.create_user(username="editor@example.com", password="password", email="editor@example.com")
+
+        # Add permission to user
+        perms = Permission.objects.create(
+            access_type="EDITOR",
+            content_type=ContentType.objects.get_for_model(ResearchhubUnifiedDocument),
+            object_id=note["unified_document"],
+            user=editor_user
+        )
+
+        # Authenticate user and invite
+        self.client.force_authenticate(editor_user)
+        response = self.client.post(f"/api/note/{note['id']}/invite_user/", {"access_type": "ADMIN", "email": "invited@example.com", "expire": 10080 })
+
+        # Get new permissions
+        self.assertEqual(response.status_code, 403)
+
+    def test_note_editor_can_update_contents(self):
+        # Create workspace note
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })       
+        note = response.data
+
+        # Create another user
+        editor_user = get_user_model().objects.create_user(username="editor@example.com", password="password", email="editor@example.com")
+
+        # Add permission to user
+        perms = Permission.objects.create(
+            access_type="EDITOR",
+            content_type=ContentType.objects.get_for_model(ResearchhubUnifiedDocument),
+            object_id=note["unified_document"],
+            user=editor_user
+        )
+
+        # Update title
+        response = self.client.patch(f"/api/note/{note['id']}/", {"title": "some title"})
+
+        # Re-fetch note
+        response = self.client.get(f"/api/note/{note['id']}/")
+        note = response.data
+        self.assertEqual(note["title"], "some title")
+
+        # Update body
+        response = self.client.post(f"/api/note_content/", {"full_src": "updated body", "note": note['id'], "plain_text": ""})
+
+        # Re-fetch note
+        response = self.client.get(f"/api/note/{note['id']}/")
+        note = response.data
+        self.assertEqual(note["latest_version"]["src"], "updated body")
+
+    def test_note_viewer_cannot_update_contents(self):
+        # Create workspace note
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "original title" })       
+        note = response.data
+
+        # Create another user
+        viewer_user = get_user_model().objects.create_user(username="editor@example.com", password="password", email="editor@example.com")
+
+        # Add permission to user
+        perms = Permission.objects.create(
+            access_type="VIEWER",
+            content_type=ContentType.objects.get_for_model(ResearchhubUnifiedDocument),
+            object_id=note["unified_document"],
+            user=viewer_user
+        )
+
+        # Authenticate as viewer
+        self.client.force_authenticate(viewer_user)
+
+        # Update title
+        response = self.client.patch(f"/api/note/{note['id']}/", {"title": "updated title"})
+        self.assertEqual(response.status_code, 403)
+
+        # Re-fetch note
+        response = self.client.get(f"/api/note/{note['id']}/")
+        note = response.data
+        self.assertEqual(note["title"], "original title")
+
+        # Update body
+        response = self.client.post(f"/api/note_content/", {"full_src": "updated body", "note": note['id'], "plain_text": ""})
+        self.assertEqual(response.status_code, 403)
+
+        # Re-fetch note
+        response = self.client.get(f"/api/note/{note['id']}/")
+        note = response.data
+        self.assertEqual(note["latest_version"], None)
+
+    def test_note_viewer_cannot_invite_others(self):
+        # Create workspace note
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })       
+        note = response.data
+
+        # Create another user
+        invited_viewer = get_user_model().objects.create_user(username="editor@example.com", password="password", email="editor@example.com")
+
+        # Add permission to user
+        perms = Permission.objects.create(
+            access_type="VIEWER",
+            content_type=ContentType.objects.get_for_model(ResearchhubUnifiedDocument),
+            object_id=note["unified_document"],
+            user=invited_viewer
+        )
+
+        # Authenticate user and invite
+        self.client.force_authenticate(invited_viewer)
+        response = self.client.post(f"/api/note/{note['id']}/invite_user/", {"access_type": "ADMIN", "email": "invited@example.com", "expire": 10080 })
+
+        # Get new permissions
+        self.assertEqual(response.status_code, 403)
+
+    def test_note_admin_can_invite_others(self):
+        # Create workspace note
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })       
+        note = response.data
+
+        # Create another user
+        invited_note_admin = get_user_model().objects.create_user(username="admin@example.com", password="password", email="admin@example.com")
+
+        # Add permission to user
+        perms = Permission.objects.create(
+            access_type="ADMIN",
+            content_type=ContentType.objects.get_for_model(ResearchhubUnifiedDocument),
+            object_id=note["unified_document"],
+            user=invited_note_admin
+        )
+
+        # Authenticate user and invite
+        self.client.force_authenticate(invited_note_admin)
+        response = self.client.post(f"/api/note/{note['id']}/invite_user/", {"access_type": "ADMIN", "email": "invited@example.com", "expire": 10080 })
+
+        # Get new permissions
+        self.assertEqual(response.status_code, 200)
+        note = Note.objects.get(id=note["id"])
+        p = note.permissions.get(user=invited_note_admin.id)
+        self.assertTrue(p)
+
+    def test_invited_user_cannot_create_org_notes(self):
+        # Create workspace note
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })       
+        note = response.data
+
+        # Create another user
+        invited_note_admin = get_user_model().objects.create_user(username="admin@example.com", password="password", email="admin@example.com")
+
+        # Add permission to user
+        perms = Permission.objects.create(
+            access_type="ADMIN",
+            content_type=ContentType.objects.get_for_model(ResearchhubUnifiedDocument),
+            object_id=note["unified_document"],
+            user=invited_note_admin
+        )
+
+        # Authenticate user and create org note
+        self.client.force_authenticate(invited_note_admin)
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_removing_note_org_access_makes_note_private(self):
+        # Create workspace note
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })
+        note = response.data
+
+        # Remove org permission
+        self.client.delete(f"/api/note/{note['id']}/remove_permission/", {"organization": self.org["id"]})
+
+        # Re-fetch note
+        response = self.client.get(f"/api/note/{note['id']}/")
+        note = response.data
+
+        self.assertEqual(note["access"], "PRIVATE")
+
+    def test_sharing_private_note_move_to_shared_context(self):
+        # create private note
+        response = self.client.post("/api/note/", {"grouping": "PRIVATE", "organization_slug": self.org["slug"], "title": "TEST" })
+        note = response.data
+
+        # Invite another user
+        self.client.post(f"/api/note/{note['id']}/invite_user/", {"access_type": "ADMIN", "email": "invited@example.com", "expire": 10080 })
+
+        # Re-fetch note
+        response = self.client.get(f"/api/note/{note['id']}/")
+        note = response.data
+
+        self.assertEqual(note["access"], "SHARED")
+
+    def test_removing_invited_user_from_shared_note_moves_note_to_private_context(self):
+        # create private note
+        response = self.client.post("/api/note/", {"grouping": "PRIVATE", "organization_slug": self.org["slug"], "title": "TEST" })
+        note = response.data
+
+        # Invite another user
+        self.client.post(f"/api/note/{note['id']}/invite_user/", {"access_type": "ADMIN", "email": "invited@example.com", "expire": 10080 })
+
+        # Re-fetch note
+        response = self.client.get(f"/api/note/{note['id']}/")
+        note = response.data
+        self.assertEqual(note["access"], "SHARED")
+
+        # Remove user access
+        self.client.patch(f"/api/note/{note['id']}/remove_invited_user/", {"email": "invited@example.com"})
+
+        # Re-fetch note
+        response = self.client.get(f"/api/note/{note['id']}/")
+        note = response.data
+        self.assertEqual(note["access"], "PRIVATE")
+
+    def test_user_with_both_viewer_and_org_permission_should_be_able_to_edit_note(self):
+        # create note
+        response = self.client.post("/api/note/", {"grouping": "WORKSPACE", "organization_slug": self.org["slug"], "title": "TEST" })
+        note = response.data
+
+        # Create viewer user
+        viewer_user = get_user_model().objects.create_user(username="user_b@example.com", password="password", email="user_b@example.com")
+
+        # Add permission to user
+        perms = Permission.objects.create(
+            access_type="VIEWER",
+            content_type=ContentType.objects.get_for_model(ResearchhubUnifiedDocument),
+            object_id=note["unified_document"],
+            user=viewer_user
+        )
+
+        # Upgrade user to org member
+        perms = Permission.objects.create(
+            access_type="MEMBER",
+            content_type=ContentType.objects.get_for_model(Organization),
+            object_id=self.org["id"],
+            user=viewer_user
+        )
+
+        # authenticate and update note
+        self.client.force_authenticate(viewer_user)
+        response = self.client.patch(f"/api/note/{note['id']}/", {"title": "some title"})
+
+        # refetch note
+        response = self.client.patch(f"/api/note/{note['id']}/")
+        self.assertEqual(response.data["title"], "updated title")
