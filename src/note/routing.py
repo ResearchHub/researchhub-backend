2a1e394bd8af948b64122de9c926a1887fa5cfaf
Leo <leo.sun@berkeley.edu>
2021-11-22 14:35:48 -0800

    updating websocket code for channels update

--- src/note/routing.py
+++ src/note/routing.py
@@ -5,6 +5,6 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(
         r'ws/(?P<organization_slug>[-\w]+)/notebook/$',
-        consumers.NoteConsumer
+        consumers.NoteConsumer.as_asgi()
      )
 ]
