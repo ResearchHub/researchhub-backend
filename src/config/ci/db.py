02e1b459fcb4dbdf64978fe33dfef0a370eb09b1
Kobe Attias <kobeattias@gmail.com>
2021-11-22 14:34:34 -0500

    test

--- src/config/ci/db.py
+++ src/config/ci/db.py
@@ -1,6 +1,6 @@
 import os
 
-NAME = 'researchhub'
+NAME = 'test_researchhub'
 HOST = os.environ.get('DB_HOST', 'localhost')
 PORT = os.environ.get('DB_PORT', '5432')
 USER = os.environ.get('DB_USER', 'rh_developer')
