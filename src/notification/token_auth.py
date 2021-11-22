5eabaa03c10d4ec5e623dec16ac72fe70412bacb
Leo <leo.sun@berkeley.edu>
2021-11-22 14:36:25 -0800

    updating websocket code for channels update

--- src/notification/token_auth.py
+++ src/notification/token_auth.py
@@ -6,34 +6,11 @@ from channels.auth import AuthMiddlewareStack
 from rest_framework.authtoken.models import Token
 
 
-# class TokenAuthMiddleware:
-#     """
-#     Token authorization middleware for Django Channels 2
-#     """
-
-#     def __init__(self, inner):
-#         self.inner = inner
-
-#     def __call__(self, scope):
-#         close_old_connections()
-#         headers = dict(scope['headers'])
-#         try:
-#             if b'sec-websocket-protocol' in headers:
-#                 token = headers[b'sec-websocket-protocol'].decode().split(', ')
-#                 token_name, token_key = token
-#                 if token_name == 'Token':
-#                     token = Token.objects.get(key=token_key)
-#                     scope['user'] = token.user
-#         except Token.DoesNotExist:
-#             scope['user'] = AnonymousUser()
-#         return self.inner(scope)
-
-
-def TokenAuthMiddlewareStack(inner):
-    return TokenAuthMiddleware(AuthMiddlewareStack(inner))
-
-
 class TokenAuthMiddleware(BaseMiddleware):
+    """
+    Token authorization middleware for Django Channels 3
+    """
+
     def __init__(self, inner):
         super().__init__(inner)
 
@@ -50,3 +27,7 @@ class TokenAuthMiddleware(BaseMiddleware):
         except Token.DoesNotExist:
             scope['user'] = AnonymousUser()
         return await super().__call__(scope, receive, send)
+
+
+def TokenAuthMiddlewareStack(inner):
+    return TokenAuthMiddleware(AuthMiddlewareStack(inner))
