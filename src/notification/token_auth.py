883fdd05dc3e1d62fb8e269084651430305c4e18
Leo <leo.sun@berkeley.edu>
2021-11-22 14:33:12 -0800

    updating django channels

--- src/notification/token_auth.py
+++ src/notification/token_auth.py
@@ -1,3 +1,4 @@
+from channels.middleware import BaseMiddleware
 from django.contrib.auth.models import AnonymousUser
 from django.db import close_old_connections
 
@@ -5,15 +6,38 @@ from channels.auth import AuthMiddlewareStack
 from rest_framework.authtoken.models import Token
 
 
-class TokenAuthMiddleware:
-    """
-    Token authorization middleware for Django Channels 2
-    """
+# class TokenAuthMiddleware:
+#     """
+#     Token authorization middleware for Django Channels 2
+#     """
 
+#     def __init__(self, inner):
+#         self.inner = inner
+
+#     def __call__(self, scope):
+#         close_old_connections()
+#         headers = dict(scope['headers'])
+#         try:
+#             if b'sec-websocket-protocol' in headers:
+#                 token = headers[b'sec-websocket-protocol'].decode().split(', ')
+#                 token_name, token_key = token
+#                 if token_name == 'Token':
+#                     token = Token.objects.get(key=token_key)
+#                     scope['user'] = token.user
+#         except Token.DoesNotExist:
+#             scope['user'] = AnonymousUser()
+#         return self.inner(scope)
+
+
+def TokenAuthMiddlewareStack(inner):
+    return TokenAuthMiddleware(AuthMiddlewareStack(inner))
+
+
+class TokenAuthMiddleware(BaseMiddleware):
     def __init__(self, inner):
-        self.inner = inner
+        super().__init__(inner)
 
-    def __call__(self, scope):
+    async def __call__(self, scope, receive, send):
         close_old_connections()
         headers = dict(scope['headers'])
         try:
@@ -25,8 +49,4 @@ class TokenAuthMiddleware:
                     scope['user'] = token.user
         except Token.DoesNotExist:
             scope['user'] = AnonymousUser()
-        return self.inner(scope)
-
-
-def TokenAuthMiddlewareStack(inner):
-    return TokenAuthMiddleware(AuthMiddlewareStack(inner))
+        return await super().__call__(scope, receive, send)
