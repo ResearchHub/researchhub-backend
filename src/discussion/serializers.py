93200f03528236ae47575cce58d664c2b85011da
Kobe Attias <kobeattias@gmail.com>
2021-12-08 11:18:18 -0500

    [User Activity] Removing mixins

--- src/discussion/serializers.py
+++ src/discussion/serializers.py
@@ -15,94 +15,56 @@ from utils.http import get_user_from_request
 from django.db.models import Q
 
 
-class CommentSerializerMixin:
-    def get_discussion_type(self, obj):
-        return Comment.__name__
+class CensorMixin:
+    def get_plain_text(self, obj):
+        return self.censor_unless_moderator(obj, obj.plain_text)
 
-    def get_unified_document(self, comment):
-        from researchhub_document.serializers import (
-          DynamicUnifiedDocumentSerializer
-        )
-        context = self.context
-        _context_fields = context.get('dis_dcs_get_unified_document', {})
-        serializer = DynamicUnifiedDocumentSerializer(
-            comment.unified_document,
-            context=context,
-            **_context_fields
-        )
-        return serializer.data
+    def get_title(self, obj):
+        return self.censor_unless_moderator(obj, obj.title)
 
-    def _replies_query(self, obj):
-        filter_by_user_id = self.context.get('_config', {}).get('filter_by_user_id', None)
+    def get_text(self, obj):
+        return self.censor_unless_moderator(obj, obj.text)
 
-        replies = obj.children.filter(is_removed=False)
-
-        if filter_by_user_id:
-            replies = replies.filter(created_by_id=filter_by_user_id)
-
-        return self.get_children_annotated(replies).order_by(
-            *self.context.get('ordering', ['-created_date'])
-        )
-
-    def get_replies(self, obj):
-        reply_queryset = self._replies_query(obj)[:PAGINATION_PAGE_SIZE]
-
-        replies = ReplySerializer(
-            reply_queryset,
-            many=True,
-            context={
-                **self.context,
-            },
-        )
-
-        return replies.data
-
-    def get_reply_count(self, obj):
-        replies = self._replies_query(obj)
-        return replies.count()
-
-    def get_thread_id(self, obj):
-        if isinstance(obj.parent, Thread):
-            return obj.parent.id
-        return None
-
-    def get_paper_id(self, obj):
-        if obj.paper:
-            return obj.paper.id
+    def censor_unless_moderator(self, obj, value):
+        if not obj.is_removed or self.requester_is_moderator():
+            return value
         else:
-            return None
+            if type(value) == str:
+                return "[{} has been removed]".format(obj._meta.model_name)
+            else:
+                return None
+
+    def requester_is_moderator(self):
+        request = self.context.get('request')
+        return (
+            request
+            and request.user
+            and request.user.is_authenticated
+            and request.user.moderator
+        )
 
 
-class ThreadSerializerMixin:
+class DynamicThreadSerializer(
+    DynamicModelFieldSerializer,
+    GenericReactionSerializerMixin,
+):
+    comment_count = serializers.SerializerMethodField()
+    created_by = serializers.SerializerMethodField()
+    paper = serializers.SerializerMethodField()
+    post = serializers.SerializerMethodField()
+    unified_document = serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
+    score = serializers.SerializerMethodField()
+    user_vote = serializers.SerializerMethodField()
+    user_flag = serializers.SerializerMethodField()
+    discussion_type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Thread
+        fields = '__all__'
+
     def get_discussion_type(self, obj):
         return Thread.__name__
-
-    def get_user_vote(self, obj):
-        user = get_user_from_request(self.context)
-        if user and not user.is_anonymous:
-            vote = obj.votes.filter(created_by=user)
-            if vote.exists():
-                return VoteSerializer(vote.last()).data
-            return False
-        return False
-
-    def get_paper_slug(self, obj):
-        if obj.paper:
-            return obj.paper.slug
-
-    def get_post_slug(self, obj):
-        if obj.post:
-            return obj.post.slug
-
-    def get_created_by(self, thread):
-        context = self.context
-        _context_fields = context.get('dis_dts_get_created_by', {})
-        serializer = DynamicUserSerializer(
-            thread.created_by,
-            context=context,
-            **_context_fields
-        )
-        return serializer.data
 
     def _comments_query(self, obj):
         filter_by_user_id = self.context.get('_config', {}).get('filter_by_user_id', None)
@@ -122,11 +84,29 @@ class ThreadSerializerMixin:
         comment_serializer = DynamicCommentSerializer(
             comments_queryset,
             many=True,
-            context={
-                **self.context,
-            },
+            context=self.context,
+            **{'_include_fields': CommentSerializer.Meta.fields},
         )
         return comment_serializer.data
+
+    def get_user_vote(self, obj):
+        user = get_user_from_request(self.context)
+        if user and not user.is_anonymous:
+            vote = obj.votes.filter(created_by=user)
+            if vote.exists():
+                return DynamicVoteSerializer(vote.last()).data
+            return False
+        return False
+
+    def get_created_by(self, thread):
+        context = self.context
+        _context_fields = context.get('dis_dts_get_created_by', {})
+        serializer = DynamicUserSerializer(
+            thread.created_by,
+            context=context,
+            **_context_fields
+        )
+        return serializer.data
 
     def get_comment_count(self, obj):
         return self._comments_query(obj).count()
@@ -180,13 +160,30 @@ class ThreadSerializerMixin:
             context=context,
             **_context_fields
         )
-        return serializer.data    
-
-    def get_comment_count(self, obj):
-        return self._comments_query(obj).count()
+        return serializer.data
 
 
-class ReplySerializerMixin:
+class DynamicReplySerializer(
+    DynamicModelFieldSerializer,
+    GenericReactionSerializerMixin,
+):
+    unified_document = serializers.SerializerMethodField()
+    discussion_type = serializers.SerializerMethodField()
+
+    created_by = MinimalUserSerializer(
+        read_only=False,
+        default=serializers.CurrentUserDefault()
+    )
+    parent = serializers.PrimaryKeyRelatedField(
+        queryset=Comment.objects.all(),
+        many=False,
+        read_only=False
+    )
+
+    class Meta:
+        model = Reply
+        fields = '__all__'
+
     def get_discussion_type(self, obj):
         return Reply.__name__
 
@@ -201,19 +198,29 @@ class ReplySerializerMixin:
             context=context,
             **_context_fields
         )
-        return serializer.data    
+        return serializer.data
 
-    def get_paper_id(self, obj):
-        if obj.paper:
-            return obj.paper.id
-        else:
-            return None
 
-    def get_thread_id(self, obj):
-        comment = obj.get_comment_of_reply()
-        if comment and isinstance(comment.parent, Thread):
-            return comment.parent.id
-        return None
+class DynamicCommentSerializer(
+    DynamicModelFieldSerializer,
+    GenericReactionSerializerMixin,
+):
+    created_by = MinimalUserSerializer(
+        read_only=False,
+        default=serializers.CurrentUserDefault()
+    )
+    reply_count = serializers.SerializerMethodField()
+    replies = serializers.SerializerMethodField()
+    thread_id = serializers.SerializerMethodField()
+    paper_id = serializers.SerializerMethodField()
+    discussion_type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+    def get_discussion_type(self, obj):
+        return Comment.__name__
 
     def _replies_query(self, obj):
         filter_by_user_id = self.context.get('_config', {}).get('filter_by_user_id', None)
@@ -233,117 +240,29 @@ class ReplySerializerMixin:
         replies = DynamicReplySerializer(
             reply_queryset,
             many=True,
-            context={
-                **self.context,
-            },
+            context=self.context,
+            **{'_include_fields': ReplySerializer.Meta.fields}
         )
-
         return replies.data
 
     def get_reply_count(self, obj):
         replies = self._replies_query(obj)
         return replies.count()
 
+    def get_thread_id(self, obj):
+        if isinstance(obj.parent, Thread):
+            return obj.parent.id
+        return None
 
-class CensorMixin:
-    def get_plain_text(self, obj):
-        return self.censor_unless_moderator(obj, obj.plain_text)
-
-    def get_title(self, obj):
-        return self.censor_unless_moderator(obj, obj.title)
-
-    def get_text(self, obj):
-        return self.censor_unless_moderator(obj, obj.text)
-
-    def censor_unless_moderator(self, obj, value):
-        if not obj.is_removed or self.requester_is_moderator():
-            return value
+    def get_paper_id(self, obj):
+        if obj.paper:
+            return obj.paper.id
         else:
-            if type(value) == str:
-                return "[{} has been removed]".format(obj._meta.model_name)
-            else:
-                return None
+            return None
 
-    def requester_is_moderator(self):
-        request = self.context.get('request')
-        return (
-            request
-            and request.user
-            and request.user.is_authenticated
-            and request.user.moderator
-        )
-
-
-class DynamicThreadSerializer(
-    DynamicModelFieldSerializer,
-    GenericReactionSerializerMixin,
-    ThreadSerializerMixin
-):
-
-    comment_count = serializers.SerializerMethodField()
-    created_by = serializers.SerializerMethodField()
-    paper = serializers.SerializerMethodField()
-    post = serializers.SerializerMethodField()
-    score = serializers.SerializerMethodField()
-    unified_document = serializers.SerializerMethodField()
-    comments = serializers.SerializerMethodField()
-    comment_count = serializers.SerializerMethodField()
-    score = serializers.SerializerMethodField()
-    user_vote = serializers.SerializerMethodField()
-    user_flag = serializers.SerializerMethodField()
-    comments = serializers.SerializerMethodField()
-    paper_slug = serializers.SerializerMethodField()
-    post_slug = serializers.SerializerMethodField()
-    promoted = serializers.SerializerMethodField()
-    document_meta = serializers.SerializerMethodField()
-    discussion_type = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Thread
-        fields = '__all__'
-
-
-
-class DynamicReplySerializer(
-    DynamicModelFieldSerializer,
-    GenericReactionSerializerMixin,
-    ReplySerializerMixin
-):
-    unified_document = serializers.SerializerMethodField()
-    discussion_type = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Reply
-        fields = '__all__'
-
-
-class DynamicCommentSerializer(
-    DynamicModelFieldSerializer,
-    GenericReactionSerializerMixin,
-    CommentSerializerMixin,
-):
-    created_by = MinimalUserSerializer(
-        read_only=False,
-        default=serializers.CurrentUserDefault()
-    )
-    reply_count = serializers.SerializerMethodField()
-    replies = serializers.SerializerMethodField()
-    score = serializers.SerializerMethodField()
-    user_vote = serializers.SerializerMethodField()
-    user_flag = serializers.SerializerMethodField()
-    thread_id = serializers.SerializerMethodField()
-    paper_id = serializers.SerializerMethodField()
-    promoted = serializers.SerializerMethodField()
-    document_meta = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Comment
-        fields = '__all__'
 
 class CommentSerializer(
-    serializers.ModelSerializer,
-    GenericReactionSerializerMixin,
-    CommentSerializerMixin,
+    serializers.ModelSerializer, GenericReactionSerializerMixin
 ):
     created_by = MinimalUserSerializer(
         read_only=False,
@@ -397,11 +316,45 @@ class CommentSerializer(
         ]
         model = Comment
 
+    def _replies_query(self, obj):
+        return self.get_children_annotated(obj).order_by(
+            *self.context.get('ordering', ['-created_date'])
+        )
+
+    def get_replies(self, obj):
+        if self.context.get('depth', 3) <= 0:
+            return []
+        reply_queryset = self._replies_query(obj)[:PAGINATION_PAGE_SIZE]
+
+        replies = ReplySerializer(
+            reply_queryset,
+            many=True,
+            context={
+                **self.context,
+                'depth': self.context.get('depth', 3) - 1,
+            },
+        )
+
+        return replies.data
+
+    def get_reply_count(self, obj):
+        replies = self._replies_query(obj)
+        return replies.count()
+
+    def get_thread_id(self, obj):
+        if isinstance(obj.parent, Thread):
+            return obj.parent.id
+        return None
+
+    def get_paper_id(self, obj):
+        if obj.paper:
+            return obj.paper.id
+        else:
+            return None
+
 
 class ThreadSerializer(
-    serializers.ModelSerializer,
-    GenericReactionSerializerMixin,
-    ThreadSerializerMixin,
+    serializers.ModelSerializer, GenericReactionSerializerMixin
 ):
     created_by = MinimalUserSerializer(
         read_only=False,
@@ -458,6 +411,47 @@ class ThreadSerializer(
         ]
         model = Thread
 
+    def get_score(self, obj):
+        return obj.calculate_score()
+
+    def get_user_vote(self, obj):
+        user = get_user_from_request(self.context)
+        if user and not user.is_anonymous:
+            vote = obj.votes.filter(created_by=user)
+            if vote.exists():
+                return VoteSerializer(vote.last()).data
+            return False
+        return False
+
+    def _comments_query(self, obj):
+        return self.get_children_annotated(obj).order_by(
+            *self.context.get('ordering', ['id'])
+        )
+
+    def get_comments(self, obj):
+        if self.context.get('depth', 3) <= 0:
+            return []
+        comments_queryset = self._comments_query(obj)[:PAGINATION_PAGE_SIZE]
+        comment_serializer = CommentSerializer(
+            comments_queryset,
+            many=True,
+            context={
+                **self.context,
+                'depth': self.context.get('depth', 3) - 1,
+            },
+        )
+        return comment_serializer.data
+
+    def get_comment_count(self, obj):
+        return self._comments_query(obj).count()
+
+    def get_paper_slug(self, obj):
+        if obj.paper:
+            return obj.paper.slug
+
+    def get_post_slug(self, obj):
+        if obj.post:
+            return obj.post.slug
 
 
 class SimpleThreadSerializer(ThreadSerializer):
@@ -470,8 +464,7 @@ class SimpleThreadSerializer(ThreadSerializer):
 
 class ReplySerializer(
     serializers.ModelSerializer,
-    GenericReactionSerializerMixin,
-    ReplySerializerMixin
+    GenericReactionSerializerMixin
 ):
     created_by = MinimalUserSerializer(
         read_only=False,
@@ -529,3 +522,40 @@ class ReplySerializer(
         ]
         model = Reply
 
+    def get_paper_id(self, obj):
+        if obj.paper:
+            return obj.paper.id
+        else:
+            return None
+
+    def get_thread_id(self, obj):
+        comment = obj.get_comment_of_reply()
+        if comment and isinstance(comment.parent, Thread):
+            return comment.parent.id
+        return None
+
+    def _replies_query(self, obj):
+        return self.get_children_annotated(obj).order_by(*self.context.get(
+            'ordering',
+            ['-created_date'])
+        )
+
+    def get_replies(self, obj):
+        if self.context.get('depth', 3) <= 0:
+            return []
+        reply_queryset = self._replies_query(obj)[:PAGINATION_PAGE_SIZE]
+
+        replies = ReplySerializer(
+            reply_queryset,
+            many=True,
+            context={
+                **self.context,
+                'depth': self.context.get('depth', 3) - 1,
+            },
+        )
+
+        return replies.data
+
+    def get_reply_count(self, obj):
+        replies = self._replies_query(obj)
+        return replies.count()
