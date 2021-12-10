db47896a805cad9f2a8bc760c7f19bf762bcc631
Leo <leo.sun@berkeley.edu>
2021-12-10 15:18:40 -0800

    testing

--- src/reputation/admin.py
+++ src/reputation/admin.py
@@ -22,33 +22,107 @@ class RewardAdminModel(admin.ModelAdmin):
     def get_queryset(self, request):
         return Distribution.objects.all() 
 
-    def render_change_form(self, request, context, *args, **kwargs):
-        context.update(
-            {
-                'title': 'Grant RSC',
-                'show_save_and_add_another': False,
-                'show_save_and_continue': False,
-                'has_delete_permission': False,
-                'has_change_permission': False,
-            }
+    # def render_change_form(self, request, context, *args, **kwargs):
+    #     context.update(
+    #         {
+    #             'title': 'Grant RSC',
+    #             'show_save_and_add_another': False,
+    #             'show_save_and_continue': False,
+    #             'has_delete_permission': False,
+    #             'has_change_permission': False,
+    #         }
+    #     )
+    #     return super().render_change_form(request, context, *args, **kwargs)
+
+    # def save_model(self, request, *args, **kwargs):
+    #     data = request._post
+    #     user_id = data.get('user_id')
+    #     amount = int(data.get('amount', 0))
+    #     give_rep = data.get('give_rep', 'off') == 'on'
+
+    #     user = User.objects.get(id=user_id)
+    #     distribution = Dist('REWARD', amount, give_rep=give_rep)
+    #     distributor = Distributor(
+    #                 distribution,
+    #                 user,
+    #                 user,
+    #                 time.time()
+    #             )
+    #     distributor.distribute()
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        images = ''
+        obj = self.model.objects.get(id=object_id)
+        user = obj.user
+        referrer = user.invited_by
+        verifications = self.model.objects.filter(user=user)
+
+        for i, verification in enumerate(verifications.iterator()):
+            if verification.file:
+                url = verification.file.url
+                image_html = f"""
+                    <div style="display:inline-grid;padding:10px">
+                        <span>
+                            <a href="{url}" target="_blank">
+                                <img src="{url}" style="max-width:300px;">
+                            </a>
+                        </span>
+                        <label for="id_file">Image {i + 1}</label>
+                    </div>
+                """
+                images += image_html
+
+        is_verified = user.author_profile.academic_verification
+        if is_verified:
+            verified = """<img src="/static/admin/img/icon-yes.svg">"""
+        elif is_verified is False:
+            verified = """<img src="/static/admin/img/icon-no.svg">"""
+        else:
+            verified = """<img src="/static/admin/img/icon-unknown.svg">"""
+
+        user_html = f"""
+            <p style="font-weight:bold;">
+                {user.id}: {user.email} / {user.first_name} {user.last_name}
+            </p>
+        """
+
+        if referrer:
+            referred_by = f"""
+                <p style="font-weight:bold;">
+                    {referrer.id}: {referrer.email} / {referrer.first_name} {referrer.last_name}
+                </p>
+            """
+        else:
+            referred_by = """<p> No Referrer </p>"""
+
+        extra_context = extra_context or {}
+        extra_context['images'] = images
+        extra_context['verified'] = verified
+        extra_context['user'] = user_html
+        extra_context['referred_by'] = referred_by
+
+        return super(RewardAdminModel, self).change_view(
+            request,
+            object_id,
+            form_url,
+            extra_context=extra_context,
         )
-        return super().render_change_form(request, context, *args, **kwargs)
 
-    def save_model(self, request, *args, **kwargs):
-        data = request._post
-        user_id = data.get('user_id')
-        amount = int(data.get('amount', 0))
-        give_rep = data.get('give_rep', 'off') == 'on'
-
-        user = User.objects.get(id=user_id)
-        distribution = Dist('REWARD', amount, give_rep=give_rep)
-        distributor = Distributor(
-                    distribution,
-                    user,
-                    user,
-                    time.time()
-                )
-        distributor.distribute()
+    def response_change(self, request, obj):
+        user = obj.user
+        if '_grant' in request.POST:
+            author_profile = user.author_profile
+            author_profile.academic_verification = True
+            author_profile.save()
+            user_distribution_record = self.distribute_referral_reward(user)
+            self.send_academic_verification_email(user, user_distribution_record)
+            return redirect('.')
+        elif '_reject' in request.POST:
+            author_profile = user.author_profile
+            author_profile.academic_verification = False
+            author_profile.save()
+            return redirect('.')
+        return super().response_change(request, obj)
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -65,5 +139,5 @@ class DistributionAdmin(admin.ModelAdmin):
     actions = [make_published]
 
 
-# admin.site.register(RewardModel, RewardAdminModel)
-admin.site.register(Distribution, DistributionAdmin)
+admin.site.register(RewardModel, RewardAdminModel)
+# admin.site.register(Distribution, DistributionAdmin)
