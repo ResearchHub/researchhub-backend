40521c7a00db6adcb0669024dcd3265eea9548cb
Leo <leo.sun@berkeley.edu>
2021-12-10 12:24:59 -0800

    adding java for cermine

--- src/.ebextensions/supervisor-setup.sh
+++ src/.ebextensions/supervisor-setup.sh
@@ -9,6 +9,13 @@ else
     echo "supervisor already installed"
 fi
 
+if [ ! -f /usr/bin/java ]; then
+    echo "installing java"
+    sudo yum install -y java-1.8.0
+else
+    echo "java already installed"
+fi
+
 if [ ! -d /etc/supervisor ]; then
     mkdir /etc/supervisor
     echo "create supervisor directory"
