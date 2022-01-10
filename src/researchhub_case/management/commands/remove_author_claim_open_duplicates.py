010c064abf8599e2925c93ba72cedeb4dc809559
Calvin Lee <calvinhlee23@gmail.com>
2022-01-10 12:56:43 -0800

    update script

--- src/researchhub_case/management/commands/remove_author_claim_open_duplicates.py
+++ src/researchhub_case/management/commands/remove_author_claim_open_duplicates.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 
 from user.models import User
 from researchhub_case.models import AuthorClaimCase
@@ -10,17 +11,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         case_creators = User.objects.filter(
           created_cases__isnull=False,
-          case_type=AUTHOR_CLAIM
+          created_cases__case_type=AUTHOR_CLAIM
         )
 
         try:
             for case_creator in case_creators.iterator():
                 created_cases = case_creator.created_cases.filter(status=OPEN)
                 for target_case in created_cases.iterator():
-                    AuthorClaimCase.objects.filter(
-                      id__isnot=target_case.id,
-                      status=OPEN,
-                      target_author=target_case.target_author,
-                    ).delete()
+                    print("Deleting duplicates for id: ", target_case.id)
+                    try:
+                        AuthorClaimCase.objects.filter(
+                          ~Q(id=target_case.id),
+                          status=OPEN,
+                          target_author=target_case.target_author,
+                        ).delete()
+                    except Exception as error:
+                        print(error)
+                        pass
         except Exception as error:
             print(error)
+            pass
+
+        print("Script remove_author_claim_open_duplicates finished")
