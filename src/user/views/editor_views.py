3763779cf9f548fd38a1d054f32a6d6a3848b3ea
Calvin Lee <calvinhlee23@gmail.com>
2022-01-12 02:48:24 -0800

    query tested

--- src/user/views/editor_views.py
+++ src/user/views/editor_views.py
@@ -16,13 +16,15 @@ from user.related_models.user_model import User
 
 
 def resolve_timeframe_for_contribution(timeframe_str):
-    keyword = 'reputation_records__created_date__gte'
+    keyword = 'created_date__gte'
     if timeframe_str == 'today':
         return {keyword: timezone.now().date()}
     elif timeframe_str == 'past_week':
         return {keyword: timezone.now().date() - timedelta(days=7)}
     elif timeframe_str == 'past_month':
         return {keyword: timezone.now().date() - timedelta(days=30)}
+    else:
+        return {}
 
 
 @api_view(http_method_names=[GET])
@@ -36,38 +38,68 @@ def get_editors_by_contributions(request):
         ).distinct()
 
         timeframe_str = request.GET.get('timeframe_str', None)
-        resolved_timeframe = resolve_timeframe_for_contribution(timeframe_str)
-        contribution_filter = Q(
-            **resolved_timeframe,
-            type__in=[
-                Contribution.SUBMITTER,
-                Contribution.COMMENTER,
-                Contribution.SUPPORTER,
-            ]
+        timeframe_query = Q(
+          **resolve_timeframe_for_contribution(timeframe_str),
         )
+
+        total_contrib_count_query = Q(
+            contributions__contribution_type__in=[
+              Contribution.COMMENTER,
+              Contribution.SUBMITTER,
+              Contribution.SUPPORTER,
+            ],
+        ) & timeframe_query
+
+        comment_count_query = Q(
+            contributions__contribution_type=Contribution.COMMENTER
+        ) & timeframe_query
+
+        submission_count_query = Q(
+            contributions__contribution_type=Contribution.SUBMITTER
+        ) & timeframe_query
+
+        support_count_query = Q(
+            contributions__contribution_type=Contribution.SUPPORTER
+        ) & timeframe_query
 
         hub_id = request.GET.get('hub_id', None)
         if (hub_id is not None):
-            editor_qs = editor_qs.filter(permissions__object_id=hub_id)
-            contribution_filter = contribution_filter & \
-                Q(unified_document__hubs__in=[hub_id])
+            contribution_hub_query = Q(
+                contributions__unified_document__hubs__in=[hub_id]
+            )
+
+            editor_qs = editor_qs.filter(
+              permissions__object_id=hub_id
+            )
+            total_contrib_count_query = \
+                total_contrib_count_query & contribution_hub_query
+            comment_count_query = \
+                comment_count_query & contribution_hub_query
+            submission_count_query = \
+                submission_count_query & contribution_hub_query
+            support_count_query = \
+                support_count_query & contribution_hub_query
+
+        editor_qs = editor_qs.prefetch_related(
+          'contributions',
+          'contributions__unified_document__hubs'
+        )
 
         editor_qs_ranked_by_contribution = \
             editor_qs.annotate(
-                contributions=Count(
-                    'id', contribution_filter
+                total_contribution_count=Count(
+                    'id', filter=total_contrib_count_query
                 ),
                 comment_count=Count(
-                    'id', Q(type__in=[Contribution.COMMENTER])
+                    'id', filter=comment_count_query
                 ),
                 submission_count=Count(
-                    'id', Q(type__in=[Contribution.COMMENTER])
+                    'id', filter=submission_count_query
                 ),
                 support_count=Count(
-                    'id', Q(type__in=[Contribution.COMMENTER])
+                    'id', filter=support_count_query
                 ),
-            ).order_by('-contribution_count')
-        import pdb; pdb.set_trace()
-        return {}
+            ).order_by('-total_contribution_count')
+        return Response({}, status=200)
     except Exception as error:
         return Response(error, status=400)
