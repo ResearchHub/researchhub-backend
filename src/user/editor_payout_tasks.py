e0ad30de4febc6e35d4154e507c102186f74132a
Calvin Lee <calvinhlee23@gmail.com>
2022-01-26 02:10:52 -0800

    script tested

--- src/user/editor_payout_tasks.py
+++ src/user/editor_payout_tasks.py
@@ -10,6 +10,7 @@ RSC_TOKEN_ID = "0xd101dcc414f310268c37eeb4cd376ccfa507f571"
 UNI_SWAP_BUNDLE_ID = 1
 UNI_SWAP_GRAPH_URI = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2"
 USD_PAY_AMOUNT_PER_MONTH = 3000
+USD_PER_RSC_PLACEHOLDER = .0001
 
 
 @apps.task
@@ -29,10 +30,13 @@ def editor_daily_payout_task():
         UNI_SWAP_GRAPH_URI,
         json={'query': query}
     )
-    payload = json.loads(request_result.text)
-    eth_per_rsc = payload['rsc']['derivedETH']
-    usd_per_eth = payload['bundle']['ethPrice']
-    rsc_per_usd = math.pow((usd_per_eth * eth_per_rsc), -1)
+    payload = json.loads(request_result.text).data
+    eth_per_rsc = float(payload['rsc']['derivedETH'])
+    usd_per_eth = float(payload['bundle']['ethPrice'])
+    rsc_per_usd = math.pow(
+        (usd_per_eth * eth_per_rsc) or USD_PER_RSC_PLACEHOLDER,
+        -1
+    )
     rsc_pay_amount = (
       (USD_PAY_AMOUNT_PER_MONTH * rsc_per_usd) / num_days_this_month
     )
