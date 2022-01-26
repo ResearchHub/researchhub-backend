0cd5f208b2b09bb5395459e937e9c64333eab316
Calvin Lee <calvinhlee23@gmail.com>
2022-01-26 02:19:46 -0800

    more formatting

--- src/user/editor_payout_tasks.py
+++ src/user/editor_payout_tasks.py
@@ -15,6 +15,11 @@ USD_PER_RSC_PLACEHOLDER = .0001
 
 @apps.task
 def editor_daily_payout_task():
+    payout_amount = get_daily_rsc_payout_amount()
+    # iterate through editors & create distribution
+
+
+def get_daily_rsc_payout_amount():
     today = datetime.date.today()
     num_days_this_month = monthrange(today.year, today.month)[1]
 
@@ -26,17 +31,20 @@ def editor_daily_payout_task():
           ethPrice
         }
     }""" % (RSC_TOKEN_ID, UNI_SWAP_BUNDLE_ID)
+    
     request_result = requests.post(
         UNI_SWAP_GRAPH_URI,
         json={'query': uni_swap_query}
     )
     payload = json.loads(request_result.text).data
+
     eth_per_rsc = float(payload['rsc']['derivedETH'])
     usd_per_eth = float(payload['bundle']['ethPrice'])
     rsc_per_usd = math.pow(
         (usd_per_eth * eth_per_rsc) or USD_PER_RSC_PLACEHOLDER,
         -1
     )
-    rsc_pay_amount = (
+
+    return (
       (USD_PAY_AMOUNT_PER_MONTH * rsc_per_usd) / num_days_this_month
     )
