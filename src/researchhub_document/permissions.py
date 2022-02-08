d03e88acd81d8e2861b1f1a55df00ed73a03ea3a
Calvin Lee <calvinhlee23@gmail.com>
2022-02-07 21:09:12 -0800

    updating permissions

--- src/researchhub_document/permissions.py
+++ src/researchhub_document/permissions.py
@@ -1,13 +1,19 @@
-from utils.permissions import AuthorizationBasedPermission
-from utils.http import PATCH, POST, PUT, GET, DELETE
-from researchhub_document.related_models.constants.document_type import (
-    HYPOTHESIS,
-    DISCUSSION,
-)
+from django.contrib.contenttypes.models import ContentType
+
+from hub.models import Hub
 from hypothesis.related_models.hypothesis import Hypothesis
+from paper.models import Paper
+from researchhub_access_group.constants import EDITOR
+from researchhub_document.related_models.constants.document_type import (
+    DISCUSSION,
+    HYPOTHESIS,
+    PAPER,
+)
 from researchhub_document.models import (
     ResearchhubPost
 )
+from utils.permissions import AuthorizationBasedPermission
+
 
 class HasDocumentCensorPermission(AuthorizationBasedPermission):
     message = 'Need to be author or moderator to delete'
@@ -16,14 +22,28 @@ class HasDocumentCensorPermission(AuthorizationBasedPermission):
         if request.user.is_authenticated is False:
             return False
 
-        model = None
-        if obj.document_type == DISCUSSION:
-            model = ResearchhubPost
-        elif obj.document_type == HYPOTHESIS:
-            model = Hypothesis
+        document_type = obj.document_type
+        doc = None
+        if document_type == DISCUSSION:
+            doc = ResearchhubPost.objects.get(unified_document_id=obj.id)
+        elif document_type == HYPOTHESIS:
+            doc = Hypothesis.objects.get(unified_document_id=obj.id)
+        elif document_type == PAPER:
+            doc = Paper.objects.get(id=obj.id)
 
-        doc = model.objects.get(unified_document_id=obj.id)
-        if doc.created_by_id == request.user.id or request.user.moderator:
+        if (doc is None):
+            return False
+
+        requestor = request.user
+        is_requestor_appropriate_editor = is_hub_editor(
+            requestor,
+            doc.hubs,
+        )
+        if (
+            requestor.user.moderator or  # moderators serve as site admin
+            is_requestor_appropriate_editor or
+            doc.created_by_id == requestor.id
+        ):
             return True
 
         return False
@@ -49,3 +69,12 @@ class HasDocumentEditingPermission(AuthorizationBasedPermission):
                     return False
 
         return True
+
+
+def is_hub_editor(requestor_user, hubs):
+    hub_content_type = ContentType.objects.get_for_model(Hub)
+    return requestor_user.permissions.filter(
+        access_type=EDITOR,
+        content_type=hub_content_type,
+        object_id__in=hubs.values_list('id', flat=True),
+    ).exists()
