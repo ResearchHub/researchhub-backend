0b1ffa45c6d9525711a4fc1fe3b4cc8d99ac6080
Leo <leo.sun@berkeley.edu>
2022-02-17 17:24:01 -0800

    updating redbeat settings

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -658,7 +658,10 @@ CELERY_TASK_ROUTES = {
 }
 CELERY_TASK_DEFAULT_QUEUE = f'{APP_ENV}_core_queue'
 
-REDBEAT_REDIS_URL = 'redis://{}:{}/0'.format(REDIS_HOST, REDIS_PORT)
+if STAGING:
+    REDBEAT_REDIS_URL = f'redis://127.0.0.1:{REDIS_PORT}/0'
+else:
+    REDBEAT_REDIS_URL = 'redis://{}:{}/0'.format(REDIS_HOST, REDIS_PORT)
 
 # Django Channels
 ASGI_APPLICATION = 'researchhub.routing.application'
