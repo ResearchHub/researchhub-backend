dc182384924b2b1bcc9cc56104ed7bea15e01179
Kobe Attias <kobeattias@gmail.com>
2022-03-09 16:46:29 -0500

    Update src/paper/views.py
    Co-authored-by: Leo Sun <leo.sun@berkeley.edu>

--- src/paper/views.py
+++ src/paper/views.py
@@ -406,7 +406,7 @@ class PaperViewSet(viewsets.ModelViewSet):
         paper.save()
         paper.reset_cache(use_celery=False)
 
-        hub_ids = list(map(lambda hub: hub.id, paper.hubs.all()))
+        hub_ids = paper.hubs.values_list('id', flat=True)
         invalidate_feed_cache(
             hub_ids,
             filters=[NEWEST, TOP, TRENDING, DISCUSSED],
