73a3036e88f68349ad208ab4436911a30cc80f00
Leo <leo.sun@berkeley.edu>
2022-03-25 17:56:39 -0700

    testing webscraping

--- src/paper/manubot.py
+++ src/paper/manubot.py
@@ -76,6 +76,108 @@ class RHCiteKey(CiteKey):
         return csl_item
 
 
+# TESTING ------------------------------------------------------------------------------------------------------------------------------
+
+import time
+from collections import Counter, OrderedDict
+from urllib.parse import urlparse
+
+import regex as re
+import requests
+from bs4 import BeautifulSoup as soup
+from requests import Session
+
+
+def start():
+    urls = (
+        Paper.objects.filter(doi__isnull=False, url__isnull=False)
+        .exclude(url="")
+        .values_list("url", flat=True)[:100]
+    )
+    results = {}
+    domains = set()
+    try:
+        for i, url in enumerate(urls.iterator()):
+            print(i)
+            o = urlparse(url)
+            hostname = o.hostname
+
+            if hostname not in domains:
+                domains.add(hostname)
+                print(f"{i}: {url}")
+            res = get_doi(url)
+            results[url] = res
+            time.sleep(1)
+        return results
+    except Exception as e:
+        print(e)
+        return results
+
+
+def regex_doi(string):
+    regex = re.compile(r"https:\/\/doi.org\/10.\d{4,9}\/[-._;()\/:a-zA-Z0-9]+")
+    res = re.findall(regex, string)
+    if res:
+        return res[0]
+    return "Could not find doi"
+
+
+def get_doi(url, timeout=5):
+    """
+    get_doi('https://onlinelibrary.wiley.com/doi/abs/10.1111/1467-9817.12313')
+    """
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0'
+    # }
+    try:
+        s = Session()
+        headers = OrderedDict(
+            {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "Accept-Encoding": "gzip, deflate",
+                "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+                "dnt": "1",
+                "Upgrade-Insecure-Requests": "1",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+            }
+        )
+        # headers = OrderedDict({
+        #     'Accept-Encoding': 'gzip, deflate, br',
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'
+        # })
+        s.headers = headers
+        res = s.get(url, headers=headers)
+        # res = requests.get(
+        #     url,
+        #     headers=headers,
+        #     timeout=timeout,
+        #     allow_redirects=True
+        # )
+        status_code = res.status_code
+        if status_code >= 200 and status_code < 400:
+            content = soup(res.content, "lxml")
+            dois = content.find_all(
+                string=re.compile(
+                    r"https:\/\/doi.org\/10.\d{4,9}\/[-._;()\/:a-zA-Z0-9]+"
+                )
+            )
+            dois = list(map(str.strip, dois))
+            dois = list(map(regex_doi, dois))
+            doi_counter = Counter(dois)
+            most_common_doi = doi_counter.most_common(1)
+
+            if most_common_doi:
+                doi = most_common_doi[0][0]
+            else:
+                doi = None
+            return doi
+        else:
+            return "Timeout"
+    except Exception as e:
+        print(e)
+        return e
+
+
 """
 from manubot.cite.citekey import CiteKey, citekey_to_csl_item, url_to_citekey
 from paper.manubot import RHCiteKey
