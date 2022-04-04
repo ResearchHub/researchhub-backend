e964be77ed29ed5f3fb1145413f6f70656fa892d
Kobe Attias <kobeattias@gmail.com>
2022-04-04 15:16:09 -0400

    removing unnecessary

--- src/peer_review/views/peer_review_view.py
+++ src/peer_review/views/peer_review_view.py
@@ -8,12 +8,3 @@ class PeerReviewViewSet(ModelViewSet):
     permission_classes = [
         IsAuthenticated,
     ]
-
-    def create_review_decision(self):
-        print('to implement')
-
-    def list(self, request, *args, **kwargs):
-        print('to implement')
-
-    def retrieve(self, request, *args, **kwargs):
-        print('to implement')
