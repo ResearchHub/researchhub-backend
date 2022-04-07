332c850232cd1597c198e1ef06fdd62348c572dd
Kobe Attias <kobeattias@gmail.com>
2022-04-07 11:35:07 -0400

    changing default queue, removing default routing

--- src/start-celery.sh
+++ src/start-celery.sh
@@ -1,1 +1,1 @@
-celery -A researchhub worker -Q default_queue,caches,hot_score,elastic_search,external_reporting,notifications,paper_misc,cermine,twitter,pull_papers,logs,purchases,contributions,author_claim -l info --concurrency=1 --prefetch-multiplier=1 -P prefork -n core
+celery -A researchhub worker -B -Q default,caches,hot_score,elastic_search,external_reporting,notifications,paper_misc,cermine,twitter,pull_papers,logs,purchases,contributions,author_claim -l info --concurrency=1 --prefetch-multiplier=1 -P prefork
