127e0fdaeb12603cd054c0cf62a52c3e8e8a8dd4
Kobe Attias <kobeattias@gmail.com>
2022-04-11 10:13:24 -0400

    #1026 Refactoring tests into different test files

---
+++ src/peer_review/tests/__init__.py
@@ -0,0 +1,3 @@
+from .test_peer_review_invite_view import PeerReviewInviteViewTests
+from .test_peer_review_view import PeerReviewViewTests
+from .test_peer_review_request_view import PeerReviewRequestViewTests
