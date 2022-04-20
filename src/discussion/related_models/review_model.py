8ee3284298c1540f370ad127f3c9de71921ac954
Kobe Attias <kobeattias@gmail.com>
2022-04-20 10:42:16 -0400

    #1062 Min, Max, Default values for score

--- src/discussion/related_models/review_model.py
+++ src/discussion/related_models/review_model.py
@@ -1,9 +1,15 @@
 from django.db import models
 from utils.models import DefaultModel
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Review(DefaultModel):
     score = models.FloatField(
-        default=None,
         null=True,
-        blank=False
+        blank=False,
+        default=1,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
     )
