d4318a5805221b7e4371400fe25e6d5d9cf73346
Kobe Attias <kobeattias@gmail.com>
2022-04-29 09:11:59 -0400

    tweaking hot score * => +

--- src/researchhub_document/hot_score_mixin.py
+++ src/researchhub_document/hot_score_mixin.py
@@ -93,7 +93,7 @@ class HotScoreMixin:
         time_score = self._get_time_score(self.created_date)
         time_score_with_magnitude = self._c(doc_vote_net_score + social_media_score) * time_score
         doc_vote_score = math.log(abs(doc_vote_net_score) + 1, 2)
-        discussion_vote_score = math.log(doc.discussion_count + 1, 2) * math.log(max(0, total_comment_vote_score) + 1, 3)
+        discussion_vote_score = math.log(doc.discussion_count + 1, 2) + math.log(max(0, total_comment_vote_score) + 1, 3)
 
         # If basic criteria needed to show in trending is not available,
         # penalize the score by subtracting time. This will result in the
