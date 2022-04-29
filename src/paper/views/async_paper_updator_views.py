2854c441899db5531601a45ca5dbfcbc6873530f
Calvin Lee <calvinhlee23@gmail.com>
2022-04-29 08:33:44 -0700

    formatting

--- src/paper/views/async_paper_updator_views.py
+++ src/paper/views/async_paper_updator_views.py
@@ -2,12 +2,14 @@ from rest_framework.viewsets import ModelViewSet
 
 from paper.models import AsyncPaperUpdator
 from paper.permissions import IsAllowedToUpdateAsyncPaper
+from paper.serializers.async_paper_updator_serializer import AsyncPaperUpdatorSerializer
 
 
 class AsyncPaperUpdatorViewSet(ModelViewSet):
     http_method_names = ["post"]
-    permission_classes = IsAllowedToUpdateAsyncPaper
+    permission_classes = [IsAllowedToUpdateAsyncPaper]
     queryset = AsyncPaperUpdator.objects.filter()
+    serializer_class = AsyncPaperUpdatorSerializer
 
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
