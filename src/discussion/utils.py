958278a387d17910ad8cd1131c5a5eb904ba17d7
Calvin Lee <calvinhlee23@gmail.com>
2022-05-09 14:38:44 -0700

    rm pdb & formatting

--- src/discussion/utils.py
+++ src/discussion/utils.py
@@ -1,22 +1,31 @@
+from django.db.models import Count, Q
+
+from discussion.reaction_models import Vote
+
+ORDERING_SCORE_ANNOTATION = Count("id", filter=Q(votes__vote_type=Vote.UPVOTE)) - Count(
+    "id", filter=Q(votes__vote_type=Vote.DOWNVOTE)
+)
+
+
 def get_thread_id_from_path(request):
     DISCUSSION = 4
     thread_id = None
-    path_parts = request.path.split('/')
-    if path_parts[DISCUSSION] == 'discussion':
+    path_parts = request.path.split("/")
+    if path_parts[DISCUSSION] == "discussion":
         try:
             thread_id = int(path_parts[DISCUSSION + 1])
         except ValueError:
-            print('Failed to get discussion id')
+            print("Failed to get discussion id")
     return thread_id
 
 
 def get_comment_id_from_path(request):
     COMMENT = 6
     comment_id = None
-    path_parts = request.path.split('/')
-    if path_parts[COMMENT] == 'comment':
+    path_parts = request.path.split("/")
+    if path_parts[COMMENT] == "comment":
         try:
             comment_id = int(path_parts[COMMENT + 1])
         except ValueError:
-            print('Failed to get comment id')
+            print("Failed to get comment id")
     return comment_id
