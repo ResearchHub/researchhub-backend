c689a956a4835fcaa6940dcb00c11c8b0a526bc5
Patrick Lu <lightning.lu7@gmail.com>
2022-05-25 12:44:23 -0400

    fix reputation amount else case

--- src/reputation/management/commands/reputation_backfill.py
+++ src/reputation/management/commands/reputation_backfill.py
@@ -30,27 +30,27 @@ class Command(BaseCommand):
                 if record.giver and record.giver.reputation < 110:
                     total_changed_records += 1
                     record.amount = 0
+                else:
+                    gives_rep = {
+                        "PAPER_UPVOTED": 1,
+                        "THREAD_UPVOTED": 1,
+                        "RESEARCHHUB_POST_UPVOTED": 1,
+                        "REPLY_UPVOTED": 1,
+                        "COMMENT_UPVOTED": 1,
+                    }
 
-                gives_rep = {
-                    "PAPER_UPVOTED": 1,
-                    "THREAD_UPVOTED": 1,
-                    "RESEARCHHUB_POST_UPVOTED": 1,
-                    "REPLY_UPVOTED": 1,
-                    "COMMENT_UPVOTED": 1,
-                }
-
-                removes_rep = {
-                    "REPLY_DOWNVOTED": -1,
-                    "COMMENT_DOWNVOTED": -1,
-                    "THREAD_DOWNVOTED": -1,
-                    "REPLY_CENSORED": -2,
-                    "COMMENT_CENSORED": -2,
-                    "THREAD_CENSORED": -2,
-                }
-                record.reputation_amount = gives_rep.get(
-                    record.distribution_type, 0
-                ) + removes_rep.get(record.distribution_type, 0)
-                record.save()
+                    removes_rep = {
+                        "REPLY_DOWNVOTED": -1,
+                        "COMMENT_DOWNVOTED": -1,
+                        "THREAD_DOWNVOTED": -1,
+                        "REPLY_CENSORED": -2,
+                        "COMMENT_CENSORED": -2,
+                        "THREAD_CENSORED": -2,
+                    }
+                    record.reputation_amount = gives_rep.get(
+                        record.distribution_type, 0
+                    ) + removes_rep.get(record.distribution_type, 0)
+                    record.save()
 
             rep_sum = rep.aggregate(rep=Sum("reputation_amount"))
             rep = rep_sum.get("rep") or 0
