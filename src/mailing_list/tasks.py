651978747dda783e013a54f59807957a4219d123
Calvin Lee <calvinhlee23@gmail.com>
2022-06-16 16:29:00 -0700

    rm print

--- src/mailing_list/tasks.py
+++ src/mailing_list/tasks.py
@@ -184,7 +184,7 @@ def send_hub_digest(frequency):
 
     # TODO find best by hub and then in mem sort for each user? more efficient?
     emails = []
-    print("users: ", users)
+
     for user in User.objects.filter(id__in=users, is_suspended=False):
         if not check_can_receive_digest(user, frequency):
             continue
