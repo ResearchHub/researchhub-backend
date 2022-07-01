c2813b5289924db7134063f6fbbb1805a5657abc
Leo <leo.sun@berkeley.edu>
2022-07-01 12:03:45 -0700

    adding is hub editor to user field for amplitude

--- src/analytics/amplitude.py
+++ src/analytics/amplitude.py
@@ -1,9 +1,8 @@
-import requests
 import json
 
-from ipware import get_client_ip
-
+import requests
 from django.contrib.gis.geoip2 import GeoIP2
+from ipware import get_client_ip
 
 from researchhub.settings import AMPLITUDE_API_KEY
 from user.models import User
@@ -14,16 +13,16 @@ geo = GeoIP2()
 
 class Amplitude:
     api_key = AMPLITUDE_API_KEY
-    api_url = 'https://api.amplitude.com/2/httpapi'
+    api_url = "https://api.amplitude.com/2/httpapi"
 
     def build_hit(self, request, data):
         hit = {}
         event_data = data.copy()
 
-        user_id = data.get('user_id', request.user.id)
+        user_id = data.get("user_id", request.user.id)
         ip, is_routable = get_client_ip(request)
 
-        hit['api_key'] = self.api_key
+        hit["api_key"] = self.api_key
 
         if user_id:
             user = User.objects.get(id=user_id)
@@ -36,57 +35,51 @@ class Amplitude:
                 invited_by_id = None
 
             user_properties = {
-                'email': user_email,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-                'reputation': user.reputation,
-                'is_suspended': user.is_suspended,
-                'probable_spammer': user.probable_spammer,
-                'invited_by_id': invited_by_id
+                "email": user_email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "reputation": user.reputation,
+                "is_suspended": user.is_suspended,
+                "probable_spammer": user.probable_spammer,
+                "invited_by_id": invited_by_id,
+                "is_hub_editor": user.is_hub_editor(),
             }
-            user_id = f'{user_email}_{user_id}'
+            user_id = f"{user_email}_{user_id}"
 
             if len(user_id) < 5:
-                user_id += '_____'
-            event_data['user_id'] = user_id
+                user_id += "_____"
+            event_data["user_id"] = user_id
         else:
             user_properties = {
-                'email': '',
-                'first_name': 'Anonymous',
-                'reputation': 0,
-                'is_suspended': False,
-                'probable_spammer': False
+                "email": "",
+                "first_name": "Anonymous",
+                "reputation": 0,
+                "is_suspended": False,
+                "probable_spammer": False,
             }
-            event_data['user_id'] = '_Anonymous_'
+            event_data["user_id"] = "_Anonymous_"
 
-        event_data['user_properties'] = user_properties
+        event_data["user_properties"] = user_properties
 
         if ip is not None:
             try:
                 geo_info = geo.city(ip)
-                event_data['ip'] = ip
-                event_data['country'] = geo_info['country_name']
-                event_data['city'] = geo_info['city']
-                event_data['region'] = geo_info['region']
-                event_data['dma'] = geo_info['dma_code']
-                event_data['location_lat'] = geo_info['latitude']
-                event_data['location_lng'] = geo_info['longitude']
+                event_data["ip"] = ip
+                event_data["country"] = geo_info["country_name"]
+                event_data["city"] = geo_info["city"]
+                event_data["region"] = geo_info["region"]
+                event_data["dma"] = geo_info["dma_code"]
+                event_data["location_lat"] = geo_info["latitude"]
+                event_data["location_lng"] = geo_info["longitude"]
             except Exception as e:
                 log_info(e)
 
-        hit['events'] = [event_data]
+        hit["events"] = [event_data]
         self.hit = json.dumps(hit)
 
     def forward_event(self):
-        headers = {
-          'Content-Type': 'application/json',
-          'Accept': '*/*'
-        }
-        request = requests.post(
-            self.api_url,
-            data=self.hit,
-            headers=headers
-        )
+        headers = {"Content-Type": "application/json", "Accept": "*/*"}
+        request = requests.post(self.api_url, data=self.hit, headers=headers)
         res = request.json()
         if request.status_code != 200:
             log_info(res)
