9b3deb0eee1a6344a66bf540b0f9491494f21a90
Leo <leo.sun@berkeley.edu>
2022-07-05 13:54:28 -0700

    more model updates

---
+++ src/reputation/related_models/term.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from utils.models import DefaultModel
+
+
+class Term(DefaultModel):
+    expiration_date = models.DateTimeField(null=True)
+    rh_pct = models.DecimalField(decimal_places=2, max_digits=5)
+    dao_pct = models.DecimalField(decimal_places=2, max_digits=5)
