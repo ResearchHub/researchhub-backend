bed8693160289629fd498f6cf0b26358e078f3f0
Leo <leo.sun@berkeley.edu>
2022-07-05 16:49:24 -0700

    finishing up migrations

--- src/reputation/related_models/author_rsc.py
+++ src/reputation/related_models/author_rsc.py
@@ -5,27 +5,6 @@ from django.db import models
 from utils.models import DefaultModel
 
 
-class AuthorRSC(models.Model):
-    author = models.ForeignKey(
-        "user.Author",
-        related_name="author_rsc",
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True,
-    )
-    paper = models.ForeignKey(
-        "paper.Paper",
-        related_name="author_rsc",
-        on_delete=models.CASCADE,
-    )
-    amount = models.DecimalField(default=0, decimal_places=10, max_digits=19)
-
-    def __str__(self):
-        return "{} {} - {} RSC".format(
-            self.author.first_name, self.author.last_name, self.amount
-        )
-
-
 def get_current_term():
     from reputation.related_models.term import Term
 
--- src/researchhub_case/related_models/author_claim_case_model.py
+++ src/researchhub_case/related_models/author_claim_case_model.py
@@ -1,73 +1,70 @@
 from django.db import models
 
+from paper.models import Paper
+from reputation.models import Escrow
 from researchhub_case.constants.case_constants import (
-    AUTHOR_CLAIM_CASE_STATUS, INITIATED
+    AUTHOR_CLAIM_CASE_STATUS,
+    INITIATED,
 )
 from researchhub_case.related_models.researchhub_case_abstract_model import (
-  AbstractResearchhubCase
+    AbstractResearchhubCase,
 )
 from user.models import Author
-from paper.models import Paper
-from reputation.models import AuthorRSC
 
 
 class AuthorClaimCase(AbstractResearchhubCase):
     provided_email = models.EmailField(
-      blank=False,
-      help_text=(
-        'Requestors may use this field to validate themselves with this email'
-      ),
-      null=False,
+        blank=False,
+        help_text=(
+            "Requestors may use this field to validate themselves with this email"
+        ),
+        null=False,
     )
     status = models.CharField(
-      choices=AUTHOR_CLAIM_CASE_STATUS,
-      default=INITIATED,
-      max_length=32,
-      null=False,
+        choices=AUTHOR_CLAIM_CASE_STATUS,
+        default=INITIATED,
+        max_length=32,
+        null=False,
     )
     # TODO: Deprecate in next iteration. No longer used.
     target_author = models.ForeignKey(
-      Author,
-      blank=False,
-      null=True,
-      on_delete=models.CASCADE,
-      related_name='related_claim_cases',
+        Author,
+        blank=False,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="related_claim_cases",
     )
     token_generated_time = models.IntegerField(
-      blank=True,
-      default=None,
-      help_text='Intentionally setting as a int field',
-      null=True,
+        blank=True,
+        default=None,
+        help_text="Intentionally setting as a int field",
+        null=True,
     )
     validation_attempt_count = models.IntegerField(
-      blank=False,
-      default=-1,
-      help_text='Number of attempts to validate themselves given token',
-      null=False
+        blank=False,
+        default=-1,
+        help_text="Number of attempts to validate themselves given token",
+        null=False,
     )
     validation_token = models.CharField(
-      blank=True,
-      db_index=True,
-      default=None,
-      help_text='See author_claim_case_post_create_signal',
-      max_length=255,
-      null=True,
-      unique=True,
+        blank=True,
+        db_index=True,
+        default=None,
+        help_text="See author_claim_case_post_create_signal",
+        max_length=255,
+        null=True,
+        unique=True,
     )
     target_paper = models.ForeignKey(
-      Paper,
-      blank=False,
-      null=True,
-      on_delete=models.CASCADE,
-      related_name='related_claim_cases',
+        Paper,
+        blank=False,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="related_claim_cases",
     )
     target_author_name = models.CharField(
-      max_length=255,
-      null=True,
-      blank=False,
+        max_length=255,
+        null=True,
+        blank=False,
     )
-    claimed_rsc = models.ManyToManyField(
-      AuthorRSC,
-      blank=True,
-      related_name="claim_case"
-    )
+    claimed_rsc = models.ManyToManyField(Escrow, blank=True, related_name="claim_case")
