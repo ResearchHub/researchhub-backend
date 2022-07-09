2629cab47dfb008715255728d1aec86ca2da81e3
Leo <leo.sun@berkeley.edu>
2022-07-08 17:07:41 -0700

    implementing payouts and refunds

--- src/reputation/views/bounty_view.py
+++ src/reputation/views/bounty_view.py
@@ -22,17 +22,20 @@ class BountyViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated | AllowAny]
 
     def create(self, request, *args, **kwargs):
-        with transaction.atomic():
-            data = request.data
-            user = request.user
-            amount = decimal.Decimal(data.get("amount", 0))
-            content_type_id = ContentType.objects.get_for_model(
-                ResearchhubUnifiedDocument
-            ).id
+        data = request.data
+        user = request.user
+        amount = decimal.Decimal(data.get("amount", 0))
+        content_type_id = ContentType.objects.get_for_model(
+            ResearchhubUnifiedDocument
+        ).id
 
-            user_balance = user.get_balance()
-            if amount <= 0 or user_balance - amount < 0:
-                return Response("Insufficient Funds", status=402)
+        user_balance = user.get_balance()
+        if amount <= 0 or user_balance - amount < 0:
+            return Response("Insufficient Funds", status=402)
+        elif amount < 50 or amount > 1000000:
+            return Response(status=400)
+
+        with transaction.atomic():
 
             escrow_data = {
                 "created_by": user.id,
@@ -60,8 +63,49 @@ class BountyViewSet(viewsets.ModelViewSet):
             return res
 
     # TODO: Permissions
+    @action(detail=True, methods=["post"])
+    def approve_bounty(self, request, pk=None):
+        data = request.data
+        amount = data.get("amount", None)
+        recipient = data.get("recipient", None)
+        solution_object_id = data.get("solution_object_id", None)
+        solution_content_type = data.get("solution_content_type", None)
+
+        if amount:
+            amount = decimal.Decimal(amount)
+
+        if (
+            amount
+            and amount <= 0
+            or not recipient
+            or not solution_object_id
+            or not solution_content_type
+        ):
+            return Response(status=400)
+
+        with transaction.atomic():
+            bounty = self.get_object()
+
+            if bounty.status != Bounty.OPEN:
+                return Response(status=400)
+
+            solution_content_type = ContentType.objects.get(model=solution_content_type)
+            bounty.solution_content_type = solution_content_type
+            bounty.solution_object_id = solution_object_id
+            escrow = bounty.escrow
+            escrow.recipient_id = recipient
+            bounty_paid = bounty.approve(payout_amount=amount)
+            escrow.save()
+            bounty.save()
+            if bounty_paid:
+                return Response(status=200)
+            else:
+                raise Exception("Error")
+                # return Response(status=400)
+
+    # TODO: Permissions
     @action(detail=False, methods=["post"])
-    def approve_bounty(self, request):
+    def potential_approve_bounty(self, request):
         data = request.data
         content_type = data.get("content_type", "")
         object_id = data.get("object_id", 0)
@@ -77,6 +121,16 @@ class BountyViewSet(viewsets.ModelViewSet):
                 bounty.approve()
 
     # TODO: Permissions
-    @action(detail=False, methods=["post"])
-    def cancel_bounty(self, request):
-        pass
+    @action(detail=True, methods=["post"])
+    def cancel_bounty(self, request, pk=None):
+        with transaction.atomic():
+            bounty = self.get_object()
+            if bounty.status != Bounty.OPEN:
+                return Response(status=400)
+
+            bounty_cancelled = bounty.cancel()
+            bounty.save()
+            if bounty_cancelled:
+                return Response(status=200)
+            else:
+                raise Exception("Error")
