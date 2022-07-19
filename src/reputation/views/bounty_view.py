06bf2bd3dc75e53d0e1f0d03f66a503d9dd855d7
Leo <leo.sun@berkeley.edu>
2022-07-19 15:40:13 -0700

    new bounty changes

--- src/reputation/views/bounty_view.py
+++ src/reputation/views/bounty_view.py
@@ -8,9 +8,14 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from purchase.models import Balance
-from reputation.models import Bounty, Escrow
+from reputation.models import Bounty, Escrow, Term
 from reputation.permissions import UserBounty
-from reputation.serializers import BountySerializer, EscrowSerializer
+from reputation.serializers import (
+    BountySerializer,
+    BountySolutionSerializer,
+    DynamicBountySerializer,
+    EscrowSerializer,
+)
 from utils.permissions import CreateOnly
 
 
@@ -18,7 +23,24 @@ class BountyViewSet(viewsets.ModelViewSet):
     queryset = Bounty.objects.all()
     serializer_class = BountySerializer
     permission_classes = [IsAuthenticated, CreateOnly | AllowAny]
-    ALLOWED_CONTENT_TYPES = ("thread",)
+    ALLOWED_CREATE_CONTENT_TYPES = ("researchhubunifieddocument",)
+    ALLOWED_APPROVE_CONTENT_TYPES = ("thread", "comment", "reply")
+
+    def _deduct_fee(self, user, gross_amount):
+        current_term = Term.objects.last()
+        rh_pct = current_term.rh_pct
+        fee = gross_amount * rh_pct
+        net_amount = gross_amount - fee
+        return net_amount, fee
+
+    def _get_create_context(self):
+        context = {
+            "rep_dbs_get_created_by": {"_include_fields": ("author_profile",)},
+            "usr_dus_get_author_profile": {
+                "_include_fields": ("id", "first_name", "last_name")
+            },
+        }
+        return context
 
     def create(self, request, *args, **kwargs):
         data = request.data
@@ -36,15 +58,16 @@ class BountyViewSet(viewsets.ModelViewSet):
         elif amount <= 50 or amount > 1000000:
             return Response({"error": "Invalid amount"}, status=400)
 
-        if item_content_type not in self.ALLOWED_CONTENT_TYPES:
+        if item_content_type not in self.ALLOWED_CREATE_CONTENT_TYPES:
             return Response({"error": "Invalid content type"}, status=400)
 
         with transaction.atomic():
+            net_amount, fee = self._deduct_fee(user, amount)
             content_type_id = ContentType.objects.get(model=item_content_type).id
             escrow_data = {
                 "created_by": user.id,
                 "hold_type": Escrow.BOUNTY,
-                "amount": amount,
+                "amount": net_amount,
                 "object_id": data.get("item_object_id", 0),
                 "content_type": content_type_id,
             }
@@ -53,18 +76,42 @@ class BountyViewSet(viewsets.ModelViewSet):
             escrow = escrow_serializer.save()
 
             data["created_by"] = user.id
-            data["amount"] = amount
+            data["amount"] = net_amount
             data["item_content_type"] = content_type_id
             data["escrow"] = escrow.id
-            res = super().create(request, *args, **kwargs)
-            amount_str = amount.to_eng_string()
+            bounty_serializer = BountySerializer(data=data)
+            bounty_serializer.is_valid(raise_exception=True)
+            bounty = bounty_serializer.save()
+
+            amount_str = net_amount.to_eng_string()
+            fee_str = fee.to_eng_string()
             Balance.objects.create(
                 user=user,
                 content_type=ContentType.objects.get_for_model(Bounty),
-                object_id=res.data["id"],
+                object_id=bounty.id,
+                amount=f"-{fee_str}",
+            )
+            Balance.objects.create(
+                user=user,
+                content_type=ContentType.objects.get_for_model(Bounty),
+                object_id=bounty.id,
                 amount=f"-{amount_str}",
             )
-            return res
+
+            context = self._get_create_context()
+            serializer = DynamicBountySerializer(
+                bounty,
+                context=context,
+                _include_fields=(
+                    "amount",
+                    "created_date",
+                    "created_by",
+                    "expiration_date",
+                    "id",
+                    "status",
+                ),
+            )
+            return Response(serializer.data, status=201)
 
     @action(
         detail=True, methods=["post"], permission_classes=[IsAuthenticated, UserBounty]
@@ -73,8 +120,8 @@ class BountyViewSet(viewsets.ModelViewSet):
         data = request.data
         amount = data.get("amount", None)
         recipient = data.get("recipient", None)
-        solution_object_id = data.get("solution_object_id", None)
-        solution_content_type = data.get("solution_content_type", None)
+        content_type = data.get("content_type", None)
+        object_id = data.get("object_id", None)
 
         if amount:
             try:
@@ -82,28 +129,33 @@ class BountyViewSet(viewsets.ModelViewSet):
             except Exception as e:
                 return Response(str(e), status=400)
 
-        if (
-            (amount and amount <= 0)
-            or not recipient
-            or not solution_object_id
-            or not solution_content_type
-        ):
+        if (amount and amount <= 0) or not recipient or not object_id:
             return Response(status=400)
+
+        if content_type not in self.ALLOWED_APPROVE_CONTENT_TYPES:
+            return Response({"error": "Invalid content type"}, status=400)
 
         with transaction.atomic():
             bounty = self.get_object()
 
             if bounty.status != Bounty.OPEN:
-                return Response(status=400)
+                return Response({"error": "Bounty is closed."}, status=400)
 
-            solution_content_type = ContentType.objects.get(model=solution_content_type)
-            bounty.solution_content_type = solution_content_type
-            bounty.solution_object_id = solution_object_id
+            content_type = ContentType.objects.get(model=content_type)
+            model_class = content_type.model_class()
+            solution = model_class.objects.get(id=object_id)
             escrow = bounty.escrow
             escrow.recipient_id = recipient
             bounty_paid = bounty.approve(payout_amount=amount)
             escrow.save()
             bounty.save()
+
+            data["bounty"] = bounty.id
+            data["created_by"] = solution.created_by.id
+            data["content_type"] = content_type.id
+            solution_serializer = BountySolutionSerializer(data=data)
+            solution_serializer.is_valid(raise_exception=True)
+            solution_serializer.save()
             if bounty_paid:
                 serializer = self.get_serializer(bounty)
                 return Response(serializer.data, status=200)
