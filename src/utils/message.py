cfe793a94924544337bff4c3461d6c0d303aa799
Leo <leo.sun@berkeley.edu>
2022-07-27 13:06:54 -0700

    reverting list cast change

--- src/utils/message.py
+++ src/utils/message.py
@@ -60,7 +60,7 @@ def send_email_message(
     subject = subject.replace("\r", "")
 
     if not isinstance(recipients, list):
-        recipients = list(recipients)
+        recipients = [recipients]
 
     if not PRODUCTION:
         subject = "[Staging] " + subject
