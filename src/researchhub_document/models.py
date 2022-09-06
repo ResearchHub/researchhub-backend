772284338b3bbedbe4e5a1c26784d1d6709cdcc1
Leo <leo.sun@berkeley.edu>
2022-09-05 17:39:15 -0700

    fixing import

--- src/researchhub_document/models.py
+++ src/researchhub_document/models.py
@@ -3,5 +3,6 @@ from .related_models.featured_content_model import FeaturedContent
 from .related_models.feed_exclusion_model import FeedExclusion
 from .related_models.researchhub_post_model import ResearchhubPost
 from .related_models.researchhub_unified_document_model import (
+    DocumentFilter,
     ResearchhubUnifiedDocument,
 )
