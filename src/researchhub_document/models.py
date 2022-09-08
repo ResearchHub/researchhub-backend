9620667d4547f080889b6e43f91f6409cce9abf3
Leo <leo.sun@berkeley.edu>
2022-09-08 14:41:29 -0700

    moving document filter to its own file

--- src/researchhub_document/models.py
+++ src/researchhub_document/models.py
@@ -1,8 +1,8 @@
 # flake8: noqa
+from .related_models.document_filter_model import DocumentFilter
 from .related_models.featured_content_model import FeaturedContent
 from .related_models.feed_exclusion_model import FeedExclusion
 from .related_models.researchhub_post_model import ResearchhubPost
 from .related_models.researchhub_unified_document_model import (
-    DocumentFilter,
     ResearchhubUnifiedDocument,
 )
