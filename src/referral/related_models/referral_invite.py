8e86d2a0011261ea15b0e1b548d61482e869645c
Kobe Attias <kobeattias@gmail.com>
2022-11-07 10:04:24 -0500

    Referral invite templates

--- src/referral/related_models/referral_invite.py
+++ src/referral/related_models/referral_invite.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.db.models import Sum
 
 from invite.models import Invitation
-from researchhub.settings import REFERRAL_PROGRAM
+from reputation.related_models.bounty import Bounty
+from researchhub.settings import BASE_FRONTEND_URL, REFERRAL_PROGRAM
 from researchhub_document.related_models.researchhub_unified_document_model import (
     ResearchhubUnifiedDocument,
 )
@@ -39,16 +40,15 @@ class ReferralInvite(Invitation):
         subject = f"{inviter_name} has invited you to complete a bounty on ResearchHub"
         uni_doc = self.unified_document
         url = uni_doc.frontend_view_link()
-        uni_doc.bounties.all()
         bounty_amount = round(
-            ResearchhubUnifiedDocument.objects.get(id=uni_doc.id)
-            .bounties.all()
-            .aggregate(Sum("amount"))["amount__sum"]
+            uni_doc.related_bounties.filter(status=Bounty.OPEN).aggregate(
+                Sum("amount")
+            )["amount__sum"]
         )
 
         email_context = {
             "inviter_name": inviter_name,
-            "referral_url": f"http://localhost:3000/referral/{inviter.referral_code}",
+            "referral_url": f"{BASE_FRONTEND_URL}/referral/{inviter.referral_code}",
             "document_title": uni_doc.get_document().title,
             "bounty_amount": bounty_amount,
             "referral_bonus": REFERRAL_PROGRAM["INVITED_EARN_AMOUNT"],
@@ -61,17 +61,14 @@ class ReferralInvite(Invitation):
         inviter = self.inviter
         email = self.recipient_email
         template = "referral_invite.txt"
-        html_template = "referral_bounty_invite.html"
+        html_template = "referral_join_invite.html"
         inviter_name = f"{inviter.first_name} {inviter.last_name}"
-        subject = f"{inviter_name} has invited you to complete a bounty on ResearchHub"
+        subject = f"{inviter_name} has invited you to join ResearchHub"
 
         email_context = {
             "inviter_name": inviter_name,
-            "referral_url": "http://localhost:3000/referral/cf41a714-defd-48c8-87df-acb66a7c18c5",
-            "document_title": "Some bounty title",
-            "bounty_amount": 470,
+            "referral_url": f"{BASE_FRONTEND_URL}/referral/{inviter.referral_code}",
             "referral_bonus": REFERRAL_PROGRAM["INVITED_EARN_AMOUNT"],
-            "document_url": "https://stackoverflow.com/questions/24912173/django-1-7-makemigrations-not-detecting-changes",
         }
 
         send_email_message([email], template, subject, email_context, html_template)
