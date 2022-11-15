e0fa598f6c152ac196de8524ed7654dd64d5d6a6
Patrick Lu <lightning.lu7@gmail.com>
2022-11-15 13:55:39 -0800

    updating amplitude script

--- src/analytics/management/commands/update_amplitude_users.py
+++ src/analytics/management/commands/update_amplitude_users.py
@@ -1,39 +1,29 @@
-import requests
 import json
 
+import requests
 from django.core.management.base import BaseCommand
 
-from user.models import User
+from analytics.amplitude import Amplitude
 from researchhub.settings import AMPLITUDE_API_KEY
+from user.models import User
 
-
-API_URL = 'https://api.amplitude.com/2/httpapi'
+API_URL = "https://api.amplitude.com/2/httpapi"
 
 
 class Command(BaseCommand):
     def get_user_props(self, user, user_email):
         # Makes one less db call if user email is passed in
         user_properties = {
-            'is_suspended': user.is_suspended,
-            'probable_spammer': user.probable_spammer
+            "is_suspended": user.is_suspended,
+            "probable_spammer": user.probable_spammer,
         }
         return user_properties
 
     def forward_amp_event(self, events):
-        event_data = {
-            'api_key': AMPLITUDE_API_KEY,
-            'events': events
-        }
+        event_data = {"api_key": AMPLITUDE_API_KEY, "events": events}
         data = json.dumps(event_data)
-        headers = {
-          'Content-Type': 'application/json',
-          'Accept': '*/*'
-        }
-        request = requests.post(
-            API_URL,
-            data=data,
-            headers=headers
-        )
+        headers = {"Content-Type": "application/json", "Accept": "*/*"}
+        request = requests.post(API_URL, data=data, headers=headers)
         res = request.json()
         if request.status_code != 200:
             res = request.json()
@@ -41,28 +31,26 @@ class Command(BaseCommand):
         print(res)
 
     def update_users(self, users):
-        print('Users')
+        print("Users")
         count = users.count()
         events = []
+        amp = Amplitude()
         for i, user in enumerate(users.iterator()):
             if (i % 1000 == 0 and i != 0) or (count - 1) == i:
                 self.forward_amp_event(events)
                 events = []
             else:
-                print(f'{i}/{count}')
+                print(f"{i}/{count}")
                 user_email = user.email
-                user_properties = self.get_user_props(user, user_email)
-                user_id = f'{user_email}_{user.id}'
-                if len(user_id) < 5:
-                    user_id += '_____'
+                user_id, user_props = amp._build_user_properties(user)
                 hit = {
-                    'user_id': user_id,
-                    'event_type': 'update_user',
-                    'user_properties': user_properties,
+                    "user_id": user_id,
+                    "event_type": "update_user",
+                    "user_properties": user_props,
                 }
                 events.append(hit)
 
     def handle(self, *args, **options):
-        user = User.objects
+        users = User.objects.filter(first_name__icontains="student")
 
-        self.update_users(user)
+        self.update_users(users)
