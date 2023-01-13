0019f0786dcbcd5166036890bfca13f71f4837cd
Calvin Lee <calvinhlee23@gmail.com>
2023-01-13 14:47:12 -0800

    signal-migration setup for rhComments & threads

--- src/researchhub_comment/signals/rh_comment_signals.py
+++ src/researchhub_comment/signals/rh_comment_signals.py
@@ -1,5 +1,22 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-@receiver(post_save, sender=Vote, dispatch_uid='update_citation_vote_score')
-def from_legacy_to_rh_comment():
+from researchhub_comment.models import RhCommentThreadModel
+from discussion.models import Comment, Reply, Thread
+
+@receiver(post_save, sender=Comment, dispatch_uid='from_legacy_comment_to_rh_comment')
+def from_legacy_comment_to_rh_comment():
+    RhCommentThreadModel
+    #  implement
+
+
+@receiver(post_save, sender=Reply, dispatch_uid='from_legacy_reply_to_rh_comment')
+def from_legacy_reply_to_rh_comment():
+    RhCommentThreadModel
+    #  implement
+
+
+@receiver(post_save, sender=Comment, dispatch_uid='from_legacy_thread_to_rh_comment')
+def from_legacy_thread_to_rh_comment():
+    RhCommentThreadModel
+    #  implement 
