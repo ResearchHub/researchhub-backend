bc3e4d5ec1776f473d4c25c117aab7f2516b3cc0
Calvin Lee <calvinhlee23@gmail.com>
2023-03-07 16:34:18 -0800

    adding back class method get_existing_thread_from_request

--- src/researchhub_comment/related_models/rh_comment_thread_model.py
+++ src/researchhub_comment/related_models/rh_comment_thread_model.py
@@ -27,6 +27,33 @@ class RhCommentThreadModel(AbstractGenericRelationModel):
     )
 
     """ --- METHODS --- """
+    @classmethod
+    def get_existing_thread_from_request(cls, request):
+        request_data = request.data
+        thread_type = request_data.get("thread_type")
+        thread_reference = request_data.get("thread_reference")
+        thread_content_model_name = request_data.get("thread_content_model_name")
+        thread_content_instance_id = request_data.get("thread_content_instance_id")
+        if (
+            thread_type is None
+            or thread_content_model_name is None
+            or thread_content_instance_id is None
+        ):
+            raise Exception(
+                f"Failed to call __retrieve_or_create_thread_from_request. \
+                thread_type: {thread_type} | thread_content_model_name: {thread_content_model_name} |\
+                thread_content_instance_id: {thread_content_instance_id}"
+            )
+        else:
+            valid_content_model = cls.get_valid_thread_content_model(
+                thread_content_model_name
+            )
+            return valid_content_model.object.get(
+                id=thread_content_instance_id
+            ).rh_threads.filter(
+                thread_reference=thread_reference, thread_type=thread_type
+            ).first()
+
     @staticmethod
     def get_valid_thread_content_model(thread_content_model_name):
         from paper.models import Paper
