ec0a461625eb5c6083fb37c254c3889fff26787e
Leo <leo.sun@berkeley.edu>
2023-04-21 14:10:34 -0700

    adding more unit tests

--- src/researchhub_comment/tests/test_comments.py
+++ src/researchhub_comment/tests/test_comments.py
@@ -6,12 +6,13 @@ from user.tests.helpers import create_moderator, create_random_default_user
 
 class CommentViewTests(APITestCase):
     def setUp(self):
+        self.paper_uploader = create_random_default_user("paper_uploader_1")
         self.user_1 = create_random_default_user("comment_user_1")
         self.user_2 = create_random_default_user("comment_user_2")
         self.user_3 = create_random_default_user("comment_user_3")
         self.user_4 = create_random_default_user("comment_user_4")
         self.moderator = create_moderator(first_name="moderator", last_name="moderator")
-        self.paper = create_paper()
+        self.paper = create_paper(uploaded_by=self.paper_uploader)
 
     def _create_comment(self, obj_name, obj_id, created_by, data):
         self.client.force_authenticate(created_by)
@@ -20,26 +21,32 @@ class CommentViewTests(APITestCase):
         )
         return res
 
-    def _create_paper_comment(self, paper_id, created_by, *args, **kwargs):
-        res = self.create_comment(
+    def _create_paper_comment(
+        self, paper_id, created_by, text="this is a test comment", **kwargs
+    ):
+        res = self._create_comment(
             "paper",
             paper_id,
             created_by,
             {
-                "comment_content_json": {"ops": [{"insert": "this is a test comment"}]},
+                "comment_content_json": {"ops": [{"insert": text}]},
+                **kwargs,
             },
         )
         return res
 
-    def _create_post_comment(self, post_id, created_by, *args, **kwargs):
+    def _create_post_comment(
+        self, post_id, created_by, text="this is a test comment", **kwargs
+    ):
         self.client.force_authenticate(created_by)
 
-        res = self.create_comment(
+        res = self._create_comment(
             "post",
             post_id,
             created_by,
             {
-                "comment_content_json": {"ops": [{"insert": "this is a test comment"}]},
+                "comment_content_json": {"ops": [{"insert": text}]},
+                **kwargs,
             },
         )
         return res
@@ -50,7 +57,7 @@ class CommentViewTests(APITestCase):
         self.client.force_authenticate(self.user_1)
 
         update_comment_res = self.client.patch(
-            f"/api/paper/{self.paper.id}/comments/f{comment.data['id']}/",
+            f"/api/paper/{self.paper.id}/comments/{comment.data['id']}/",
             {
                 "comment_content_json": {
                     "ops": [{"insert": "this is an updated test comment"}]
@@ -60,3 +67,51 @@ class CommentViewTests(APITestCase):
 
         self.assertEqual(update_comment_res.status_code, 200)
         return update_comment_res
+
+    def test_non_comment_creator_cant_edit(self):
+        creator = self.user_1
+        comment = self._create_paper_comment(self.paper.id, creator)
+        self.client.force_authenticate(self.user_2)
+
+        update_comment_res = self.client.patch(
+            f"/api/paper/{self.paper.id}/comments/{comment.data['id']}/",
+            {
+                "comment_content_json": {
+                    "ops": [{"insert": "this is a failed updated test comment"}]
+                },
+            },
+        )
+
+        self.assertEqual(update_comment_res.status_code, 403)
+        return update_comment_res
+
+    def test_nested_discussion_counts(self):
+        creator_1 = self.user_1
+        creator_2 = self.user_2
+        creator_3 = self.user_3
+        parent_comment_1 = self._create_paper_comment(self.paper.id, creator_1)
+        parent_comment_2 = self._create_paper_comment(self.paper.id, creator_2)
+        comment_1 = self._create_paper_comment(
+            self.paper.id, creator_3, parent_id=parent_comment_1.data["id"]
+        )
+        comment_2 = self._create_paper_comment(
+            self.paper.id, creator_3, parent_id=parent_comment_2.data["id"]
+        )
+        comment_3 = self._create_paper_comment(
+            self.paper.id, creator_3, parent_id=comment_2.data["id"]
+        )
+        comment_4 = self._create_paper_comment(
+            self.paper.id, creator_3, parent_id=comment_3.data["id"]
+        )
+        comment_5 = self._create_paper_comment(
+            self.paper.id, creator_1, parent_id=comment_4.data["id"]
+        )
+        comment_6 = self._create_paper_comment(
+            self.paper.id, creator_2, parent_id=comment_5.data["id"]
+        )
+
+        res = self.client.get(f"/api/paper/{self.paper.id}/")
+
+        self.assertEqual(comment_1.status_code, 200)
+        self.assertEqual(comment_6.status_code, 200)
+        self.assertEqual(res.data["discussion_count"], 8)
