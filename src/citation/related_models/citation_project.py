666f709af93cda57bceeada3744f9f70370f364b
Calvin Lee <calvinhlee23@gmail.com>
2023-05-03 08:28:12 -0700

    migration made for parent-child relationship for projects

--- src/citation/related_models/citation_project.py
+++ src/citation/related_models/citation_project.py
@@ -1,13 +1,18 @@
 from django.db import models
 from jsonschema import validate
-from django.core.validators import FileExtensionValidator
 
-from citation.constants import CITATION_TYPE_CHOICES
 from user.models import Organization
 from utils.models import DefaultAuthenticatedModel
 
 
 class CitationProject(DefaultAuthenticatedModel):
+    parent = models.ForeignKey(
+        "self",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="children",
+    )
     project_name = models.CharField(
         max_length=1024,
     )
