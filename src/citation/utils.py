cf22d896cd0da70c950b4680bb1195d1bf10fe81
Patrick Lu <lightning.lu7@gmail.com>
2023-05-16 16:08:04 -0700

    removing serializer

--- src/citation/utils.py
+++ src/citation/utils.py
@@ -1,23 +1,20 @@
 import pdf2doi
 
 from citation.constants import CITATION_TYPE_FIELDS, JOURNAL_ARTICLE
+from citation.models import CitationEntry
 from citation.schema import generate_json_for_journal
-from citation.serializers import CitationEntrySerializer
 
 
 def get_citation_entry_from_pdf(pdf, user_id, organization_id, project_id):
     conversion = pdf2doi.pdf2doi_singlefile(pdf)
     json = generate_json_for_journal(conversion)
-    data = {
-        "citation_type": JOURNAL_ARTICLE,
-        "fields": json,
-        "created_by_id": user_id,
-        "organization_id": organization_id,
-        "attachment": pdf,
-        "doi": json["DOI"],
-        "project_id": project_id,
-    }
-    serializer = CitationEntrySerializer(data=data)
-    serializer.is_valid(raise_exception=True)
-    entry = serializer.save()
+    entry = CitationEntry.objects.create(
+        citation_type=JOURNAL_ARTICLE,
+        fields=json,
+        created_by_id=user_id,
+        organization_id=organization_id,
+        attachment=pdf,
+        doi=json["DOI"],
+        project_id=project_id,
+    )
     return entry
