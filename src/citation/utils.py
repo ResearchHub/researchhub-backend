da63e251a8f1aebff6a92e1062caf159d303122b
Patrick Lu <lightning.lu7@gmail.com>
2023-05-16 15:51:41 -0700

    using citationEntrySerializer

--- src/citation/utils.py
+++ src/citation/utils.py
@@ -1,19 +1,24 @@
 import pdf2doi
 
-from citation.models import CitationEntry
 from citation.constants import CITATION_TYPE_FIELDS, JOURNAL_ARTICLE
+from citation.models import CitationEntry
 from citation.schema import generate_json_for_journal
+from citation.serializers import CitationEntrySerializer
+
 
 def get_citation_entry_from_pdf(pdf, user_id, organization_id, project_id):
     conversion = pdf2doi.pdf2doi_singlefile(pdf)
     json = generate_json_for_journal(conversion)
-    entry = CitationEntry.objects.create(
-        citation_type=JOURNAL_ARTICLE,
-        fields=json,
-        created_by_id=user_id,
-        organization_id=organization_id,
-        attachment=pdf,
-        doi=json['DOI'],
-        project_id=project_id
-    )
+    data = {
+        "citation_type": JOURNAL_ARTICLE,
+        "fields": json,
+        "created_by_id": user_id,
+        "organization_id": organization_id,
+        "attachment": pdf,
+        "doi": json["DOI"],
+        "project_id": project_id,
+    }
+    serializer = CitationEntrySerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    entry = serializer.save()
     return entry
