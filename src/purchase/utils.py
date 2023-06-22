7957c19e21cc712887f3a96090b3303528a03aa1
Leo <leo.sun@berkeley.edu>
2023-06-22 14:05:34 -0700

    commiting new utils file

---
+++ src/purchase/utils.py
@@ -0,0 +1,32 @@
+import time
+
+from reputation.distributions import create_purchase_distribution
+from reputation.distributor import Distributor
+from reputation.models import Escrow
+
+
+def distribute_support_to_authors(paper, purchase, amount):
+    registered_authors = paper.authors.all()
+    total_author_count = paper.true_author_count()
+
+    rewarded_rsc = 0
+    rsc_per_author = amount / total_author_count
+    for author in registered_authors.iterator():
+        recipient = author.user
+        distribution = create_purchase_distribution(recipient, rsc_per_author)
+        distributor = Distributor(
+            distribution, recipient, purchase, time.time(), purchase.user
+        )
+        distributor.distribute()
+        rewarded_rsc += rsc_per_author
+
+    store_leftover_paper_support(paper, purchase, amount - rewarded_rsc)
+
+
+def store_leftover_paper_support(paper, purchase, leftover_amount):
+    Escrow.objects.create(
+        created_by=purchase.user,
+        amount_holding=leftover_amount,
+        item=paper,
+        hold_type=Escrow.AUTHOR_RSC,
+    )
