d850a17ac22858f1f7062d8c04684a7f83b24226
Leo <leo.sun@berkeley.edu>
2023-06-22 15:43:11 -0700

    fixing tests

--- src/purchase/utils.py
+++ src/purchase/utils.py
@@ -10,15 +10,16 @@ def distribute_support_to_authors(paper, purchase, amount):
     total_author_count = paper.true_author_count()
 
     rewarded_rsc = 0
-    rsc_per_author = amount / total_author_count
-    for author in registered_authors.iterator():
-        recipient = author.user
-        distribution = create_purchase_distribution(recipient, rsc_per_author)
-        distributor = Distributor(
-            distribution, recipient, purchase, time.time(), purchase.user
-        )
-        distributor.distribute()
-        rewarded_rsc += rsc_per_author
+    if total_author_count:
+        rsc_per_author = amount / total_author_count
+        for author in registered_authors.iterator():
+            recipient = author.user
+            distribution = create_purchase_distribution(recipient, rsc_per_author)
+            distributor = Distributor(
+                distribution, recipient, purchase, time.time(), purchase.user
+            )
+            distributor.distribute()
+            rewarded_rsc += rsc_per_author
 
     store_leftover_paper_support(paper, purchase, amount - rewarded_rsc)
 
