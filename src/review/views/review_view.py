95ff4ee6e8da8123ff0cf6dcc8c4eb56c53697a6
Leo <leo.sun@berkeley.edu>
2023-06-23 17:54:56 -0700

    minor bugfixes

--- src/review/views/review_view.py
+++ src/review/views/review_view.py
@@ -4,18 +4,14 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 
-from discussion.models import Thread
 from discussion.reaction_views import ReactionViewActionMixin
 from researchhub_document.models import ResearchhubUnifiedDocument
 from researchhub_document.related_models.constants.document_type import (
     FILTER_PEER_REVIEWED,
 )
-from researchhub_document.related_models.constants.filters import DISCUSSED, HOT
-from researchhub_document.utils import get_doc_type_key, reset_unified_document_cache
 from review.models.review_model import Review
 from review.permissions import AllowedToUpdateReview
 from review.serializers import ReviewSerializer
-from utils.sentry import log_error
 from utils.throttles import THROTTLE_CLASSES
 
 
@@ -49,24 +45,5 @@ class ReviewViewSet(viewsets.ModelViewSet, ReactionViewActionMixin):
 
         response = super().create(request, *args, **kwargs)
         unified_document.update_filter(FILTER_PEER_REVIEWED)
-
-        return response
-
-    def update(self, request, *args, **kwargs):
-        response = super().update(request, *args, **kwargs)
-
-        try:
-            thread = Thread.objects.get(review_id=response.data["id"])
-            doc = thread.unified_document
-            doc_type = get_doc_type_key(doc)
-            hubs = list(doc.hubs.all().values_list("id", flat=True))
-
-            reset_unified_document_cache(
-                hub_ids=hubs,
-                document_type=[doc_type, "all"],
-                filters=[DISCUSSED, HOT],
-            )
-        except Exception as e:
-            log_error(e)
 
         return response
