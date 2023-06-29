2b14de5ab5d57041c234fe666bc5d3eeb22a3a14
Leo <leo.sun@berkeley.edu>
2023-06-29 16:58:27 -0700

    removing development check for citations

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -90,6 +90,7 @@ ALLOWED_HOSTS = [
     "researchhub-web-researchhub.vercel.app",
     r"^https:\/\/(\w)*[-]*(researchhub+)([-](\w)*)*(.vercel.app){1}/",
     r"https:\/\/(\w)*[-]*(researchhub+)([-](\w)*)*(.vercel.app){1}",
+    r"researchhub(-[0-9]?)\.ngrok\.io",
 ]
 
 if not (PRODUCTION or STAGING):
@@ -484,7 +485,10 @@ AWS_SECRET_ACCESS_KEY = os.environ.get(
 )
 
 # AWS Lambda
-LAMBDA_IPS = ["35.82.37.191", "35.83.152.234"]
+if DEVELOPMENT:
+    LAMBDA_IPS = ["127.0.0.1", "localhost"]
+else:
+    LAMBDA_IPS = ["35.82.37.191", "35.83.152.234"]
 
 AWS_SCHOLARLY_LAMBDA = (
     "arn:aws:lambda:us-west-2:794128250202:function:ResearchHub-Scholarly"
