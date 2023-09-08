11cd2bdd2b3975ca8863a1fbcb2fb7ae2bdb0571
Kobe Attias <kobeattias@gmail.com>
2023-09-07 22:33:21 -0400

    suggester for hub

--- src/search/documents/hub.py
+++ src/search/documents/hub.py
@@ -1,36 +1,58 @@
 from django_elasticsearch_dsl import fields as es_fields
 from django_elasticsearch_dsl.registries import registry
+from elasticsearch_dsl import analyzer, token_filter, tokenizer
 
 from hub.models import Hub
+from search.analyzers import content_analyzer, title_analyzer
+
 from .base import BaseDocument
 
-from search.analyzers import (
-    title_analyzer,
-    content_analyzer
+edge_ngram_filter = token_filter(
+    "edge_ngram_filter",
+    type="edge_ngram",
+    min_gram=1,
+    max_gram=20,
+)
+
+edge_ngram_analyzer = analyzer(
+    "edge_ngram_analyzer",
+    tokenizer="standard",
+    filter=["lowercase", edge_ngram_filter],
 )
 
 
 @registry.register_document
 class HubDocument(BaseDocument):
     auto_refresh = True
-
-    paper_count = es_fields.IntegerField(attr='paper_count')
-    subscriber_count = es_fields.IntegerField(attr='subscriber_count')
-    discussion_count = es_fields.IntegerField(attr='discussion_count')
-    description = es_fields.TextField(attr='description', analyzer=content_analyzer)
-    name = es_fields.TextField(attr='name', analyzer=title_analyzer)
+    description = es_fields.TextField(attr="description", analyzer=content_analyzer)
+    name_suggest = es_fields.Completion()
+    name = es_fields.TextField(
+        analyzer=edge_ngram_analyzer,
+        search_analyzer="standard",
+    )
 
     class Index:
-        name = 'hub'
+        name = "hub"
 
     class Django:
         model = Hub
         fields = [
-            'id',
-            'acronym',
-            'is_locked',
+            "id",
+            "slug",
         ]
 
+    # Used specifically for "autocomplete" style suggest feature
+    def prepare_name_suggest(self, instance):
+        return {
+            "input": instance.name.split() + [instance.name],
+            "weight": 1,
+        }
+
+    def prepare(self, instance):
+        data = super().prepare(instance)
+        data["name_suggest"] = self.prepare_name_suggest(instance)
+
+        return data
 
     def should_remove_from_index(self, obj):
         if obj.is_removed or obj.is_locked:
--- src/search/views/hub.py
+++ src/search/views/hub.py
@@ -1,10 +1,14 @@
+from django_elasticsearch_dsl_drf.constants import SUGGESTER_COMPLETION
 from django_elasticsearch_dsl_drf.filter_backends import (
     CompoundSearchFilterBackend,
     DefaultOrderingFilterBackend,
+    FilteringFilterBackend,
+    OrderingFilterBackend,
+    SuggesterFilterBackend,
 )
-from elasticsearch_dsl import Search
-from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
 from django_elasticsearch_dsl_drf.pagination import LimitOffsetPagination
+from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from elasticsearch_dsl import Search
 
 from search.backends.multi_match_filter import MultiMatchSearchFilterBackend
 from search.documents.hub import HubDocument
@@ -17,43 +21,47 @@ class HubDocumentView(DocumentViewSet):
     permission_classes = [ReadOnly]
     serializer_class = HubDocumentSerializer
     pagination_class = LimitOffsetPagination
-    lookup_field = 'id'
+    lookup_field = "id"
     filter_backends = [
-        MultiMatchSearchFilterBackend,
         CompoundSearchFilterBackend,
         DefaultOrderingFilterBackend,
+        MultiMatchSearchFilterBackend,
+        SuggesterFilterBackend,
     ]
+    filter_fields = {
+        "name": {"field": "name", "lookups": ["match"]},
+    }
+    multi_match_search_fields = {
+        "name": {"field": "name", "boost": 1},
+    }
+    suggester_fields = {
+        "name_suggest": {
+            "field": "name_suggest",
+            "suggesters": ["completion"],
+            "options": {
+                "size": 5,
+            },
+        },
+    }
 
     search_fields = {
-        'name': {'boost': 1, 'fuzziness': 1},
-        'acronym': {'boost': 1, 'fuzziness': 1},
-        'description': {'boost': 1, 'fuzziness': 1},
-    }
-
-    multi_match_search_fields = {
-        'name': {'boost': 1},
-        'acronym': {'boost': 1},
-        'description': {'boost': 1},
-    }
-
-    multi_match_options = {
-        'operator': 'and',
-        'type': 'cross_fields',
-        'analyzer': 'content_analyzer',
+        "name": {"boost": 1, "fuzziness": 1},
+        "acronym": {"boost": 1, "fuzziness": 1},
+        "description": {"boost": 1, "fuzziness": 1},
     }
 
     def __init__(self, *args, **kwargs):
-        self.search = Search(index=['hub'])
-        super(HubDocumentView, self).__init__(*args, **kwargs)    
+        self.search = Search(index=["hub"])
+        super(HubDocumentView, self).__init__(*args, **kwargs)
 
     def _filter_queryset(self, request):
         queryset = self.search
 
         for backend in list(self.filter_backends):
             queryset = backend().filter_queryset(
-            request,
-            queryset,
-            self,
-        )
+                request,
+                queryset,
+                self,
+            )
 
         return queryset
