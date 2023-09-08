9e4ede4f7078ebb747c53835152595b25011dd7d
Kobe Attias <kobeattias@gmail.com>
2023-09-08 16:23:40 -0400

    Fixing backfill issue by date

--- src/paper/management/commands/backfill_concepts.py
+++ src/paper/management/commands/backfill_concepts.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--start-date", type=str, help="Start date in YYYY-MM-DD format."
+            "--start_date", type=str, help="Start date in YYYY-MM-DD format."
         )
         parser.add_argument("--doi", type=str, help="DOI of a specific paper.")
 
@@ -24,6 +24,7 @@ class Command(BaseCommand):
         if start_date_str:
             start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
             papers = Paper.objects.filter(created_date__gte=start_date)
+            print(f"Found {papers.count()} papers created after {start_date_str}")
         elif doi:
             papers = Paper.objects.filter(doi=doi)
         else:
@@ -31,37 +32,45 @@ class Command(BaseCommand):
             return
 
         for paper in papers:
-            print(
-                "Fetching concepts from OA for paper: "
-                + str(paper.id)
-                + " doi: "
-                + paper.doi
-            )
-            result = open_alex.get_data_from_doi(doi)
-            if result:
-                paper_concepts = open_alex.hydrate_paper_concepts(
-                    result.get("concepts", [])
+            try:
+                print(
+                    "Fetching concepts from OA for paper: "
+                    + str(paper.id)
+                    + " doi: "
+                    + paper.doi
                 )
-                for paper_concept in paper_concepts:
-                    concept = Concept.create_or_update(paper_concept)
-                    paper.unified_document.concepts.add(
-                        concept,
-                        through_defaults={
-                            "relevancy_score": paper_concept["score"],
-                            "level": paper_concept["level"],
-                        },
+                result = open_alex.get_data_from_doi(paper.doi)
+                if result:
+                    paper_concepts = open_alex.hydrate_paper_concepts(
+                        result.get("concepts", [])
                     )
+                    for paper_concept in paper_concepts:
+                        concept = Concept.create_or_update(paper_concept)
+                        paper.unified_document.concepts.add(
+                            concept,
+                            through_defaults={
+                                "relevancy_score": paper_concept["score"],
+                                "level": paper_concept["level"],
+                            },
+                        )
 
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Successfully backfilled concepts for paper {paper.id}"
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f"Successfully backfilled concepts for paper {paper.id}"
+                        )
                     )
-                )
-            else:
+                else:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            f"Failed to backfill concepts for paper {paper.id}"
+                        )
+                    )
+            except Exception as e:
                 self.stdout.write(
                     self.style.ERROR(
                         f"Failed to backfill concepts for paper {paper.id}"
                     )
                 )
+                self.stdout.write(self.style.ERROR(str(e)))
 
         self.stdout.write(self.style.SUCCESS("Backfill process completed!"))
