b133be183184aa78f9b73fd509dc5ed5fe095dae
Kobe Attias <kobeattias@gmail.com>
2023-09-07 22:33:21 -0400

    suggester for hub

--- src/search/serializers/hub.py
+++ src/search/serializers/hub.py
@@ -1,45 +1,17 @@
-from rest_framework import serializers
+from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
 
 from hub.models import Hub
+from search.documents import HubDocument
 
 
-class HubDocumentSerializer(serializers.ModelSerializer):
-    slug = serializers.SerializerMethodField()
-    hub_image = serializers.SerializerMethodField()
-
-    def get_slug(self, hit):
-        slug = ''
-        try:
-            obj = Hub.objects.get(id=hit['id'])
-            slug = obj.slug
-        except Exception as e:
-            print(e)
-            pass
-
-        return slug
-
-    def get_hub_image(self, hit):
-        img = ''
-        try:
-            obj = Hub.objects.get(id=hit['id'])
-            img = obj.hub_image.url
-        except Exception as e:
-            print(e)
-            pass
-
-        return img        
+class HubDocumentSerializer(DocumentSerializer):
+    document = HubDocument
 
     class Meta(object):
         model = Hub
         fields = [
-            'id',
-            'name',
-            'acronym',
-            'description',
-            'is_locked',
-            'hub_image',
-            'paper_count',
-            'subscriber_count',
-            'discussion_count',
-            'slug',
+            "id",
+            "name",
+            "description",
+            "slug",
         ]
