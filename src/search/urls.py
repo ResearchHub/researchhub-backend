dad37fd88e22df8178393ca8a109473a80f64cba
Kobe Attias <kobeattias@gmail.com>
2023-09-30 22:20:00 -0400

    Fix: Missing search routes (#1365)
    * adding back specific search urls
    
    * Fixing hubs name clash

--- src/search/urls.py
+++ src/search/urls.py
@@ -3,13 +3,25 @@ from rest_framework.routers import DefaultRouter
 
 from search.views import (
     CombinedView,
+    HubDocumentView,
     HubSuggesterDocumentView,
+    PaperDocumentView,
+    PersonDocumentView,
+    PostDocumentView,
+    ThreadDocumentView,
     UserSuggesterDocumentView,
 )
 
 router = DefaultRouter()
+person = router.register(r"person", PersonDocumentView, basename="person_document")
+paper = router.register(r"paper", PaperDocumentView, basename="paper_document")
+post = router.register(r"post", PostDocumentView, basename="post_document")
+thread = router.register(r"thread", ThreadDocumentView, basename="thread_document")
+hub = router.register(r"hub", HubDocumentView, basename="hub_document")
 user = router.register(r"user", UserSuggesterDocumentView, basename="user_document")
-hub = router.register(r"hub", HubSuggesterDocumentView, basename="hub_document")
+hub_suggester = router.register(
+    r"hubs", HubSuggesterDocumentView, basename="hub_document"
+)
 
 urlpatterns = [
     re_path(r"^", include(router.urls)),
