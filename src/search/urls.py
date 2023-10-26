16e4aeb5c356a00737c9403fcceb19954f00bffc
Patrick Lu <lightning.lu7@gmail.com>
2023-10-26 11:33:49 -0700

    add back search endpoint

--- src/search/urls.py
+++ src/search/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
 from search.views import (
+    CitationEntryDocumentView,
     CombinedView,
     HubDocumentView,
     HubSuggesterDocumentView,
@@ -19,6 +20,9 @@ post = router.register(r"post", PostDocumentView, basename="post_document")
 thread = router.register(r"thread", ThreadDocumentView, basename="thread_document")
 hub = router.register(r"hub", HubDocumentView, basename="hub_document")
 user = router.register(r"user", UserSuggesterDocumentView, basename="user_document")
+citation = router.register(
+    r"citation", CitationEntryDocumentView, basename="citation_document"
+)
 hub_suggester = router.register(
     r"hubs", HubSuggesterDocumentView, basename="hub_document"
 )
