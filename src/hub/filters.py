409cb1a6e4db051c86869b8f27f736d79254531f
Manveer Basra <m13basra@gmail.com>
2023-10-26 17:40:41 -0700

    Exclude votes from hubs popularity score

--- src/hub/filters.py
+++ src/hub/filters.py
@@ -1,11 +1,7 @@
-from datetime import timedelta
-
 from django.contrib.postgres.search import TrigramSimilarity
-from django.db.models import Count, F, FileField, Q
-from django.utils import timezone
+from django.db.models import F, FileField
 from django_filters import rest_framework as filters
 
-from discussion.reaction_models import Vote
 
 from .models import Hub
 
@@ -17,26 +13,9 @@ class ScoreOrderingFilter(filters.OrderingFilter):
 
     def filter(self, qs, value):
         if value and any(v in ["score", "-score"] for v in value):
-            two_weeks_ago = timezone.now().date() - timedelta(days=14)
-            num_upvotes = Count(
-                "papers__votes__vote_type",
-                filter=Q(
-                    papers__votes__vote_type=Vote.UPVOTE,
-                    papers__votes__created_date__gte=two_weeks_ago,
-                ),
-            )
-            num_downvotes = Count(
-                "papers__votes__vote_type",
-                filter=Q(
-                    papers__votes__vote_type=Vote.DOWNVOTE,
-                    papers__votes__created_date__gte=two_weeks_ago,
-                ),
-            )
-
             DISCUSSION_FACTOR = 10
             score = (
-                (num_upvotes - num_downvotes)
-                + DISCUSSION_FACTOR * F("discussion_count")
+                DISCUSSION_FACTOR * F("discussion_count")
                 + F("paper_count")
             )
 
