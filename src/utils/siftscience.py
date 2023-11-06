a67ebf766ed9eb69db4d117c64e2a219f4167f37
Patrick Lu <lightning.lu7@gmail.com>
2023-11-06 11:34:19 -0800

    only ban users not verified

--- src/utils/siftscience.py
+++ src/utils/siftscience.py
@@ -81,7 +81,12 @@ def update_user_risk_score(user, tracked_content):
 
 def check_user_risk(user):
     sift_risk_score = user.sift_risk_score
-    if sift_risk_score and sift_risk_score > 90 and user.id not in (34581,):
+    if (
+        sift_risk_score
+        and sift_risk_score > 90
+        and user.id not in (34581,)
+        and not user.is_verified
+    ):
         user.set_suspended(is_manual=False)
 
 
