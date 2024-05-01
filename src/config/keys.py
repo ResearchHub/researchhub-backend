cc99e358af5d03068b27c8b75c0b44221dd5a3a6
Gregor Zurowski <gregor@zurowski.net>
2024-05-01 15:58:11 +0000

    Remove unused Infura config and docs

--- src/config/keys.py
+++ src/config/keys.py
@@ -5,10 +5,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "development")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 
-INFURA_PROJECT_ID = os.environ.get("INFURA_PROJECT_ID", "")
-INFURA_PROJECT_SECRET = os.environ.get("INFURA_PROJECT_SECRET", "")
-INFURA_RINKEBY_ENDPOINT = f"https://goerli.infura.io/v3/{INFURA_PROJECT_ID}"
-
 ORCID_CLIENT_ID = os.environ.get("ORCID_CLIENT_ID")
 ORCID_CLIENT_SECRET = os.environ.get("ORCID_CLIENT_SECRET")
 ORCID_ACCESS_TOKEN = os.environ.get("ORCID_ACCESS_TOKEN")
