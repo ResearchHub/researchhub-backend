4652eaf6b2cf7ffb81bf92260c8e78d949e47fb2
thetechnocrat-dev <josh.mcmenemy@openzyme.bio>
2024-05-14 21:04:35 +0000

    first attempt at adding s3 local stack

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -13,4 +13,12 @@ python manage.py migrate
 python manage.py collectstatic --no-input
 cd -
 
+# Install AWS CLI
+echo "Post create script adding aws cli."
+pip install awscli
+
+# Create S3 bucket in LocalStack
+echo "Post create script creating local S3 bucket."
+aws --endpoint-url=http://localstack:4566 s3 mb s3://local-researchhub-bucket
+
 echo "Post create script complete."
