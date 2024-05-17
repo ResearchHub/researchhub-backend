c79411d9a2c676ace7640a185d40dfd4ce691e81
Kobe Attias <kobeattias@gmail.com>
2024-05-16 21:55:54 -0400

    [Author] Adding i10 index and country code to author model

---
+++ src/paper/related_models/work_authorship_model.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+from utils.models import DefaultModel
+
+
+class WorkAuthorship(DefaultModel):
+    FIRST_AUTHOR_POSITION = "first"
+    MIDDLE_AUTHOR_POSITION = "middle"
+    LAST_AUTHOR_POSITION = "last"
+
+    POSITION_CHOICES = [
+        (FIRST_AUTHOR_POSITION, "First"),
+        (MIDDLE_AUTHOR_POSITION, "Middle"),
+        (LAST_AUTHOR_POSITION, "Last"),
+    ]
+
+    institutions = models.ManyToManyField(
+        "institution.Institution",
+        related_name="authors",
+        blank=True,
+        null=True,
+    )
+
+    author = models.ForeignKey(
+        "Author",
+        on_delete=models.CASCADE,
+        related_name="authorships",
+    )
+
+    author_position = models.CharField(
+        max_length=10,
+        choices=POSITION_CHOICES,
+        null=False,
+        blank=False,
+    )
+
+    is_corresponding = models.BooleanField(
+        default=False,
+    )
+
+    raw_author_name = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
