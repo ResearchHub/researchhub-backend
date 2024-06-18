970b614f7bfa2f637a05a96a20e86f2b510cda62
Gregor Zurowski <gregor@zurowski.net>
2024-06-18 12:40:44 +0000

    Remove peer review model and code

--- src/peer_review/models.py
+++
@@ -1,3 +0,0 @@
-from .related_models.peer_review_request_model import PeerReviewRequest
-from .related_models.peer_review_model import PeerReview, PeerReviewDecision
-from .related_models.peer_review_invite_model import PeerReviewInvite
