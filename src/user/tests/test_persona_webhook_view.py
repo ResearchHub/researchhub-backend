515daf1d3d035371c0776d3bfa6c0cb1555e91d9
Gregor Zurowski <gregor@zurowski.net>
2024-07-09 11:55:30 +0000

    Add unit tests

---
+++ src/user/tests/test_persona_webhook_view.py
@@ -0,0 +1,57 @@
+from django.test import SimpleTestCase, override_settings
+import os
+
+
+class PersonaWebhookViewTests(SimpleTestCase):
+
+    WEBHOOK_SECRET = "wbhsec_researchhub"
+
+    def setUp(self):
+        self.webhook_approved_body = self.read_test_file(
+            "persona_webhook_approved.json"
+        )
+
+    def read_test_file(self, filename):
+        file_path = os.path.join(os.path.dirname(__file__), "test_files", filename)
+        with open(file_path, "r") as file:
+            return file.read()
+
+    @override_settings(PERSONA_WEBHOOK_SECRET=WEBHOOK_SECRET)
+    def test_post_webhook_without_signature(self):
+        response = self.client.post(
+            "/webhooks/persona/",
+            self.webhook_approved_body,
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message": "Unauthorized"})
+
+    @override_settings(PERSONA_WEBHOOK_SECRET=WEBHOOK_SECRET)
+    def test_post_webhook(self):
+        response = self.client.post(
+            "/webhooks/persona/",
+            self.webhook_approved_body,
+            content_type="application/json",
+            headers={
+                "Persona-Signature": "t=1720448965,v1=6d7a6f2356a65fd3a1734457430a1a2fe0c566349e0d472c45cb9281a7d3b68d"
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Webhook successfully processed"})
+
+    @override_settings(PERSONA_WEBHOOK_SECRET=WEBHOOK_SECRET)
+    def test_post_webhook_with_invalid_signature(self):
+        body = self.webhook_approved_body
+        response = self.client.post(
+            "/webhooks/persona/",
+            body,
+            content_type="application/json",
+            headers={
+                "Persona-Signature": "t=1720448965,v1=aaabbb2356a65fd3a1734457430a1a2fe0c566349e0d472c45cb9281a7d3b68d"
+            },
+        )
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"message": "Unauthorized"})
