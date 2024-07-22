5da6a408aaa22b160cfd05ae9d6331b3e5a898b5
Gregor Zurowski <gregor@zurowski.net>
2024-07-22 11:16:51 +0000

    Add test with invalid user ID

--- src/user/tests/test_views.py
+++ src/user/tests/test_views.py
@@ -252,3 +252,7 @@ class UserPopoverTests(APITestCase):
         res = self.client.get("/api/popover/1000/get_user/")
         self.assertEqual(res.status_code, 404)
         self.assertEqual(res.data["detail"], "Not found.")
+
+    def test_popover_for_invalid_id(self):
+        res = self.client.get("/api/popover/INVALID/get_user/")
+        self.assertEqual(res.status_code, 404)
