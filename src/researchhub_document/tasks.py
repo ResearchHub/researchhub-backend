c1adfb41d00a8a500c427361f25ce594d6f02606
Gregor Zurowski <gregor@zurowski.net>
2024-07-22 07:43:29 +0000

    Only calculate hot score if there is a document

--- src/researchhub_document/tasks.py
+++ src/researchhub_document/tasks.py
@@ -43,7 +43,8 @@ def recalc_hot_score_task(instance_content_type_id, instance_id):
         elif model_name == "citation":
             uni_doc = model_class.objects.get(id=instance_id).source
 
-        uni_doc.calculate_hot_score_v2(should_save=True)
+        if uni_doc:
+            uni_doc.calculate_hot_score_v2(should_save=True)
     except Exception as error:
         sentry.log_error(error)
 
