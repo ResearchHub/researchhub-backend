d77e79682ff7f3bc6b209ed6d807f58202079dd3
Gregor Zurowski <gregor@zurowski.net>
2024-07-23 08:07:17 +0000

    Make changes to copy of automated peer review bounty

--- src/discussion/reaction_views.py
+++ src/discussion/reaction_views.py
@@ -456,7 +456,17 @@ def create_automated_bounty(item):
                     {
                         "insert": "If you have any questions you may join our discord (https://discord.gg/researchhub) or email our Editor Lead, Maulik Dhandha (maulik.editor@researchhub.foundation)"
                     },
-                    {"attributes": {"list": "ordered"}, "insert": "\n"},
+                    {"attributes": {"list": "ordered"}, "insert": "\n\n"},
+                    {
+                        "insert": "We will award $150 in ResearchCoin (RSC) each for up to "
+                    },
+                    {
+                        "attributes": {"bold": True},
+                        "insert": "3 high quality and rigorous peer reviews ",
+                    },
+                    {
+                        "insert": "on this preprint.",
+                    },
                 ]
             }
 
