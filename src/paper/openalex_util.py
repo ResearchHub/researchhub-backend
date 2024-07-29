50278e6afb53bb4b83bc21f93700783fb5009141
Gregor Zurowski <gregor@zurowski.net>
2024-07-29 17:41:22 +0000

    Fix lookup of authorships to prevent duplicate records
    The lookup of authorship should be only based on author_id
    and paper_id. Other fields should not be considered.

--- src/paper/openalex_util.py
+++ src/paper/openalex_util.py
@@ -233,13 +233,18 @@ def process_openalex_authorships(openalex_authorships, related_paper_id):
             # Associate paper with author
             related_paper.authors.add(author)
 
+            is_corresponding = oa_authorship.get("is_corresponding")
+            raw_author_name = oa_authorship.get("author", {}).get("display_name")
+
             # Find or create authorship
-            authorship, created = Authorship.objects.get_or_create(
+            authorship, _ = Authorship.objects.get_or_create(
                 author=author,
-                author_position=author_position,
                 paper=related_paper,
-                is_corresponding=oa_authorship.get("is_corresponding"),
-                raw_author_name=oa_authorship.get("author", {}).get("display_name"),
+                defaults={
+                    "author_position": author_position,
+                    "is_corresponding": is_corresponding,
+                    "raw_author_name": raw_author_name,
+                },
             )
 
             authors_in_this_work.append(author)
