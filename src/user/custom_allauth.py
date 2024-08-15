7a510223c7fd1c951962bf2e6e52d1d0c1d2eeb5
Gregor Zurowski <gregor@zurowski.net>
2024-08-15 07:49:13 +0000

    [Minor] Organize imports

--- src/user/custom_allauth.py
+++ src/user/custom_allauth.py
@@ -1,13 +1,9 @@
 from allauth.account.adapter import DefaultAccountAdapter, get_adapter
 from allauth.account.forms import ResetPasswordForm
 from allauth.account.utils import user_pk_to_url_str
-from dj_rest_auth.registration.serializers import RegisterSerializer
 from dj_rest_auth.serializers import PasswordResetSerializer
 
-from researchhub.settings import (
-    ASSETS_BASE_URL,
-    BASE_FRONTEND_URL,
-)
+from researchhub.settings import ASSETS_BASE_URL, BASE_FRONTEND_URL
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
