898da2995ecc3c4923633bad63c25cff2effa73c
Gregor Zurowski <gregor@zurowski.net>
2024-10-09 23:11:26 +0000

    Remove distribution amount model

--- src/reputation/models.py
+++ src/reputation/models.py
@@ -4,7 +4,6 @@ from reputation.related_models.bounty_fee import BountyFee
 from reputation.related_models.contribution import Contribution
 from reputation.related_models.deposit import Deposit
 from reputation.related_models.distribution import Distribution
-from reputation.related_models.distribution_amount import DistributionAmount
 from reputation.related_models.escrow import Escrow
 from reputation.related_models.paid_status_mixin import PaidStatusModelMixin
 from reputation.related_models.paper_reward import HubCitationValue, PaperReward
