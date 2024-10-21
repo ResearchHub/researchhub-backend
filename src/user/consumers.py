789407c753c18d8a354e9dc3ac6ec6e041955d9f
Gregor Zurowski <gregor@zurowski.net>
2024-10-21 08:44:55 +0000

    [Minor] Remove unused imports

--- src/user/consumers.py
+++ src/user/consumers.py
@@ -1,6 +1,5 @@
 import json
 
-from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 from channels.exceptions import StopConsumer
 from channels.generic.websocket import AsyncWebsocketConsumer
