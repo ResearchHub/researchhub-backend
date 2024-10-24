a31e8833f1998472a090742dd39a512c29d4236c
koutst <koutst@protonmail.com>
2024-10-24 20:21:11 +0000

    Add test for dynamic paper serializer

--- src/paper/tests/test_serializers.py
+++ src/paper/tests/test_serializers.py
@@ -2,8 +2,8 @@ from django.forms.models import model_to_dict
 from django.test import TestCase
 
 from hub.tests.helpers import create_hub
-from paper.models import Paper, PaperVersion
-from paper.serializers import PaperSerializer
+from paper.models import PaperVersion
+from paper.serializers import DynamicPaperSerializer, PaperSerializer
 from paper.tests import helpers
 
 
@@ -55,6 +55,52 @@ class PaperSerializersTests(TestCase):
         )
 
         serialized2 = PaperSerializer(paper2)
+        self.assertEqual(serialized2.data["version"], 2)
+        self.assertEqual(
+            serialized2.data["version_list"],
+            [
+                {
+                    "version": 1,
+                    "paper_id": paper.id,
+                    "message": "Test Message",
+                    "published_date": paper.paper_publish_date,
+                },
+                {
+                    "version": 2,
+                    "paper_id": paper2.id,
+                    "message": "Test Message 2",
+                    "published_date": paper2.paper_publish_date,
+                },
+            ],
+        )
+
+    def test_dynamic_paper_serializer_paper_versions(self):
+        paper = helpers.create_paper(title="Serialized Paper Title")
+        PaperVersion.objects.create(
+            paper=paper, version=1, base_doi="10.1234/test", message="Test Message"
+        )
+
+        serialized = DynamicPaperSerializer(paper)
+        self.assertEqual(serialized.data["version"], 1)
+        self.assertEqual(
+            serialized.data["version_list"],
+            [
+                {
+                    "version": 1,
+                    "paper_id": paper.id,
+                    "message": "Test Message",
+                    "published_date": paper.paper_publish_date,
+                }
+            ],
+        )
+
+        # Create another version
+        paper2 = helpers.create_paper(title="Serialized Paper Title V2")
+        PaperVersion.objects.create(
+            paper=paper2, version=2, base_doi="10.1234/test", message="Test Message 2"
+        )
+
+        serialized2 = DynamicPaperSerializer(paper2)
         self.assertEqual(serialized2.data["version"], 2)
         self.assertEqual(
             serialized2.data["version_list"],
