c730b99706d99827091ccfa6f3515f7ee2d7bf34
Gregor Zurowski <gregor@zurowski.net>
2024-11-17 17:22:29 +0000

    Check for valid URLs

--- src/purchase/serializers/checkout_serializer.py
+++ src/purchase/serializers/checkout_serializer.py
@@ -4,8 +4,8 @@ import paper
 
 
 class CheckoutSerializer(serializers.Serializer):
-    success_url = serializers.CharField(required=True)
-    failure_url = serializers.CharField(required=True)
+    success_url = serializers.URLField(required=True)
+    failure_url = serializers.URLField(required=True)
     paper = serializers.PrimaryKeyRelatedField(
         queryset=paper.models.Paper.objects.all(),
         required=True,
