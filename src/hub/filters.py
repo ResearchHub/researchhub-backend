7cc70673704b65be83d05b95e605c9ad10e1d4dd
Gregor Zurowski <gregor@zurowski.net>
2024-11-18 14:12:56 +0000

    Remove unused fuzzy filtering

--- src/hub/filters.py
+++ src/hub/filters.py
@@ -1,4 +1,3 @@
-from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models import FileField
 from django_filters import rest_framework as filters
 
@@ -6,9 +5,6 @@ from .models import Hub
 
 class HubFilter(filters.FilterSet):
     name__iexact = filters.Filter(field_name="name", lookup_expr="iexact")
-    name__fuzzy = filters.Filter(
-        field_name="name", method="name_trigram_similarity_search"
-    )
 
     class Meta:
         model = Hub
@@ -18,10 +14,3 @@ class HubFilter(filters.FilterSet):
                 "filter_class": filters.CharFilter,
             }
         }
-
-    def name_trigram_similarity_search(self, qs, name, value):
-        qs = qs.annotate(similarity=TrigramSimilarity(name, value)).filter(
-            similarity__gt=0.15
-        )
-        qs = qs.order_by("-similarity")
-        return qs
