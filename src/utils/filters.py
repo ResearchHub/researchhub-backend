519cc1cad27a4786a88c0cbb6bf7655ae0601b9f
Gregor Zurowski <gregor@zurowski.net>
2024-11-18 14:22:08 +0000

    [Minor] Apply formatter

--- src/utils/filters.py
+++ src/utils/filters.py
@@ -2,35 +2,35 @@ from django.db.models import Q
 from django_filters import rest_framework as filters
 
 FIELD_LOOKUPS = (
-    'exact',
-    'iexact',
-    'contains',
-    'icontains',
-    'in',
-    'gt',
-    'gte',
-    'lt',
-    'lte',
-    'startswith',
-    'istartswith',
-    'endswith',
-    'iendswith',
-    'range',
-    'date',
-    'year',
-    'iso_year',
-    'month',
-    'day',
-    'week',
-    'week_day',
-    'quarter',
-    'time',
-    'hour',
-    'minute',
-    'second',
-    'isnull',
-    'regex',
-    'iregex'
+    "exact",
+    "iexact",
+    "contains",
+    "icontains",
+    "in",
+    "gt",
+    "gte",
+    "lt",
+    "lte",
+    "startswith",
+    "istartswith",
+    "endswith",
+    "iendswith",
+    "range",
+    "date",
+    "year",
+    "iso_year",
+    "month",
+    "day",
+    "week",
+    "week_day",
+    "quarter",
+    "time",
+    "hour",
+    "minute",
+    "second",
+    "isnull",
+    "regex",
+    "iregex",
 )
 
 
@@ -43,13 +43,13 @@ class ListExcludeFilter(filters.CharFilter):
         """
         remove empty items in case of ?number=1,,2
         """
-        return [v for v in value_list if v != u'']
+        return [v for v in value_list if v != ""]
 
     def customize(self, value):
         return value
 
     def filter(self, qs, value):
-        multiple_vals = value.split(u",")
+        multiple_vals = value.split(",")
         multiple_vals = self.sanitize(multiple_vals)
         multiple_vals = map(self.customize, multiple_vals)
         f = Q()
@@ -60,17 +60,18 @@ class ListExcludeFilter(filters.CharFilter):
 
 
 class OrFilter(filters.CharFilter):
-    '''
+    """
     Syntax:
 
     ?or_filter=field1|field2|field3~value1,value2
 
-    '''
-    def __init__(self, **kwargs):
-        self.model = kwargs.pop('model', None)
+    """
 
-        if self.model is None or not hasattr(self.model, '_meta'):
-            raise ValueError('no model provided to or_filter')
+    def __init__(self, **kwargs):
+        self.model = kwargs.pop("model", None)
+
+        if self.model is None or not hasattr(self.model, "_meta"):
+            raise ValueError("no model provided to or_filter")
 
         # Force field_name to be or_filter?
         # if kwargs.get('field_name', 'or_filter') != 'or_filter':
@@ -78,26 +79,25 @@ class OrFilter(filters.CharFilter):
         # kwargs['field_name'] = 'or_filter'
 
         # Set field_name to or_filter if none provided
-        if kwargs.get('field_name') is None:
-            kwargs['field_name'] = 'or_filter'
+        if kwargs.get("field_name") is None:
+            kwargs["field_name"] = "or_filter"
 
         super(OrFilter, self).__init__(**kwargs)
 
     def sanitize_keys(self, keys):
-        """
-        """
+        """ """
         return [k for k in keys if self.is_valid_field(k)]
 
     def is_valid_field(self, key):
-        return key != u'' and self.get_field(key, self.model)
+        return key != "" and self.get_field(key, self.model)
 
     def get_field(self, field, model):
-        if '__' in field:
-            if field[field.index('__') + 2:] in FIELD_LOOKUPS:
-                return self.get_field(field[:field.index('__')], model)
+        if "__" in field:
+            if field[field.index("__") + 2 :] in FIELD_LOOKUPS:
+                return self.get_field(field[: field.index("__")], model)
             return self.get_field(
-                field[field.index('__') + 2:],
-                model._meta.get_field(field[:field.index('__')]).related_model
+                field[field.index("__") + 2 :],
+                model._meta.get_field(field[: field.index("__")]).related_model,
             )
         else:
             return model._meta.get_field(field)
@@ -106,31 +106,30 @@ class OrFilter(filters.CharFilter):
         """
         remove empty items in case of ~1,,2
         """
-        return [v for v in value_list if v != u'']
+        return [v for v in value_list if v != ""]
 
     def sanitize_value(self, key, value):
-        """
-        """
+        """ """
         if not value:
-            raise ValueError('no value provided')
+            raise ValueError("no value provided")
 
         internal_type = self.get_field(key, self.model).get_internal_type()
-        if internal_type == 'BooleanField':
-            return value.lower() == 'true'
-        elif internal_type == 'AutoField' or internal_type == 'IntegerField':
+        if internal_type == "BooleanField":
+            return value.lower() == "true"
+        elif internal_type == "AutoField" or internal_type == "IntegerField":
             return int(value)
         else:
             return value
 
     def filter(self, qs, value):
-        if value == u'':
+        if value == "":
             return qs
 
-        key_names, values = value.split(u'~')
-        keys = key_names.split(u'|')
+        key_names, values = value.split("~")
+        keys = key_names.split("|")
 
         sanitized_keys = self.sanitize_keys(keys)
-        sanitized_values = self.sanitize_values(values.split(u','))
+        sanitized_values = self.sanitize_values(values.split(","))
 
         f = Q()
         for k in sanitized_keys:
