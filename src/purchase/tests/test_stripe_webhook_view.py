825d77c644bba07f2d6a1660e8fd4c25709bdce8
Gregor Zurowski <gregor@zurowski.net>
2024-11-20 13:31:03 +0000

    Store details of processed checkout webhook events

--- src/purchase/tests/test_stripe_webhook_view.py
+++ src/purchase/tests/test_stripe_webhook_view.py
@@ -6,6 +6,10 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 
+from paper.related_models.paper_model import Paper
+from purchase.related_models.payment_model import Payment
+from user.related_models.user_model import User
+
 
 class StripeWebhookTestCase(TestCase):
     def setUp(self):
@@ -15,12 +19,21 @@ class StripeWebhookTestCase(TestCase):
     @patch("stripe.Webhook.construct_event")
     def test_webhook_with_checkout_session_completed(self, mock_construct_event):
         # Arrange
+        paper = Paper.objects.create()
+        user = User.objects.create()
+
         event_data = {
             "type": "checkout.session.completed",
             "data": {
                 "object": {
-                    "id": "paymentIntentId1",
+                    "id": "id1",
                     "amount_total": 1000,
+                    "currency": "usd",
+                    "payment_intent": "paymentIntentId1",
+                    "metadata": {
+                        "paper_id": paper.id,
+                        "user_id": user.id,
+                    },
                 },
             },
         }
@@ -37,6 +50,42 @@ class StripeWebhookTestCase(TestCase):
         # Assert
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {"message": "Webhook successfully processed"})
+
+        payment = Payment.objects.get(external_payment_id="paymentIntentId1")
+        self.assertIsNotNone(payment)
+
+    @patch("stripe.Webhook.construct_event")
+    def test_webhook_with_checkout_session_missing_metadata(self, mock_construct_event):
+        # Arrange
+        user = User.objects.create()
+
+        event_data = {
+            "type": "checkout.session.completed",
+            "data": {
+                "object": {
+                    "id": "id1",
+                    "amount_total": 1000,
+                    "currency": "usd",
+                    "payment_intent": "paymentIntentId1",
+                    "metadata": {
+                        # paper_id is missing!
+                        "user_id": user.id,
+                    },
+                },
+            },
+        }
+        mock_construct_event.return_value = event_data
+
+        # Act
+        response = self.client.post(
+            self.url,
+            data=json.dumps(event_data),
+            content_type="application/json",
+            headers={"Stripe-Signature": self.valid_signature},
+        )
+
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     @patch("stripe.Webhook.construct_event")
     def test_webhook_with_payment_intent_succeeded(self, mock_construct_event):
