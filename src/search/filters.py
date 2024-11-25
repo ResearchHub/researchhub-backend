055863ae01be7ada9ceb9b6c5d97899014702898
Gregor Zurowski <gregor@zurowski.net>
2024-11-25 16:47:25 +0000

    [Minor] Apply formatter

--- src/search/filters.py
+++ src/search/filters.py
@@ -4,40 +4,33 @@ from rest_framework import filters
 
 class ElasticsearchFuzzyFilter(filters.SearchFilter):
 
-    def filter_queryset(
-        self,
-        request,
-        queryset,
-        view,
-        search=None,
-        limit=None
-    ):
+    def filter_queryset(self, request, queryset, view, search=None, limit=None):
         """
         Builds and executes the elastic search query, returning the response.
         """
         if search is None:
-            search = getattr(view, 'search')
-        fields = getattr(view, 'search_fields')
-        terms = ' '.join(self.get_search_terms(request))
+            search = getattr(view, "search")
+        fields = getattr(view, "search_fields")
+        terms = " ".join(self.get_search_terms(request))
 
         search_query = Q(
-            'function_score',
+            "function_score",
             query={
-                'multi_match': {
-                    'query': terms,
-                    'fields': fields,
-                    'fuzziness': 'AUTO',
+                "multi_match": {
+                    "query": terms,
+                    "fields": fields,
+                    "fuzziness": "AUTO",
                 }
             },
             functions=[
                 query.SF(
-                    'script_score',
+                    "script_score",
                     script={
-                        'lang': 'painless',
-                        'inline': "if (!doc.containsKey('score')) { return _score; } else { return (Math.max(0, doc['score'].value) * 10) + _score; }"
-                    }
+                        "lang": "painless",
+                        "inline": "if (!doc.containsKey('score')) { return _score; } else { return (Math.max(0, doc['score'].value) * 10) + _score; }",
+                    },
                 )
-            ]
+            ],
         )
 
         es = search.query(search_query)
