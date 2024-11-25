e89805c773e077858ebdc4249d989863438aad16
Gregor Zurowski <gregor@zurowski.net>
2024-11-25 13:10:02 +0000

    Add debounce period to signal processor

--- src/search/celery.py
+++ src/search/celery.py
@@ -1,9 +1,13 @@
 from celery import shared_task
 from django.apps import apps
+from django.core.cache import cache
 from django_elasticsearch_dsl.registries import registry
 from django_elasticsearch_dsl.signals import RealTimeSignalProcessor
 
 import utils.sentry as sentry
+
+# The debounce period in seconds
+DEBOUNCE_PERIOD = 10
 
 
 class CelerySignalProcessor(RealTimeSignalProcessor):
@@ -14,31 +18,43 @@ class CelerySignalProcessor(RealTimeSignalProcessor):
         model_name = model.__name__
 
         if model in registry._models:
-            self.registry_update_task.apply_async(
-                (pk, app_label, model_name), countdown=3
-            )
+            cache_key = f"registry_update_task_{app_label}_{model_name}_{pk}"
+            if not cache.get(cache_key):
+                self.registry_update_task.apply_async(
+                    (pk, app_label, model_name), countdown=DEBOUNCE_PERIOD
+                )
+                # Add cache entry to prevent duplicate tasks within debounce period
+                cache.set(cache_key, True, timeout=DEBOUNCE_PERIOD)
 
         if model in registry._related_models:
-            self.registry_update_related_task.apply_async(
-                (pk, app_label, model_name), countdown=3
-            )
+            cache_key = f"registry_update_related_task_{app_label}_{model_name}_{pk}"
+            if not cache.get(cache_key):
+                self.registry_update_related_task.apply_async(
+                    (pk, app_label, model_name), countdown=DEBOUNCE_PERIOD
+                )
+                # Add cache entry to prevent duplicate tasks within debounce period
+                cache.set(cache_key, True, timeout=DEBOUNCE_PERIOD)
 
     @shared_task(ignore_result=True)
     def registry_update_task(pk, app_label, model_name):
         try:
             model = apps.get_model(app_label, model_name)
+            instance = model.objects.get(pk=pk)
+            registry.update(instance)
         except LookupError as e:
             sentry.log_error(e)
+        except model.DoesNotExist:
+            # No-op: Instance was deleted before it could be updated.
             pass
-        else:
-            registry.update(model.objects.get(pk=pk))
 
     @shared_task(ignore_result=True)
     def registry_update_related_task(pk, app_label, model_name):
         try:
             model = apps.get_model(app_label, model_name)
+            instance = model.objects.get(pk=pk)
+            registry.update_related(instance)
         except LookupError as e:
             sentry.log_error(e)
+        except model.DoesNotExist:
+            # No-op: Instance was deleted before it could be updated.
             pass
-        else:
-            registry.update_related(model.objects.get(pk=pk))
