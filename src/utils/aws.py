7d40545e99f47b3e398f11336ad164de6f13b9d4
Gregor Zurowski <gregor@zurowski.net>
2024-12-02 16:24:08 +0000

    Remove unused AWS utility functions

--- src/utils/aws.py
+++ src/utils/aws.py
@@ -19,21 +19,6 @@ def get_s3_object_name(key):
     return parts[-1]
 
 
-def get_s3_url(bucket, key, with_credentials=False):
-    s3 = "s3://"
-    if with_credentials is True:
-        return f"{s3}{settings.AWS_ACCESS_KEY_ID}:{settings.AWS_SECRET_ACCESS_KEY}@{bucket}{key}"
-    return f"{s3}{bucket}{key}"
-
-
-def http_to_s3(url, with_credentials=False):
-    parsed = urlparse(url)
-    bucket = parsed.netloc.split(".", maxsplit=1)[0]
-    key = parsed.path
-
-    return get_s3_url(bucket, key, with_credentials=with_credentials)
-
-
 def upload_to_s3(data, folder):
     ALLOWED_EXTENSIONS = ["pdf"]
     content_type = data.content_type.split("/")[1]
