612dd209e4a269bd542e6b27ff6ed6abb4f90794
Gregor Zurowski <gregor@zurowski.net>
2024-12-03 10:03:00 +0000

    Remove skipped tests

--- src/paper/tests/test_views.py
+++ src/paper/tests/test_views.py
@@ -1,6 +1,5 @@
 import json
 import random
-from unittest import skip
 from unittest.mock import PropertyMock, patch
 
 from django.test import Client, TestCase
@@ -550,116 +549,6 @@ class PaperViewsTests(TestCase):
         self.assertEqual(response.status_code, 200)
         result = response.data
         self.assertEqual(result["base_rewards"], 100)
-
-    @skip
-    def test_search_by_url_arxiv(self):
-        url = self.base_url + "search_by_url/"
-        data = {"url": "https://arxiv.org/abs/1407.3561v1", "search": True}
-        response = get_authenticated_post_response(self.user, url, data)
-        self.assertEqual(response.status_code, 200)
-        result = response.data
-        self.assertEqual(result["url"], data["url"])
-        self.assertFalse(result["url_is_pdf"])
-        self.assertEqual(
-            result["csl_item"]["title"],
-            "IPFS - Content Addressed, Versioned, P2P File System",
-        )
-        self.assertEqual(
-            result["oa_pdf_location"]["url_for_pdf"],
-            "https://arxiv.org/pdf/1407.3561v1.pdf",
-        )
-        self.assertIsInstance(result["search"], list)
-
-    @skip
-    def test_search_by_url_arxiv_pdf(self):
-        url = self.base_url + "search_by_url/"
-        data = {"url": "https://arxiv.org/pdf/1407.3561.pdf"}
-        response = get_authenticated_post_response(self.user, url, data)
-        self.assertEqual(response.status_code, 200)
-        result = response.data
-        self.assertEqual(result["url"], data["url"])
-        self.assertTrue(result["url_is_pdf"])
-        self.assertFalse(result["url_is_unsupported_pdf"])
-        self.assertEqual(
-            result["csl_item"]["title"],
-            "IPFS - Content Addressed, Versioned, P2P File System",
-        )
-        pdf_location = result["oa_pdf_location"]
-        self.assertEqual(
-            pdf_location["url_for_pdf"], "https://arxiv.org/pdf/1407.3561.pdf"
-        )
-        self.assertEqual(pdf_location["license"], "cc-by")
-        self.assertEqual(
-            pdf_location["url_for_landing_page"], "https://arxiv.org/abs/1407.3561"
-        )
-        self.assertFalse("search" in result)
-
-    @skip
-    def test_search_by_url_publisher(self):
-        url = self.base_url + "search_by_url/"
-        data = {"url": "https://www.nature.com/articles/s41586-019-1099-1"}
-        response = get_authenticated_post_response(self.user, url, data)
-        self.assertEqual(response.status_code, 200)
-        result = response.data
-        self.assertEqual(result["url"], data["url"])
-        self.assertFalse(result["url_is_pdf"])
-        self.assertFalse(result["url_is_unsupported_pdf"])
-        self.assertEqual(
-            result["csl_item"]["title"],
-            "Restoration of brain circulation and cellular functions hours post-mortem",
-        )  # noqa E501
-        self.assertEqual(result["csl_item"]["DOI"], "10.1038/s41586-019-1099-1")
-
-    @skip
-    def test_search_by_url_doi(self):
-        url = self.base_url + "search_by_url/"
-        data = {"url": "https://doi.org/10.1038/ng.3259"}
-        response = get_authenticated_post_response(self.user, url, data)
-        self.assertEqual(response.status_code, 200)
-        result = response.data
-        self.assertEqual(result["url"], data["url"])
-        self.assertFalse(result["url_is_pdf"])
-        self.assertFalse(result["url_is_unsupported_pdf"])
-        self.assertEqual(
-            result["csl_item"]["title"],
-            "Understanding multicellular function and disease with human tissue-specific networks",
-        )  # noqa E501
-        self.assertEqual(result["csl_item"]["DOI"], "10.1038/ng.3259")
-        self.assertEqual(
-            result["oa_pdf_location"]["url_for_pdf"],
-            "http://europepmc.org/articles/pmc4828725?pdf=render",
-        )
-
-    @skip
-    def test_search_by_url_pmid(self):
-        """
-        Search by PMID without a DOI from an inactive journal
-        """
-        url = self.base_url + "search_by_url/"
-        data = {"url": "https://www.ncbi.nlm.nih.gov/pubmed/18888140"}
-        response = get_authenticated_post_response(self.user, url, data)
-        self.assertEqual(response.status_code, 200)
-        result = response.data
-        self.assertEqual(result["url"], data["url"])
-        self.assertFalse(result["url_is_pdf"])
-        self.assertFalse(result["url_is_unsupported_pdf"])
-        self.assertEqual(
-            result["csl_item"]["title"],
-            "[Major achievements in the second plan year in the Soviet Union].",
-        )  # noqa E501
-        self.assertIsNone(result["oa_pdf_location"])
-
-    @skip
-    def test_search_by_url_unsupported_pdf(self):
-        url = self.base_url + "search_by_url/"
-        data = {"url": "https://bitcoin.org/bitcoin.pdf"}
-        response = get_authenticated_post_response(self.user, url, data)
-        self.assertEqual(response.status_code, 200)
-        result = response.data
-        self.assertTrue(result["url_is_pdf"])
-        self.assertTrue(result["url_is_unsupported_pdf"])
-        self.assertEqual(result["csl_item"]["URL"], data["url"])
-        self.assertEqual(result["oa_pdf_location"]["url_for_pdf"], data["url"])
 
     def test_search_by_url_bad(self):
         url = self.base_url + "search_by_url/"
