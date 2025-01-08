1ace1bf82568f01f2050a8719b999fe1a9f17c14
Gregor Zurowski <gregor@zurowski.net>
2025-01-08 12:09:42 +0000

    [Minor] Apply formatter

--- src/ethereum/tests/test_lib.py
+++ src/ethereum/tests/test_lib.py
@@ -7,30 +7,27 @@ from ethereum.lib import convert_reputation_amount_to_token_amount, get_private_
 
 class EthereumLibTests(TestCase):
     def setUp(self):
-        self.token_ticker = 'RSC'
+        self.token_ticker = "RSC"
 
     def test_convert_reputation_amount_to_token_amount(self):
         rep = 1
         integer, decimal = convert_reputation_amount_to_token_amount(
-            self.token_ticker,
-            rep
+            self.token_ticker, rep
         )
         self.assertEqual(integer, 1000000000000000000)
-        self.assertEqual(decimal, '1.0')
+        self.assertEqual(decimal, "1.0")
         rep = 12
         integer, decimal = convert_reputation_amount_to_token_amount(
-            self.token_ticker,
-            rep
+            self.token_ticker, rep
         )
         self.assertEqual(integer, 12000000000000000000)
-        self.assertEqual(decimal, '12.0')
+        self.assertEqual(decimal, "12.0")
         rep = 210
         integer, decimal = convert_reputation_amount_to_token_amount(
-            self.token_ticker,
-            rep
+            self.token_ticker, rep
         )
         self.assertEqual(integer, 210000000000000000000)
-        self.assertEqual(decimal, '210.0')
+        self.assertEqual(decimal, "210.0")
 
     def test_convert_reputation_amount_to_token_amount_with_negatives(self):
         rep = -5
