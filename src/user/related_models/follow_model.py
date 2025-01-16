87baa6021859496bd9e5a63a91c8cde804e7cfc1
Gregor Zurowski <gregor@zurowski.net>
2025-01-16 15:57:30 +0000

    Add initial version of follow model

---
+++ src/user/related_models/follow_model.py
@@ -0,0 +1,35 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from utils.models import DefaultModel
+
+
+class Follow(DefaultModel):
+    ALLOWED_FOLLOW_MODELS = ["hub", "paper", "user"]
+
+    user = models.ForeignKey(
+        "user.User",
+        on_delete=models.CASCADE,
+        related_name="following",
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={"model__in": ALLOWED_FOLLOW_MODELS},
+    )
+    object_id = models.PositiveIntegerField()
+    followed_object = GenericForeignKey("content_type", "object_id")
+
+    class Meta:
+        unique_together = ("user", "content_type", "object_id")
+
+    def clean(self):
+        if self.content_type.model not in self.ALLOWED_FOLLOW_MODELS:
+            raise ValidationError(f"Invalid follow model: {self.content_type.model}")
+        super().clean()
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
