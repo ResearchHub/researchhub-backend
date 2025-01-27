02cbe505613e392e0e1827458bad04cf5b05ccab
koutst <koutst@protonmail.com>
2025-01-27 15:02:22 +0000

    Include parent, action, user in feed entry model

--- src/feed/models.py
+++ src/feed/models.py
@@ -1,8 +1,26 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
+from user.models import User
+from utils.models import DefaultModel
 
-class FeedEntry(models.Model):
-    content_type = models.CharField(max_length=255)
-    content_object = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(auto_now_add=True)
+
+class FeedEntry(DefaultModel):
+    PUBLISH = "PUBLISH"
+    action_choices = [
+        (PUBLISH, "PUBLISH"),
+    ]
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
+
+    parent_content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, null=True, blank=True
+    )
+    parent_object_id = models.PositiveIntegerField(null=True, blank=True)
+    parent_item = GenericForeignKey("parent_content_type", "parent_object_id")
+
+    action = models.TextField(choices=action_choices)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
