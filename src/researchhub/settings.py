830ea5b66fba4eba35cfc1e6e055cc9936995de6
Gregor Zurowski <gregor@zurowski.net>
2025-02-05 14:04:01 +0000

    Add staging domain for new web application

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -187,6 +187,8 @@ CORS_ORIGIN_WHITELIST = [
     "https://researchhub.com",
     "https://staging.researchhub.com",
     "https://www.staging.researchhub.com",
+    "https://v2.staging.researchhub.com",
+    "https://www.v2.staging.researchhub.com",
     "http://127.0.0.1:3000",
     "https://word.researchhub.com",
     "https://web-v2-eight.vercel.app",
