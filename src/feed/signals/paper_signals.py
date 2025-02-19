049ed00029703dcaa26878a3926fb209aeb5198c
koutst <koutst@protonmail.com>
2025-02-19 21:40:44 +0000

    Fix paper signals and tests

--- src/feed/signals/paper_signals.py
+++ src/feed/signals/paper_signals.py
@@ -2,7 +2,9 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
-from feed.tasks import create_feed_entry
+from feed.tasks import create_feed_entry, delete_feed_entry
+from hub.models import Hub
+from paper.models import Paper
 from researchhub_document.related_models.researchhub_unified_document_model import (
     ResearchhubUnifiedDocument,
 )
@@ -43,6 +45,27 @@ def handle_unified_doc_hubs_changed(sender, instance, action, pk_set, **kwargs):
                     paper.id,
                     ContentType.objects.get_for_model(paper).id,
                     "PUBLISH",
+                    hub.id,
+                    ContentType.objects.get_for_model(hub).id,
+                ),
+                priority=1,
+            )
+    elif action == "post_remove":
+        for hub_id in pk_set:
+            if (
+                isinstance(instance, ResearchhubUnifiedDocument)
+                and instance.document_type == "PAPER"
+            ):
+                hub = Hub.objects.get(id=hub_id)
+                paper = instance.paper
+            else:  # instance is Hub
+                hub = instance
+                paper = Paper.objects.get(id=hub_id)
+
+            delete_feed_entry.apply_async(
+                args=(
+                    paper.id,
+                    ContentType.objects.get_for_model(paper).id,
                     hub.id,
                     ContentType.objects.get_for_model(hub).id,
                 ),
--- src/feed/tests/signals/test_paper_signals.py
+++ src/feed/tests/signals/test_paper_signals.py
@@ -1,5 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.utils import timezone
 
 from feed.models import FeedEntry
@@ -8,6 +8,7 @@ from paper.tests.helpers import create_paper
 
 
 class PaperSignalsTests(TestCase):
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True, CELERY_TASK_EAGER_PROPAGATES=True)
     def setUp(self):
         self.hub = create_hub(name="Test Hub")
         self.paper = create_paper(title="Test Paper")
@@ -15,16 +16,24 @@ class PaperSignalsTests(TestCase):
         self.paper.save()
         self.paper.unified_document.hubs.add(self.hub)
 
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True, CELERY_TASK_EAGER_PROPAGATES=True)
     def test_feed_entries_are_created_when_hubs_are_added(self):
         """Test that feed entries are created when unified document hubs are added."""
         feed_entries = FeedEntry.objects.filter(
             content_type=ContentType.objects.get_for_model(self.paper),
             object_id=self.paper.id,
         )
-        self.assertEqual(len(feed_entries), self.paper.unified_document.hubs.count())
+        self.assertEqual(len(feed_entries), self.paper.hubs.count())
 
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True, CELERY_TASK_EAGER_PROPAGATES=True)
     def test_feed_entries_are_deleted_when_hubs_are_removed(self):
         """Test that feed entries are deleted when unified document hubs are removed."""
+        feed_entries = FeedEntry.objects.filter(
+            content_type=ContentType.objects.get_for_model(self.paper),
+            object_id=self.paper.id,
+        )
+        self.assertEqual(len(feed_entries), self.paper.hubs.count())
+
         initial_hub = self.paper.unified_document.hubs.first()
         self.paper.unified_document.hubs.remove(initial_hub)
 
@@ -32,4 +41,4 @@ class PaperSignalsTests(TestCase):
             content_type=ContentType.objects.get_for_model(self.paper),
             object_id=self.paper.id,
         )
-        self.assertEqual(len(feed_entries), self.paper.unified_document.hubs.count())
+        self.assertEqual(len(feed_entries), self.paper.hubs.count())
