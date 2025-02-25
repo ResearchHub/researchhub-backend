6ccbca58c4c1af968acb4c916246430c3cc2ce21
Gregor Zurowski <gregor@zurowski.net>
2025-02-25 17:52:16 +0000

    [Minor] Remove unused variables

--- src/researchhub/views/views.py
+++ src/researchhub/views/views.py
@@ -6,17 +6,17 @@ from django.template.loader import render_to_string
 from researchhub.settings import BASE_DIR
 
 
-def index(request):
+def index():
     return HttpResponse("Authenticate with a token in the Authorization header.")
 
 
-def permissions(request):
+def permissions():
     path = os.path.join(BASE_DIR, "static", "researchhub", "user_permissions.json")
     with open(path, "r") as file:
         data = file.read()
     return HttpResponse(content=data, content_type="application/json")
 
 
-def robots_txt(request):
+def robots_txt():
     content = render_to_string("robots.txt")
     return HttpResponse(content, content_type="text/plain")
