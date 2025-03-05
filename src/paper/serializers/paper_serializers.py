34c93f4978bdebde26cc1c08db1fa1bdc98d78d0
Gregor Zurowski <gregor@zurowski.net>
2025-03-05 09:02:50 +0000

    Fix `Flag` import (from `reaction_models`)

--- src/paper/serializers/paper_serializers.py
+++ src/paper/serializers/paper_serializers.py
@@ -11,7 +11,7 @@ from django.http import QueryDict
 
 import utils.sentry as sentry
 from citation.constants import JOURNAL_ARTICLE
-from discussion.models import Flag as GrmFlag
+from discussion.reaction_models import Flag as GrmFlag
 from discussion.reaction_models import Vote as GrmVote
 from discussion.reaction_serializers import (
     DynamicVoteSerializer as DynamicGrmVoteSerializer,
