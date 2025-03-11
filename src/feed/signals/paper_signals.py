b3b74ad97bcd1f4ee9899346c5b08c0c2822854f
Gregor Zurowski <gregor@zurowski.net>
2025-03-11 11:59:46 +0000

    Wrap paper feed entry creation into try/catch block

--- src/feed/signals/paper_signals.py
+++ src/feed/signals/paper_signals.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
@@ -15,6 +17,8 @@ The signal handlers are responsible for creating and deleting feed entries
 when papers are added to or removed from hubs.
 """
 
+logger = logging.getLogger(__name__)
+
 
 @receiver(
     m2m_changed,
@@ -22,55 +26,75 @@ when papers are added to or removed from hubs.
     dispatch_uid="unified_doc_hubs_changed",
 )
 def handle_unified_doc_hubs_changed(sender, instance, action, pk_set, **kwargs):
-    if action == "post_add":
-        for entity_id in pk_set:
-            if (
-                isinstance(instance, ResearchhubUnifiedDocument)
-                and instance.document_type == "PAPER"
-            ):
-                hub = instance.hubs.get(id=entity_id)
-                paper = instance.paper
-            elif isinstance(instance, Hub):  # instance is Hub
-                hub = instance
-                paper = hub.related_documents.get(id=entity_id).paper
-            else:
-                continue
+    try:
+        if action == "post_add":
+            create_paper_feed_entries(instance, pk_set)
+        elif action == "post_remove":
+            delete_paper_feed_entries(instance, pk_set)
+    except Exception as e:
+        action = "create" if action == "post_add" else "delete"
+        logger.error(
+            f"Failed to {action} feed entries for unified doc hubs changed: {e}"
+        )
 
-            # We order feed entries by publish date, so we don't need to
-            # create feed entries for papers that don't have a publish date
-            if paper.paper_publish_date is None:
-                continue
 
-            create_feed_entry.apply_async(
-                args=(
-                    paper.id,
-                    ContentType.objects.get_for_model(paper).id,
-                    "PUBLISH",
-                    hub.id,
-                    ContentType.objects.get_for_model(hub).id,
-                ),
-                priority=1,
-            )
-    elif action == "post_remove":
-        for entity_id in pk_set:
-            if (
-                isinstance(instance, ResearchhubUnifiedDocument)
-                and instance.document_type == "PAPER"
-            ):
-                hub = Hub.objects.get(id=entity_id)
-                paper = instance.paper
-            elif isinstance(instance, Hub):  # instance is Hub
-                hub = instance
-                paper = hub.related_documents.get(id=entity_id).paper
-            else:
-                continue
+def create_paper_feed_entries(instance, pk_set):
+    """
+    Create feed entries when papers are added to hubs.
+    """
+    for entity_id in pk_set:
+        if (
+            isinstance(instance, ResearchhubUnifiedDocument)
+            and instance.document_type == "PAPER"
+        ):
+            hub = instance.hubs.get(id=entity_id)
+            paper = instance.paper
+        elif isinstance(instance, Hub):  # instance is Hub
+            hub = instance
+            paper = hub.related_documents.get(id=entity_id).paper
+        else:
+            continue
 
-            delete_feed_entry.apply_async(
-                args=(
-                    paper.id,
-                    ContentType.objects.get_for_model(paper).id,
-                    hub.id,
-                    ContentType.objects.get_for_model(hub).id,
-                ),
-                priority=1,
-            )
+        # We order feed entries by publish date, so we don't need to
+        # create feed entries for papers that don't have a publish date
+        if paper.paper_publish_date is None:
+            continue
+
+        create_feed_entry.apply_async(
+            args=(
+                paper.id,
+                ContentType.objects.get_for_model(paper).id,
+                "PUBLISH",
+                hub.id,
+                ContentType.objects.get_for_model(hub).id,
+            ),
+            priority=1,
+        )
+
+
+def delete_paper_feed_entries(instance, pk_set):
+    """
+    Delete feed entries when papers are removed from hubs.
+    """
+    for entity_id in pk_set:
+        if (
+            isinstance(instance, ResearchhubUnifiedDocument)
+            and instance.document_type == "PAPER"
+        ):
+            hub = Hub.objects.get(id=entity_id)
+            paper = instance.paper
+        elif isinstance(instance, Hub):  # instance is Hub
+            hub = instance
+            paper = hub.related_documents.get(id=entity_id).paper
+        else:
+            continue
+
+        delete_feed_entry.apply_async(
+            args=(
+                paper.id,
+                ContentType.objects.get_for_model(paper).id,
+                hub.id,
+                ContentType.objects.get_for_model(hub).id,
+            ),
+            priority=1,
+        )
