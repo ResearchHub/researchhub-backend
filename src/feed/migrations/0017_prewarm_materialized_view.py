87e5ec62b96363cb2a45d56d19256288c2aed614
Gregor Zurowski <gregor@zurowski.net>
2025-04-08 10:03:15 +0000

    Prewarm feed views with `pq_prewarm` extension

---
+++ src/feed/migrations/0017_prewarm_materialized_view.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("feed", "0016_feedentrylatest"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+                CREATE EXTENSION IF NOT EXISTS pg_prewarm;
+                """,
+            reverse_sql="""
+                DROP EXTENSION IF EXISTS pg_prewarm;
+            """,
+        ),
+        migrations.RunSQL(
+            sql="""
+                SELECT pg_prewarm('feed_feedentry_popular');
+                SELECT pg_prewarm('feed_feedentry_latest');
+                """
+        ),
+    ]
