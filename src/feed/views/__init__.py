b201828e291e01eb47ef1681fb850ab5af11bdb7
koutst <koutst@protonmail.com>
2025-05-15 16:32:59 +0000

    Add a pseudo feed for RH Journal page

--- src/feed/views/__init__.py
+++ src/feed/views/__init__.py
@@ -4,8 +4,10 @@ This module contains all the feed-related views.
 
 from .feed_view import FeedViewSet
 from .funding_feed_view import FundingFeedViewSet
+from .journal_feed_view import JournalFeedViewSet
 
 __all__ = [
     "FeedViewSet",
     "FundingFeedViewSet",
+    "JournalFeedViewSet",
 ]
