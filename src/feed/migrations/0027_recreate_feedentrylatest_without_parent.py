3afb16aebab35c5552c12292e1e0a8a4158b36f9
Gregor Zurowski <gregor@zurowski.net>
2025-05-16 08:58:53 +0000

    Recreate feed materialized views without generic parent relation

---
+++ src/feed/migrations/0027_recreate_feedentrylatest_without_parent.py
@@ -0,0 +1,57 @@
+# Manually created
+
+from django.conf import settings
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("feed", "0026_recreate_feedentrypopular_without_parent"),
+    ]
+
+    def get_sql():
+        # Limit data to the last 30 days in production
+        limit_data = (
+            "WHERE fe.created_date >= (NOW() - INTERVAL '30 days')"
+            if settings.PRODUCTION
+            else ""
+        )
+
+        sql = f"""
+                CREATE MATERIALIZED VIEW feed_feedentry_latest AS
+                    SELECT
+                        fe.id,
+                        fe.content_type_id,
+                        fe.object_id,
+                        fe.content,
+                        fe.metrics,
+                        fe.action,
+                        fe.action_date,
+                        fe.user_id,
+                        fe.unified_document_id,
+                        fe.created_date,
+                        fe.updated_date
+                    FROM
+                        feed_feedentry fe
+                    {limit_data}
+                    ORDER BY
+                        fe.action_date DESC;
+
+            CREATE UNIQUE INDEX feed_feedentry_latest_unique_idx ON feed_feedentry_latest (id);
+            CREATE INDEX feed_feedentry_latest_action_date_idx ON feed_feedentry_latest (action_date DESC);
+            CREATE INDEX feed_feedentry_latest_user_idx ON feed_feedentry_latest (user_id);
+
+            """.format(
+            limit_data
+        )
+        return sql
+
+    operations = [
+        migrations.RunSQL(
+            "DROP MATERIALIZED VIEW IF EXISTS feed_feedentry_latest;",
+        ),
+        migrations.RunSQL(
+            sql=get_sql(),
+        ),
+    ]
