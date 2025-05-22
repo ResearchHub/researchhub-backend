6114a18075763f3062679b3af87a08af2786e70e
Gregor Zurowski <gregor@zurowski.net>
2025-05-22 14:24:39 +0000

    Register `Payment` model

--- src/purchase/models.py
+++ src/purchase/models.py
@@ -1,6 +1,7 @@
 from .related_models.aggregate_purchase_model import AggregatePurchase
 from .related_models.balance_model import Balance
 from .related_models.fundraise_model import Fundraise
+from .related_models.payment_model import Payment
 from .related_models.purchase_model import Purchase
 from .related_models.rsc_exchange_rate_model import RscExchangeRate
 from .related_models.support_model import Support
@@ -9,6 +10,7 @@ from .related_models.wallet_model import Wallet
 migratables = (
     AggregatePurchase,
     Balance,
+    Payment,
     Purchase,
     RscExchangeRate,
     Support,
