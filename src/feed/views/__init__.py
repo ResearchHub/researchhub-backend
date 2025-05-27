573dbbc76421fd06b1ff7706f33fe90763625784
Gregor Zurowski <gregor@zurowski.net>
2025-05-27 12:31:46 +0000

    Add initial ES feed entries view

--- src/feed/views/__init__.py
+++ src/feed/views/__init__.py
@@ -3,11 +3,13 @@ This module contains all the feed-related views.
 """
 
 from .feed_view import FeedViewSet
+from .feed_view_v2 import FeedV2ViewSet
 from .funding_feed_view import FundingFeedViewSet
 from .journal_feed_view import JournalFeedViewSet
 
 __all__ = [
     "FeedViewSet",
+    "FeedV2ViewSet",
     "FundingFeedViewSet",
     "JournalFeedViewSet",
 ]
