9d04426a695ca7c27d24aa6c5122aa53b6063378
Gregor Zurowski <gregor@zurowski.net>
2025-05-27 12:34:48 +0000

    Register ES feed entries view

--- src/researchhub/urls.py
+++ src/researchhub/urls.py
@@ -34,7 +34,12 @@ import researchhub_document.views as researchhub_document_views
 import search.urls
 import user.views
 from citation.views import CitationEntryViewSet, CitationProjectViewSet
-from feed.views import FeedViewSet, FundingFeedViewSet, JournalFeedViewSet
+from feed.views import (
+    FeedV2ViewSet,
+    FeedViewSet,
+    FundingFeedViewSet,
+    JournalFeedViewSet,
+)
 from organizations.views import NonprofitFundraiseLinkViewSet, NonprofitOrgViewSet
 from paper.views import paper_upload_views
 from purchase.views import stripe_webhook_view
@@ -189,6 +194,8 @@ router.register(
 router.register(r"fundraise", purchase.views.FundraiseViewSet, basename="fundraise")
 
 router.register(r"feed", FeedViewSet, basename="feed")
+
+router.register(r"feed_v2", FeedV2ViewSet, basename="feed_v2")
 
 router.register(r"funding_feed", FundingFeedViewSet, basename="funding_feed")
 
