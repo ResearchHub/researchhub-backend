876b0436f20839cd9c4c7aa4264e966fee5fffcc
Gregor Zurowski <gregor@zurowski.net>
2025-06-04 15:41:03 +0000

    Remove temporary, hard-coded email confirmation URL

--- src/user/tests/test_custom_allauth.py
+++ src/user/tests/test_custom_allauth.py
@@ -15,34 +15,6 @@ class CustomAccountAdapterTests(SimpleTestCase):
         self.adapter = CustomAccountAdapter()
 
     @override_settings(
-        PRODUCTION=True,
-        BASE_FRONTEND_URL="https://www.researchhub.com",
-    )
-    def test_get_email_confirmation_url_production(self):
-        # Arrange
-        confirm = DummyEmailConfirmation("prod")
-
-        # Act
-        url = self.adapter.get_email_confirmation_url(self.request, confirm)
-
-        # Assert
-        self.assertEqual(url, "https://new.researchhub.com/verify/prod")
-
-    @override_settings(
-        STAGING=True,
-        BASE_FRONTEND_URL="https://www.researchhub.com",
-    )
-    def test_get_email_confirmation_url_staging(self):
-        # Arrange
-        confirm = DummyEmailConfirmation("staging")
-
-        # Act
-        url = self.adapter.get_email_confirmation_url(self.request, confirm)
-
-        # Assert
-        self.assertEqual(url, "https://v2.staging.researchhub.com/verify/staging")
-
-    @override_settings(
         BASE_FRONTEND_URL="https://default.researchhub.com",
     )
     def test_get_email_confirmation_url_default(self):
