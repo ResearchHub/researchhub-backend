bbf345b9f6c333eb803a0d78339082459b2bb0cd
Gregor Zurowski <gregor@zurowski.net>
2025-06-17 08:47:44 +0000

    Remove v2 hosts from CORS allow list

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -189,11 +189,8 @@ CORS_ORIGIN_WHITELIST = [
     "https://www.staging.researchhub.com",
     "https://old.staging.researchhub.com",
     "https://www.old.staging.researchhub.com",
-    "https://v2.staging.researchhub.com",
-    "https://www.v2.staging.researchhub.com",
     "http://127.0.0.1:3000",
     "https://word.researchhub.com",
-    "https://web-v2-eight.vercel.app",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
