91072a82344864e53574d43f2ca0a55c2bb514e9
Gregor Zurowski <gregor@zurowski.net>
2025-07-28 11:09:05 +0000

    Extract payment logic into business service

--- src/purchase/views/checkout_view.py
+++ src/purchase/views/checkout_view.py
@@ -1,13 +1,12 @@
 import logging
 
-import stripe
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from purchase.related_models.payment_model import PaymentPurpose
 from purchase.serializers.checkout_serializer import CheckoutSerializer
+from purchase.services.payment_service import PaymentService
 
 logger = logging.getLogger(__name__)
 
@@ -27,72 +26,22 @@ class CheckoutView(APIView):
 
         data = serializer.validated_data
         amount = data.get("amount", None)
-        purpose = data.get("purpose")
         paper_id = data.get("paper", None)
-
-        product_name = self.get_name_for_purpose(purpose)
-        unit_amount = self.get_amount_for_purpose(purpose, amount)
+        purpose = data.get("purpose")
 
         try:
-            session = stripe.checkout.Session.create(
-                payment_method_types=["card"],
-                line_items=[
-                    {
-                        "price_data": {
-                            "currency": "usd",
-                            "product_data": {
-                                "name": product_name,
-                            },
-                            "unit_amount": unit_amount,
-                        },
-                        "quantity": 1,
-                    },
-                ],
-                mode="payment",
+            session_data = PaymentService().create_checkout_session(
+                user_id=user_id,
+                purpose=purpose,
+                amount=amount,
+                paper_id=paper_id,
                 success_url=data.get("success_url", None),
                 cancel_url=data.get("failure_url", None),
-                metadata={
-                    "user_id": user_id,
-                    **(
-                        # Include paper_id only if purpose is APC
-                        {"paper_id": paper_id}
-                        if purpose == PaymentPurpose.APC and paper_id
-                        else {}
-                    ),
-                },
             )
 
-            return Response(
-                {
-                    "id": session.get("id"),
-                    "url": session.get("url"),
-                },
-                status=status.HTTP_200_OK,
-            )
+            return Response(session_data, status=status.HTTP_200_OK)
         except Exception as e:
             logger.error("Error creating checkout session: %s", e)
             return Response(
                 {"message": "Failed to create checkout session"}, status=500
             )
-
-    def get_name_for_purpose(self, purpose) -> str:
-        """
-        Helper method to get the name for the payment purpose.
-        """
-        if purpose == PaymentPurpose.APC:
-            return "Article Processing Charge"
-        elif purpose == PaymentPurpose.RSC_PURCHASE:
-            return "ResearchCoin (RSC) Purchase"
-        else:
-            return "Unknown Purpose"
-
-    def get_amount_for_purpose(self, purpose, amount) -> int:
-        """
-        Helper method to get the amount for the payment purpose.
-        The amount is hard-coded for APC, while for other purposes,
-        it uses the provided amount or defaults to 0.
-        """
-        if purpose == "APC":
-            return 30000
-        else:
-            return amount if amount else 0
--- src/purchase/views/stripe_webhook_view.py
+++ src/purchase/views/stripe_webhook_view.py
@@ -2,15 +2,13 @@ import logging
 
 import stripe
 from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from analytics.amplitude import track_event
-from paper.related_models.paper_model import Paper
-from purchase.related_models.payment_model import Payment, PaymentProcessor
+from purchase.services.payment_service import PaymentService
 
 logger = logging.getLogger(__name__)
 
@@ -61,7 +59,9 @@ class StripeWebhookView(APIView):
             match event_type:
                 case "checkout.session.completed":
                     checkout_session = event["data"]["object"]
-                    self.insertPayment(checkout_session)
+                    PaymentService().insert_payment_from_checkout_session(
+                        checkout_session
+                    )
                     logger.info(
                         "Completed checkout session ID=%s processed",
                         checkout_session["id"],
@@ -85,26 +85,4 @@ class StripeWebhookView(APIView):
 
         return Response(
             {"message": "Webhook successfully processed"}, status=status.HTTP_200_OK
-        )
-
-    def insertPayment(self, checkout_session):
-        """
-        Insert payment record into the database.
-        """
-        if "paper_id" not in checkout_session["metadata"]:
-            raise ValueError("Missing paper_id in Stripe metadata")
-        if "user_id" not in checkout_session["metadata"]:
-            raise ValueError("Missing user_id in Stripe metadata")
-
-        user_id = checkout_session["metadata"]["user_id"]
-        paper_id = checkout_session["metadata"]["paper_id"]
-
-        Payment.objects.create(
-            amount=checkout_session["amount_total"],
-            currency=checkout_session["currency"].upper(),
-            external_payment_id=checkout_session["payment_intent"],
-            payment_processor=PaymentProcessor.STRIPE,
-            object_id=paper_id,
-            content_type=ContentType.objects.get_for_model(Paper),
-            user_id=user_id,
         )
