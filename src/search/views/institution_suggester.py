9509fb8ae1bce18a5de5734fb40d049317c3beae
koutst <koutst@protonmail.com>
2025-08-06 17:57:22 +0000

    Replace use of django-elasticsearch-dsl-drf in search serializers and views

--- src/search/views/institution_suggester.py
+++ src/search/views/institution_suggester.py
@@ -1,17 +1,15 @@
-from django_elasticsearch_dsl_drf.filter_backends import (
-    OrderingFilterBackend,
-    SuggesterFilterBackend,
-)
-from django_elasticsearch_dsl_drf.pagination import LimitOffsetPagination
-from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from rest_framework.decorators import action
 
 from search.backends.multi_match_filter import MultiMatchSearchFilterBackend
+from search.base.filters import OrderingFilterBackend, SuggesterFilterBackend
+from search.base.pagination import LimitOffsetPagination
+from search.base.viewsets import ElasticsearchViewSet
 from search.documents.institution import InstitutionDocument
 from search.serializers.institution import InstitutionDocumentSerializer
 from utils.permissions import ReadOnly
 
 
-class InstitutionSuggesterDocumentView(DocumentViewSet):
+class InstitutionSuggesterDocumentView(ElasticsearchViewSet):
     document = InstitutionDocument
     permission_classes = [ReadOnly]
     serializer_class = InstitutionDocumentSerializer
@@ -41,9 +39,14 @@ class InstitutionSuggesterDocumentView(DocumentViewSet):
     suggester_fields = {
         "suggestion_phrases": {
             "field": "suggestion_phrases",
-            "suggesters": ["completion"],
-            "options": {
-                "size": 5,
-            },
+            "type": "completion",
         },
     }
+
+    @action(detail=False, methods=["get"])
+    def suggest(self, request, *args, **kwargs):
+        """
+        Suggest endpoint for backward compatibility.
+        Delegates to the list view which handles suggestions via query params.
+        """
+        return self.list(request, *args, **kwargs)
--- src/search/views/journal.py
+++ src/search/views/journal.py
@@ -1,18 +1,13 @@
-from django_elasticsearch_dsl_drf.filter_backends import (
-    CompoundSearchFilterBackend,
-    DefaultOrderingFilterBackend,
-)
-from django_elasticsearch_dsl_drf.pagination import LimitOffsetPagination
-from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
-from elasticsearch_dsl import Search
-
 from search.backends.multi_match_filter import MultiMatchSearchFilterBackend
+from search.base.filters import DefaultOrderingFilterBackend, SearchFilterBackend
+from search.base.pagination import LimitOffsetPagination
+from search.base.viewsets import ElasticsearchViewSet
 from search.documents.journal import JournalDocument
 from search.serializers.hub import HubDocumentSerializer
 from utils.permissions import ReadOnly
 
 
-class JournalDocumentView(DocumentViewSet):
+class JournalDocumentView(ElasticsearchViewSet):
     document = JournalDocument
     permission_classes = [ReadOnly]
     serializer_class = HubDocumentSerializer
@@ -20,7 +15,7 @@ class JournalDocumentView(DocumentViewSet):
     lookup_field = "id"
     filter_backends = [
         MultiMatchSearchFilterBackend,
-        CompoundSearchFilterBackend,
+        SearchFilterBackend,
         DefaultOrderingFilterBackend,
     ]
 
@@ -41,19 +36,3 @@ class JournalDocumentView(DocumentViewSet):
         "type": "cross_fields",
         "analyzer": "content_analyzer",
     }
-
-    def __init__(self, *args, **kwargs):
-        self.search = Search(index=["journal"])
-        super(JournalDocumentView, self).__init__(*args, **kwargs)
-
-    def _filter_queryset(self, request):
-        queryset = self.search
-
-        for backend in list(self.filter_backends):
-            queryset = backend().filter_queryset(
-                request,
-                queryset,
-                self,
-            )
-
-        return queryset
