b6172668065834bdc0284bcb29e46a78afcf6a3b
Gregor Zurowski <gregor@zurowski.net>
2025-08-08 09:16:42 +0000

    Remove citation code

--- src/citation/serializers/__init__.py
+++
@@ -1,5 +0,0 @@
-from citation.serializers.citation_entry_serializer import (
-    CitationEntrySerializer,
-    DynamicCitationEntrySerializer,
-)
-from citation.serializers.citation_project_serializer import CitationProjectSerializer
