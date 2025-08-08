e6c2f60987f21004869496d088af81082451d26b
Gregor Zurowski <gregor@zurowski.net>
2025-08-08 15:56:47 +0000

    Use `OPENSEARCH_HOST` variable

--- src/researchhub/settings.py
+++ src/researchhub/settings.py
@@ -629,7 +629,7 @@ stripe.api_version = "2024-09-30.acacia"
 
 # Search (Elastic)
 
-OPENSEARCH_HOST = os.environ.get("OPENSEARCH_HOST", keys.ELASTICSEARCH_HOST)
+OPENSEARCH_HOST = os.environ.get("OPENSEARCH_HOST", keys.OPENSEARCH_HOST)
 
 OPENSEARCH_DSL = {
     "default": {
