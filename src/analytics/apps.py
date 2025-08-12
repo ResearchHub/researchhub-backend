3deced069dd1e8de3ecc10657ab01c517e10a7ff
nicktytarenko <nicktytarenko@gmail.com>
2025-08-12 18:31:58 +0000

    clean up

--- src/analytics/apps.py
+++ src/analytics/apps.py
@@ -2,13 +2,4 @@ from django.apps import AppConfig
 
 
 class AnalyticsConfig(AppConfig):
-    name = "analytics"
-
-    def ready(self):
-        """
-        Import signals when the app is ready to ensure they are registered.
-        """
-        try:
-            import analytics.signals  # noqa
-        except ImportError:
-            pass
+    name = 'analytics'
