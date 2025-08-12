93fbec24516bed459c0eade634f9197367750488
Gregor Zurowski <gregor@zurowski.net>
2025-08-12 15:10:21 +0000

    Use `should_index_object` instead of custom `should_remove_from_index`

--- src/search/documents/hub.py
+++ src/search/documents/hub.py
@@ -50,7 +50,11 @@ class HubDocument(BaseDocument):
         ]
 
     def get_queryset(self, filter_=None, exclude=None, count=None):
-        return super().get_queryset(filter_=filter_, exclude=exclude, count=count).exclude(namespace="journal")
+        return (
+            super()
+            .get_queryset(filter_=filter_, exclude=exclude, count=count)
+            .exclude(namespace="journal")
+        )
 
     # Used specifically for "autocomplete" style suggest feature
     def prepare_name_suggest(self, instance):
@@ -79,8 +83,5 @@ class HubDocument(BaseDocument):
 
         return data
 
-    def should_remove_from_index(self, obj):
-        if obj.is_removed:
-            return True
-
-        return False
+    def should_index_object(self, obj):
+        return not obj.is_removed
