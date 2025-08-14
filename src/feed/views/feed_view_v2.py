9bf9323613b7d0d4480540a7e138efa4a0ce8019
Gregor Zurowski <gregor@zurowski.net>
2025-08-14 08:39:56 +0000

    Move feed document to search app

--- src/feed/views/feed_view_v2.py
+++ src/feed/views/feed_view_v2.py
@@ -3,7 +3,6 @@ from opensearchpy import Q
 from feed.document_serializers.feed_document_serializer import (
     FeedEntryDocumentSerializer,
 )
-from feed.documents.feed_document import FeedEntryDocument
 from feed.views.common import FeedPagination
 from feed.views.feed_view_mixin import FeedViewMixin
 from search.base.filters import (
@@ -13,6 +12,7 @@ from search.base.filters import (
     SearchFilterBackend,
 )
 from search.base.viewsets import ElasticsearchViewSet
+from search.documents.feed import FeedEntryDocument
 
 
 class FeedV2ViewSet(FeedViewMixin, ElasticsearchViewSet):
@@ -112,5 +112,7 @@ class FeedV2ViewSet(FeedViewMixin, ElasticsearchViewSet):
                 queryset = queryset.sort("-hot_score")
             case "latest" | "following":
                 queryset = queryset.sort("-action_date")
+
+        return queryset
 
         return queryset
