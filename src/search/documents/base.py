46145ee7f122436bc882259c8250d910ddbbb074
Gregor Zurowski <gregor@zurowski.net>
2025-08-14 09:43:56 +0000

    Simplify implementation without overriding private methods

--- src/search/documents/base.py
+++ src/search/documents/base.py
@@ -1,57 +1,51 @@
 from django.db import models
 from django_opensearch_dsl import Document
-from django_opensearch_dsl.apps import DODConfig
 
 
 class BaseDocument(Document):
 
     def update(self, thing, action, *args, refresh=None, using=None, **kwargs):
         """
-        Overriding parent method to include an additional bulk
-        operation for removing objects from the index that are soft-deleted.
+        Override to handle soft deletes by removing documents from index
+        when should_index_object returns False.
 
         See: https://github.com/Codoc-os/django-opensearch-dsl/blob/e6cead9123ff9b67390c438876ca1ee313749cff/django_opensearch_dsl/documents.py#L238C9-L238C80
         """
-
-        if refresh is None:
-            refresh = getattr(
-                self.Index, "auto_refresh", DODConfig.auto_refresh_enabled()
-            )
-
         if isinstance(thing, models.Model):
             object_list = [thing]
         else:
             object_list = thing
 
-        objects_to_remove = []
-        objects_to_index = []
-        for obj in object_list:
-            if not self.should_index_object(obj):
-                objects_to_remove.append(obj)
-            else:
-                objects_to_index.append(obj)
-
         # AWS OpenSearch has instance-type based limits for the size of HTTP payloads.
         # See: https://docs.aws.amazon.com/opensearch-service/latest/developerguide/limits.html#network-limits
         # Set max_chunk_bytes to 8MB to safely stay under the 10MB limit
-        bulk_kwargs = kwargs.copy()
-        bulk_kwargs.setdefault("max_chunk_bytes", 8 * 1024 * 1024)  # 8MB
+        kwargs.setdefault("max_chunk_bytes", 8 * 1024 * 1024)  # 8MB
 
-        index_result = self._bulk(
-            self._get_actions(objects_to_index, action="index"),
-            *args,
-            refresh=refresh,
-            using=using,
-            **bulk_kwargs,
-        )
-        delete_result = self._bulk(
-            self._get_actions(objects_to_remove, action="delete"),
-            *args,
-            refresh=refresh,
-            using=using,
-            **bulk_kwargs,
-        )
-        return (
-            index_result[0] + delete_result[0],
-            index_result[1] + delete_result[1],
-        )
+        # Split objects based on whether they should be indexed
+        to_index = []
+        to_delete = []
+        for obj in object_list:
+            if self.should_index_object(obj):
+                to_index.append(obj)
+            else:
+                to_delete.append(obj)
+
+        # Process both operations and combine results
+        total_success = 0
+        total_errors = []
+
+        if to_index:
+            success, errors = super().update(
+                to_index, "index", *args, refresh=refresh, using=using, **kwargs
+            )
+            total_success += success
+            total_errors.extend(errors)
+
+        if to_delete:
+            success, errors = super().update(
+                to_delete, "delete", *args, refresh=refresh, using=using, **kwargs
+            )
+            total_success += success
+            total_errors.extend(errors)
+
+        return (total_success, total_errors)
