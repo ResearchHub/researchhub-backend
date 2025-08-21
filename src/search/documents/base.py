b8f0d61e7a5c32bca55e74e390d0e910632603c0
Gregor Zurowski <gregor@zurowski.net>
2025-08-21 17:35:41 +0000

    Invoke search prepare methods with graceful error handling

--- src/search/documents/base.py
+++ src/search/documents/base.py
@@ -1,6 +1,9 @@
+import logging
 from typing import override
 
 from django_opensearch_dsl import Document
+
+logger = logging.getLogger(__name__)
 
 
 class BaseDocument(Document):
@@ -9,10 +12,21 @@ class BaseDocument(Document):
     def _get_actions(self, object_list, action):
         """
         Override the base `_get_actions` method to support soft-delete behavior.
+        Additionally, any exceptions from the prepare_[field] methods will be
+        logged without aborting the indexing process.
         """
         for object_instance in object_list:
             if action == "delete" or self.should_index_object(object_instance):
-                yield self._prepare_action(object_instance, action)
+                # Execute `prepare` methods with graceful error handling to avoid
+                # aborting the indexing process:
+                try:
+                    yield self._prepare_action(object_instance, action)
+                except Exception as e:
+                    logger.warning(
+                        f"Failed to index {self.__class__.__name__} "
+                        f"id={object_instance.id}: {e}"
+                    )
+                    continue
             else:
                 # delete soft-deleted objects (`should_index_object` is False)
                 yield self._prepare_action(object_instance, "delete")
