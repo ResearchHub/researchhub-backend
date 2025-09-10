ad8a8030333cd37181795a679c3640ca7a1e410d
Gregor Zurowski <gregor@zurowski.net>
2025-09-10 16:36:47 +0000

    Use Sentry to notify about paper pull errors

--- src/paper/ingestion/tasks.py
+++ src/paper/ingestion/tasks.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 
 from paper.ingestion.clients.biorxiv import BioRxivClient, BioRxivConfig
 from researchhub.celery import app
+from utils.sentry import log_error
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +11,7 @@ logger = logging.getLogger(__name__)
 @app.task
 def pull_bioarxiv_papers():
     """
-    Temporary task for testing the BioRxiv client for pulling papers.
+    Temporary task for testing the bioRxiv client for pulling papers.
     """
     config = BioRxivConfig(
         rate_limit=1.0,
@@ -23,10 +24,11 @@ def pull_bioarxiv_papers():
     try:
         until = datetime.now()
         since = until - timedelta(days=1)
-        logger.info(f"Starting to pull papers from BioRxiv from {since} to {until}")
+        logger.info(f"Starting to pull papers from bioRxiv from {since} to {until}")
 
         papers = client.fetch_recent(since=since, until=until)
 
-        logger.info(f"Fetched {len(papers)} papers from BioRxiv.")
+        logger.info(f"Fetched {len(papers)} papers from bioRxiv.")
     except Exception as e:
-        logger.error(f"Error fetching papers: {e}")
+        log_error(e, message="Error fetching papers from bioRxiv")
+        logger.error(f"Error fetching papers from bioRxiv: {e}")
