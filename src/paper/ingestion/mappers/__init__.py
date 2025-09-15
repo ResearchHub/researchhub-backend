c2ff02ce7e512bcc419fafacbab46ac819eab5d7
Taki Koutsomitis <koutst@protonmail.com>
2025-09-15 14:25:35 -0400

    fix formatting

--- src/paper/ingestion/mappers/__init__.py
+++ src/paper/ingestion/mappers/__init__.py
@@ -4,8 +4,10 @@ Mapper modules for transforming source data to domain models.
 
 from .base import BaseMapper
 from .biorxiv import BioRxivMapper
+from .chemrxiv import ChemRxivMapper
 
 __all__ = [
     "BaseMapper",
     "BioRxivMapper",
+    "ChemRxivMapper",
 ]
