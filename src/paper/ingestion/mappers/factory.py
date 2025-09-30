0fd83db27eae3eac819b042fc3c9d37d0fc83a04
Gregor Zurowski <gregor@zurowski.net>
2025-09-30 11:31:34 +0000

    Use factory to create mappers

---
+++ src/paper/ingestion/mappers/factory.py
@@ -0,0 +1,33 @@
+"""
+Factory for creating mapper instances with hub mapper configuration.
+"""
+
+from typing import Dict
+
+from hub.mappers.external_category_mapper import ExternalCategoryMapper
+from paper.ingestion.constants import IngestionSource
+
+from .arxiv import ArXivMapper
+from .base import BaseMapper
+from .biorxiv import BioRxivMapper
+from .chemrxiv import ChemRxivMapper
+from .medrxiv import MedRxivMapper
+
+
+class MapperFactory:
+    """
+    Factory for creating mapper instances with their configuration.
+    """
+
+    _hub_mapper = ExternalCategoryMapper()
+
+    def create_mappers(self) -> Dict[IngestionSource, BaseMapper]:
+        """
+        Create all mapper instances.
+        """
+        return {
+            IngestionSource.ARXIV: ArXivMapper(hub_mapper=self._hub_mapper),
+            IngestionSource.BIORXIV: BioRxivMapper(hub_mapper=self._hub_mapper),
+            IngestionSource.CHEMRXIV: ChemRxivMapper(hub_mapper=self._hub_mapper),
+            IngestionSource.MEDRXIV: MedRxivMapper(hub_mapper=self._hub_mapper),
+        }
