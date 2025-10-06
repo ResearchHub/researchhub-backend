631a89334947ff030d5ec7a8ef38f5163bd09c1a
Gregor Zurowski <gregor@zurowski.net>
2025-10-06 10:46:59 +0000

    Add scheduled job for paper metrics

--- src/researchhub/celery.py
+++ src/researchhub/celery.py
@@ -186,4 +186,12 @@ app.conf.beat_schedule = {
             "queue": QUEUE_PULL_PAPERS,
         },
     },
+    "paper-metrics-update": {
+        "task": "paper.ingestion.tasks.update_recent_papers_with_metrics",
+        "schedule": crontab(hour=3, minute=0),
+        "options": {
+            "priority": 2,
+            "queue": QUEUE_PAPER_MISC,
+        },
+    },
 }
