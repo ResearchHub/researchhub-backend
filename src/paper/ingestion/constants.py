9d93cdc57d65f5119aa3dfbab141b11d45657a66
Gregor Zurowski <gregor@zurowski.net>
2025-10-22 15:05:10 +0000

    Add ArXiv OAI-PMH to ingestion source constants

--- src/paper/ingestion/constants.py
+++ src/paper/ingestion/constants.py
@@ -5,6 +5,7 @@ class IngestionSource(Enum):
     """Supported ingestion sources."""
 
     ARXIV = "arxiv"
+    ARXIV_OAIPMH = "arxiv_oaipmh"
     BIORXIV = "biorxiv"
     CHEMRXIV = "chemrxiv"
     MEDRXIV = "medrxiv"
