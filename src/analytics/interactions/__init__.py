df4063624545d414a6db479642d3aaf9b255b36f
Kobe Attias <kobeattias@gmail.com>
2025-10-27 16:50:00 -0400

    Revert "[User Interactions Mapper] Mapping votes to UserInteractions"
    This reverts commit eb302fcb36656cc6c45caa098cf7ac11ebe7644c.

--- src/analytics/interactions/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base_interaction_mapper import BaseInteractionMapper
-from .upvote_interaction_mapper import UpvoteInteractionMapper
-
-__all__ = ["BaseInteractionMapper", "UpvoteInteractionMapper"]
