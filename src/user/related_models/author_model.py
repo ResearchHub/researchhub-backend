d6e955a3f51d8999c67b3abc897787b7fd224fad
Gregor Zurowski <gregor@zurowski.net>
2025-10-29 11:40:39 +0000

    Move author indexing properties as prepare method into person document

--- src/user/related_models/author_model.py
+++ src/user/related_models/author_model.py
@@ -250,36 +250,10 @@ class Author(models.Model):
         return paper_count_res.get("paper_count") or 0
 
     @property
-    def person_types_indexing(self):
-        person_types = ["author"]
-        if self.user is not None:
-            person_types.append("user")
-
-        return person_types
-
-    @property
-    def institutions_indexing(self):
-        if self.institutions is not None:
-            return [
-                {
-                    "id": author_institution.institution.id,
-                    "name": author_institution.institution.display_name,
-                }
-                for author_institution in self.institutions.all()
-            ]
-        return None
-
-    @property
     def university_indexing(self):
         if self.university is not None:
             return self.university
         return None
-
-    @property
-    def user_reputation_indexing(self):
-        if self.user is not None:
-            return self.user.reputation
-        return 0
 
     @property
     def achievements(self):
