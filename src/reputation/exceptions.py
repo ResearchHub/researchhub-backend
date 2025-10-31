d1bdabf92c62541f6769c6c5bcbaabf3f3b5a5a3
Gregor Zurowski <gregor@zurowski.net>
2025-10-31 10:13:51 +0000

    Remove unused exception

--- src/reputation/exceptions.py
+++ src/reputation/exceptions.py
@@ -17,19 +17,6 @@ class ReputationDistributorError(Error):
         self.message = message
 
 
-class ReputationSignalError(Error):
-    """Raised for errors related to signals in the `reputation` app.
-
-    Attributes:
-        trigger -- error that triggered this one
-        message -- explanation of this error
-    """
-
-    def __init__(self, trigger, message):
-        self.trigger = trigger
-        self.message = message
-
-
 class WithdrawalError(Error):
     """Raised for errors related to withdrawal.
 
