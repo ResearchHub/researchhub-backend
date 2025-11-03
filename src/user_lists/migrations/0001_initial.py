1f07c594231defe9ec847a636cad9a7714e4c86a
Michael Canova <michael_canova-at-174250149065>
2025-11-03 10:57:43 -0500

    (Api) User List model

---
+++ src/user_lists/migrations/0001_initial.py
@@ -0,0 +1,71 @@
+import django.db.models.deletion
+from django.conf import settings
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ('researchhub_document', '0068_alter_researchhubpost_document_type_and_more'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_date', models.DateTimeField(auto_now_add=True)),
+                ('updated_date', models.DateTimeField(auto_now=True)),
+                ('is_removed', models.BooleanField(default=False)),
+                ('is_removed_date', models.DateTimeField(blank=True, default=None, null=True)),
+                ('name', models.CharField(max_length=120)),
+                ('is_public', models.BooleanField(default=False)),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='created_%(app_label)s_%(class)s', to=settings.AUTH_USER_MODEL)),
+                ('updated_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='updated_%(app_label)s_%(class)s', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['name'],
+            },
+        ),
+        migrations.CreateModel(
+            name='ListItem',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created_date', models.DateTimeField(auto_now_add=True)),
+                ('updated_date', models.DateTimeField(auto_now=True)),
+                ('is_removed', models.BooleanField(default=False)),
+                ('is_removed_date', models.DateTimeField(blank=True, default=None, null=True)),
+                ('is_public', models.BooleanField(default=False)),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='created_%(app_label)s_%(class)s', to=settings.AUTH_USER_MODEL)),
+                ('parent_list', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='user_lists.list')),
+                ('unified_document', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='user_list_items', to='researchhub_document.researchhubunifieddocument')),
+                ('updated_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='updated_%(app_label)s_%(class)s', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['-created_date'],
+            },
+        ),
+        migrations.AddConstraint(
+            model_name='list',
+            constraint=models.UniqueConstraint(condition=models.Q(('is_removed', False)), fields=('created_by', 'name'), name='unique_not_removed_name_per_user'),
+        ),
+        migrations.AddConstraint(
+            model_name='listitem',
+            constraint=models.UniqueConstraint(condition=models.Q(('is_removed', False)), fields=('parent_list', 'unified_document'), name='unique_not_removed_document_per_list'),
+        ),
+        migrations.AddIndex(
+            model_name='list',
+            index=models.Index(fields=['created_by', 'name', 'is_removed'], name='idx_list_user_name_removed'),
+        ),
+        migrations.AddIndex(
+            model_name='list',
+            index=models.Index(fields=['created_by', 'is_removed'], name='idx_list_user_removed'),
+        ),
+        migrations.AddIndex(
+            model_name='listitem',
+            index=models.Index(fields=['parent_list', 'is_removed'], name='idx_listitem_list_removed'),
+        ),
+    ]
---
+++ src/user_lists/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+from researchhub_document.related_models.researchhub_unified_document_model import (
+    ResearchhubUnifiedDocument,
+)
+from utils.models import DefaultAuthenticatedModel, SoftDeletableModel
+
+
+class List(DefaultAuthenticatedModel, SoftDeletableModel):
+    name = models.CharField(max_length=120)
+    is_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.created_by}:{self.name}"
+
+    class Meta:
+        ordering = ["name"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["created_by", "name"],
+                condition=models.Q(is_removed=False),
+                name="unique_not_removed_name_per_user",
+            )
+        ]
+        indexes = [
+            models.Index(
+                fields=["created_by", "name", "is_removed"],
+                name="idx_list_user_name_removed",
+            ),
+            models.Index(
+                fields=["created_by", "is_removed"],
+                name="idx_list_user_removed",
+            ),
+        ]
+
+
+class ListItem(DefaultAuthenticatedModel, SoftDeletableModel):
+    parent_list = models.ForeignKey(
+        List,
+        on_delete=models.CASCADE,
+        related_name="items",
+    )
+
+    unified_document = models.ForeignKey(
+        ResearchhubUnifiedDocument,
+        on_delete=models.CASCADE,
+        related_name="user_list_items",
+    )
+
+    is_public = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_date"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["parent_list", "unified_document"],
+                condition=models.Q(is_removed=False),
+                name="unique_not_removed_document_per_list",
+            )
+        ]
+        indexes = [
+            models.Index(
+                fields=["parent_list", "is_removed"],
+                name="idx_listitem_list_removed",
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.created_by}:{self.parent_list}: \
+            {self.unified_document.get_client_doc_type()}:\
+                {self.unified_document.get_url()}"
+
