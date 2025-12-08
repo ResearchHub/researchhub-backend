4ef08f653b9129cb72cce8b4b660a3116317f5be
Michael Canova <michael_canova-at-174250149065>
2025-12-08 13:13:31 -0500

    [User Lists] Consolidating migrations

--- src/user_lists/migrations/0003_list_default_functionality.py
+++ src/user_lists/migrations/0003_list_default_functionality.py
@@ -4,10 +4,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('user_lists', '0004_list_is_default'),
+        ('user_lists', '0002_alter_list_updated_by_alter_list_updated_date_and_more'),
     ]
 
     operations = [
+        migrations.AlterField(
+            model_name='list',
+            name='name',
+            field=models.CharField(blank=True, max_length=120, null=True),
+        ),
+        migrations.AddField(
+            model_name='list',
+            name='is_default',
+            field=models.BooleanField(default=False),
+        ),
         migrations.AddConstraint(
             model_name='list',
             constraint=models.UniqueConstraint(
