6706d273044b574f21c5e9612d3642b354f8f4ac
Michael Canova <michael_canova-at-174250149065>
2026-01-02 11:52:30 -0500

    [User Lists] First setup for sharing user lists

--- src/user_lists/views.py
+++ src/user_lists/views.py
@@ -2,11 +2,13 @@ from django.db import IntegrityError
 from django.db.models import Count, Prefetch, Q
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework.pagination import PageNumberPagination
+
+from researchhub.permissions import IsObjectOwner
 
 from .models import List, ListItem
 from .serializers import ListOverviewSerializer, ListSerializer, ListItemSerializer
@@ -17,15 +19,20 @@ class ListPagination(PageNumberPagination):
     max_page_size = 100
 
 class ListViewSet(viewsets.ModelViewSet):
-    queryset = List.objects.filter(is_removed=False)
+    queryset = List.objects.all()
     serializer_class = ListSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsObjectOwner]
     pagination_class = ListPagination
 
     def get_queryset(self):
-        return self.queryset.filter(created_by=self.request.user).annotate(
+        base_qs = self.queryset.annotate(
             item_count=Count("items", filter=Q(items__is_removed=False))
         ).order_by("-updated_date")
+
+        if self.action == "retrieve":
+            return base_qs
+
+        return base_qs.filter(created_by=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
@@ -43,58 +50,58 @@ class ListViewSet(viewsets.ModelViewSet):
         return Response({"lists": serializer.data}, status=status.HTTP_200_OK)
 
 class ListItemViewSet(CreateModelMixin, ListModelMixin, DestroyModelMixin, viewsets.GenericViewSet):
-    queryset = ListItem.objects.filter(is_removed=False)
+    queryset = ListItem.objects.all()
     serializer_class = ListItemSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = ListPagination
-    lookup_field = 'item_id'
-    lookup_url_kwarg = 'item_id'
+    lookup_field = "item_id"
+    lookup_url_kwarg = "item_id"
 
     def get_queryset(self):
-        list_id = self.kwargs.get('list_id')
-        qs = self.queryset.filter(
-            parent_list__created_by=self.request.user
-        ).select_related(
-            'unified_document'
+        list_id = self.kwargs.get("list_id")
+        qs = self.queryset.select_related(
+            "unified_document"
         ).prefetch_related(
-            'unified_document__paper',
-            'unified_document__posts'
+            "unified_document__paper",
+            "unified_document__posts"
         )
-        
+
+        if self.action != "list":
+            qs = qs.filter(parent_list__created_by=self.request.user)
+
         if list_id:
             qs = qs.filter(parent_list_id=list_id, unified_document__is_removed=False)
-        
+
         return qs
 
     def list(self, request, *args, **kwargs):
-        list_id = kwargs.get('list_id')
-        
-        check_list_exists = List.objects.filter(id=list_id, created_by=request.user, is_removed=False).exists()
-        if not check_list_exists:
+        list_id = kwargs.get("list_id")
+
+        if not List.objects.filter(id=list_id).exists():
             return Response({"error": "List not found"}, status=status.HTTP_404_NOT_FOUND)
-        
+
         return super().list(request, *args, **kwargs)
 
     def _get_or_create_default_list(self, user):
         default_list, _ = List.objects.get_or_create(
             created_by=user,
             is_default=True,
-            defaults={'name': None, 'is_removed': False}
+            defaults={"name": None, "is_removed": False}
         )
         return default_list
 
     def create(self, request, *args, **kwargs):
         data = request.data
-        # when creating a new item, if no parent list is provided in the POST request (one click add to list instead of selecting a list), get or create the default list
-        if not data.get('parent_list'):
+        # Use default list if no parent_list provided
+        if not data.get("parent_list"):
             default_list = self._get_or_create_default_list(request.user)
-            data = {'unified_document': data.get('unified_document'), 'parent_list': default_list.id}
-        
+            data = {"unified_document": data.get("unified_document"), "parent_list": default_list.id}
+
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         try:
             instance = serializer.save(created_by=request.user)
-            instance.parent_list.save(update_fields=['updated_date'])
+            instance.parent_list.save(update_fields=["updated_date"])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except IntegrityError:
             return Response({"error": "Document already exists in list"}, status=status.HTTP_400_BAD_REQUEST)
@@ -102,10 +109,10 @@ class ListItemViewSet(CreateModelMixin, ListModelMixin, DestroyModelMixin, views
     def perform_destroy(self, instance):
         parent_list = instance.parent_list
         super().perform_destroy(instance)
-        parent_list.save(update_fields=['updated_date'])
+        parent_list.save(update_fields=["updated_date"])
 
     def get_object(self):
         queryset = self.filter_queryset(self.get_queryset())
-        item_id = self.kwargs.get('item_id')
+        item_id = self.kwargs.get("item_id")
         obj = get_object_or_404(queryset, id=item_id)
         return obj
