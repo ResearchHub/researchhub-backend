a7ee969ba8f635616a8293933c8da647e61198ae
Gregor Zurowski <gregor@zurowski.net>
2026-01-05 12:12:07 +0000

    Remove empty social account adapter

--- src/oauth/adapters.py
+++
@@ -1,8 +0,0 @@
-from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-
-
-class SocialAccountAdapter(DefaultSocialAccountAdapter):
-    def save_user(self, request, sociallogin, form=None):  # Saves new user
-        saved_user = super().save_user(request, sociallogin, form)
-
-        return saved_user
