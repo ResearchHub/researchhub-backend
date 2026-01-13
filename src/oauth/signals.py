418dd5869aa6ad3e872a144b76f4f7ffcb6d3619
Gregor Zurowski <gregor@zurowski.net>
2026-01-13 11:55:26 +0000

    Remove broken receiver for social signup
    The signal has never worked since the receiver imports was
    broken. Additionally, the imports for `_social_login` and
    `pre_social_login` were incorrect. This was essentially dead
    code and the actual functionality is handled in the helpers file.

--- src/oauth/signals.py
+++ src/oauth/signals.py
@@ -1,20 +1,7 @@
 from allauth.account.signals import user_signed_up
-from allauth.socialaccount.helpers import _social_login
-from django.db.models.signals import pre_social_login
 from django.dispatch import receiver
 
 from oauth.services.user_signup_service import UserSignupService
-
-
-@receiver(pre_social_login)
-def link_to_existing_user(sender, request, sociallogin, **kwargs):
-    user = request.user
-    if user:
-        # If the user exists, connect this new social login to that user
-        sociallogin.connect(request, user)
-        # Notify the system that we took care of this login attempt
-        _social_login(request, sociallogin)
-    # No user found, continue with regular login process
 
 
 @receiver(user_signed_up)
