a346f0b9cd7ad33bfa7cdb785f3386bd4b44f64e
Gregor Zurowski <gregor@zurowski.net>
2026-01-29 10:29:55 +0000

    Move Endaoment views and tests to top level folders

--- src/purchase/endaoment/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from purchase.endaoment.views.endaoment_views import (
-    EndaomentCallbackView,
-    EndaomentConnectView,
-    EndaomentStatusView,
-)
-
-__all__ = ["EndaomentConnectView", "EndaomentCallbackView", "EndaomentStatusView"]
