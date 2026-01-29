b16c67c91b7376ed023f792cc302db8ac4d4790d
Gregor Zurowski <gregor@zurowski.net>
2026-01-29 10:20:44 +0000

    Add view for Endaoment OAuth flow

---
+++ src/purchase/endaoment/views/__init__.py
@@ -0,0 +1,7 @@
+from purchase.endaoment.views.endaoment_views import (
+    EndaomentCallbackView,
+    EndaomentConnectView,
+    EndaomentStatusView,
+)
+
+__all__ = ["EndaomentConnectView", "EndaomentCallbackView", "EndaomentStatusView"]
