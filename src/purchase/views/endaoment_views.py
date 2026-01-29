c78d28898c4223b41f882378d56fc6e0cddfe8ed
Gregor Zurowski <gregor@zurowski.net>
2026-01-29 10:56:11 +0000

    Fix import

--- src/purchase/views/endaoment_views.py
+++ src/purchase/views/endaoment_views.py
@@ -7,7 +7,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from purchase.endaoment.services import EndaomentService
+from purchase.endaoment import EndaomentService
 
 logger = logging.getLogger(__name__)
 
