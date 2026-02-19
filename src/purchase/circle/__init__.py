a681c8ecb4eabb1a9d4679d36d14aa046ee09d83
Taki Koutsomitis <koutst@protonmail.com>
2026-02-19 23:25:42 +0000

    Remove unnecessary import

--- src/purchase/circle/__init__.py
+++ src/purchase/circle/__init__.py
@@ -1,9 +1,7 @@
 from purchase.circle.client import CircleWalletClient
 from purchase.circle.service import CircleWalletService
-from purchase.circle.webhook import verify_webhook_signature
 
 __all__ = [
     "CircleWalletClient",
     "CircleWalletService",
-    "verify_webhook_signature",
 ]
