7f5ddd6a193ad50cd46970179319f7fcbfb8ca52
Taki Koutsomitis <koutst@protonmail.com>
2026-03-25 15:44:23 +0000

    Update staking config defaults

--- src/reputation/migrations/0116_seed_staking_config.py
+++ src/reputation/migrations/0116_seed_staking_config.py
@@ -5,8 +5,8 @@ def seed_staking_config(apps, schema_editor):
     StakingConfig = apps.get_model("reputation", "StakingConfig")
     if not StakingConfig.objects.filter(is_active=True).exists():
         StakingConfig.objects.create(
-            emission_per_year=0,
-            circulating_supply=0,
+            emission_per_year=9_500_000,
+            circulating_supply=215_052_673,
             staked_fraction=0,
             avg_multiplier=1,
             is_active=True,
