62068315bc92e2643ba4e529535358a32761940a
Taki Koutsomitis <koutst@protonmail.com>
2026-03-28 17:43:36 +0000

    Update release date, remove staging gating

--- src/reputation/services/staking_yield_service.py
+++ src/reputation/services/staking_yield_service.py
@@ -19,9 +19,7 @@ logger = logging.getLogger(__name__)
 QUANTIZE_8 = Decimal("0.00000001")
 
 # Halving schedule constants
-STAKING_RELEASE_DATE = date(
-    2026, 3, 27
-)  # TODO: Set actual release date when deploying to prod
+STAKING_RELEASE_DATE = date(2026, 4, 13)
 INITIAL_YEARLY_EMISSION = Decimal("9500000")
 HALVING_PERIOD_DAYS = 64 * 365  # 64 years in days
 
