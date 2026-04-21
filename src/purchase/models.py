32efa67c8d867d3b4771a819871392b6118ca197
Gregor Zurowski <gregor@zurowski.net>
2026-04-21 14:58:43 +0000

    Remove support model

--- src/purchase/models.py
+++ src/purchase/models.py
@@ -8,7 +8,6 @@ from .related_models.payment_model import Payment
 from .related_models.purchase_model import Purchase
 from .related_models.rsc_exchange_rate_model import RscExchangeRate
 from .related_models.rsc_purchase_fee import RscPurchaseFee
-from .related_models.support_model import Support
 from .related_models.usd_fundraise_contribution_model import UsdFundraiseContribution
 from .related_models.wallet_model import Wallet
 
@@ -23,7 +22,6 @@ migratables = (
     Purchase,
     RscExchangeRate,
     RscPurchaseFee,
-    Support,
     UsdFundraiseContribution,
     Wallet,
 )
