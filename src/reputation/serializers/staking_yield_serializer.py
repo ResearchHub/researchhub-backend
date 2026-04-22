d0981b6df807970a2e51398dc0e447caaf9c8a19
Taki Koutsomitis <koutst@protonmail.com>
2026-04-22 14:04:37 +0000

    Add balance lot multiplier details to staking yield endpoint

--- src/reputation/serializers/staking_yield_serializer.py
+++ src/reputation/serializers/staking_yield_serializer.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
 
 
+class StakingBalanceLotSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(max_digits=19, decimal_places=8)
+    created_date = serializers.DateField()
+    effective_start_date = serializers.DateField()
+    age_days = serializers.IntegerField()
+    current_multiplier = serializers.DecimalField(max_digits=19, decimal_places=8)
+    next_multiplier = serializers.DecimalField(
+        max_digits=19, decimal_places=8, allow_null=True
+    )
+    days_until_next_multiplier = serializers.IntegerField(allow_null=True)
+    next_multiplier_date = serializers.DateField(allow_null=True)
+
+
 class StakingYieldDetailsSerializer(serializers.Serializer):
     is_staking_opted_in = serializers.BooleanField()
     staking_opted_in_date = serializers.DateTimeField(allow_null=True)
@@ -10,6 +23,7 @@ class StakingYieldDetailsSerializer(serializers.Serializer):
     total_yield_earned = serializers.DecimalField(max_digits=19, decimal_places=8)
     latest_accrual_date = serializers.DateField(allow_null=True)
     apy = serializers.FloatField()
+    balance_lots = StakingBalanceLotSerializer(many=True)
 
 
 class StakingYieldEarnedSinceSerializer(serializers.Serializer):
