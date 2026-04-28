3bbfa3e629e59a45cebde213f7a8e8416aaf53d7
Gregor Zurowski <gregor@zurowski.net>
2026-04-28 05:47:12 +0000

    Remove permissions view test

--- src/researchhub/tests/test_views.py
+++ src/researchhub/tests/test_views.py
@@ -1,7 +1,5 @@
-import os
-from unittest.mock import mock_open, patch
+from unittest.mock import patch
 
-from django.conf import settings
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -20,22 +18,6 @@ class ViewsTestCase(TestCase):
             response.content.decode("utf-8"),
             "Authenticate with a token in the Authorization header.",
         )
-
-    @patch("builtins.open", new_callable=mock_open, read_data='{"permissions": "test"}')
-    def test_permissions_view(self, mock_file):
-        # Arrange
-        expected_path = os.path.join(
-            settings.BASE_DIR, "static", "researchhub", "user_permissions.json"
-        )
-
-        # Act
-        response = self.client.get(reverse("permissions"))
-
-        # Assert
-        mock_file.assert_called_once_with(expected_path, "r")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response["Content-Type"], "application/json")
-        self.assertEqual(response.content.decode("utf-8"), '{"permissions": "test"}')
 
     @patch("researchhub.views.views.render_to_string")
     def test_robots_txt_view(self, mock_render):
