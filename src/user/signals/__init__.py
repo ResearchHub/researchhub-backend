13fbcdafb519508a9244742d819e643cea45d22b
Gregor Zurowski <gregor@zurowski.net>
2026-05-18 10:17:03 +0000

    Apply formatter for imports

--- src/user/signals/__init__.py
+++ src/user/signals/__init__.py
@@ -1,2 +1,4 @@
-from . import funding_activity_signals  # noqa: F401
-from . import signals  # noqa: F401
+from . import (
+    funding_activity_signals,  # noqa: F401
+    signals,  # noqa: F401
+)
