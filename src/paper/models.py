7df44c9c81e27ff8e1b8a3900b2647324fd522a3
Gregor Zurowski <gregor@zurowski.net>
2026-05-19 08:03:15 +0000

    Organize import in paper model export shim

--- src/paper/models.py
+++ src/paper/models.py
@@ -2,9 +2,6 @@ from paper.related_models.citation_model import Citation
 from paper.related_models.paper_model import (
     ARXIV_IDENTIFIER,
     DOI_IDENTIFIER,
-    HELP_TEXT_IS_PUBLIC,
-    HELP_TEXT_IS_REMOVED,
-    HOT_SCORE_WEIGHT,
     Figure,
     Paper,
     PaperFetchLog,
@@ -15,3 +12,16 @@ from paper.related_models.paper_version import (
     PaperSeriesDeclaration,
     PaperVersion,
 )
+
+__all__ = [
+    "ARXIV_IDENTIFIER",
+    "DOI_IDENTIFIER",
+    "Citation",
+    "Figure",
+    "Paper",
+    "PaperFetchLog",
+    "PaperSeries",
+    "PaperSeriesDeclaration",
+    "PaperSubmission",
+    "PaperVersion",
+]
