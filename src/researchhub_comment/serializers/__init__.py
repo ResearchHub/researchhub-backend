6512453e0f3c46c0301a290d7fdba8c1a2e8a430
Gregor Zurowski <gregor@zurowski.net>
2026-05-21 05:31:47 +0000

    [Minor] Organize imports

--- src/researchhub_comment/serializers/__init__.py
+++ src/researchhub_comment/serializers/__init__.py
@@ -6,3 +6,10 @@ from researchhub_comment.serializers.rh_comment_thread_serializer import (
     DynamicRhThreadSerializer,
     RhCommentThreadSerializer,
 )
+
+__all__ = [
+    "RhCommentSerializer",
+    "DynamicRhCommentSerializer",
+    "RhCommentThreadSerializer",
+    "DynamicRhThreadSerializer",
+]
