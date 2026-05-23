916041937e60edd9a35b862bbffaae97a2f64de4
Michael Canova <michael_canova-at-174250149065>
2026-05-23 17:50:03 -0400

    [Risk Score] Test fixes

--- src/user/migrations/0145_riskscore_transfer.py
+++ src/user/migrations/0145_riskscore_transfer.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("user", "0143_delete_userapitoken"),
+        ("user", "0144_user_email_lower_idx"),
         ("risk_score", "0001_initial"),
         ("contenttypes", "0002_remove_content_type_name"),
     ]
