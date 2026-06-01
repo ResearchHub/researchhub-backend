dbfc2a6c98e15397274d64e64ba41500fc7af655
Gregor Zurowski <gregor@zurowski.net>
2026-06-01 10:54:20 +0000

    Remove claim runtime dependencies

--- src/researchhub_case/apps.py
+++ src/researchhub_case/apps.py
@@ -3,6 +3,3 @@ from django.apps import AppConfig
 
 class ResearchhubCaseConfig(AppConfig):
     name = "researchhub_case"
-
-    def ready(self):
-        import researchhub_case.signals  # noqa: F401
