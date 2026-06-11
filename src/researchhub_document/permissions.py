769d771c3e7d2e7c82d194962c9f97864c60658b
Gregor Zurowski <gregor@zurowski.net>
2026-06-11 05:51:54 +0000

    Remove unused utility function

--- src/researchhub_document/permissions.py
+++ src/researchhub_document/permissions.py
@@ -1,12 +1,4 @@
-from paper.models import Paper
 from researchhub_document.models import ResearchhubPost
-from researchhub_document.related_models.constants.document_type import (
-    PAPER,
-    RESEARCHHUB_POST_DOCUMENT_TYPES,
-)
-from researchhub_document.related_models.researchhub_unified_document_model import (
-    ResearchhubUnifiedDocument,
-)
 from utils.permissions import AuthorizationBasedPermission
 
 
@@ -28,15 +20,3 @@ class HasDocumentEditingPermission(AuthorizationBasedPermission):
                 return False
 
         return True
-
-
-def get_uni_doc_related_model(unified_document):
-    if not isinstance(unified_document, ResearchhubUnifiedDocument):
-        return None
-    doc_type = unified_document.document_type
-    if doc_type in RESEARCHHUB_POST_DOCUMENT_TYPES:
-        return ResearchhubPost
-    elif doc_type == PAPER:
-        return Paper
-    else:
-        return None
