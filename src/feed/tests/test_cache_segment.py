89aaa95cd79c380ff7ff4d1cc3dfc72bce9523ae
nicktytarenko <nicktytarenko@gmail.com>
2026-06-11 19:17:20 +0000

    unused var

--- src/feed/tests/test_cache_segment.py
+++ src/feed/tests/test_cache_segment.py
@@ -17,7 +17,6 @@ from researchhub_document.related_models.researchhub_unified_document_model impo
     ResearchhubUnifiedDocument,
 )
 from user.tests.helpers import (
-    create_hub_editor,
     create_random_authenticated_user,
 )
 from utils.test_helpers import AWSMockTestCase
