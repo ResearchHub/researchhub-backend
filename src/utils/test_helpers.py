b3d6e3618c086db7cfa123471a6fa9e66ef7dba1
Gregor Zurowski <gregor@zurowski.net>
2026-06-11 19:25:01 +0000

    Remove unused test_concurrently helper

--- src/utils/test_helpers.py
+++ src/utils/test_helpers.py
@@ -3,7 +3,6 @@ import random
 import secrets
 import string
 import threading
-import time
 from unittest.mock import MagicMock, patch
 
 from allauth.account.models import EmailAddress
@@ -345,50 +344,6 @@ class DatabaseThread(threading.Thread):
     def run(self):
         super().run()
         connection.close()
-
-
-# Copied from
-# https://www.caktusgroup.com/blog/2009/05/26/testing-django-views-for-concurrency-issues/
-def test_concurrently(runs, delay=None):
-    """
-    Add this decorator to small pieces of code that you want to test
-    concurrently to make sure they don't raise exceptions when run at the
-    same time.  E.g., some Django views that do a SELECT and then a subsequent
-    INSERT might fail when the INSERT assumes that the data has not changed
-    since the SELECT.
-    """
-
-    def test_concurrently_decorator(test_func):
-        def wrapper(*args, **kwargs):
-            exceptions = []
-
-            def call_test_func():
-                try:
-                    test_func(*args, **kwargs)
-                except Exception as e:
-                    exceptions.append(e)
-                    raise
-
-            threads = []
-            for i in range(runs):
-                threads.append(DatabaseThread(target=call_test_func))
-            for t in threads:
-                if delay is not None:
-                    time.sleep(delay)
-                t.start()
-            for t in threads:
-                if delay is not None:
-                    time.sleep(delay)
-                t.join()
-            if exceptions:
-                raise Exception(
-                    "test_concurrently intercepted %s exceptions: %s"
-                    % (len(exceptions), exceptions)
-                )
-
-        return wrapper
-
-    return test_concurrently_decorator
 
 
 def create_test_user(
