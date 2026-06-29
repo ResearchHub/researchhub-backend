2f8b295e6d57ae9b91cbae1f341397926d46bf6c
Gregor Zurowski <gregor@zurowski.net>
2026-06-29 10:02:18 +0000

    Add simple Stripe client factory

---
+++ src/purchase/services/stripe_client.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+
+STRIPE_API_VERSION = "2024-09-30.acacia"
+"""
+The Strip API version to use for all requests.
+"""
+
+
+def get_stripe():
+    """
+    Returns a configured Stripe client instance.
+    """
+    import stripe  # delay until needed
+
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    stripe.api_version = STRIPE_API_VERSION
+
+    return stripe
