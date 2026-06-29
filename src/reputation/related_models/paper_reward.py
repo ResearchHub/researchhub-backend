1e972bf8b8cb96deb351298660a6561d64378b25
Gregor Zurowski <gregor@zurowski.net>
2026-06-29 11:17:50 +0000

    Remove method for calculating paper rewards

--- src/reputation/related_models/paper_reward.py
+++ src/reputation/related_models/paper_reward.py
@@ -23,60 +23,6 @@ class HubCitationValue(models.Model):
 
     created_date = models.DateTimeField(auto_now_add=True)
 
-    @classmethod
-    def calculate_rsc_reward(
-        cls, paper, citation_change, is_open_data, is_preregistered
-    ):
-        hub_citation_value = cls.get_hub_citation_value(
-            paper.unified_document.get_primary_hub()
-        )
-        return hub_citation_value.rsc_reward_algo(
-            citation_change, is_open_data, is_preregistered
-        )
-
-    @classmethod
-    def get_hub_citation_value(cls, hub):
-        return cls.objects.filter(hub=hub).order_by("created_date").last()
-
-    def rsc_reward_algo(self, citation_change, is_open_data, is_preregistered):
-        hub_citation_variables = None
-        for bin_range, bin_value in sorted(
-            self.variables["citations"]["bins"].items(),
-            key=lambda item: eval(item[0])[1],
-            reverse=True,
-        ):
-            bin_range = eval(bin_range)
-            bin_value = eval(bin_value)
-            if citation_change >= bin_range[0]:
-                # Since the bins are sorted by the upper bound, we can break once we
-                # find the a bin that citation is greater than lower bound.
-                hub_citation_variables = bin_value
-                citation_change = min(citation_change, bin_range[1])
-                break
-
-        rsc_reward_with_multipliers = 10 ** (
-            math.log(citation_change, 10) * hub_citation_variables["slope"]
-            + hub_citation_variables["intercept"]
-        )
-
-        rsc_reward = rsc_reward_with_multipliers
-        base_rsc_reward = rsc_reward_with_multipliers / (
-            OPEN_ACCESS_MULTIPLIER * OPEN_DATA_MULTIPLIER * PREREGISTERED_MULTIPLIER
-        )
-
-        if not is_open_data:
-            rsc_reward -= base_rsc_reward * OPEN_DATA_MULTIPLIER
-
-        if not is_preregistered:
-            rsc_reward -= base_rsc_reward * PREREGISTERED_MULTIPLIER
-
-        return rsc_reward * REWARD_MULTIPLIER
-
-    # This method is used to calculate the base reward for the initial paper claim.
-    @classmethod
-    def calculate_base_claim_rsc_reward(cls, paper):
-        return cls.calculate_rsc_reward(paper, paper.citations, False, False)
-
 
 class PaperReward(models.Model):
     paper = models.ForeignKey("paper.Paper", on_delete=models.CASCADE, db_index=True)
