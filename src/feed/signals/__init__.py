f00b9fa4e75eeac05d41096f05bfcb77dc363e13
Gregor Zurowski <gregor@zurowski.net>
2026-07-13 13:16:39 +0000

    Remove wildcard imports in feed app

--- src/feed/signals/__init__.py
+++ src/feed/signals/__init__.py
@@ -1,8 +1,37 @@
-from .bounty_signals import *  # noqa: F401, F403
-from .comment_signals import *  # noqa: F401, F403
-from .document_signals import *  # noqa: F401, F403
-from .paper_signals import *  # noqa: F401, F403
-from .post_signals import *  # noqa: F401, F403
-from .purchase_signals import *  # noqa: F401, F403
-from .review_signals import *  # noqa: F401, F403
-from .vote_signals import *  # noqa: F401, F403
+from .bounty_signals import (
+    handle_bounty_delete_update_feed_entries,
+    handle_bounty_update_feed_entries,
+)
+from .comment_signals import handle_comment_created_or_removed
+from .document_signals import (
+    delete_feed_entries_for_unified_document,
+    handle_document_hubs_changed,
+    handle_unified_document_removed,
+)
+from .paper_signals import handle_paper_external_metadata_updated
+from .post_signals import handle_post_create_feed_entry
+from .purchase_signals import (
+    handle_purchase_feed_entry,
+    handle_usd_contribution_feed_entry,
+    refresh_feed_entries_on_grant_application,
+    refresh_feed_entries_on_grant_application_delete,
+)
+from .review_signals import handle_review_created_or_updated
+from .vote_signals import handle_feed_vote
+
+__all__ = [
+    "delete_feed_entries_for_unified_document",
+    "handle_bounty_delete_update_feed_entries",
+    "handle_bounty_update_feed_entries",
+    "handle_comment_created_or_removed",
+    "handle_document_hubs_changed",
+    "handle_feed_vote",
+    "handle_paper_external_metadata_updated",
+    "handle_post_create_feed_entry",
+    "handle_purchase_feed_entry",
+    "handle_review_created_or_updated",
+    "handle_unified_document_removed",
+    "handle_usd_contribution_feed_entry",
+    "refresh_feed_entries_on_grant_application",
+    "refresh_feed_entries_on_grant_application_delete",
+]
