bd4412aabb7e4fefe5ddbb6bfec096624c8d4f13
Gregor Zurowski <gregor@zurowski.net>
2026-07-13 13:16:06 +0000

    Remove wildcard imports in review app

--- src/review/signals/__init__.py
+++ src/review/signals/__init__.py
@@ -1,1 +1,9 @@
-from .review_signals import *  # noqa
+from .review_signals import (
+    mark_review_assessed_on_bounty_award,
+    mark_review_assessed_on_purchase,
+)
+
+__all__ = [
+    "mark_review_assessed_on_bounty_award",
+    "mark_review_assessed_on_purchase",
+]
