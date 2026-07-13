affeee2095e47d2c6759490aa6a88bc4d42996a6
Gregor Zurowski <gregor@zurowski.net>
2026-07-13 13:15:49 +0000

    Remove wildcard imports in user app

--- src/user/views/__init__.py
+++ src/user/views/__init__.py
@@ -1,11 +1,29 @@
-# flake8: noqa
 from user.views.audit_views import AuditViewSet
 from user.views.author_views import AuthorViewSet
 from user.views.contribution_views import ContributionViewSet
-from user.views.editor_views import *
+from user.views.editor_views import (
+    get_editors_by_contributions,
+    get_hub_active_contributors,
+)
 from user.views.gatekeeper_view import GatekeeperViewSet
 from user.views.leaderboard_views import LeaderboardViewSet
-from user.views.moderator_view import *
+from user.views.moderator_view import ModeratorView
 from user.views.organization_view import OrganizationViewSet
-from user.views.persona_webhook_view import *
-from user.views.user_views import *
+from user.views.persona_webhook_view import PersonaWebhookView
+from user.views.user_views import MajorViewSet, UniversityViewSet, UserViewSet
+
+__all__ = [
+    "AuditViewSet",
+    "AuthorViewSet",
+    "ContributionViewSet",
+    "GatekeeperViewSet",
+    "LeaderboardViewSet",
+    "MajorViewSet",
+    "ModeratorView",
+    "OrganizationViewSet",
+    "PersonaWebhookView",
+    "UniversityViewSet",
+    "UserViewSet",
+    "get_editors_by_contributions",
+    "get_hub_active_contributors",
+]
