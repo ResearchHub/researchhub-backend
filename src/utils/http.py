9eadbd1c07c6ff8f1fa210856eed92f74a93dfc7
Gregor Zurowski <gregor@zurowski.net>
2026-07-15 16:47:34 +0000

    Remove unused HTTP utilities

--- src/utils/http.py
+++ src/utils/http.py
@@ -1,49 +1,8 @@
-import random
-import time
-
-import cloudscraper
-import requests
-
-
 def get_user_from_request(ctx):
     request = ctx.get("request")
     if request and hasattr(request, "user"):
         return request.user
     return None
-
-
-def scraper_get_url(url: str, timeout: int = 5) -> requests.Response:
-    """
-    Perform a GET request to retrieve the response headers
-    for `url`. If `url` is invalid or returns a bad status code,
-    a subclass of `requests.exceptions.RequestException` will be raised.
-    """
-    scraper = cloudscraper.create_scraper()
-
-    response = scraper.get(url, timeout=timeout, stream=True)
-    if response.status_code in (403, 404):
-        response.close()  # close previous response
-        time.sleep(random.uniform(0, 1))  # wait before retrying
-        response = scraper.get(url, timeout=timeout, stream=True)
-        response.raise_for_status()
-
-    return response
-
-
-def check_url_contains_pdf(url) -> bool:
-    if url is None:
-        return False
-    try:
-        with scraper_get_url(url) as resp:
-            if "sciencedirect" in url and "download=false" in url:
-                return resp.status_code < 400
-
-            headers = resp.headers
-            content_type = headers.get("content-type", "")
-            filename = headers.get("filename", "")
-            return "application/pdf" in content_type or ".pdf" in filename
-    except Exception:
-        return False
 
 
 def get_client_ip(request):
