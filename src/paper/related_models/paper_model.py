51a8d2553a84016503315006a0778010d144366e
Gregor Zurowski <gregor@zurowski.net>
2026-07-20 17:37:16 +0000

    [Minor] Remove unused imports

--- src/paper/related_models/paper_model.py
+++ src/paper/related_models/paper_model.py
@@ -17,7 +17,6 @@ from paper.storage.figure_storage import FigureStorage
 from paper.utils import get_csl_item
 from purchase.models import Purchase
 from reputation.models import Score, ScoreChange
-from reputation.related_models.paper_reward import HubCitationValue
 from researchhub.settings import TESTING
 from researchhub_comment.models import RhCommentThreadModel
 from user.related_models.user_model import User
