c9021b9a2f19ff95a725c6d061f6553f84bea3aa
Gregor Zurowski <gregor@zurowski.net>
2026-07-22 09:50:08 +0000

    Remove `noqa` marker

--- src/researchhub_document/models.py
+++ src/researchhub_document/models.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 from .related_models.document_filter_model import DocumentFilter
 from .related_models.featured_content_model import FeaturedContent
 from .related_models.research_journey_model import ResearchJourney
