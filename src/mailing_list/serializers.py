ba7e838e83db917e973ec6f7c79531de2a3b0698
Gregor Zurowski <gregor@zurowski.net>
2026-07-23 10:45:21 +0000

    Simplify serializer since there is only one subscription left

--- src/mailing_list/serializers.py
+++ src/mailing_list/serializers.py
@@ -1,27 +1,16 @@
 from rest_framework import serializers
 
-from mailing_list.models import (
-    CommentSubscription,
-    EmailRecipient,
-)
+from mailing_list.models import CommentSubscription, EmailRecipient
 
 
-def _get_model_serializer(model_arg):
-    class GenericSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = model_arg
-            fields = "__all__"
-
-    return GenericSerializer
-
-
-_SUBSCRIPTION_SERIALIZERS = {
-    model: _get_model_serializer(model) for model in (CommentSubscription,)
-}
+class CommentSubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommentSubscription
+        fields = "__all__"
 
 
 class EmailRecipientSerializer(serializers.ModelSerializer):
-    comment_subscription = serializers.SerializerMethodField()
+    comment_subscription = CommentSubscriptionSerializer(read_only=True)
     user = serializers.CurrentUserDefault()
 
     class Meta:
@@ -41,13 +30,3 @@ class EmailRecipientSerializer(serializers.ModelSerializer):
             "updated_date",
             "next_cursor",
         ]
-
-    def get_comment_subscription(self, obj):
-        return self._get_subscription(CommentSubscription, obj)
-
-    def _get_subscription(self, model, obj):
-        try:
-            subscription = model.objects.get(email_recipient=obj)
-            return _SUBSCRIPTION_SERIALIZERS[model](subscription).data
-        except model.DoesNotExist:
-            return None
