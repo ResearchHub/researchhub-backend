5a1ed042e814b3aaa035c12286aee4a2e2285d81
Gregor Zurowski <gregor@zurowski.net>
2026-07-23 08:17:50 +0000

    Remove custom plaintext renderer

--- src/utils/renderers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import renderers
-
-
-class PlainTextRenderer(renderers.BaseRenderer):
-    media_type = "text/plain"
-    format = "txt"
-
-    def render(self, data, media_type=None, renderer_context=None):
-        return data.encode(self.charset)
