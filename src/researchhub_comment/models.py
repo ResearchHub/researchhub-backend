d0196fc9bd5677b93cbea82ff1e3666ff9676179
Gregor Zurowski <gregor@zurowski.net>
2026-07-28 08:05:57 +0000

    Organize convenience imports for models

--- src/researchhub_comment/models.py
+++ src/researchhub_comment/models.py
@@ -1,6 +1,11 @@
-# flake8: noqa - importable modelsC
 from .related_models.rh_comment_model import RhCommentModel
 from .related_models.rh_comment_thread_model import (
     RhCommentThreadManager,
     RhCommentThreadModel,
 )
+
+__all__ = [
+    "RhCommentModel",
+    "RhCommentThreadModel",
+    "RhCommentThreadManager",
+]
