ff3a7a5278159386c0704fb303e3d478bb5c5b47
Gregor Zurowski <gregor@zurowski.net>
2026-07-30 09:59:31 +0000

    Add business service for managing email subscription

---
+++ src/mailing_list/services/__init__.py
@@ -0,0 +1,6 @@
+from mailing_list.services.email_subscription_service import (
+    EmailSubscriptionService,
+    InvalidUnsubscribeCodeError,
+)
+
+__all__ = ["EmailSubscriptionService", "InvalidUnsubscribeCodeError"]
