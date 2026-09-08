ee81b4e155bf201fef4ebaf96992241eebf302d1
Gregor Zurowski <gregor@zurowski.net>
2026-09-08 13:02:07 +0000

    Rename logging config and move into subfolder

--- docker/package-beanstalk.sh
+++ docker/package-beanstalk.sh
@@ -6,7 +6,9 @@ image_uri=$1
 output_directory=$2
 script_directory=$(dirname -- "$0")
 
-mkdir -p "$output_directory"
+mkdir -p \
+  "$output_directory" \
+  "$output_directory/beanstalk.d"
 
 sed \
   "s|__API_IMAGE_URI__|$image_uri|" \
@@ -14,11 +16,14 @@ sed \
   > "$output_directory/docker-compose.yml"
 
 cp \
-  "$script_directory/datadog-logs.yaml" \
-  "$output_directory/datadog-logs.yaml"
+  "$script_directory/beanstalk.d/conf.yaml" \
+  "$output_directory/beanstalk.d/conf.yaml"
 
 rm -f "$output_directory/deploy.zip"
-zip -j \
-  "$output_directory/deploy.zip" \
-  "$output_directory/docker-compose.yml" \
-  "$output_directory/datadog-logs.yaml"
+(
+  cd "$output_directory"
+  zip \
+    deploy.zip \
+    docker-compose.yml \
+    beanstalk.d/conf.yaml
+)
