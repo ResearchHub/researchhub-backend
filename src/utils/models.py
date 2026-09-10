489fc71677ca5bbdc4eaef04b82db09df323610a
Gregor Zurowski <gregor@zurowski.net>
2026-09-10 08:11:36 +0000

    Order model fields and classes following Django Style Guide

--- src/utils/models.py
+++ src/utils/models.py
@@ -19,9 +19,6 @@ class DefaultModel(models.Model):
 
 
 class DefaultAuthenticatedModel(models.Model):
-    class Meta:
-        abstract = True
-
     created_by = models.ForeignKey(
         USER_MODEL,
         on_delete=models.CASCADE,
@@ -42,11 +39,11 @@ class DefaultAuthenticatedModel(models.Model):
         auto_now=True,
     )
 
-
-class AbstractGenericRelationModel(DefaultAuthenticatedModel):
     class Meta:
         abstract = True
 
+
+class AbstractGenericRelationModel(DefaultAuthenticatedModel):
     # Below the mandatory fields for generic relation
     content_type = models.ForeignKey(
         ContentType,
@@ -59,19 +56,23 @@ class AbstractGenericRelationModel(DefaultAuthenticatedModel):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
 
+    class Meta:
+        abstract = True
+
 
 class SoftDeletableModel(models.Model):
     """Adapted from https://github.com/jazzband/django-model-utils"""
+
+    all_objects = models.Manager()
 
     is_public = models.BooleanField(default=True)
     is_removed = models.BooleanField(default=False)
     is_removed_date = models.DateTimeField(default=None, null=True, blank=True)
 
+    objects = SoftDeletableManager()
+
     class Meta:
         abstract = True
-
-    objects = SoftDeletableManager()
-    all_objects = models.Manager()
 
     def delete(self, soft=True, *args, **kwargs):
         """Sets `is_removed` True when `soft` is True instead of deleting.
@@ -146,13 +147,13 @@ class PaidStatusModelMixin(models.Model):
         (PENDING, PENDING),
     ]
 
-    class Meta:
-        abstract = True
-
     paid_date = models.DateTimeField(default=None, null=True)
     paid_status = models.CharField(
         max_length=255, choices=PAID_STATUS_CHOICES, default=INITIATED, null=True
     )
+
+    class Meta:
+        abstract = True
 
     def set_paid_failed(self):
         self.paid_status = self.FAILED
