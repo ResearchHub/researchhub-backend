c9cdcddb749d7ae91602750e7c43c064464d30d6
Gregor Zurowski <gregor@zurowski.net>
2026-09-22 09:14:46 +0000

    Add utility function to read private IP address from ECS task

--- src/utils/aws_metadata.py
+++ src/utils/aws_metadata.py
@@ -1,7 +1,28 @@
+import os
+
 import requests
 
 EC2_METADATA_URL = "http://169.254.169.254/latest"
+ECS_METADATA_URI_VARIABLE = "ECS_CONTAINER_METADATA_URI_V4"
 TIMEOUT = 1
+
+
+def private_ip() -> str:
+    """
+    Returns the private IP of the current instance or ECS task.
+    """
+    if ECS_METADATA_URI_VARIABLE in os.environ:
+        return ecs_task_private_ip()
+    return ec2_private_ip()
+
+
+def ecs_task_private_ip() -> str:
+    """
+    Reads the private IP of the ECS task from the ECS container metadata endpoint.
+    """
+    response = requests.get(os.environ[ECS_METADATA_URI_VARIABLE], timeout=TIMEOUT)
+    response.raise_for_status()
+    return response.json()["Networks"][0]["IPv4Addresses"][0]
 
 
 def ec2_private_ip() -> str:
